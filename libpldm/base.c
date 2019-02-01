9670e2e1b2c3f54d8618fd7483d5d127304fbe40
Deepak Kodihalli <dkodihal@in.ibm.com>
2019-02-01 04:09:13 -0600

    Implement discovery commands
    This commit does the following
    - Implements the GetPLDMTypes and GetPLDMCommands commands. These are
      commands that need to be handled by a PLDM device as part of the
      initial PLDM discovery.
    - Sets up the build infrastructure: separate libraries for PLDM
      encode/decode libs and the PLDM responder.
    
    Change-Id: I65fa222d2a681c473f579c8e30d84faaf94fe754
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ libpldm/base.c
@@ -0,0 +1,52 @@
+#include <endian.h>
+#include <string.h>
+
+#include "base.h"
+
+int encode_get_types_req(uint8_t instance_id, uint8_t *buffer)
+{
+	return PLDM_SUCCESS;
+}
+
+int encode_get_commands_req(uint8_t instance_id, uint8_t type,
+			    struct pldm_version_t version, uint8_t *buffer)
+{
+	uint8_t *dst = buffer + PLDM_REQUEST_HEADER_LEN_BYTES;
+	memcpy(dst, &type, sizeof(type));
+	dst += sizeof(type);
+	memcpy(dst, &version, sizeof(version));
+
+	return PLDM_SUCCESS;
+}
+
+int encode_get_types_resp(uint8_t instance_id, uint8_t completion_code,
+			  const uint8_t *types, uint8_t *buffer)
+{
+	if (completion_code == PLDM_SUCCESS) {
+		uint8_t *dst = buffer + PLDM_RESPONSE_HEADER_LEN_BYTES;
+		memcpy(dst, types, PLDM_MAX_TYPES / 8);
+	}
+
+	return PLDM_SUCCESS;
+}
+
+int decode_get_commands_req(const uint8_t *request, uint8_t *type,
+			    struct pldm_version_t *version)
+{
+	*type = *(request + PLDM_REQUEST_HEADER_LEN_BYTES);
+	memcpy(version, (struct pldm_version_t *)(type + sizeof(*type)),
+	       sizeof(*version));
+
+	return PLDM_SUCCESS;
+}
+
+int encode_get_commands_resp(uint8_t instance_id, uint8_t completion_code,
+			     const uint8_t *commands, uint8_t *buffer)
+{
+	if (completion_code == PLDM_SUCCESS) {
+		uint8_t *dst = buffer + PLDM_RESPONSE_HEADER_LEN_BYTES;
+		memcpy(dst, commands, PLDM_MAX_CMDS_PER_TYPE / 8);
+	}
+
+	return PLDM_SUCCESS;
+}
