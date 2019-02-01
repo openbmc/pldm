cada4a5ceb39f5efdabde836739e2585b454c0db
Deepak Kodihalli <dkodihal@in.ibm.com>
2019-02-01 04:09:13 -0600

    Implement discovery commands
    This commit does the following
    - Implements the GetPLDMTypes and GetPLDMCommands commands. These are
      commands that need to be handled by a PLDM device as part of the
      initial PLDM discovery.
    - Sets up the build infrastructure: separate libraries for PLDM
      encode/decode libs and the PLDM responder.
    
    Change-Id: I65fa222d2a681c473f579c8e30d84faaf94fe754
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ libpldm/base.c
@@ -0,0 +1,50 @@
+#include <endian.h>
+#include <string.h>
+
+#include "base.h"
+
+int encode_get_types_req(uint8_t instance_id, uint8_t *buffer) { return OK; }
+
+int encode_get_commands_req(uint8_t instance_id, uint8_t type, ver32 version,
+			    uint8_t *buffer)
+{
+	uint8_t *dst = buffer + REQUEST_HEADER_LEN_BYTES;
+	memcpy(dst, &type, sizeof(type));
+	dst += sizeof(type);
+	version = htole32(version);
+	memcpy(dst, &version, sizeof(version));
+
+	return OK;
+}
+
+int encode_get_types_resp(uint8_t instance_id, uint8_t completion_code,
+			  const uint8_t *types, uint8_t *buffer)
+{
+	if (completion_code == SUCCESS) {
+		uint8_t *dst = buffer + RESPONSE_HEADER_LEN_BYTES;
+		memcpy(dst, types, MAX_TYPES / sizeof(uint8_t));
+	}
+
+	return OK;
+}
+
+int decode_get_commands_req(const uint8_t *request, uint8_t *type,
+			    ver32 *version)
+{
+	*type = *(request + REQUEST_HEADER_LEN_BYTES);
+	*version = *((ver32 *)(type + sizeof(uint8_t)));
+	*version = le32toh(*version);
+
+	return OK;
+}
+
+int encode_get_commands_resp(uint8_t instance_id, uint8_t completion_code,
+			     const uint8_t *commands, uint8_t *buffer)
+{
+	if (completion_code == SUCCESS) {
+		uint8_t *dst = buffer + RESPONSE_HEADER_LEN_BYTES;
+		memcpy(dst, commands, MAX_CMDS_PER_TYPE / sizeof(uint8_t));
+	}
+
+	return OK;
+}
---
+++ libresponder/base.cpp
@@ -0,0 +1,75 @@
+#include "libpldm/base.h"
+
+#include "base.hpp"
+
+#include <array>
+#include <map>
+#include <stdexcept>
+#include <vector>
+
+namespace pldm
+{
+namespace responder
+{
+
+enum SupportedTypes
+{
+    BASE = 0x00,
+};
+
+enum SupportedCmds
+{
+    GET_PLDM_TYPES = 0x4,
+    GET_PLDM_COMMANDS = 0x5,
+};
+
+using Cmd = std::vector<uint8_t>;
+
+static const std::map<Type, Cmd> capabilities{
+    {BASE, {GET_PLDM_TYPES, GET_PLDM_COMMANDS}}};
+
+void getPLDMTypes()
+{
+    std::array<uint8_t, RESPONSE_HEADER_LEN_BYTES + GET_TYPES_RESP_DATA_BYTES>
+        response{};
+    // DSP0240 has this a bitfield8[N], where N = 0 to 7
+    std::array<uint8_t, 8> types{};
+
+    for (const auto& type : capabilities)
+    {
+        auto index = type.first / 8;
+        // <Type Number> = <Array Index> * 8 + <bit position>
+        auto bit = type.first - (index * 8);
+        types[index] |= 1 << bit;
+    }
+
+    encode_get_types_resp(0, SUCCESS, types.data(), response.data());
+}
+
+void getPLDMCommands(Type type, ver32 version)
+{
+    std::array<uint8_t,
+               RESPONSE_HEADER_LEN_BYTES + GET_COMMANDS_RESP_DATA_BYTES>
+        response{};
+    // DSP0240 has this a bitfield8[N], where N = 0 to 31
+    std::array<uint8_t, 32> types{};
+
+    if (capabilities.find(type) == capabilities.end())
+    {
+        encode_get_commands_resp(0, ERROR_INVALID_PLDM_TYPE, nullptr, nullptr);
+        return;
+    }
+
+    for (const auto& cmd : capabilities.at(type))
+    {
+        auto index = cmd / 8;
+        // <Type Number> = <Array Index> * 8 + <bit position>
+        auto bit = cmd - (index * 8);
+        types[index] |= 1 << bit;
+    }
+
+    encode_get_commands_resp(0, SUCCESS, types.data(), response.data());
+}
+
+} // namespace responder
+} // namespace pldm
