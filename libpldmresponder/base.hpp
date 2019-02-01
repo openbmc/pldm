9465343351af05b75b75b57d1f6f71d71abcf6e3
Deepak Kodihalli <dkodihal@in.ibm.com>
2019-02-01 04:09:13 -0600

    Implement discovery commands
    This commit does the following
    - Implements the GetPLDMTypes and GetPLDMCommands commands. These are
      commands that need to be handled by a PLDM device as part of the
      initial PLDM discovery.
    - Sets up the build infrastructure: separate libraries for PLDM
      encode/decode libs and the PLDM responder.
    
    Change-Id: I65fa222d2a681c473f579c8e30d84faaf94fe754
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ libpldmresponder/base.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdint.h>
+#include <vector>
+
+namespace pldm
+{
+
+using Type = uint8_t;
+
+namespace responder
+{
+
+using Response = std::vector<uint8_t>;
+using Request = const uint8_t*;
+
+Response getPLDMTypes(Request payload, size_t payloadLen);
+Response getPLDMCommands(Request payload, size_t payloadLen);
+
+} // namespace responder
+} // namespace pldm
