5db426d6ea5351238d53807ecacc6723f2f67533
Tom Joseph <tomjoseph@in.ibm.com>
2019-02-07 16:17:07 +0530

    Implement API to pack and unpack PLDM header
    - The pack_pldm_header API packs the PLDM header into the
      PLDM message.
    - The unpack_pldm_header API unpacks the PLDM header from the
      PLDM message.
    
    Change-Id: I60b96954395cdcd1cecf306b7aee4c4de6c0f35a
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- libpldm/base.c
+++ libpldm/base.c
@@ -3,6 +3,65 @@
 
 #include "base.h"
 
+int pack_pldm_header(const struct pldm_header_info *hdr, struct pldm_msg_t *msg)
+{
+	if (msg == NULL || hdr == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	if (hdr->msg_type != RESPONSE && hdr->msg_type != REQUEST &&
+	    hdr->msg_type != ASYNC_REQUEST_NOTIFY) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	if (hdr->instance > PLDM_INSTANCE_MAX) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	if (hdr->pldm_type > (PLDM_MAX_TYPES - 1)) {
+		return PLDM_ERROR_INVALID_PLDM_TYPE;
+	}
+
+	uint8_t datagram = (hdr->msg_type == ASYNC_REQUEST_NOTIFY) ? 1 : 0;
+
+	if (hdr->msg_type == RESPONSE) {
+		msg->request = RESPONSE;
+	} else if (hdr->msg_type == REQUEST ||
+		   hdr->msg_type == ASYNC_REQUEST_NOTIFY) {
+		msg->request = REQUEST;
+	}
+	msg->datagram = datagram;
+	msg->reserved = 0;
+	msg->instance_id = hdr->instance;
+	msg->header_ver = 0;
+	msg->type = hdr->pldm_type;
+	msg->command = hdr->command;
+
+	return PLDM_SUCCESS;
+}
+
+int unpack_pldm_header(const struct pldm_msg_t *msg, const size_t size,
+		       struct pldm_header_info *hdr, size_t *payload_size)
+{
+	if (msg == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	if (msg->request == RESPONSE) {
+		hdr->msg_type = RESPONSE;
+	} else {
+		hdr->msg_type =
+		    msg->datagram ? ASYNC_REQUEST_NOTIFY : REQUEST;
+	}
+
+	hdr->instance = msg->instance_id;
+	hdr->pldm_type = msg->type;
+	hdr->command = msg->command;
+	*payload_size = size - sizeof(struct pldm_msg_t) + sizeof(uint8_t*);
+
+	return PLDM_SUCCESS;
+}
+
 int encode_get_types_req(uint8_t instance_id, struct pldm_msg_t *msg)
 {
 	return PLDM_SUCCESS;
