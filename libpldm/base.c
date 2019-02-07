732b507c3cd4b1af695428eead318a983fd91441
Tom Joseph <tomjoseph@in.ibm.com>
2019-02-07 16:17:07 +0530

    Implement API's to pack and unpack PLDM messages.
    Change-Id: I60b96954395cdcd1cecf306b7aee4c4de6c0f35a
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- libpldm/base.c
+++ libpldm/base.c
@@ -3,9 +3,87 @@
 
 #include "base.h"
 
+int pack_pldm_message(void *ptr, struct pldm_header_info *hdr) {
+  if (ptr == NULL || hdr == NULL) {
+    return FAIL;
+  }
+
+  if (hdr->msg_type != RESPONSE && hdr->msg_type != REQUEST &&
+      hdr->msg_type != ASYNC_REQUEST_NOTIFY) {
+    return FAIL;
+  }
+
+  if (hdr->instance > PLDM_INSTANCE_MAX || hdr->pldm_type > PLDM_TYPE_MAX) {
+    return FAIL;
+  }
+
+  struct pldm_header_req *header_req = NULL;
+  struct pldm_header_resp *header_resp = NULL;
+  uint8_t datagram = (hdr->msg_type == ASYNC_REQUEST_NOTIFY) ? 1 : 0;
+
+  if (hdr->msg_type == RESPONSE) {
+    header_resp = (struct pldm_header_resp *)ptr;
+    header_resp->request = RESPONSE;
+    header_resp->datagram = 0;
+    header_resp->reserved = 0;
+    header_resp->instance_id = hdr->instance;
+    header_resp->header_ver = 0;
+    header_resp->type = hdr->pldm_type;
+    header_resp->command = hdr->command;
+    header_resp->completion_code = hdr->completion_code;
+  } else if (hdr->msg_type == REQUEST ||
+             hdr->msg_type == ASYNC_REQUEST_NOTIFY) {
+    header_req = (struct pldm_header_req *)ptr;
+    header_req->request = REQUEST;
+    header_req->datagram = datagram;
+    header_req->reserved = 0;
+    header_req->instance_id = hdr->instance;
+    header_req->header_ver = 0;
+    header_req->type = hdr->pldm_type;
+    header_req->command = hdr->command;
+  }
+
+  return OK;
+}
+
+int unpack_pldm_message(void *ptr, size_t size, struct pldm_header_info *hdr,
+                        size_t *offset, size_t *payload_size) {
+  if (ptr == NULL) {
+    return FAIL;
+  }
+
+  struct pldm_header_req *header_req = (struct pldm_header_req *)ptr;
+  struct pldm_header_resp *header_resp = NULL;
+
+  if (header_req->request == RESPONSE) {
+    if (size < PLDM_RESPONSE_HEADER_SIZE) {
+      return FAIL;
+    }
+    header_resp = (struct pldm_header_resp *)ptr;
+    hdr->msg_type = RESPONSE;
+    hdr->completion_code = header_resp->completion_code;
+    *offset = PLDM_RESPONSE_HEADER_SIZE;
+    *payload_size = size - PLDM_RESPONSE_HEADER_SIZE;
+  } else {
+    if (size < PLDM_REQUEST_HEADER_SIZE) {
+      return FAIL;
+    }
+    hdr->msg_type = header_req->datagram ? ASYNC_REQUEST_NOTIFY : REQUEST;
+    *offset = PLDM_REQUEST_HEADER_SIZE;
+    *payload_size = size - PLDM_REQUEST_HEADER_SIZE;
+  }
+
+  hdr->instance = header_req->instance_id;
+  hdr->pldm_type = header_req->type;
+  hdr->command = header_req->command;
+
+  return OK;
+}
+
 int encode_get_types_req(uint8_t instance_id,
 			 uint8_t *buffer)
 {
+    return OK;
 }
 
 int encode_get_commands_req(uint8_t instance_id,
