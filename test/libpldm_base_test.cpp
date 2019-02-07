136cc025de0be289865a1a82ffbf744ddd07cf8c
Tom Joseph <tomjoseph@in.ibm.com>
2019-02-07 16:17:07 +0530

    Implement API to pack and unpack PLDM header
    - The pack_pldm_header API packs the PLDM header into the
      PLDM message.
    - The unpack_pldm_header API unpacks the PLDM header from the
      PLDM message.
    
    Change-Id: I60b96954395cdcd1cecf306b7aee4c4de6c0f35a
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- test/libpldm_base_test.cpp
+++ test/libpldm_base_test.cpp
@@ -6,6 +6,200 @@
 
 #include <gtest/gtest.h>
 
+TEST(PackPLDMMessage, BadPathTest)
+{
+    struct pldm_header_info hdr;
+    struct pldm_header_info* hdr_ptr = NULL;
+    pldm_msg_t msg{};
+
+    // PLDM header information pointer is NULL
+    auto rc = pack_pldm_header(hdr_ptr, &msg);
+    ASSERT_EQ(rc, PLDM_ERROR_INVALID_DATA);
+
+    // PLDM message pointer is NULL
+    rc = pack_pldm_header(&hdr, nullptr);
+    ASSERT_EQ(rc, PLDM_ERROR_INVALID_DATA);
+
+    // PLDM header information pointer and PLDM message pointer is NULL
+    rc = pack_pldm_header(hdr_ptr, nullptr);
+    ASSERT_EQ(rc, PLDM_ERROR_INVALID_DATA);
+
+    // RESERVED message type
+    hdr.msg_type = RESERVED;
+    rc = pack_pldm_header(&hdr, &msg);
+    ASSERT_EQ(rc, PLDM_ERROR_INVALID_DATA);
+
+    // Instance ID out of range
+    hdr.msg_type = REQUEST;
+    hdr.instance = 32;
+    rc = pack_pldm_header(&hdr, &msg);
+    ASSERT_EQ(rc, PLDM_ERROR_INVALID_DATA);
+
+    // PLDM type out of range
+    hdr.msg_type = REQUEST;
+    hdr.instance = 31;
+    hdr.pldm_type = 64;
+    rc = pack_pldm_header(&hdr, &msg);
+    ASSERT_EQ(rc, PLDM_ERROR_INVALID_PLDM_TYPE);
+}
+
+TEST(PackPLDMMessage, RequestMessageGoodPath)
+{
+    struct pldm_header_info hdr;
+    pldm_msg_t msg{};
+
+    // Message type is REQUEST and lower range of the field values
+    hdr.msg_type = REQUEST;
+    hdr.instance = 0;
+    hdr.pldm_type = 0;
+    hdr.command = 0;
+
+    auto rc = pack_pldm_header(&hdr, &msg);
+    ASSERT_EQ(rc, PLDM_SUCCESS);
+    ASSERT_EQ(msg.request, 1);
+    ASSERT_EQ(msg.datagram, 0);
+    ASSERT_EQ(msg.instance_id, 0);
+    ASSERT_EQ(msg.type, 0);
+    ASSERT_EQ(msg.command, 0);
+
+    // Message type is REQUEST and upper range of the field values
+    hdr.instance = 31;
+    hdr.pldm_type = 63;
+    hdr.command = 255;
+
+    rc = pack_pldm_header(&hdr, &msg);
+    ASSERT_EQ(rc, PLDM_SUCCESS);
+    ASSERT_EQ(msg.request, 1);
+    ASSERT_EQ(msg.datagram, 0);
+    ASSERT_EQ(msg.instance_id, 31);
+    ASSERT_EQ(msg.type, 63);
+    ASSERT_EQ(msg.command, 255);
+
+    // Message type is ASYNC_REQUEST_NOTIFY
+    hdr.msg_type = ASYNC_REQUEST_NOTIFY;
+
+    rc = pack_pldm_header(&hdr, &msg);
+    ASSERT_EQ(rc, PLDM_SUCCESS);
+    ASSERT_EQ(rc, PLDM_SUCCESS);
+    ASSERT_EQ(msg.request, 1);
+    ASSERT_EQ(msg.datagram, 1);
+    ASSERT_EQ(msg.instance_id, 31);
+    ASSERT_EQ(msg.type, 63);
+    ASSERT_EQ(msg.command, 255);
+}
+
+TEST(PackPLDMMessage, ResponseMessageGoodPath)
+{
+    struct pldm_header_info hdr;
+    pldm_msg_t msg{};
+
+    // Message type is RESPONSE and lower range of the field values
+    hdr.msg_type = RESPONSE;
+    hdr.instance = 0;
+    hdr.pldm_type = 0;
+    hdr.command = 0;
+
+    auto rc = pack_pldm_header(&hdr, &msg);
+    ASSERT_EQ(rc, PLDM_SUCCESS);
+    ASSERT_EQ(msg.request, 0);
+    ASSERT_EQ(msg.datagram, 0);
+    ASSERT_EQ(msg.instance_id, 0);
+    ASSERT_EQ(msg.type, 0);
+    ASSERT_EQ(msg.command, 0);
+
+    // Message type is RESPONSE and upper range of the field values
+    hdr.instance = 31;
+    hdr.pldm_type = 63;
+    hdr.command = 255;
+
+    rc = pack_pldm_header(&hdr, &msg);
+    ASSERT_EQ(rc, PLDM_SUCCESS);
+    ASSERT_EQ(msg.request, 0);
+    ASSERT_EQ(msg.datagram, 0);
+    ASSERT_EQ(msg.instance_id, 31);
+    ASSERT_EQ(msg.type, 63);
+    ASSERT_EQ(msg.command, 255);
+}
+
+TEST(UnpackPLDMMessage, BadPathTest)
+{
+    struct pldm_header_info hdr;
+    size_t resp_size = 0;
+
+    // PLDM message pointer is NULL
+    auto rc = unpack_pldm_header(nullptr, 0, &hdr, &resp_size);
+    ASSERT_EQ(rc, PLDM_ERROR_INVALID_DATA);
+}
+
+TEST(UnpackPLDMMessage, RequestMessageGoodPath)
+{
+    struct pldm_header_info hdr;
+    pldm_msg_t msg{};
+    size_t resp_size = 0;
+
+    // Unpack PLDM request message and lower range of field values
+    msg.request = 1;
+    auto rc = unpack_pldm_header(&msg, sizeof(msg) - sizeof(uint8_t*), &hdr,
+                                 &resp_size);
+    ASSERT_EQ(rc, PLDM_SUCCESS);
+    ASSERT_EQ(hdr.msg_type, REQUEST);
+    ASSERT_EQ(hdr.instance, 0);
+    ASSERT_EQ(hdr.pldm_type, 0);
+    ASSERT_EQ(hdr.command, 0);
+    ASSERT_EQ(resp_size, 0);
+
+    // Unpack PLDM async request message and lower range of field values
+    msg.datagram = 1;
+    rc = unpack_pldm_header(&msg, sizeof(msg) - sizeof(uint8_t*), &hdr,
+                            &resp_size);
+    ASSERT_EQ(rc, PLDM_SUCCESS);
+    ASSERT_EQ(hdr.msg_type, ASYNC_REQUEST_NOTIFY);
+
+    // Unpack PLDM request message and upper range of field values
+    msg.datagram = 0;
+    msg.instance_id = 31;
+    msg.type = 63;
+    msg.command = 255;
+    rc = unpack_pldm_header(&msg, sizeof(msg) - sizeof(uint8_t*) + 2, &hdr,
+                            &resp_size);
+    ASSERT_EQ(rc, PLDM_SUCCESS);
+    ASSERT_EQ(hdr.msg_type, REQUEST);
+    ASSERT_EQ(hdr.instance, 31);
+    ASSERT_EQ(hdr.pldm_type, 63);
+    ASSERT_EQ(hdr.command, 255);
+    ASSERT_EQ(resp_size, 2);
+}
+
+TEST(UnpackPLDMMessage, ResponseMessageGoodPath)
+{
+    struct pldm_header_info hdr;
+    pldm_msg_t msg{};
+    size_t resp_size = 0;
+
+    // Unpack PLDM response message and lower range of field values
+    auto rc = unpack_pldm_header(&msg, sizeof(msg) - sizeof(uint8_t*), &hdr,
+                                 &resp_size);
+    ASSERT_EQ(rc, PLDM_SUCCESS);
+    ASSERT_EQ(hdr.msg_type, RESPONSE);
+    ASSERT_EQ(hdr.instance, 0);
+    ASSERT_EQ(hdr.pldm_type, 0);
+    ASSERT_EQ(hdr.command, 0);
+    ASSERT_EQ(resp_size, 0);
+
+    // Unpack PLDM response message and upper range of field values
+    msg.instance_id = 31;
+    msg.type = 63;
+    msg.command = 255;
+    rc = unpack_pldm_header(&msg, sizeof(msg) - sizeof(uint8_t*) + 2, &hdr,
+                            &resp_size);
+    ASSERT_EQ(rc, PLDM_SUCCESS);
+    ASSERT_EQ(hdr.msg_type, RESPONSE);
+    ASSERT_EQ(hdr.instance, 31);
+    ASSERT_EQ(hdr.pldm_type, 63);
+    ASSERT_EQ(hdr.command, 255);
+    ASSERT_EQ(resp_size, 2);
+}
+
 TEST(GetPLDMCommands, testEncodeRequest)
 {
     pldm_msg_t request{};
