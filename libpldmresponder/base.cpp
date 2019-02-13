e437c4c747aea9cd693c2e7ce6ed248dbcef8449
Sampa Misra <sampmisr@in.ibm.com>
2019-02-13 03:49:43 -0600

    Implement GetPLDMVersion command
    This commit implements the GetPLDMVersion which is required
    as part of the base PLDM support to know the version of a
    given PLDM type.
    
    Change-Id: I1bcbd938c5b6833f62e0ee2f474b04d76b6970d9
    Signed-off-by: Sampa Misra <sampmisr@in.ibm.com>

--- libpldmresponder/base.cpp
+++ libpldmresponder/base.cpp
@@ -15,6 +15,11 @@ namespace responder
 enum SupportedTypes
 {
     BASE = 0x00,
+    SMBIOS = 0x01,
+    PLATFORM = 0x02,
+    BIOS = 0x03,
+    FRU = 0x04,
+    FWUPDATE = 0x05,
 };
 
 enum SupportedCmds
@@ -27,6 +32,12 @@ using Cmd = std::vector<uint8_t>;
 
 static const std::map<Type, Cmd> capabilities{
     {BASE, {GET_PLDM_TYPES, GET_PLDM_COMMANDS}}};
+
+static const std::map<Type, uint32_t> version_map{
+    {BASE, 0xF3F71061}, // TODO need to put exact version
+    {FRU, 0xF3F71161},
+    {BIOS, 0x1001F700},
+};
 
 Response getPLDMTypes(Request payload, size_t payloadLen)
 {
@@ -86,6 +97,27 @@ Response getPLDMCommands(Request payload, size_t payloadLen)
 
     encode_get_commands_resp(0, SUCCESS, types.data(), response.data());
     return response;
+}
+
+void GetPLDMVersion(uint32_t transferHandle, uint8_t transferFlag, uint8_t type)
+{
+    // TODO multipart transfer
+    std::array<uint8_t, PLDM_RESPONSE_HEADER_LEN_BYTES +
+                            PLDM_GET_VERSION_RESP_DATA_BYTES>
+        response{};
+
+    auto version = new uint32_t;
+    auto search = version_map.find(type);
+
+    if (search == version_map.end())
+    {
+        encode_get_version_resp(0, ERROR_INVALID_PLDM_TYPE, 0, 0, nullptr, 0,
+                                nullptr, 0);
+        return;
+    }
+    *version = search->second;
+    encode_get_version_resp(0, SUCCESS, 0x0, 0x05, version, sizeof(uint32_t),
+                            response.data(), response.size());
 }
 
 } // namespace responder
