08019a6d3f0750f357f3ecdb134fcc11170fcb35
Deepak Kodihalli <dkodihal@in.ibm.com>
2019-03-03 23:01:05 -0600

    Implement command handler registration
    Implement a way for the PLDM responder library to register handlers for
    specific PLDM commands. This is as per the registration scheme
    documented in README.md.
    
    Change-Id: Ib2b9d84a4006d10d65590d8a35cd9318e3d46b44
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ registration.cpp
@@ -0,0 +1,32 @@
+#include "registration.hpp"
+
+#include <map>
+
+namespace pldm
+{
+
+namespace responder
+{
+
+namespace internal
+{
+using CommandHandler = std::map<uint8_t, Handler>;
+std::map<uint8_t, CommandHandler> typeHandlers;
+} // namespace internal
+
+void registerHandler(uint8_t pldmType, uint8_t pldmCommand, Handler&& handler)
+{
+    using namespace internal;
+    CommandHandler ch{{pldmCommand, std::move(handler)}};
+    typeHandlers.emplace(pldmType, std::move(ch));
+}
+
+void invokeHandler(uint8_t pldmType, uint8_t pldmCommand,
+                   const pldm_msg_payload* request, pldm_msg* response)
+{
+    using namespace internal;
+    typeHandlers.at(pldmType).at(pldmCommand)(request, response);
+}
+
+} // namespace responder
+} // namespace pldm
