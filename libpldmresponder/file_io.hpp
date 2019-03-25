214e984ad559f55cd5a5b0f78068b32eed4f2193
Tom Joseph <tomjoseph@in.ibm.com>
2019-03-25 13:52:16 +0530

    Implement the responder for ReadFileIntoMemory
    Change-Id: I07bc79bbed7e7a020010a425df4acd51acf1bb0d
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

---
+++ libpldmresponder/file_io.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <stdint.h>
+
+#include <vector>
+
+#include "libpldm/base.h"
+#include "libpldm/file_io.h"
+
+namespace pldm
+{
+
+using Type = uint8_t;
+
+namespace responder
+{
+
+/** @brief Handler for ReadFileInto
+ *
+ *  @param[in] request - Request message payload
+ *  @param[out] response - Response message written here
+ */
+void readFileIntoMemory(const pldm_msg_payload* request, pldm_msg* response);
+
+} // namespace responder
+} // namespace pldm
