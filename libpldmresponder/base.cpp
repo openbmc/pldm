99c58bcd53fea2894078631058bd7c8f7d2d0735
vkaverap <vkaverap@in.ibm.com>
2019-04-03 05:33:52 -0500

    PLDM Message Structure Change
    PLDM payload message structure consisted of a pointer to the payload
    meesage and the payload length. This structure is removed with this
    commit and the PLDM message structure now has the PLDM header struct
    and an array of size 1 that is the starting byte of message payload.
    This is done because it is not necessary to have an additional structure
    just for the payload length. Payload length can be the input parameter or
    computed by the decode or encode  function respectively. Also address of
    first byte of payload is enough to obtain the entire payload message. With
    this change, PLDM code is optimised as one entire message structure variable
    is removed.
    
    Change-Id: Ia46d852b8b16bfc7cf04f38435bd4079ad33c66b
    Signed-off-by: vkaverap <vkaverap@in.ibm.com>

--- libpldmresponder/base.cpp
+++ libpldmresponder/base.cpp
@@ -22,7 +22,7 @@ static const std::map<Type, ver32_t> versions{
     {PLDM_BASE, {0xF1, 0xF0, 0xF0, 0x00}},
 };
 
-void getPLDMTypes(const pldm_msg_payload* request, pldm_msg* response)
+std::vector<uint8_t> getPLDMTypes(const pldm_msg* request, size_t payloadLength)
 {
     // DSP0240 has this as a bitfield8[N], where N = 0 to 7
     std::array<bitfield8_t, 8> types{};
@@ -34,30 +34,40 @@ void getPLDMTypes(const pldm_msg_payload* request, pldm_msg* response)
         types[index].byte |= 1 << bit;
     }
 
-    encode_get_types_resp(0, PLDM_SUCCESS, types.data(), response);
+    std::vector<uint8_t> response(
+        sizeof(pldm_msg_hdr) + PLDM_GET_TYPES_RESP_BYTES, 0);
+    auto responsePtr = reinterpret_cast<pldm_msg*>(response.data());
+    encode_get_types_resp(0, PLDM_SUCCESS, types.data(), responsePtr);
+
+    return response;
 }
 
-void getPLDMCommands(const pldm_msg_payload* request, pldm_msg* response)
+std::vector<uint8_t> getPLDMCommands(const pldm_msg* request,
+                                     size_t payloadLength)
 {
     ver32_t version{};
     Type type;
 
-    if (request->payload_length != (sizeof(version) + sizeof(type)))
-    {
-        encode_get_commands_resp(0, PLDM_ERROR_INVALID_LENGTH, nullptr,
-                                 response);
-        return;
-    }
+    std::vector<uint8_t> response(
+        sizeof(pldm_msg_hdr) + PLDM_GET_COMMANDS_RESP_BYTES, 0);
+    auto responsePtr = reinterpret_cast<pldm_msg*>(response.data());
 
-    decode_get_commands_req(request, &type, &version);
+    uint8_t rc = decode_get_commands_req(request->payload, payloadLength, &type,
+                                         &version);
+
+    if (rc != PLDM_SUCCESS)
+    {
+        encode_get_commands_resp(0, rc, nullptr, responsePtr);
+        return response;
+    }
 
     // DSP0240 has this as a bitfield8[N], where N = 0 to 31
     std::array<bitfield8_t, 32> cmds{};
     if (capabilities.find(type) == capabilities.end())
     {
         encode_get_commands_resp(0, PLDM_ERROR_INVALID_PLDM_TYPE, nullptr,
-                                 response);
-        return;
+                                 responsePtr);
+        return response;
     }
 
     for (const auto& cmd : capabilities.at(type))
@@ -68,24 +78,28 @@ void getPLDMCommands(const pldm_msg_payload* request, pldm_msg* response)
         cmds[index].byte |= 1 << bit;
     }
 
-    encode_get_commands_resp(0, PLDM_SUCCESS, cmds.data(), response);
+    encode_get_commands_resp(0, PLDM_SUCCESS, cmds.data(), responsePtr);
+
+    return response;
 }
 
-void getPLDMVersion(const pldm_msg_payload* request, pldm_msg* response)
+void getPLDMVersion(const pldm_msg* request, size_t payloadLength,
+                    std::vector<uint8_t>& response)
 {
     uint32_t transferHandle;
     Type type;
     uint8_t transferFlag;
 
-    if (request->payload_length !=
-        (sizeof(transferHandle) + sizeof(type) + sizeof(transferFlag)))
+    auto responsePtr = reinterpret_cast<pldm_msg*>(response.data());
+
+    uint8_t rc = decode_get_version_req(request->payload, payloadLength,
+                                        &transferHandle, &transferFlag, &type);
+
+    if (rc != PLDM_SUCCESS)
     {
-        encode_get_version_resp(0, PLDM_ERROR_INVALID_LENGTH, 0, 0, nullptr, 0,
-                                response);
+        encode_get_version_resp(0, rc, 0, 0, nullptr, 4, responsePtr);
         return;
     }
-
-    decode_get_version_req(request, &transferHandle, &transferFlag, &type);
 
     ver32_t version{};
     auto search = versions.find(type);
@@ -93,13 +107,15 @@ void getPLDMVersion(const pldm_msg_payload* request, pldm_msg* response)
     if (search == versions.end())
     {
         encode_get_version_resp(0, PLDM_ERROR_INVALID_PLDM_TYPE, 0, 0, nullptr,
-                                0, response);
+                                4, responsePtr);
         return;
     }
 
     memcpy(&version, &(search->second), sizeof(version));
     encode_get_version_resp(0, PLDM_SUCCESS, 0, PLDM_START_AND_END, &version,
-                            sizeof(pldm_version), response);
+                            sizeof(pldm_version), responsePtr);
+
+    return;
 }
 
 } // namespace responder
