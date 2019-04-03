e4eb1453b8366f09afcdd717f27bda500cbe41bb
vkaverap <vkaverap@in.ibm.com>
2019-04-03 05:33:52 -0500

    Encode-Decode for SetStateEffecterStates commands
    Change-Id: I1436c8730553b5a1aed8cda1fa90b8742e5be228
    Signed-off-by: vkaverap <vkaverap@in.ibm.com>

--- libpldm/platform.c
+++ libpldm/platform.c
@@ -22,10 +22,48 @@ int encode_set_state_effecter_states_resp(uint8_t instance_id,
 	return rc;
 }
 
-int decode_set_state_effecter_states_req(const struct pldm_msg_payload *msg,
-					 uint16_t *effecter_id,
-					 uint8_t *comp_effecter_count,
-					 set_effecter_state_field *field)
+int encode_set_state_effecter_states_req(
+    uint8_t instance_id, uint16_t effecter_id, uint8_t comp_effecter_count,
+    set_effecter_state_field *stateField, uint8_t stateField_size, struct pldm_msg *msg)
+{
+	struct pldm_header_info header = {0};
+	int rc = PLDM_SUCCESS;
+
+	header.msg_type = PLDM_REQUEST;
+	header.instance = instance_id;
+	header.pldm_type = PLDM_PLATFORM;
+	header.command = PLDM_SET_STATE_EFFECTER_STATES;
+
+	if ((rc = pack_pldm_header(&header, &(msg->hdr))) > PLDM_SUCCESS) {
+		return rc;
+	}
+
+	uint8_t *encoded_msg = msg->body.payload;
+	effecter_id = htole16(effecter_id);
+	memcpy(encoded_msg, &effecter_id, sizeof(effecter_id));
+	encoded_msg += sizeof(effecter_id);
+	memcpy(encoded_msg, &comp_effecter_count, sizeof(comp_effecter_count));
+	encoded_msg += sizeof(comp_effecter_count);
+	memcpy(encoded_msg, stateField, stateField_size);
+
+	return PLDM_SUCCESS;
+}
+
+int decode_set_state_effecter_states_resp(const struct pldm_msg_payload *msg,
+					  uint8_t *completion_code)
+{
+	if (msg == NULL || completion_code == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	*completion_code = *(uint8_t *)msg->payload;
+
+	return PLDM_SUCCESS;
+}
+
+int decode_set_state_effecter_states_req(
+    const struct pldm_msg_payload *msg, uint16_t *effecter_id,
+    uint8_t *comp_effecter_count, set_effecter_state_field *field)
 {
 	if (msg == NULL || effecter_id == NULL || comp_effecter_count == NULL ||
 	    field == NULL) {
--- libpldm/platform.h
+++ libpldm/platform.h
@@ -29,9 +29,9 @@ enum pldm_platform_commands {
 
 typedef struct state_field_for_state_effecter_set {
 	uint8_t set_request;    //!< Whether to change the state
-	uint8_t effecter_state; //!< Expected state of the effecter
+	uint8_t effecter_state; //!< Expected state of the effecter	
 } __attribute__((packed)) set_effecter_state_field;
-
+	
 /* Responder */
 
 /* SetStateEffecterStates */
@@ -70,6 +70,34 @@ int decode_set_state_effecter_states_req(const struct pldm_msg_payload *msg,
 					 uint16_t *effecter_id,
 					 uint8_t *comp_effecter_count,
 					 set_effecter_state_field *field);
+
+/** @brief Create a PLDM request message for SetStateEffecterStates
+ *
+ *  @param[in] instance_id - Message's instance id
+ *  @param[in] effecter_id - used to identify and access the effecter
+ *  @param[in] comp_effecter_count - number of individual sets of effecter
+ *         information. Upto eight sets of state effecter info can be accessed
+ *         for a given effecter.
+ *  @param[in] field - each unit is an instance of the stateFileld structure
+ *             that is used to set the requested state for a particular effecter
+ *             within the state effecter.
+ *  @param[out] msg - Message will be written to this
+ *  @return pldm_completion_codes
+ *  @note  Caller is responsible for memory alloc and dealloc of param
+ *         'msg.body.payload'
+ */
+
+int encode_set_state_effecter_states_req(
+    uint8_t instance_id, uint16_t effecter_id, uint8_t comp_effecter_count,
+    set_effecter_state_field *stateField, uint8_t stateField_size, struct pldm_msg *msg);
+
+/** @brief Decode SetStateEffecterStates request data
+ *  @param[in] msg - Request message payload
+ *  @param[out] completion_code - PLDM completion code
+ *  @return pldm_completion_codes
+ */
+int decode_set_state_effecter_states_resp(const struct pldm_msg_payload *msg,
+					  uint8_t *completion_code);
 
 #ifdef __cplusplus
 }
