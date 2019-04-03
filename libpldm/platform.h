94ffbcf7c94cfdab7b48b389bb7e704c1ee573cc
vkaverap <vkaverap@in.ibm.com>
2019-04-03 05:33:52 -0500

    Encode-Decode for SetStateEffecterStates commands
    Change-Id: I1436c8730553b5a1aed8cda1fa90b8742e5be228
    Signed-off-by: vkaverap <vkaverap@in.ibm.com>

--- libpldm/platform.h
+++ libpldm/platform.h
@@ -71,6 +71,40 @@ int decode_set_state_effecter_states_req(const struct pldm_msg_payload *msg,
 					 uint8_t *comp_effecter_count,
 					 set_effecter_state_field *field);
 
+/** @brief Create a PLDM request message for SetStateEffecterStates
+ *
+ *  @param[in] instance_id - Message's instance id
+ *  @param[in] effecter_id - used to identify and access the effecter
+ *  @param[in] comp_effecter_count - number of individual sets of effecter
+ *         information. Upto eight sets of state effecter info can be accessed
+ *         for a given effecter.
+ *  @param[in] state_field_size - total size of array of state field structs
+ *  @param[out] field - each unit is an instance of the stateFileld structure
+ *         that is used to set the requested state for a particular effecter
+ *         within the state effecter. This field holds the starting address of
+ *         the stateField values. The user is responsible to allocate the
+ *         memory prior to calling this command per state_field_size.
+ *  @param[out] msg - Message will be written to this
+ *  @return pldm_completion_codes
+ *  @note  Caller is responsible for memory alloc and dealloc of param
+ *         'msg.body.payload'
+ */
+
+int encode_set_state_effecter_states_req(uint8_t instance_id,
+					 uint16_t effecter_id,
+					 uint8_t comp_effecter_count,
+					 set_effecter_state_field *field,
+					 size_t state_field_size,
+					 struct pldm_msg *msg);
+
+/** @brief Decode SetStateEffecterStates response data
+ *  @param[in] msg - Request message payload
+ *  @param[out] completion_code - PLDM completion code
+ *  @return pldm_completion_codes
+ */
+int decode_set_state_effecter_states_resp(const struct pldm_msg_payload *msg,
+					  uint8_t *completion_code);
+
 #ifdef __cplusplus
 }
 #endif
