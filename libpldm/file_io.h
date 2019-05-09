bc4c6ae0dbe4262b31c080041ac5d06181c9e762
Tom Joseph <tomjoseph@in.ibm.com>
2019-05-09 20:03:52 +0530

    Implement WriteFileFromMemory command
    WriteFileFromMemory command is implemented and the ReadFileIntoMemory
    command implementation is refactored.
    
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

--- libpldm/file_io.h
+++ libpldm/file_io.h
@@ -12,22 +12,24 @@ extern "C" {
 
 /** @brief PLDM Commands in FileIO type
  */
-enum pldm_fileio_commands { PLDM_READ_FILE_MEMORY = 0x6 };
+enum pldm_fileio_commands {
+	PLDM_READ_FILE_INTO_MEMORY = 0x6,
+	PLDM_WRITE_FILE_FROM_MEMORY = 0x7,
+};
 
 /** @brief PLDM Command specific codes
  */
 enum pldm_fileio_completion_codes {
 	PLDM_INVALID_FILE_HANDLE = 0x80,
 	PLDM_DATA_OUT_OF_RANGE = 0x81,
-	PLDM_INVALID_READ_LENGTH = 0x82
+	PLDM_INVALID_READ_LENGTH = 0x82,
+	PLDM_INVALID_WRITE_LENGTH = 0x83,
 };
 
-#define PLDM_READ_FILE_MEM_REQ_BYTES 20
-#define PLDM_READ_FILE_MEM_RESP_BYTES 5
+#define PLDM_RW_FILE_MEM_REQ_BYTES 20
+#define PLDM_RW_FILE_MEM_RESP_BYTES 5
 
-/* ReadFileIntoMemory */
-
-/** @brief Decode ReadFileIntoMemory commands request data
+/** @brief Decode ReadFileIntoMemory and WriteFileFromMemory commands request
  *
  *  @param[in] msg - Request message payload
  *  @param[out] fileHandle - A handle to the file
@@ -37,15 +39,14 @@ enum pldm_fileio_completion_codes {
  *                        written to
  *  @return pldm_completion_codes
  */
-int decode_read_file_memory_req(const struct pldm_msg_payload *msg,
-				uint32_t *fileHandle, uint32_t *offset,
-				uint32_t *length, uint64_t *address);
+int decode_rw_file_memory_req(const struct pldm_msg_payload *msg,
+			      uint32_t *fileHandle, uint32_t *offset,
+			      uint32_t *length, uint64_t *address);
 
-/* GetPLDMVersion */
-
-/** @brief Create a PLDM response for ReadFileIntoMemory
+/** @brief Create a PLDM response for ReadFileIntoMemory and WriteFileFromMemory
  *
  *  @param[in] instance_id - Message's instance id
+ *  @param[in] command - PLDM command
  *  @param[in] completion_code - PLDM completion code
  *  @param[in] length - Number of bytes read. This could be less than what the
 			 requester asked for.
@@ -54,8 +55,9 @@ int decode_read_file_memory_req(const struct pldm_msg_payload *msg,
  *  @note  Caller is responsible for memory alloc and dealloc of param
  *         'msg.body.payload'
  */
-int encode_read_file_memory_resp(uint8_t instance_id, uint8_t completion_code,
-				 uint32_t length, struct pldm_msg *msg);
+int encode_rw_file_memory_resp(uint8_t instance_id, uint8_t command,
+			       uint8_t completion_code, uint32_t length,
+			       struct pldm_msg *msg);
 
 #ifdef __cplusplus
 }
--- libpldmresponder/file_io.cpp
+++ libpldmresponder/file_io.cpp
@@ -1,4 +1,5 @@
 #include "file_io.hpp"
+
 #include "registration.hpp"
 
 #include <fcntl.h>
@@ -7,14 +8,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include <array>
 #include <cstring>
-#include <filesystem>
 #include <fstream>
-#include <iostream>
-#include <map>
-#include <stdexcept>
-#include <vector>
+#include <phosphor-logging/log.hpp>
 
 #include "libpldm/base.h"
 
@@ -24,89 +20,114 @@ namespace pldm
 namespace responder
 {
 
-namespace fs = std::filesystem;
-
 namespace fileio
 {
+
 void registerHandlers()
 {
-registerHandler(PLDM_FILE_IO, PLDM_READ_FILE_MEMORY, std::move(readFileIntoMemory));
+    registerHandler(PLDM_FILE_IO, PLDM_READ_FILE_INTO_MEMORY,
+                    std::move(readFileIntoMemory));
+    registerHandler(PLDM_FILE_IO, PLDM_WRITE_FILE_FROM_MEMORY,
+                    std::move(writeFileFromMemory));
 }
 
+namespace fs = std::filesystem;
+using namespace phosphor::logging;
 
-}
+namespace dma
+{
 
+/** @struct AspeedXdmaOp
+ *
+ * Structure representing XDMA operation
+ */
 struct AspeedXdmaOp
 {
-    uint8_t upstream;
-    uint64_t hostAddr;
-    uint32_t len;
+    uint8_t upstream;  //!< boolean indicating the direction of the DMA
+                       //!< operation, true means a transfer from BMC to host.
+    uint64_t hostAddr; //!< the DMA address on the host side, configured by
+                       //!< PCI subsystem.
+    uint32_t len;      //!< the size of the transfer in bytes, it should be a
+                       //!< multiple of 16 bytes
 } __attribute__((packed));
 
-int dmaTransferData(const fs::path& file, const uint32_t offset,
-                    const uint32_t length, const uint64_t address)
+constexpr auto xdmaDev = "/dev/xdma";
+
+int DMA::transferDataHost(const fs::path& path, uint32_t offset,
+                          uint32_t length, uint64_t address, bool upstream)
 {
-    const size_t pageSize = getpagesize();
+    static const size_t pageSize = getpagesize();
     uint32_t numPages = length / pageSize;
-    uint32_t pageLength = numPages * pageSize;
-    if (length > pageLength)
-        pageLength += pageSize;
-    //fprintf(stderr, "dmaTransferData pageLength = %d\n", pageLength);
+    uint32_t pageAlignedLength = numPages * pageSize;
 
-    int rc = 0;
+    if (length > pageAlignedLength)
+    {
+        pageAlignedLength += pageSize;
+    }
+
+    auto mmapCleanup = [pageAlignedLength](void* vgaMem) {
+        munmap(vgaMem, pageAlignedLength);
+    };
+
     int fd = -1;
-    const char* xdmaDev = "/dev/xdma";
-    void* vgaMem = nullptr;
-    std::ifstream stream(file.string());
-
+    int rc = 0;
     fd = open(xdmaDev, O_RDWR);
     if (fd < 0)
     {
-        rc = -ENODEV;
-        goto done;
+        rc = -errno;
+        log<level::ERR>("Failed to open the XDMA device", entry("RC=%d", rc));
+        return rc;
     }
 
-    vgaMem = mmap(NULL, pageLength, PROT_WRITE, MAP_SHARED, fd, 0);
-    if (!vgaMem)
+    utils::CustomFD xdmaFd(fd);
+
+    void* vgaMem;
+    vgaMem = mmap(nullptr, pageAlignedLength, upstream ? PROT_WRITE : PROT_READ,
+                  MAP_SHARED, xdmaFd(), 0);
+    if (MAP_FAILED == vgaMem)
     {
-        rc = -ENOMEM;
-        goto done;
+        rc = -errno;
+        log<level::ERR>("Failed to mmap the XDMA device", entry("RC=%d", rc));
+        return rc;
     }
 
-    stream.seekg(offset);
-    stream.read(static_cast<char*>(vgaMem), length);
-    //for (int i = 0; i<1488;i++)
-    //{
-    //     fprintf(stderr, "vgaMem[%d] = 0x%X ",i, *((char *)vgaMem + i));
-    //}
-    //fprintf(stderr, "\n");
+    std::unique_ptr<void, decltype(mmapCleanup)> vgaMemPtr(vgaMem, mmapCleanup);
 
+    if (upstream)
+    {
+        std::ifstream stream(path.string());
 
-    struct AspeedXdmaOp xdmaOp;
-    xdmaOp.upstream = 1;
+        stream.seekg(offset);
+        stream.read(static_cast<char*>(vgaMemPtr.get()), length);
+    }
+
+    AspeedXdmaOp xdmaOp;
+    xdmaOp.upstream = upstream ? 1 : 0;
     xdmaOp.hostAddr = address;
     xdmaOp.len = length;
 
-    rc = write(fd, &xdmaOp, sizeof(xdmaOp));
+    rc = write(xdmaFd(), &xdmaOp, sizeof(xdmaOp));
     if (rc < 0)
     {
         rc = -errno;
-    }
-    
-   //fprintf(stderr, "rc = %d", rc);
-done:
-    if (vgaMem)
-    {
-        munmap(vgaMem, pageLength);
+
+        log<level::ERR>("Failed to execute the DMA operation",
+                        entry("RC=%d", rc));
+        return rc;
     }
 
-    if (fd >= 0)
+    if (!upstream)
     {
-        close(fd);
+        std::ofstream stream(path.string());
+
+        stream.seekp(offset);
+        stream.write(static_cast<const char*>(vgaMemPtr.get()), length);
     }
 
-    return rc;
+    return 0;
 }
+
+} // namespace dma
 
 void readFileIntoMemory(const pldm_msg_payload* request, pldm_msg* response)
 {
@@ -118,15 +139,22 @@ void readFileIntoMemory(const pldm_msg_payload* request, pldm_msg* response)
     if (request->payload_length != (sizeof(fileHandle) + sizeof(offset) +
                                     sizeof(length) + sizeof(address)))
     {
-        encode_read_file_memory_resp(0, PLDM_ERROR_INVALID_LENGTH, 0, response);
+        encode_rw_file_memory_resp(0, PLDM_READ_FILE_INTO_MEMORY,
+                                   PLDM_ERROR_INVALID_LENGTH, 0, response);
         return;
     }
 
-    decode_read_file_memory_req(request, &fileHandle, &offset, &length,
-                                &address);
+    decode_rw_file_memory_req(request, &fileHandle, &offset, &length, &address);
+
+    if (length % dma::minSize)
+    {
+        log<level::ERR>("Readlength is not a multiple of 16");
+        encode_rw_file_memory_resp(0, PLDM_READ_FILE_INTO_MEMORY,
+                                   PLDM_INVALID_READ_LENGTH, 0, response);
+        return;
+    }
 
     std::string readFilePath;
-
     if (fileHandle == 0)
     {
         readFilePath = "/var/lib/pldm/PHYP-NVRAM";
@@ -137,76 +165,92 @@ void readFileIntoMemory(const pldm_msg_payload* request, pldm_msg* response)
     }
     else
     {
-        std::cerr << "Invalid file handle" << "\n";
-        encode_read_file_memory_resp(0, PLDM_INVALID_FILE_HANDLE, 0, response);
+        log<level::ERR>("Invalid file handle");
+        encode_rw_file_memory_resp(0, PLDM_READ_FILE_INTO_MEMORY,
+                                   PLDM_INVALID_FILE_HANDLE, 0, response);
     }
-
-    uint32_t origLength = length;
-
     fs::path path{readFilePath};
+
     if (!fs::exists(path))
     {
-        std::cerr << "File does not exist"
-                  << "\n";
-        encode_read_file_memory_resp(0, PLDM_INVALID_FILE_HANDLE, 0, response);
-        return;
-    }
 
-    constexpr uint32_t minLength = 16;
-    if (length < minLength)
-    {
-        std::cerr << "Readlength is less than 16"
-                  << "\n";
-        encode_read_file_memory_resp(0, PLDM_INVALID_READ_LENGTH, 0, response);
+        log<level::ERR>("File does not exist");
+        encode_rw_file_memory_resp(0, PLDM_READ_FILE_INTO_MEMORY,
+                                   PLDM_INVALID_FILE_HANDLE, 0, response);
         return;
     }
 
     auto fileSize = fs::file_size(path);
     if (offset + length > fileSize)
     {
-        std::cerr << "Data out of range"
-                  << "\n";
-        encode_read_file_memory_resp(0, PLDM_DATA_OUT_OF_RANGE, 0, response);
+        log<level::ERR>("Data out of range");
+        encode_rw_file_memory_resp(0, PLDM_READ_FILE_INTO_MEMORY,
+                                   PLDM_DATA_OUT_OF_RANGE, 0, response);
         return;
     }
 
-    // There is a restriction on the maximum size to 16MB - 4096B. This should be made
-    // into a configurable parameter 16773120
-    constexpr size_t maxDMASize = (16 * 1024 * 1024) - 4096;
-
-    while (length > 0)
-    {
-        if (length > maxDMASize)
-        {
-            std::cerr << "offset = " << offset << "length = " << length
-                      << "address = " << address << "\n";
-                        auto rc = dmaTransferData(path, offset, maxDMASize,
-                        address); if (rc < 0)
-                        {
-                            encode_read_file_memory_resp(0, PLDM_ERROR, 0,
-                            response); return;
-                        }
-            offset += maxDMASize;
-            length -= maxDMASize;
-            address += maxDMASize;
-            std::cerr << "offset = " << offset << "length = " << length
-                      << "address = " << address << "\n";
-        }
-        else
-        {
-                        auto rc = dmaTransferData(path, offset, length, address);
-                        if (rc < 0)
-                        {
-                            encode_read_file_memory_resp(0, PLDM_ERROR, 0, response);
-                            return;
-                        }
-            std::cerr << "Last: offset = " << offset << "length = " << length
-                      << "address = " << address << "\n";
-            encode_read_file_memory_resp(0, PLDM_SUCCESS, origLength, response);
-            return;
-        }
-    }
+    using namespace dma;
+    DMA intf;
+    transferAll<DMA>(&intf, PLDM_READ_FILE_INTO_MEMORY, path, offset, length,
+                     address, true, response);
 }
 
+void writeFileFromMemory(const pldm_msg_payload* request, pldm_msg* response)
+{
+    uint32_t fileHandle = 0;
+    uint32_t offset = 0;
+    uint32_t length = 0;
+    uint64_t address = 0;
+
+    if (request->payload_length != (sizeof(fileHandle) + sizeof(offset) +
+                                    sizeof(length) + sizeof(address)))
+    {
+        encode_rw_file_memory_resp(0, PLDM_WRITE_FILE_FROM_MEMORY,
+                                   PLDM_ERROR_INVALID_LENGTH, 0, response);
+        return;
+    }
+
+    decode_rw_file_memory_req(request, &fileHandle, &offset, &length, &address);
+
+    if (length % dma::minSize)
+    {
+        log<level::ERR>("Writelength is not a multiple of 16");
+        encode_rw_file_memory_resp(0, PLDM_WRITE_FILE_FROM_MEMORY,
+                                   PLDM_INVALID_WRITE_LENGTH, 0, response);
+        return;
+    }
+
+    std::string readFilePath;
+    if (fileHandle == 0)
+    {
+        readFilePath = "/var/lib/pldm/PHYP-NVRAM";
+    }
+    else if (fileHandle == 1)
+    {
+        readFilePath = "/var/lib/pldm/PHYP-NVRAM-CKSUM";
+    }
+    else
+    {
+        log<level::ERR>("Invalid file handle");
+        encode_rw_file_memory_resp(0, PLDM_WRITE_FILE_FROM_MEMORY,
+                                   PLDM_INVALID_FILE_HANDLE, 0, response);
+    }
+    fs::path path{readFilePath};
+
+    if (!fs::exists(path))
+    {
+        log<level::ERR>("File does not exist");
+        encode_rw_file_memory_resp(0, PLDM_WRITE_FILE_FROM_MEMORY,
+                                   PLDM_INVALID_FILE_HANDLE, 0, response);
+        return;
+    }
+
+    using namespace dma;
+    DMA intf;
+    transferAll<DMA>(&intf, PLDM_WRITE_FILE_FROM_MEMORY, path, offset, length,
+                     address, false, response);
+}
+
+} // namespace fileio
 } // namespace responder
 } // namespace pldm
