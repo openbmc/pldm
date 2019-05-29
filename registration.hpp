c2ebcb7b361d70cb8166c94938de2ea5538d6834
Jinu Joy Thomas <jinu.joy.thomas@in.ibm.com>
2019-05-29 05:22:31 -0500

    Implement the PLDM Daemon. Implement command handler registration
    PLDM daemon is implemented as a daemon which receives and send the response.
    PLDM messages received by the PLDM daemon from the MCTP daemon are routed
    to the respective command handler to create a response. This response will
    be send back by the PLDM daemon to the requester.
    
    PLDM daemon and MCTP daemon interact with each other using UNIX domain sockets.
    
    Implemented a way for the PLDM responder library to register handlers for
    specific PLDM commands. This is as per the registration scheme
    documented in README.md.
    
    Support for enabling verbose in the PLDM Demon (tracing the receive and
    response message packets) are conditionally compiled.You would need to
    provide the --enable-pldmdverbose flag to configure to enable it.
    
    Change-Id: I22cfd85103bce167239219fbcc59c25b09528211
    Signed-off-by: Jinu Joy Thomas <jinu.joy.thomas@in.ibm.com>

---
+++ registration.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <functional>
+#include <map>
+
+#include "libpldm/base.h"
+
+namespace pldm
+{
+
+namespace responder
+{
+
+using Handler =
+    std::function<void(const pldm_msg_payload* request, pldm_msg* response)>;
+
+/** @brief Register a handler for input PLDM command
+ *
+ *  @param[in] pldmType - PLDM type code
+ *  @param[in] pldmCommand - PLDM command code
+ *  @param[in] handler - PLDM command handler
+ */
+void registerHandler(uint8_t pldmType, uint8_t pldmCommand, Handler&& handler);
+
+/** @brief Invoke a handler for input PLDM command
+ *
+ *  @param[in] pldmType - PLDM type code
+ *  @param[in] pldmCommand - PLDM command code
+ *  @param[in] request - PLDM request message's payload
+ *  @param[out] response - PLDM response message
+ */
+void invokeHandler(uint8_t pldmType, uint8_t pldmCommand,
+                   const pldm_msg_payload* request, pldm_msg* response);
+
+} // namespace responder
+} // namespace pldm
