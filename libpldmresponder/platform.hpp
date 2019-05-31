d9e81900cad69422b057b7f304322f90c99c76b8
Sampa Misra <sampmisr@in.ibm.com>
2019-05-31 01:28:55 -0500

    libpldmresponder: implement setStateEffecterStates
    This commit implements the handler for setStateEffecterStates response.
    Apart from that it actually sets the effecter for PLDM_BOOT_PROGRESS
    state. This is used when host sends setStateEffecterStates to mark
    any change in hypervisor state. The currently supported states are
    "StandBy" and "BootComplete" as per
    xyz.openbmc_project.State.OperatingSystem.Status
    
    Change-Id: I205627b2b8a796a0dd4a200ac3d59c1d19d71f01
    Signed-off-by: Sampa Misra <sampmisr@in.ibm.com>

--- libpldmresponder/platform.hpp
+++ libpldmresponder/platform.hpp
@@ -2,9 +2,11 @@
 
 #include <stdint.h>
 
+#include <map>
 #include <vector>
 
 #include "libpldm/platform.h"
+#include "libpldm/states.h"
 
 namespace pldm
 {
@@ -21,6 +23,26 @@ namespace responder
  *  @param[out] Response - Response message written here
  */
 Response getPDR(const pldm_msg* request, size_t payloadLength);
+
+/** @brief Handler for setStateEffecterStates
+ *
+ *  @param[in] request - Request message payload
+ *  @param[in] payloadLength - Request payload length
+ *  @param[out] Response - Response message written here
+ */
+Response setStateEffecterStates(const pldm_msg* request, size_t payloadLength);
+
+/** @brief Function to actually set the effecter requested by host
+ *  @param[in] effecterId - Effecter ID sent by the host to act on
+ *  @param[in] compEffecterCnt - Composite effecter count for the effecter
+ *  @param[in] stateField - The state field data for each of the states, equal
+ *        to compEffecterCnt in number
+ *  @param[out] - Success or failure in setting the states. Returns failure if
+ *        atleast one state fails to be set
+ */
+
+int setBmcEffecter(uint16_t& effecterId, uint8_t& compEffecterCnt,
+                   set_effecter_state_field* stateField);
 
 } // namespace responder
 } // namespace pldm
