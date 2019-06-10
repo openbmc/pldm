7b489ad5760761d41de7d52857b05a576017bf39
Priyanga <priyram1@in.ibm.com>
2019-06-10 01:59:45 -0500

    Simplify encode/decode for PLDM platform commands
    The encode/decode for PLDM platform commands was implemented
    using extensive pointer arithmetic and memcpys. To reduce the
    pointer usage and also for the simple assignments, structures
    have been created for each of the PLDM platform commands.
    
    Change-Id: I6cf1a4b67b7a50325d2a2845245d937d71e13cb7
    Signed-off-by: Priyanga <priyram1@in.ibm.com>

--- libpldm/platform.h
+++ libpldm/platform.h
@@ -32,12 +32,22 @@ typedef struct state_field_for_state_effecter_set {
 	uint8_t effecter_state; //!< Expected state of the effecter
 } __attribute__((packed)) set_effecter_state_field;
 
+/** @struct PLDM_SetStateEffecterStates_Request
+ *
+ *  Structure representing PLDM set state effecter states request.
+ */
+struct pldm_set_state_effecter_states_req {
+	uint16_t effecter_id;
+	uint8_t comp_effecter_count;
+	set_effecter_state_field field[8];
+} __attribute__((packed));
+
 /* Responder */
 
 /* SetStateEffecterStates */
 
 /** @brief Create a PLDM response message for SetStateEffecterStates
- *
+
  *  @param[in] instance_id - Message's instance id
  *  @param[in] completion_code - PLDM completion code
  *  @param[out] msg - Message will be written to this
