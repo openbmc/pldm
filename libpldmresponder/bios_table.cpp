2ae0474265b57266b11d966893ce816affe00635
Deepak Kodihalli <dkodihal@in.ibm.com>
2019-06-19 13:25:31 +0530

    Add APIs to store/load BIOS tables
    This commit implements C++ APIs to store a PLDM BIOS table into
    persistent storage, and to load the same back into memory. This commit
    also defines C structs representing the different BIOS tables.
    
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>
    Change-Id: I4a771a368c6931464f45ae4a8f467b579c7a5d74

---
+++ libpldmresponder/bios_table.cpp
@@ -0,0 +1,47 @@
+#include "bios_table.hpp"
+
+namespace pldm
+{
+
+namespace responder
+{
+
+namespace bios
+{
+
+BIOSTable::BIOSTable(const char* filePath) : filePath(filePath)
+{
+}
+
+bool BIOSTable::isEmpty() const
+{
+    bool empty = false;
+    try
+    {
+        empty = fs::is_empty(filePath);
+    }
+    catch (fs::filesystem_error& e)
+    {
+        return true;
+    }
+    return empty;
+}
+
+void BIOSTable::store(const Table& table)
+{
+    std::ofstream stream(filePath.string(), std::ios::out | std::ios::binary);
+    stream.write(reinterpret_cast<const char*>(table.data()), table.size());
+}
+
+void BIOSTable::load(Response& response) const
+{
+    auto currSize = response.size();
+    auto fileSize = fs::file_size(filePath);
+    response.resize(currSize + fileSize);
+    std::ifstream stream(filePath.string(), std::ios::in | std::ios::binary);
+    stream.read(reinterpret_cast<char*>(response.data() + currSize), fileSize);
+}
+
+} // namespace bios
+} // namespace responder
+} // namespace pldm
