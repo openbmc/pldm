babb09cc7f0da952fdf7e74866f6dd14b93840ef
Lakshminarayana R. Kammath <lkammath@in.ibm.com>
2019-06-24 00:51:47 -0500

    Initial version of pldmtool code
    Tested following PLDM commands:
        1) GetPLDMTypes
        2) GetPLDMVersion
    
    Tested : Verified the RAW data response.
    
    pldmtool -c GetPLDMTypes
    DEBUG:  Success in creating the socket : RC = [0]
    DEBUG:  Success in connecting to the socket : RC = [0]
    DEBUG:  Success in sending message type as pldm to mctp : RC = [0]
    DEBUG:  MCTP initialization Success : RC = [3]
    DEBUG:  Encoded request succesfully : RC = [0]
    INFO:  Request Message:
    INFO:  08 01 9b 00 04 00 00
    DEBUG:  Write to socket successful : RC = [7]
    INFO:  Total length [7]
    Loopback response message:
    INFO:  08 01 9b 00 04 00 00
    INFO:  Entity ID = 8
    INFO:  PLDM Type = 1
    DEBUG:  On first recv(), responseMsg == requestMsg
    INFO:  Total length [14]
    INFO:  Socket recv() successful.
    Response Message:
    INFO:  08 01 00 00 04 00 01 00 00 00 00 00 00 00
    DEBUG:  Shutdown Socket successful, RC=0
    
    pldmtool -c GetPLDMVersion
    DEBUG:  Success in creating the socket : RC = [0]
    DEBUG:  Success in connecting to the socket : RC = [0]
    DEBUG:  Success in sending message type as pldm to mctp : RC = [0]
    DEBUG:  MCTP initialization Success : RC = [3]
    DEBUG:  Encoded request succesfully : RC = [0]
    INFO:  Request Message:
    INFO:  08 01 9b 00 03 00 00 00 00 01 00
    DEBUG:  Write to socket successful : RC = [11]
    INFO:  Total length [11]
    Loopback response message:
    INFO:  08 01 9b 00 03 00 00 00 00 01 00
    INFO:  Entity ID = 8
    INFO:  PLDM Type = 1
    DEBUG:  On first recv(), responseMsg == requestMsg
    INFO:  Total length [15]
    INFO:  Socket recv() successful.
    Response Message:
    INFO:  08 01 00 00 03 00 00 00 00 00 05 f1 f0 f0 00
    DEBUG:  Shutdown Socket successful, RC=0
    
    Signed-off-by: Lakshminarayana R. Kammath <lkammath@in.ibm.com>
    Change-Id: I758b5f5cf03ad9b91ce47144fe46cd79b41381f3

---
+++ tool/inc/logger.hpp
@@ -0,0 +1,101 @@
+#ifndef PLDM_LOGGER_H_
+#define PLDM_LOGGER_H_
+
+#include <string.h>
+
+#include <cstdarg>
+#include <cstdio>
+
+#define LOG_MSG_LENGTH 1024
+
+class Logger
+{
+  public:
+    Logger()
+    {
+    }
+    virtual ~Logger()
+    {
+    }
+    enum LogLevel
+    {
+        DEBUG_LVL, // Messages used only for debugging
+        INFO_LVL,  // Informational messages; something is unusual but not wrong
+        WARNING_LVL, // There's an indication of trouble, but it may be okay.
+        ERROR_LVL,   // A problem has occurred, but processing can continue
+    };
+    virtual int log_msg(LogLevel level, const char* fmt,
+                        va_list args) const = 0;
+
+    /** @brief print the message with selected arguments
+     *
+     *  @param[in] level - The type of information
+     *  @param[in] format - The information
+     *  @param[in] arguments - Arguments to print
+     *
+     *  @return - None
+     */
+    static int Log(LogLevel level, const char* fmt, va_list args);
+
+    /** @brief print the message with selected level
+     *
+     *  @param[in] level -  Log level
+     *  @param[in] format - The information
+     *
+     *  @return - None
+     */
+    static void Log(LogLevel level, const char* fmt, ...);
+
+    /** @brief print the debug log
+     *
+     *  @param[in] format - The information
+     *
+     *  @return - None
+     */
+    static void Debug(const char* fmt, ...);
+
+    /** @brief print the info log
+     *
+     *  @param[in] format - The information
+     *
+     *  @return - None
+     */
+    static void Info(const char* fmt, ...);
+
+    /** @brief print the warning log
+     *
+     *  @param[in] format - The information
+     *
+     *  @return - None
+     */
+    static void Warning(const char* fmt, ...);
+
+    /** @brief print the error log
+     *
+     *  @param[in] format - The information
+     *
+     *  @return - None
+     */
+    static void Error(const char* fmt, ...);
+
+  protected:
+    static void set_instance(Logger* logger)
+    {
+        instance_ = logger;
+    }
+
+  private:
+    // Disallow copying.
+    Logger(const Logger&);
+    void operator=(const Logger&);
+    static Logger* instance_;
+};
+
+#define STR(x) #x
+#define STRINGIFY(x) STR(x)
+#define FILE_LINE __FILE__ ", Line " STRINGIFY(__LINE__) ": "
+#define LOG_D(fmt, ...) Logger::Debug(fmt, __VA_ARGS__)
+#define LOG_E(fmt, ...) Logger::Error(FILE_LINE fmt, __VA_ARGS__)
+#define LOG_W(fmt, ...) Logger::Warning(fmt, __VA_ARGS__)
+#define LOG_I(fmt, ...) Logger::Info(fmt, __VA_ARGS__)
+#endif // PLDM_LOGGER_H_
