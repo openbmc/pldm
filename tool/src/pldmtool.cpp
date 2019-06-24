9d0af4d2fb3957b8962645bc901585572ffbfd23
Lakshminarayana R. Kammath <lkammath@in.ibm.com>
2019-06-24 00:51:47 -0500

    Initial version of pldmtool code
    Tested following PLDM commands:
        1) GetPLDMTypes
        2) GetPLDMVersion
    
    Tested : Verified the RAW data response.
    
    pldmtool -c GetPLDMTypes
    DEBUG:  Success in creating the socket : RC = [0]
    DEBUG:  Success in connecting to the socket : RC = [0]
    DEBUG:  Success in sending message type as pldm to mctp : RC = [0]
    DEBUG:  MCTP initialization Success : RC = [3]
    DEBUG:  Encoded request succesfully : RC = [0]
    INFO:  Request Message:
    INFO:  08 01 9b 00 04 00 00
    DEBUG:  Write to socket successful : RC = [7]
    INFO:  Total length [7]
    Loopback response message:
    INFO:  08 01 9b 00 04 00 00
    INFO:  Entity ID = 8
    INFO:  PLDM Type = 1
    DEBUG:  On first recv(), responseMsg == requestMsg
    INFO:  Total length [14]
    INFO:  Socket recv() successful.
    Response Message:
    INFO:  08 01 00 00 04 00 01 00 00 00 00 00 00 00
    DEBUG:  Shutdown Socket successful, RC=0
    
    pldmtool -c GetPLDMVersion
    DEBUG:  Success in creating the socket : RC = [0]
    DEBUG:  Success in connecting to the socket : RC = [0]
    DEBUG:  Success in sending message type as pldm to mctp : RC = [0]
    DEBUG:  MCTP initialization Success : RC = [3]
    DEBUG:  Encoded request succesfully : RC = [0]
    INFO:  Request Message:
    INFO:  08 01 9b 00 03 00 00 00 00 01 00
    DEBUG:  Write to socket successful : RC = [11]
    INFO:  Total length [11]
    Loopback response message:
    INFO:  08 01 9b 00 03 00 00 00 00 01 00
    INFO:  Entity ID = 8
    INFO:  PLDM Type = 1
    DEBUG:  On first recv(), responseMsg == requestMsg
    INFO:  Total length [15]
    INFO:  Socket recv() successful.
    Response Message:
    INFO:  08 01 00 00 03 00 00 00 00 00 05 f1 f0 f0 00
    DEBUG:  Shutdown Socket successful, RC=0
    
    Signed-off-by: Lakshminarayana R. Kammath <lkammath@in.ibm.com>
    Change-Id: I758b5f5cf03ad9b91ce47144fe46cd79b41381f3

---
+++ tool/src/pldmtool.cpp
@@ -0,0 +1,38 @@
+#include "pldmtool.hpp"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+    int rc = 0;
+    std::string pldm_cmd;
+    std::set<std::string> pldm_cmd_items = {"GetTID", "GetPLDMVersion",
+                                            "GetPLDMTypes", "GetPLDMCommands"};
+    bool verbose_flag;
+
+
+    CLI::App app{"OpenBMC PLDMTOOL"};
+    app.add_set("-c, --command", pldm_cmd, pldm_cmd_items,
+                "PLDM command to test", true)
+        ->required()
+        ->group("Standard");
+    app.add_flag("-v, --verbose", verbose_flag, "Verbose [debug information] ");
+    CLI11_PARSE(app, argc, argv);
+
+    std::string action{argv[2]};
+
+    const CallbackMap& callbacks = Registration::getCallbacks();
+
+    auto callback = callbacks.find(action);
+
+    if (callback == callbacks.end())
+    {
+        rc = -1;
+        return rc;
+    }
+
+    callback->second();
+
+    return (rc < 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+}
