27693a41cfee7c82d5c4ec9aa82ecaf53ec07a4a
Lakshminarayana R. Kammath <lkammath@in.ibm.com>
2019-06-24 00:51:47 -0500

    PLDMTOOL : Implement a PLDM requester tool.
    Implemented a way for sending PLDM requests for specific PLDM commands.
    This tool will interact with MCTP daemon using UNIX domain socket, The
    response got from socket will be displayed on the console in raw format.
    
    Tested following PLDM commands:
        1) GetPLDMTypes
        2) GetPLDMVersion
    
    Tested : Verified the RAW response data.
    
    ./pldmtool -c GetPLDMTypes
    Encoded request succesfully : RC = 0
    Request Message:
    08 01 9b 00 04 00 00
    Success in creating the socket : RC = 3
    Success in connecting to socket : RC = 0
    Success in sending message type as pldm to mctp : RC = 0
    Write to socket successful : RC = 7
    Total length:7
    Loopback response message:
    08 01 9b 00 04 00 00
    On first recv(),response == request : RC = 0
    Total length: 14
    Shutdown Socket successful :  RC = 0
    Socket recv() successful : RC = 0
    Response Message :
    08 01 00 00 04 00 01 00 00 00 00 00 00 00
    
    ./pldmtool -c GetPLDMVersion base
    PLDM Type requested : base
    Encoded request succesfully : RC = 0
    Request Message:
    08 01 9b 00 03 00 00 00 00 01 00
    Success in creating the socket : RC = 3
    Success in connecting to socket : RC = 0
    Success in sending message type as pldm to mctp : RC = 0
    Write to socket successful : RC = 11
    Total length:11
    Loopback response message:
    08 01 9b 00 03 00 00 00 00 01 00
    On first recv(),response == request : RC = 0
    Total length: 15
    Shutdown Socket successful :  RC = 0
    Socket recv() successful : RC = 0
    Response Message:
    08 01 00 00 03 00 00 00 00 00 05 f1 f0 f0 00
    
    ./pldmtool -h
    PLDM requester tool for OpenBMC
    Usage: ./pldmtool [OPTIONS] —command... [GetPLDMTypes] [GetPLDMVersion] SUBCOMMA                                                                             ND
    
    Positionals:
      —command TEXT ... REQUIRED
                                  PLDM request command
      GetPLDMTypes TEXT           Get PLDM Type
      GetPLDMVersion TEXT         Get PLDM Version
    
    Options:
      -h,--help                   Print this help message and exit
      -c TEXT ... REQUIRED        PLDM request command
    
    Subcommands:
      BASE                        PLDM Command Type = BASE
      BIOS                        PLDM Command Type = BIOS
      OEM                         PLDM Command Type = OEM
    
    Signed-off-by: Lakshminarayana R. Kammath <lkammath@in.ibm.com>
    Change-Id: I758b5f5cf03ad9b91ce47144fe46cd79b41381f3

---
+++ tool/pldmtool.cpp
@@ -0,0 +1,42 @@
+#include "handler.hpp"
+
+#include <CLI/CLI.hpp>
+
+int main(int argc, char** argv)
+{
+
+    CLI::App app{"PLDM requester tool for OpenBMC"};
+
+    // TODO: To enable it later
+    // bool verbose_flag = false;
+    // app.add_flag("-v, --verbose", verbose_flag, "Output debug logs ");
+    std::vector<std::string> args{};
+    app.add_option("-c, —command", args, "PLDM request command")->required();
+
+    std::string pldmCmdName;
+    app.add_option("GetPLDMTypes", pldmCmdName, "Get PLDM Type");
+    app.add_option("GetPLDMVersion", pldmCmdName, "Get PLDM Version");
+
+    app.add_subcommand("base", "PLDM Command Type = base");
+    app.add_subcommand("bios", "PLDM Command Type = bios");
+    app.add_subcommand("oem", "PLDM Command Type = oem");
+
+    CLI11_PARSE(app, argc, argv);
+
+    Handler handler;
+
+    // Parse args to program
+    std::string cmdName = args[0];
+    int rc = 0;
+
+    try
+    {
+        handler.dispatcher.at(cmdName)(std::move(args));
+    }
+    catch (const std::out_of_range& e)
+    {
+        std::cerr << cmdName << " is not supported!" << std::endl;
+        rc = -1;
+    }
+    return (rc < 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+}
