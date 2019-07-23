fa259a53e283978acc22129150c9a14f2494672e
PriyangaRamasamy <priyanga24@in.ibm.com>
2019-07-23 12:18:40 +0530

    libpldm: encode GetFruRecordTable request api
    This commit implements encode request api for
    Get FRU record table which is defined in
    PLDM FRU Data Specification DSP0257_1.0.0.
    
    Signed-off-by: PriyangaRamasamy <priyanga24@in.ibm.com>
    Change-Id: I8b276f4796175e0a44da9e719a3a1949d29ece86
    
    libpldm: decode GetFruRecordTable response api
    
    This commit implements decode response api for
    GetFruRecordTable which is defined in
    PLDM FRU Data Specification DSP0257_1.0.0.
    
    Change-Id: I97642b25142f523c349b4ccc19c9a002eb9d6e0d
    Signed-off-by: PriyangaRamasamy <priyanga24@in.ibm.com>

--- libpldm/fru.c
+++ libpldm/fru.c
@@ -192,3 +192,67 @@ int encode_fru_record(uint8_t *fru_table, size_t total_size, size_t *curr_size,
 
 	return PLDM_SUCCESS;
 }
+
+int encode_get_fru_record_table_req(uint8_t instance_id,
+				    uint32_t data_transfer_handle,
+				    uint8_t transfer_operation_flag,
+				    struct pldm_msg *msg, size_t payload_length)
+
+{
+	struct pldm_header_info header = {0};
+	int rc = PLDM_ERROR_INVALID_DATA;
+
+	header.msg_type = PLDM_REQUEST;
+	header.instance = instance_id;
+	header.pldm_type = PLDM_FRU;
+	header.command = PLDM_GET_FRU_RECORD_TABLE;
+
+	if (msg == NULL) {
+		return rc;
+	}
+	if (payload_length != sizeof(struct pldm_get_fru_record_table_req)) {
+		return PLDM_ERROR_INVALID_LENGTH;
+	}
+	if ((rc = pack_pldm_header(&header, &(msg->hdr))) > PLDM_SUCCESS) {
+		return rc;
+	}
+
+	struct pldm_get_fru_record_table_req *req =
+	    (struct pldm_get_fru_record_table_req *)msg->payload;
+	req->data_transfer_handle = htole32(data_transfer_handle);
+	req->transfer_operation_flag = transfer_operation_flag;
+
+	return PLDM_SUCCESS;
+}
+
+int decode_get_fru_record_table_resp(
+    const struct pldm_msg *msg, size_t payload_length, uint8_t *completion_code,
+    uint32_t *next_data_transfer_handle, uint8_t *transfer_flag,
+    uint8_t *fru_record_table_data, size_t *fru_record_table_length)
+{
+	if (msg == NULL || completion_code == NULL ||
+	    next_data_transfer_handle == NULL || transfer_flag == NULL ||
+	    fru_record_table_data == NULL || fru_record_table_length == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	*completion_code = msg->payload[0];
+	if (PLDM_SUCCESS != *completion_code) {
+		return PLDM_SUCCESS;
+	}
+	if (payload_length <= PLDM_GET_FRU_RECORD_TABLE_MIN_RESP_BYTES) {
+		return PLDM_ERROR_INVALID_LENGTH;
+	}
+
+	struct pldm_get_fru_record_table_resp *resp =
+	    (struct pldm_get_fru_record_table_resp *)msg->payload;
+
+	*next_data_transfer_handle = le32toh(resp->next_data_transfer_handle);
+	*transfer_flag = resp->transfer_flag;
+	memcpy(fru_record_table_data, resp->fru_record_table_data,
+	       payload_length - PLDM_GET_FRU_RECORD_TABLE_MIN_RESP_BYTES);
+	*fru_record_table_length =
+	    payload_length - PLDM_GET_FRU_RECORD_TABLE_MIN_RESP_BYTES;
+
+	return PLDM_SUCCESS;
+}
