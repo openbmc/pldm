b4742138912b9427d4421b3e0f6387bd90a3a91f
PriyangaRamasamy <priyanga24@in.ibm.com>
2019-07-30 12:33:53 +0530

    libpldm: decode GetFruRecordTable response api
    Change-Id: I97642b25142f523c349b4ccc19c9a002eb9d6e0d
    Signed-off-by: PriyangaRamasamy <priyanga24@in.ibm.com>

--- test/libpldm_fru_test.cpp
+++ test/libpldm_fru_test.cpp
@@ -44,3 +44,70 @@ TEST(GetFruRecordTable, testBadEncodeRequest)
 
     ASSERT_EQ(rc, PLDM_ERROR_INVALID_DATA);
 }
+
+TEST(GetFruRecordTable, testGoodDecodeResponse)
+{
+    uint8_t completion_code = PLDM_SUCCESS;
+    uint32_t next_data_transfer_handle = 0x32323232;
+    uint8_t transfer_flag = 5;
+
+    std::vector<uint8_t> responseMsg(sizeof(pldm_msg_hdr) +
+                                     PLDM_GET_FRU_RECORD_TABLE_RESP_BYTES +
+                                     next_data_transfer_handle + transfer_flag);
+    auto responsePtr = reinterpret_cast<pldm_msg*>(responseMsg.data());
+    size_t payload_length = responseMsg.size() - sizeof(pldm_msg_hdr);
+    auto response =
+        reinterpret_cast<pldm_get_fru_record_table_resp*>(responsePtr->payload);
+
+    response->completion_code = completion_code;
+    response->next_data_transfer_handle = next_data_transfer_handle;
+    response->transfer_flag = transfer_flag;
+
+    size_t fru_record_offset = sizeof(completion_code) +
+                               sizeof(next_data_transfer_handle) +
+                               sizeof(transfer_flag);
+
+    uint8_t ret_completion_code = 0;
+    uint32_t ret_next_data_transfer_handle = 0;
+    uint8_t ret_transfer_flag = 0;
+    size_t ret_fru_record_offset = 0;
+
+    // Invoke decode get FRU record table response api
+    auto rc = decode_get_fru_record_table_resp(
+        responsePtr, payload_length, &ret_completion_code,
+        &ret_next_data_transfer_handle, &ret_transfer_flag,
+        &ret_fru_record_offset);
+    ASSERT_EQ(rc, PLDM_SUCCESS);
+    ASSERT_EQ(completion_code, ret_completion_code);
+    ASSERT_EQ(next_data_transfer_handle, ret_next_data_transfer_handle);
+    ASSERT_EQ(transfer_flag, ret_transfer_flag);
+    ASSERT_EQ(fru_record_offset, ret_fru_record_offset);
+}
+
+TEST(GetFruRecordTable, testBadDecodeResponse)
+{
+    std::array<uint8_t,
+               sizeof(pldm_msg_hdr) + PLDM_GET_FRU_RECORD_TABLE_RESP_BYTES>
+        responseMsg{};
+
+    auto responsePtr = reinterpret_cast<pldm_msg*>(responseMsg.data());
+
+    uint8_t completion_code = 0;
+    uint32_t next_data_transfer_handle = 0;
+    uint8_t transfer_flag = 0;
+    size_t fru_record_offset = 0;
+
+    // Payload message is missing
+    auto rc = decode_get_fru_record_table_resp(
+        NULL, 0, &completion_code, &next_data_transfer_handle, &transfer_flag,
+        &fru_record_offset);
+
+    ASSERT_EQ(rc, PLDM_ERROR_INVALID_DATA);
+
+    // Payload length is invalid
+    rc = decode_get_fru_record_table_resp(responsePtr, 0, &completion_code,
+                                          &next_data_transfer_handle,
+                                          &transfer_flag, &fru_record_offset);
+
+    ASSERT_EQ(rc, PLDM_ERROR_INVALID_LENGTH);
+}
