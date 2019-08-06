4b204c71e52854e1a377953feda27868bfc957d5
vkaverap <vkaverap@in.ibm.com>
2019-08-05 22:57:11 -0500

    FILE I/O: Implement encode/decode for ReadFileByTypeIntoMemory and WriteFileByTypeFromMemory
    Change-Id: I822034c5f59943a78129136089822e552b2461bb
    Signed-off-by: vkaverap <vkaverap@in.ibm.com>

--- oem/ibm/libpldm/file_io.c
+++ oem/ibm/libpldm/file_io.c
@@ -381,3 +381,119 @@ int encode_write_file_resp(uint8_t instance_id, uint8_t completion_code,
 
 	return PLDM_SUCCESS;
 }
+
+int decode_rw_file_type_memory_req(const struct pldm_msg *msg,
+				   size_t payload_length, uint16_t *file_type,
+				   uint32_t *file_handle, uint32_t *offset,
+				   uint32_t *length, uint64_t *address)
+{
+	if (msg == NULL || file_type == NULL || file_handle == NULL ||
+	    offset == NULL || length == NULL || address == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	if (payload_length != PLDM_RW_FILE_TYPE_MEM_REQ_BYTES) {
+		return PLDM_ERROR_INVALID_LENGTH;
+	}
+
+	struct pldm_read_write_file_type_memory_req *request =
+	    (struct pldm_read_write_file_type_memory_req *)msg->payload;
+	*file_type = le16toh(request->file_type);
+	if (*file_type != PLDM_FILE_ERROR_LOG) {
+		return PLDM_INVALID_FILE_TYPE;
+	}
+	*file_handle = le32toh(request->file_handle);
+	*offset = le32toh(request->offset);
+	*length = le32toh(request->length);
+	*address = le64toh(request->address);
+
+	return PLDM_SUCCESS;
+}
+
+int encode_rw_file_type_memory_resp(uint8_t instance_id, uint8_t command,
+				    uint8_t completion_code, uint32_t length,
+				    struct pldm_msg *msg)
+{
+	struct pldm_header_info header = {0};
+	int rc = PLDM_SUCCESS;
+
+	if (msg == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	header.msg_type = PLDM_RESPONSE;
+	header.instance = instance_id;
+	header.pldm_type = PLDM_OEM;
+	header.command = command;
+
+	if ((rc = pack_pldm_header(&header, &(msg->hdr))) > PLDM_SUCCESS) {
+		return rc;
+	}
+
+	struct pldm_read_write_file_type_memory_resp *response =
+	    (struct pldm_read_write_file_type_memory_resp *)msg->payload;
+	response->completion_code = completion_code;
+	if (response->completion_code == PLDM_SUCCESS) {
+		response->length = htole32(length);
+	}
+
+	return PLDM_SUCCESS;
+}
+
+int encode_rw_file_type_memory_req(uint8_t instance_id, uint8_t command,
+				   uint16_t file_type, uint32_t file_handle,
+				   uint32_t offset, uint32_t length,
+				   uint64_t address, struct pldm_msg *msg)
+{
+	struct pldm_header_info header = {0};
+	int rc = PLDM_SUCCESS;
+
+	if (msg == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	if (file_type != PLDM_FILE_ERROR_LOG) {
+		return PLDM_INVALID_FILE_TYPE;
+	}
+
+	header.msg_type = PLDM_REQUEST;
+	header.instance = instance_id;
+	header.pldm_type = PLDM_OEM;
+	header.command = command;
+
+	if ((rc = pack_pldm_header(&header, &(msg->hdr))) > PLDM_SUCCESS) {
+		return rc;
+	}
+
+	struct pldm_read_write_file_type_memory_req *req =
+	    (struct pldm_read_write_file_type_memory_req *)msg->payload;
+	req->file_type = htole16(file_type);
+	req->file_handle = htole32(file_handle);
+	req->offset = htole32(offset);
+	req->length = htole32(length);
+	req->address = htole64(address);
+
+	return PLDM_SUCCESS;
+}
+
+int decode_rw_file_type_memory_resp(const struct pldm_msg *msg,
+				    size_t payload_length,
+				    uint8_t *completion_code, uint32_t *length)
+{
+	if (msg == NULL || length == NULL || completion_code == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	if (payload_length != PLDM_RW_FILE_TYPE_MEM_RESP_BYTES) {
+		return PLDM_ERROR_INVALID_LENGTH;
+	}
+
+	struct pldm_read_write_file_type_memory_resp *response =
+	    (struct pldm_read_write_file_type_memory_resp *)msg->payload;
+	*completion_code = response->completion_code;
+	if (*completion_code == PLDM_SUCCESS) {
+		*length = le32toh(response->length);
+	}
+
+	return PLDM_SUCCESS;
+}
