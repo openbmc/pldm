3f46e9da4cab8f7ffb012e3a3d9ac19e2ad394ba
Sampa Misra <sampmisr@in.ibm.com>
2019-08-22 04:36:47 -0500

    libpldmresponder: implement oem file handler
    This commit implements a framework for handling oem file types
    received to/from host. Along with that it also implements the responder
    for oem command WriteFileByTypeFromMemory and processes pels received
    from host
    
    Change-Id: Ice866aed0343b90769013c4be31a0c730f6e6bcd
    Signed-off-by: Sampa Misra <sampmisr@in.ibm.com>

---
+++ oem/ibm/libpldmresponder/file_io_by_type.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "file_io.hpp"
+
+namespace pldm
+{
+
+using namespace pldm::responder::dma;
+namespace responder
+{
+
+namespace oem_file_type
+{
+
+/**
+ *  @class FileHandler
+ *
+ *  Base class to handle read/write of all oem file types
+ *  which may/may not need to be maintained in a filetable.
+ */
+class FileHandler
+{
+  public:
+    /** @brief Factory method to process an oem file type. Individual file types
+     *  need to override this method to do the file specific processing
+     *
+     *  @param[in] xdmaInterface - DMA interface
+     *  @param[in] command - PLDM command
+     *  @param[in] upstream - indicates direction of the transfer; true
+     * indicates transfer to the host
+     *  @return PLDM status code
+     */
+    virtual int handle(DMA* xdmaInterface, uint8_t command, bool upstream) = 0;
+
+    /** @brief Set all the mandatory parameters to process a file
+     *
+     *  @param[in] newfileHandle - file handle
+     *  @param[in] newOffset - offset in the file
+     *  @param[in] newLength - length of the data to transfer
+     *  @param[in] newAddress - DMA address on the host
+     *  @param[in] newPath - pathname of the file to transfer data from or to
+     *  @return none
+     */
+    void setHandler(uint32_t newfileHandle, uint32_t newOffset,
+                    uint32_t newLength, uint64_t newAddress, fs::path newPath)
+    {
+        fileHandle = newfileHandle;
+        offset = newOffset;
+        length = newLength;
+        address = newAddress;
+        path = newPath;
+    }
+    uint32_t getLength()
+    {
+        return length;
+    }
+
+    uint32_t getOffset()
+    {
+        return offset;
+    }
+
+    uint64_t getAddress()
+    {
+        return address;
+    }
+
+    virtual ~FileHandler()
+    {
+    }
+
+  private:
+    uint32_t fileHandle;
+    uint32_t offset;
+    uint32_t length;
+    uint64_t address;
+    fs::path path;
+};
+
+/** @class PelHandler
+ *
+ *  @brief Inherits and implements FileHandler. This class is used
+ *  to read/write pels .
+ */
+class PelHandler : public FileHandler
+{
+  public:
+    int handle(DMA* xdmaInterface, uint8_t command, bool upstream);
+
+    ~PelHandler()
+    {
+    }
+};
+
+} // namespace oem_file_type
+
+} // namespace responder
+
+} // namespace pldm
