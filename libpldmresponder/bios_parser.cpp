afba9cb69fabce10ec8c399a7429bc552c57af02
Carol Wang <wangkair@cn.ibm.com>
2019-08-26 17:14:26 +0800

    Implement attribute table with type string
    Tested:
    Following are the tables constructed from the sample json file present at
    "test/bios_jsons/
    
    $ hexdump -C stringTable
    00000000  00 00 07 00 41 6c 6c 6f  77 65 64 01 00 10 00 43  |....Allowed....C|
    00000010  6f 64 65 55 70 64 61 74  65 50 6f 6c 69 63 79 02  |odeUpdatePolicy.|
    00000020  00 0a 00 43 6f 6e 63 75  72 72 65 6e 74 03 00 0a  |...Concurrent...|
    00000030  00 44 69 73 72 75 70 74  69 76 65 04 00 0a 00 46  |.Disruptive....F|
    00000040  57 42 6f 6f 74 53 69 64  65 05 00 0f 00 48 4d 43  |WBootSide....HMC|
    00000050  4d 61 6e 61 67 65 64 53  74 61 74 65 06 00 10 00  |ManagedState....|
    00000060  49 6e 62 61 6e 64 43 6f  64 65 55 70 64 61 74 65  |InbandCodeUpdate|
    00000070  07 00 0a 00 4e 6f 74 41  6c 6c 6f 77 65 64 08 00  |....NotAllowed..|
    00000080  03 00 4f 66 66 09 00 02  00 4f 6e 0a 00 04 00 50  |..Off....On....P|
    00000090  65 72 6d 0b 00 04 00 54  65 6d 70 0c 00 0c 00 73  |erm....Temp....s|
    000000a0  74 72 5f 65 78 61 6d 70  6c 65 31 0d 00 0c 00 73  |tr_example1....s|
    000000b0  74 72 5f 65 78 61 6d 70  6c 65 32 0e 00 0c 00 73  |tr_example2....s|
    000000c0  74 72 5f 65 78 61 6d 70  6c 65 33 00 6c 11 89 d4  |tr_example3.l...|
    
    $ hexdump -C attributeTable
    00000000  00 00 00 01 00 02 02 00  03 00 01 00 01 00 00 04  |................|
    00000010  00 02 0a 00 0b 00 01 00  02 00 00 05 00 02 08 00  |................|
    00000020  09 00 01 01 03 00 00 06  00 02 00 00 07 00 01 00  |................|
    00000030  04 00 01 0c 00 01 01 00  64 00 03 00 61 62 63 05  |........d...abc.|
    00000040  00 01 0d 00 02 00 00 64  00 00 00 06 00 01 0e 00  |.......d........|
    00000050  00 01 00 64 00 02 00 65  66 00 00 00 72 ef 0c 2a  |...d...ef...r..*|
    
    Change-Id: I272c59b96b5aa14571d01eeeaccd504ca7ac168b
    Signed-off-by: Carol Wang <wangkair@cn.ibm.com>

--- libpldmresponder/bios_parser.cpp
+++ libpldmresponder/bios_parser.cpp
@@ -15,6 +15,41 @@ using Json = nlohmann::json;
 namespace fs = std::filesystem;
 using namespace phosphor::logging;
 
+namespace
+{
+
+const std::vector<Json> emptyJsonList{};
+const Json emptyJson{};
+
+} // namespace
+
+int parseBiosJsonFile(const char* dirPath, std::string fileName, Json& fileData)
+{
+    int rc = 0;
+
+    fs::path filePath(dirPath);
+    filePath /= fileName;
+
+    std::ifstream jsonFile(filePath);
+    if (!jsonFile.is_open())
+    {
+        log<level::ERR>("BIOS string config file does not exist",
+                        entry("FILE=%s", filePath.c_str()));
+        rc = -1;
+    }
+    else
+    {
+        fileData = Json::parse(jsonFile, nullptr, false);
+        if (fileData.is_discarded())
+        {
+            log<level::ERR>("Parsing config file failed");
+            rc = -1;
+        }
+    }
+
+    return rc;
+}
+
 namespace bios_enum
 {
 
@@ -165,8 +200,7 @@ int setupValueLookup(const char* dirPath)
         return rc;
     }
 
-    static const std::vector<Json> emptyList{};
-    auto entries = fileData.value("entries", emptyList);
+    auto entries = fileData.value("entries", emptyJsonList);
     // Iterate through each JSON object in the config file
     for (const auto& entry : entries)
     {
@@ -187,10 +221,10 @@ int setupValueLookup(const char* dirPath)
         }
 
         std::optional<internal::DBusMapping> dBusMap = std::nullopt;
-        static const Json empty{};
+
         if (entry.count("dbus") != 0)
         {
-            auto dBusEntry = entry.value("dbus", empty);
+            auto dBusEntry = entry.value("dbus", emptyJson);
             dBusMap = std::make_optional<internal::DBusMapping>();
             dBusMap.value().objectPath = dBusEntry.value("object_path", "");
             dBusMap.value().interface = dBusEntry.value("interface", "");
@@ -251,6 +285,152 @@ CurrentValues getAttrValue(const AttrName& attrName)
 
 } // namespace bios_enum
 
+namespace bios_string
+{
+
+/** @brief BIOS string types
+ */
+enum bios_string_types
+{
+    UNKNOWN = 0x00,
+    ASCII = 0x01,
+    HEX = 0x02,
+    UTF_8 = 0x03,
+    UTF_16LE = 0x04,
+    UTF_16BE = 0x05,
+    VENDOR_SPECIFIC = 0xFF
+};
+
+std::map<std::string, uint8_t> strTypeMap{{"Unknown", UNKNOWN},
+                                          {"ASCII", ASCII},
+                                          {"Hex", HEX},
+                                          {"UTF-8", UTF_8},
+                                          {"UTF-16LE", UTF_16LE},
+                                          {"UTF-16LE", UTF_16LE},
+                                          {"Vendor Specific", VENDOR_SPECIFIC}};
+
+namespace internal
+{
+
+using Value = std::string;
+
+/** @struct DBusMapping
+ *
+ *  Data structure for storing information regarding BIOS string attribute
+ *  and the D-Bus object for the attribute.
+ */
+struct DBusMapping
+{
+    std::string objectPath;   //!< D-Bus object path
+    std::string interface;    //!< D-Bus interface
+    std::string propertyName; //!< D-Bus property name
+};
+
+/** @brief Map containing the possible and the default values for the BIOS
+ *         string type attributes.
+ */
+AttrValuesMap valueMap;
+
+/** @brief Map containing the optional D-Bus property information about the
+ *         BIOS string type attributes.
+ */
+std::map<AttrName, std::optional<DBusMapping>> attrLookup;
+
+} // namespace internal
+
+int setupValueLookup(const char* dirPath)
+{
+    int rc = 0;
+
+    if (!internal::valueMap.empty() && !internal::attrLookup.empty())
+    {
+        return rc;
+    }
+
+    Json fileData;
+    rc = parseBiosJsonFile(dirPath, bIOSStrJson, fileData);
+    if (rc != 0)
+    {
+        return rc;
+    }
+
+    auto entries = fileData.value("entries", emptyJsonList);
+    // Iterate through each JSON object in the config file
+    for (const auto& entry : entries)
+    {
+        std::string attr = entry.value("attribute_name", "");
+        // Transfer string type from string to enum
+        std::string strTypeTmp = entry.value("string_type", "Unknown");
+        auto iter = strTypeMap.find(strTypeTmp);
+        if (iter == strTypeMap.end())
+        {
+            log<level::ERR>(
+                "Wrong string type",
+                phosphor::logging::entry("STRING_TYPE=%s", strTypeTmp.c_str()));
+            return -1;
+        }
+        uint8_t strType = iter->second;
+
+        uint16_t minStrLen = entry.value("minimum_string_length", 0);
+        uint16_t maxStrLen = entry.value("maximum_string_length", 0);
+        if (maxStrLen - minStrLen < 0)
+        {
+            log<level::ERR>(
+                "Maximum string length is smaller than minimum string length");
+            return -1;
+        }
+        uint16_t defaultStrLen = entry.value("default_string_length", 0);
+        std::string defaultStr = entry.value("default_string", "");
+        if ((defaultStrLen == 0) && (defaultStr.size() > 0))
+        {
+            log<level::ERR>(
+                "Default string length is 0, but default string is existing");
+            return -1;
+        }
+
+        // dbus handling
+        std::optional<internal::DBusMapping> dBusMap;
+
+        if (entry.count("dbus") != 0)
+        {
+            auto dBusEntry = entry.value("dbus", emptyJson);
+            dBusMap = std::make_optional<internal::DBusMapping>();
+            dBusMap->objectPath = dBusEntry.value("object_path", "");
+            dBusMap->interface = dBusEntry.value("interface", "");
+            dBusMap->propertyName = dBusEntry.value("property_name", "");
+            if (dBusMap->objectPath.empty() || dBusMap->interface.empty() ||
+                dBusMap->propertyName.empty())
+            {
+                log<level::ERR>(
+                    "Invalid dbus config",
+                    phosphor::logging::entry("OBJPATH=%s",
+                                             dBusMap->objectPath.c_str()),
+                    phosphor::logging::entry("INTERFACE=%s",
+                                             dBusMap->interface.c_str()),
+                    phosphor::logging::entry("PROPERTY_NAME=%s",
+                                             dBusMap->propertyName.c_str()));
+                return -1;
+            }
+        }
+
+        internal::attrLookup.emplace(attr, std::move(dBusMap));
+
+        // Defaulting all the types of attributes to BIOSEnumeration
+        internal::valueMap.emplace(std::move(attr),
+                                   std::make_tuple(false, strType, minStrLen,
+                                                   maxStrLen, defaultStrLen,
+                                                   std::move(defaultStr)));
+    }
+
+    return rc;
+}
+
+const AttrValuesMap& getValues()
+{
+    return internal::valueMap;
+}
+} // namespace bios_string
+
 Strings getStrings(const char* dirPath)
 {
     Strings biosStrings{};
@@ -279,8 +459,7 @@ Strings getStrings(const char* dirPath)
             continue;
         }
 
-        static const std::vector<Json> emptyList{};
-        auto entries = fileData.value("entries", emptyList);
+        auto entries = fileData.value("entries", emptyJsonList);
 
         // Iterate through each entry in the config file
         for (auto& entry : entries)
