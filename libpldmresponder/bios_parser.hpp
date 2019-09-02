8c179d324823a401036e337402365472bc5f1ede
Carol Wang <wangkair@cn.ibm.com>
2019-09-02 16:34:10 +0800

    Implement attribute value table with type string
    Change-Id: I40c782ae8753340c964cf067e53e7947b18e9e35
    Signed-off-by: Carol Wang <wangkair@cn.ibm.com>

--- libpldmresponder/bios_parser.hpp
+++ libpldmresponder/bios_parser.hpp
@@ -124,6 +124,12 @@ static_assert(attrTableSize == sizeof(uint16_t) + sizeof(uint8_t) +
                                    sizeof(uint16_t) + sizeof(uint8_t) +
                                    sizeof(uint16_t) + sizeof(uint16_t) +
                                    sizeof(uint16_t));
+/* attrValueTableSize is the sum of fixed length of members which construct a
+ * string attribute value table, including attr_handle(uint16_t),
+ * attr_type(uint8_t), CurrentStringLength(uint16_t)*/
+constexpr auto attrValueTableSize = 5;
+static_assert(attrValueTableSize ==
+              sizeof(uint16_t) + sizeof(uint8_t) + sizeof(uint16_t));
 
 /** @brief Get the possible values and the default values for the
  *         BIOSString and BIOSStringReadOnly types
@@ -133,15 +139,13 @@ static_assert(attrTableSize == sizeof(uint16_t) + sizeof(uint8_t) +
  */
 const AttrValuesMap& getValues();
 
-// using CurrentValues = std::vector<std::string>;
-
 /** @brief Get the current values for the BIOS Attribute
  *
  *  @param[in] attrName - BIOS attribute name
  *
  *  @return BIOS attribute value
  */
-// CurrentValues getAttrValue(const AttrName& attrName);
+std::string getAttrValue(const AttrName& attrName);
 
 } // namespace bios_string
 
