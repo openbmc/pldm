be6b802387486316c72c92ae3f47a05858bf8293
John Wang <wangzqbj@inspur.com>
2019-09-27 14:20:15 +0800

    Fix a possible buffer overflow issue
    This commit fixes a possible overflow issue that would
    occur when comparing strings
    
    Signed-off-by: John Wang <wangzqbj@inspur.com>
    Change-Id: I6c21ab289c81d6e8617ca765aecbef55f177f6ec

--- libpldmresponder/bios.cpp
+++ libpldmresponder/bios.cpp
@@ -246,7 +246,7 @@ StringHandle findStringHandle(const std::string& name,
     {
         hdl = tableEntry->string_handle;
         uint16_t len = tableEntry->string_length;
-        if (memcmp(name.c_str(), tableEntry->name, len) == 0)
+        if (name.compare(0, name.length(), tableEntry->name, len) == 0)
         {
             break;
         }
