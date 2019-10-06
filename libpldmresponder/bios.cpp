751540c6b2ff9b9853f5eeb61b022e01826742a2
John Wang <wangzqbj@inspur.com>
2019-10-06 16:34:29 +0800

    bios: Implement an iterator for bios attribute table
    Traversing bios attribute table should be platform-independent, so
    implement a c-version bios attribute table iterator in libpldm, also
    implement a c++ helper function to traverse a bios table.
    
    Signed-off-by: John Wang <wangzqbj@inspur.com>
    Change-Id: I2afcaac306603410e711aa4772ba35b5e0b131d5

--- libpldmresponder/bios.cpp
+++ libpldmresponder/bios.cpp
@@ -766,6 +766,32 @@ void constructAttrValueTable(const BIOSTable& BIOSAttributeTable,
 
 } // end namespace bios_type_string
 
+void traverseBIOSAttrTable(const Table& biosAttrTable,
+                           AttrTableEntryHandler handler)
+{
+    struct pldm_bios_table_iter* iter = pldm_bios_table_iter_create(
+        biosAttrTable.data(), biosAttrTable.size(), PLDM_BIOS_ATTR_TABLE);
+    while (true)
+    {
+        const struct pldm_bios_attr_table_entry* table_entry =
+            pldm_bios_table_attr_entry_next(iter);
+        if (table_entry == nullptr)
+        {
+            break;
+        }
+        try
+        {
+            handler(table_entry);
+        }
+        catch (const std::exception& e)
+        {
+            log<level::ERR>("handler fails when traversing BIOSAttrTable",
+                            entry("ERROR=%s", e.what()));
+        }
+    }
+    pldm_bios_table_iter_free(iter);
+}
+
 using typeHandler =
     std::function<void(const BIOSTable& BIOSStringTable,
                        const char* biosJsonDir, Table& attributeTable)>;
