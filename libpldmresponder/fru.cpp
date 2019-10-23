f330ac23eeabf5f5f83b9fdb19935ae789d403d5
Deepak Kodihalli <dkodihal@in.ibm.com>
2019-10-23 03:26:15 -0500

    WIP GetFRURecordTable
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>
    Change-Id: I16d36df2834b65bfa5f69164d3a40eeac2c7f357

--- libpldmresponder/fru.cpp
+++ libpldmresponder/fru.cpp
@@ -1,6 +1,11 @@
 #include "fru.hpp"
 
 #include "registration.hpp"
+#include "utils.hpp"
+
+#include <boost/crc.hpp>
+#include <cstring>
+#include <sdbusplus/bus.hpp>
 
 namespace pldm
 {
@@ -13,25 +18,175 @@ namespace fru
 
 void registerHandlers()
 {
-    registerHandler(PLDM_FRU, PLDM_GET_FRU_RECORD_TABLE_METADATA, std::move(getFRURecordTableMetadata));
+    registerHandler(PLDM_FRU, PLDM_GET_FRU_RECORD_TABLE_METADATA,
+                    std::move(getFRURecordTableMetadata));
+    registerHandler(PLDM_FRU, PLDM_GET_FRU_RECORD_TABLE,
+                    std::move(getFRURecordTable));
 }
 
 } // namespace fru
 
-Response getFRURecordTableMetadata(const pldm_msg* request, size_t /*payloadLength*/)
+namespace internal
 {
+
+FruImpl table(FRU_JSONS_DIR);
+FruIntf<FruImpl> intf(table);
+
+} // namespace internal
+
+Response getFRURecordTableMetadata(const pldm_msg* request,
+                                   size_t /*payloadLength*/)
+{
+    using namespace internal;
     constexpr auto major = 0x01;
     constexpr auto minor = 0x00;
     constexpr auto maxSize = 0xFFFFFFFF;
 
-    Response response(
-        sizeof(pldm_msg_hdr) + PLDM_GET_FRU_RECORD_TABLE_METADATA_RESP_BYTES, 0);
+    Response response(sizeof(pldm_msg_hdr) +
+                          PLDM_GET_FRU_RECORD_TABLE_METADATA_RESP_BYTES,
+                      0);
     auto responsePtr = reinterpret_cast<pldm_msg*>(response.data());
 
-    encode_get_fru_record_table_metadata_resp(request->hdr.instance_id, PLDM_SUCCESS, major, minor, maxSize, 0,0,0,0,
-                                          responsePtr);
+    encode_get_fru_record_table_metadata_resp(
+        request->hdr.instance_id, PLDM_SUCCESS, major, minor, maxSize,
+        intf.size(), intf.numRSI(), intf.numRecords(), intf.checkSum(),
+        responsePtr);
+
+    return response;
+}
+
+Response getFRURecordTable(const pldm_msg* request, size_t payloadLength)
+{
+    using namespace internal;
+    Response response(sizeof(pldm_msg_hdr), 0);
+    auto responsePtr = reinterpret_cast<pldm_msg*>(response.data());
+
+    if (payloadLength != PLDM_GET_FRU_RECORD_TABLE_REQ_BYTES)
+    {
+        encode_get_fru_record_table_resp(request->hdr.instance_id,
+                                         PLDM_ERROR_INVALID_LENGTH, 0,
+                                         PLDM_START_AND_END, responsePtr);
+        return response;
+    }
+
+    encode_get_fru_record_table_resp(request->hdr.instance_id, PLDM_SUCCESS, 0,
+                                     PLDM_START_AND_END, responsePtr);
+
+    intf.getFRUTable(response);
+
     return response;
 }
 
 } // namespace responder
+
+using namespace pldm::responder;
+
+FruImpl::FruImpl(const std::string& configPath)
+{
+    auto handle = fru_parser::get(configPath);
+    auto dbusInfo = handle.inventoryLookup();
+
+    // Read the all the inventory D-Bus objects
+    auto bus = sdbusplus::bus::new_default();
+    auto method = bus.new_method_call(
+        std::get<0>(dbusInfo).c_str(), std::get<1>(dbusInfo).c_str(),
+        "org.freedesktop.DBus.ObjectManager", "GetManagedObjects");
+    auto reply = bus.call(method);
+
+    ObjectValueTree objects;
+    reply.read(objects);
+
+    // Populate all the interested Item types to a map for easy lookup
+    std::map<DbusInterface, uint8_t> itemIntfsMap;
+    auto itemIntfs = std::get<2>(dbusInfo);
+    std::transform(std::begin(itemIntfs), std::end(itemIntfs),
+                   std::inserter(itemIntfsMap, itemIntfsMap.end()),
+                   [](DbusInterface intf) { return std::make_pair(intf, 0); });
+
+    const std::string itemTemplate{"xyz.openbmc_project.Inventory.Item"};
+
+    for (const auto& object : objects)
+    {
+        const auto& interfaces = object.second;
+
+        for (const auto& interface : interfaces)
+        {
+            if (interface.first.substr(0, interface.first.find_last_of('.')) ==
+                itemTemplate)
+            {
+                if (itemIntfsMap.find(interface.first) != itemIntfsMap.end())
+                {
+                    // Add try-catch for getRecordInfo
+                    auto recordInfos = handle.getRecordInfo(interface.first);
+                    populateRecord(interfaces, recordInfos);
+                }
+            }
+        }
+    }
+}
+
+void FruImpl::populateRecord(
+    const pldm::responder::DbusInterfaceMap& interfaces,
+    const fru_parser::FruRecordInfos& recordInfos)
+{
+    for (auto const& [recType, encType, fieldInfos] : recordInfos)
+    {
+        std::vector<uint8_t> tlvs;
+        uint8_t numFRUFields = 0;
+        for (auto const& [intf, prop, propType, fieldTypeNum] : fieldInfos)
+        {
+            try
+            {
+                auto propValue = interfaces.at(intf).at(prop);
+                numFRUFields++;
+                tlvs.emplace_back(fieldTypeNum);
+                if (propType == "bytearray")
+                {
+                    auto byteArray = std::get<std::vector<uint8_t>>(propValue);
+                    tlvs.emplace_back(byteArray.size());
+                    tlvs.insert(std::end(tlvs), std::begin(byteArray),
+                                std::end(byteArray));
+                }
+                else if (propType == "string")
+                {
+                    auto str = std::get<std::string>(propValue);
+                    tlvs.emplace_back(str.size());
+                    tlvs.insert(std::end(tlvs), std::begin(str), std::end(str));
+                }
+            }
+            catch (const std::out_of_range& e)
+            {
+                continue;
+            }
+        }
+
+        auto curSize = table.size();
+        table.resize(curSize + recHeaderSize + tlvs.size());
+        encode_fru_record(table.data(), table.size(), &curSize, nextRSI(),
+                          recType, numFRUFields, encType, tlvs.data(),
+                          tlvs.size());
+        numrecs++;
+    }
+}
+
+void FruImpl::getFRUTable(Response& response)
+{
+    using namespace pldm::responder::utils;
+
+    auto hdrSize = response.size();
+    auto padBytes = getNumPadBytes(table.size());
+    response.resize(hdrSize + table.size() + padBytes + sizeof(checksum), 0);
+    std::copy(response.begin() + hdrSize, table.begin(), table.end());
+
+    // Calculate the checksum
+    boost::crc_32_type result;
+    result.process_bytes(response.data(), response.size());
+    checksum = result.checksum();
+
+    // Copy the checksum to response data
+    auto iter = response.begin() + hdrSize + table.size() + padBytes;
+    std::copy_n(reinterpret_cast<const uint8_t*>(&checksum), sizeof(checksum),
+                iter);
+}
+
 } // namespace pldm
--- libpldmresponder/fru.hpp
+++ libpldmresponder/fru.hpp
@@ -2,6 +2,12 @@
 
 #include "config.h"
 
+#include "fru_parser.hpp"
+
+#include <map>
+#include <sdbusplus/message.hpp>
+#include <string>
+#include <variant>
 #include <vector>
 
 #include "libpldm/fru.h"
@@ -23,13 +29,138 @@ void registerHandlers();
 
 } // namespace fru
 
+using DbusInterface = std::string;
+using DbusProperty = std::string;
+using Value =
+    std::variant<bool, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t,
+                 uint64_t, double, std::string, std::vector<uint8_t>>;
+using PropertyMap = std::map<DbusProperty, Value>;
+using DbusInterfaceMap = std::map<DbusInterface, PropertyMap>;
+using ObjectValueTree =
+    std::map<sdbusplus::message::object_path, DbusInterfaceMap>;
+
 /** @brief Handler for GetFRURecordTableMetadata
  *
  *  @param[in] request - Request message payload
  *  @param[in] payloadLength - Request payload length
  *  @param[out] Response - Response message written here
  */
-Response getFRURecordTableMetadata(const pldm_msg* request, size_t payloadLength);
+Response getFRURecordTableMetadata(const pldm_msg* request,
+                                   size_t payloadLength);
+
+Response getFRURecordTable(const pldm_msg* request, size_t payloadLength);
 
 } // namespace responder
+
+template <typename T>
+struct FruIntf
+{
+  public:
+    FruIntf(T& t) : impl(t)
+    {
+    }
+
+    /** @brief Total length of the FRU table in bytes, this excludes the pad
+     *         bytes and the checksum.
+     *
+     *  @return size of the FRU table
+     */
+    uint32_t size() const
+    {
+        return impl.size();
+    }
+
+    uint32_t checkSum() const
+    {
+        return impl.checkSum();
+    }
+
+    uint16_t numRSI() const
+    {
+        return impl.numRSI();
+    }
+
+    uint16_t numRecords() const
+    {
+        return impl.numRecords();
+    }
+
+    void getFRUTable(Response& response)
+    {
+        return impl.getFRUTable(response);
+    }
+
+  private:
+    T& impl;
+};
+
+class FruImpl
+{
+  public:
+    /* @brief Header size for FRU record, it includes the FRU record set
+     *        identifier, FRU record type, Number of FRU fields, Encoding type
+     *        of FRU fields
+     */
+    static constexpr size_t recHeaderSize =
+        sizeof(struct pldm_fru_record_data_format) -
+        sizeof(struct pldm_fru_record_tlv);
+
+    /** @brief The file table is initialised by parsing the config file
+     *         containing information about the files.
+     *
+     * @param[in] fileTableConfigPath - path to the json file containing
+     *                                  information
+     */
+    FruImpl(const std::string& configPath);
+
+    /** @brief Total length of the FRU table in bytes, this excludes the pad
+     *         bytes and the checksum.
+     *
+     *  @return size of the FRU table
+     */
+    uint32_t size() const
+    {
+        return table.size();
+    }
+
+    uint32_t checkSum() const
+    {
+        return checksum;
+    }
+
+    uint16_t numRSI() const
+    {
+        return rsi;
+    }
+
+    uint16_t numRecords() const
+    {
+        return numrecs;
+    }
+
+    void getFRUTable(Response& response);
+
+  private:
+    uint16_t nextRSI()
+    {
+        return ++rsi;
+    }
+
+    uint16_t rsi = 0;
+    uint16_t numrecs = 0;
+    std::vector<uint8_t> table;
+    uint32_t checksum = 0;
+
+    void populateFRUTable();
+
+    /** @brief Parse the FRU_Master.json file and populate the D-Bus lookup
+     *         information which provides the service, root D-Bus path and the
+     *         item interfaces.
+     *
+     *  @param[in] filePath - file path to FRU_Master.json
+     */
+    void populateRecord(const pldm::responder::DbusInterfaceMap& interfaces,
+                        const fru_parser::FruRecordInfos& recordInfos);
+};
+
 } // namespace pldm
