ee9d23cf20eb1497c243e0d77d68dfbb6aa286d2
Deepak Kodihalli <dkodihal@in.ibm.com>
2019-10-23 03:26:15 -0500

    WIP GetFRURecordTable
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>
    Change-Id: I16d36df2834b65bfa5f69164d3a40eeac2c7f357

--- libpldmresponder/fru.hpp
+++ libpldmresponder/fru.hpp
@@ -2,6 +2,13 @@
 
 #include "config.h"
 
+#include "fru_parser.hpp"
+#include "handler.hpp"
+
+#include <map>
+#include <sdbusplus/message.hpp>
+#include <string>
+#include <variant>
 #include <vector>
 
 #include "libpldm/fru.h"
@@ -9,27 +16,173 @@
 namespace pldm
 {
 
-using Response = std::vector<uint8_t>;
-
 namespace responder
 {
+
+using Value =
+    std::variant<bool, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t,
+                 uint64_t, double, std::string, std::vector<uint8_t>>;
+using PropertyMap = std::map<dbus::Property, Value>;
+using DbusInterfaceMap = std::map<dbus::Interface, PropertyMap>;
+using ObjectValueTree =
+    std::map<sdbusplus::message::object_path, DbusInterfaceMap>;
 
 namespace fru
 {
 
-/** @brief Register handlers for commands from the FRU spec
- */
-void registerHandlers();
+class Handler : public CmdHandler
+{
+
+  public:
+    Handler()
+    {
+        handlers.emplace(PLDM_GET_FRU_RECORD_TABLE_METADATA,
+                         [this](const pldm_msg* request, size_t payloadLength) {
+                             return this->getFRURecordTableMetadata(
+                                 request, payloadLength);
+                         });
+
+        handlers.emplace(PLDM_GET_FRU_RECORD_TABLE,
+                         [this](const pldm_msg* request, size_t payloadLength) {
+                             return this->getFRURecordTable(request,
+                                                            payloadLength);
+                         });
+    }
+
+    /** @brief Handler for Get FRURecordTableMetadata
+     *
+     *  @param[in] request - Request message payload
+     *  @param[in] payloadLength - Request payload length
+     *
+     *  @return PLDM response message
+     */
+    Response getFRURecordTableMetadata(const pldm_msg* request,
+                                       size_t payloadLength);
+
+    /** @brief Handler for GetFRURecordTable
+     *
+     *  @param[in] request - Request message payload
+     *  @param[in] payloadLength - Request payload length
+     *
+     *  @return PLDM response message
+     */
+    Response getFRURecordTable(const pldm_msg* request, size_t payloadLength);
+};
 
 } // namespace fru
 
-/** @brief Handler for GetFRURecordTableMetadata
- *
- *  @param[in] request - Request message payload
- *  @param[in] payloadLength - Request payload length
- *  @param[out] Response - Response message written here
- */
-Response getFRURecordTableMetadata(const pldm_msg* request, size_t payloadLength);
+template <typename T>
+struct FruIntf
+{
+  public:
+    FruIntf(T& t) : impl(t)
+    {
+    }
+
+    /** @brief Total length of the FRU table in bytes, this excludes the pad
+     *         bytes and the checksum.
+     *
+     *  @return size of the FRU table
+     */
+    uint32_t size() const
+    {
+        return impl.size();
+    }
+
+    uint32_t checkSum() const
+    {
+        return impl.checkSum();
+    }
+
+    uint16_t numRSI() const
+    {
+        return impl.numRSI();
+    }
+
+    uint16_t numRecords() const
+    {
+        return impl.numRecords();
+    }
+
+    void getFRUTable(Response& response)
+    {
+        return impl.getFRUTable(response);
+    }
+
+  private:
+    T& impl;
+};
+
+class FruImpl
+{
+  public:
+    /* @brief Header size for FRU record, it includes the FRU record set
+     *        identifier, FRU record type, Number of FRU fields, Encoding type
+     *        of FRU fields
+     */
+    static constexpr size_t recHeaderSize =
+        sizeof(struct pldm_fru_record_data_format) -
+        sizeof(struct pldm_fru_record_tlv);
+
+    /** @brief The file table is initialised by parsing the config file
+     *         containing information about the files.
+     *
+     * @param[in] fileTableConfigPath - path to the json file containing
+     *                                  information
+     */
+    FruImpl(const std::string& configPath);
+
+    /** @brief Total length of the FRU table in bytes, this excludes the pad
+     *         bytes and the checksum.
+     *
+     *  @return size of the FRU table
+     */
+    uint32_t size() const
+    {
+        return table.size() - padBytes;
+    }
+
+    uint32_t checkSum() const
+    {
+        return checksum;
+    }
+
+    uint16_t numRSI() const
+    {
+        return rsi;
+    }
+
+    uint16_t numRecords() const
+    {
+        return numrecs;
+    }
+
+    void getFRUTable(Response& response);
+
+  private:
+    uint16_t nextRSI()
+    {
+        return ++rsi;
+    }
+
+    uint16_t rsi = 0;
+    uint16_t numrecs = 0;
+    uint8_t padBytes = 0;
+    std::vector<uint8_t> table;
+    uint32_t checksum = 0;
+
+    void populateFRUTable();
+
+    /** @brief Parse the FRU_Master.json file and populate the D-Bus lookup
+     *         information which provides the service, root D-Bus path and the
+     *         item interfaces.
+     *
+     *  @param[in] filePath - file path to FRU_Master.json
+     */
+    void populateRecord(const pldm::responder::DbusInterfaceMap& interfaces,
+                        const fru_parser::FruRecordInfos& recordInfos);
+};
 
 } // namespace responder
+
 } // namespace pldm
