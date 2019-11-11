0030e14b7813ed0e8140a28c3e7a775d51fc3d7a
John Wang <wangzqbj@inspur.com>
2019-11-11 15:55:11 +0800

    tool: Refactor GetPLDMTypes Command
    Signed-off-by: John Wang <wangzqbj@inspur.com>
    Change-Id: I658b2c8094e9e5d972d786b26f9f8d52bc011981

--- tool/pldm_base_cmd.cpp
+++ tool/pldm_base_cmd.cpp
@@ -2,11 +2,78 @@
 
 #include "pldm_cmd_helper.hpp"
 
+#include <map>
 #include <string>
 
 constexpr uint8_t PLDM_ENTITY_ID = 8;
 constexpr uint8_t MCTP_MSG_TYPE_PLDM = 1;
 constexpr uint8_t PLDM_LOCAL_INSTANCE_ID = 0;
+
+class GetPLDMTypes : public CommandInterface
+{
+  public:
+    ~GetPLDMTypes() = default;
+    GetPLDMTypes(const GetPLDMTypes&) = default;
+    GetPLDMTypes(GetPLDMTypes&&) = default;
+    GetPLDMTypes& operator=(const GetPLDMTypes&) = default;
+    GetPLDMTypes& operator=(GetPLDMTypes&&) = default;
+    GetPLDMTypes() : CommandInterface("GetPLDMTypes")
+    {
+    }
+
+    std::pair<int, std::vector<uint8_t>>
+        createRequestMsg(std::vector<std::string>& args) override
+    {
+        std::ignore = args;
+        std::vector<uint8_t> requestMsg(sizeof(pldm_msg_hdr));
+        auto request = reinterpret_cast<pldm_msg*>(requestMsg.data());
+        auto rc = encode_get_types_req(PLDM_LOCAL_INSTANCE_ID, request);
+        return {rc, requestMsg};
+    }
+
+    void parseResponseMsg(pldm_msg* responsePtr, size_t payloadLength) override
+    {
+        uint8_t cc;
+        std::vector<bitfield8_t> types(8);
+        auto rc = decode_get_types_resp(responsePtr, payloadLength, &cc,
+                                        types.data());
+        if (rc != PLDM_SUCCESS || cc != PLDM_SUCCESS)
+        {
+            std::cout << "Response Message Error: "
+                      << "rc=" << rc << ",cc=" << cc << std::endl;
+            return;
+        }
+
+        printPldmTypes(types);
+    }
+
+  private:
+    void printPldmTypes(std::vector<bitfield8_t>& types)
+    {
+        const std::map<int, const char*> pldmTypeMap = {{0, "base"},
+                                                        {2, "platform"},
+                                                        {3, "bios"},
+                                                        {4, "fru"},
+                                                        {0x3f, "oem"}};
+        std::cout << "Supported types:";
+        for (int i = 0; i < PLDM_MAX_TYPES; i++)
+        {
+            bitfield8_t b = types[i / 8];
+            if (b.byte & (1 << i % 8))
+            {
+                std::cout << " " << i;
+                auto it = pldmTypeMap.find(i);
+                if (it != pldmTypeMap.end())
+                {
+
+                    std::cout << "(" << it->second << ")";
+                }
+            }
+        }
+
+        std::cout << std::endl;
+    }
+};
 
 using namespace std;
 
@@ -14,50 +81,9 @@ using namespace std;
  * Main function that handles the GetPLDMTypes response callback via mctp
  *
  */
-void getPLDMTypes(vector<std::string>&& /*args*/)
+void getPLDMTypes(vector<std::string>&& args)
 {
-    // Create and encode the request message
-    vector<uint8_t> requestMsg(sizeof(pldm_msg_hdr) +
-                               sizeof(MCTP_MSG_TYPE_PLDM) +
-                               sizeof(PLDM_ENTITY_ID));
-    auto request = reinterpret_cast<pldm_msg*>(requestMsg.data());
-
-    // Encode the get_types request message
-    uint8_t instanceId = PLDM_LOCAL_INSTANCE_ID;
-    auto returnCode = encode_get_types_req(instanceId, request);
-    if (returnCode)
-    {
-        cerr << "Failed to encode request msg for GetPLDMType : RC = "
-             << returnCode << endl;
-        return;
-    }
-    cout << "Encoded request succesfully : RC = " << returnCode << endl;
-
-    // Insert the PLDM message type and EID at the begining of the request msg.
-    requestMsg.insert(requestMsg.begin(), MCTP_MSG_TYPE_PLDM);
-    requestMsg.insert(requestMsg.begin(), PLDM_ENTITY_ID);
-
-    cout << "Request Message:" << endl;
-    printBuffer(requestMsg);
-
-    // Create the response message
-    vector<uint8_t> responseMsg;
-
-    // Compares the response with request packet on first socket recv() call.
-    // If above condition is qualified then, reads the actual response from
-    // the socket to output buffer responseMsg.
-    returnCode = mctpSockSendRecv(requestMsg, responseMsg);
-    if (!returnCode)
-    {
-        cout << "Socket recv() successful : RC = " << returnCode << endl;
-        cout << "Response Message : " << endl;
-        printBuffer(responseMsg);
-    }
-    else
-    {
-        cerr << "Failed to recieve from socket : RC = " << returnCode << endl;
-        return;
-    }
+    GetPLDMTypes().exec(args);
 }
 
 /*
