54f3952b98494bce18b0e11f0326158e8a54f342
Deepak Kodihalli <dkodihal@in.ibm.com>
2019-11-11 02:41:43 -0600

    pldmd: implement PLDM.Requester D-Bus API
    Implement D-Bus API defined at
    https://gerrit.openbmc-project.xyz/#/c/openbmc/phosphor-dbus-interfaces/+/27077/.
    
    This commit provides an implementation of the PLDM instance id as per
    DSP0240 v1.0.0. Ids are tracked per MCTP EID.
    
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>
    Change-Id: I8a1ef213646529b71d9bf33edda67537f7c0f32a

---
+++ instance_id.cpp
@@ -0,0 +1,30 @@
+#include "instance_id.hpp"
+
+#include <stdexcept>
+
+namespace pldm
+{
+
+uint8_t InstanceId::next()
+{
+    uint8_t idx = 0;
+    while (idx < id.size() && id.test(idx))
+    {
+        ++idx;
+    }
+
+    if (idx == id.size())
+    {
+        throw std::runtime_error("No free instance ids");
+    }
+
+    id.set(idx);
+    return idx;
+}
+
+void InstanceId::markFree(uint8_t instanceId)
+{
+    id.set(instanceId, false);
+}
+
+} // namespace pldm
