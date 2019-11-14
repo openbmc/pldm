616d0aac6fe54c027b2dec4366db63244d528f77
John Wang <wangzqbj@inspur.com>
2019-11-14 15:33:42 +0800

    libpldm: util: Convert version filed to string
    Convert version filed to string, see DSP0240 5.5
    
    Signed-off-by: John Wang <wangzqbj@inspur.com>
    Change-Id: Ife36c0e0ae916173fa73309b490cd380a43b3db6

--- test/libpldm_utils_test.cpp
+++ test/libpldm_utils_test.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <vector>
 
 #include "libpldm/utils.h"
@@ -9,4 +10,44 @@ TEST(Crc32, CheckSumTest)
     const char* password = "123456789";
     auto checksum = crc32(password, 9);
     EXPECT_EQ(checksum, 0xcbf43926);
+}
+
+TEST(Ver2string, Ver2string)
+{
+    ver32_t version{0xf3, 0xf7, 0x10, 0x61};
+    const char* vstr = "3.7.10a";
+    char buffer[1024];
+    auto rc = ver2str(&version, buffer, sizeof(buffer));
+    EXPECT_EQ(rc, std::strlen(vstr));
+    EXPECT_STREQ(vstr, buffer);
+
+    version = {0x10, 0x01, 0xf7, 0x00};
+    vstr = "10.01.7";
+    rc = ver2str(&version, buffer, sizeof(buffer));
+    EXPECT_EQ(rc, std::strlen(vstr));
+    EXPECT_STREQ(vstr, buffer);
+
+    version = {0xf3, 0xf1, 0xff, 0x00};
+    vstr = "3.1";
+    rc = ver2str(&version, buffer, sizeof(buffer));
+    EXPECT_EQ(rc, std::strlen(vstr));
+    EXPECT_STREQ(vstr, buffer);
+
+    version = {0xf1, 0xf0, 0xff, 0x61};
+    vstr = "1.0a";
+    rc = ver2str(&version, buffer, sizeof(buffer));
+    EXPECT_EQ(rc, std::strlen(vstr));
+    EXPECT_STREQ(vstr, buffer);
+
+    rc = ver2str(&version, buffer, 3);
+    EXPECT_EQ(rc, 2);
+    EXPECT_STREQ("1.", buffer);
+
+    rc = ver2str(&version, buffer, 1);
+    EXPECT_EQ(rc, 0);
+    EXPECT_STREQ("", buffer);
+
+    rc = ver2str(&version, buffer, 0);
+    EXPECT_EQ(rc, -1);
+    EXPECT_STREQ("", buffer);
 }
