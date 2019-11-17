13d8ca49d0059254ee21b521264e6ca8e29abcf6
Tom Joseph <tomjoseph@in.ibm.com>
2019-11-17 22:21:45 +0530

    FRU Parsing for platform specific config files
    Change-Id: I600cfb7c6dcf529465b2618a2e040aa1e66c1607
    Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

---
+++ test/libpldmresponder_fru_test.cpp
@@ -0,0 +1,53 @@
+#include "libpldmresponder/fru_parser.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(InventoryLookup, allScenarios)
+{
+    using namespace fru_parser;
+
+    FruRecordInfos board{{1,
+                          1,
+                          {{"xyz.openbmc_project.Inventory.Decorator.Asset",
+                            "PartNumber", "string", 3},
+                           {"xyz.openbmc_project.Inventory.Decorator.Asset",
+                            "SerialNumber", "string", 4}}},
+                         {254,
+                          1,
+                          {{"com.ibm.ipzvpd.VINI", "RT", "string", 2},
+                           {"com.ibm.ipzvpd.VINI", "B3", "bytearray", 3}}}};
+
+    FruRecordInfos cpu{{1,
+                        1,
+                        {{"xyz.openbmc_project.Inventory.Decorator.Asset",
+                          "PartNumber", "string", 3},
+                         {"xyz.openbmc_project.Inventory.Decorator.Asset",
+                          "SerialNumber", "string", 4}}}};
+
+//    ASSERT_THROW(fru_parser::get("./fru_json"), std::exception);
+//    ASSERT_THROW(fru_parser::get("./fru_jsons/malformed1"), std::exception);
+
+    auto handle = fru_parser::get("./fru_jsons/good");
+
+    auto boardInfos =
+        handle.getRecordInfo("xyz.openbmc_project.Inventory.Item.Board");
+    auto cpuInfos =
+        handle.getRecordInfo("xyz.openbmc_project.Inventory.Item.Cpu");
+    ASSERT_EQ(boardInfos.size(), 2);
+    ASSERT_EQ(cpuInfos.size(), 1);
+    ASSERT_EQ(cpu == cpuInfos, true);
+    ASSERT_EQ(board == boardInfos, true);
+
+    DBusLookupInfo lookupInfo{"xyz.openbmc_project.Inventory.Manager",
+                              "/xyz/openbmc_project/inventory/system/",
+                              {"xyz.openbmc_project.Inventory.Item.Board",
+                               "xyz.openbmc_project.Inventory.Item.Cpu"}};
+
+    auto dbusInfo = handle.inventoryLookup();
+    ASSERT_EQ(dbusInfo == lookupInfo, true);
+
+//    // Search for an invalid Item Type
+//    ASSERT_THROW(
+//        handle.getRecordInfo("xyz.openbmc_project.Inventory.Item.DIMM"),
+//        std::exception);
+}
