919a40643dd2bbbc72ea119f12f94c5fe8eb4046
vkaverap <vkaverap@in.ibm.com>
2019-11-19 01:47:35 -0600

    FILE I/O : Implement encode/decode (responder flow only) for FileAck
    Change-Id: Ia363f6850f415f5ce9694976ddf1d225091fb1c4
    Signed-off-by: vkaverap <vkaverap@in.ibm.com>

--- oem/ibm/libpldm/file_io.h
+++ oem/ibm/libpldm/file_io.h
@@ -21,6 +21,7 @@ enum pldm_fileio_commands {
 	PLDM_READ_FILE_BY_TYPE_INTO_MEMORY = 0x8,
 	PLDM_WRITE_FILE_BY_TYPE_FROM_MEMORY = 0x9,
 	PLDM_NEW_FILE_AVAILABLE = 0xA,
+	PLDM_FILE_ACK = 0xD,
 };
 
 /** @brief PLDM Command specific codes
@@ -60,6 +61,8 @@ enum pldm_fileio_file_type {
 #define PLDM_RW_FILE_BY_TYPE_MEM_RESP_BYTES 5
 #define PLDM_NEW_FILE_REQ_BYTES 10
 #define PLDM_NEW_FILE_RESP_BYTES 1
+#define PLDM_FILE_ACK_REQ_BYTES 6
+#define PLDM_FILE_ACK_RESP_BYTES 1
 
 /** @struct pldm_read_write_file_memory_req
  *
@@ -508,6 +511,66 @@ int encode_new_file_req(uint8_t instance_id, uint16_t file_type,
  *  @return pldm_completion_codes
  */
 int decode_new_file_resp(const struct pldm_msg *msg, size_t payload_length,
+			 uint8_t *completion_code);
+
+/** @struct pldm_file_ack_req
+ *
+ *  Structure representing FileAck request
+ */
+struct pldm_file_ack_req {
+	uint16_t file_type;   //!< Type of file
+	uint32_t file_handle; //!< Handle to file
+} __attribute__((packed));
+
+/** @struct pldm_file_ack_resp
+ *
+ *  Structure representing NewFile response data
+ */
+struct pldm_file_ack_resp {
+	uint8_t completion_code; //!< Completion code
+} __attribute__((packed));
+
+/** @brief Decode FileAck command request data
+ *
+ *  @param[in] msg - Pointer to PLDM request message
+ *  @param[in] payload_length - Length of request payload
+ *  @param[in] file_type - Type of the file
+ *  @param[out] file_handle - A handle to the file
+ *  @return pldm_completion_codes
+ */
+int decode_file_ack_req(const struct pldm_msg *msg, size_t payload_length,
+			uint16_t *file_type, uint32_t *file_handle);
+
+/** @brief Create a PLDM response for FileAck
+ *
+ *  @param[in] instance_id - Message's instance id
+ *  @param[in] completion_code - PLDM completion code
+ *  @param[in,out] msg - Message will be written to this
+ *  @return pldm_completion_codes
+ *  @note  Caller is responsible for memory alloc and dealloc of param 'msg'
+ */
+int encode_file_ack_resp(uint8_t instance_id, uint8_t completion_code,
+			 struct pldm_msg *msg);
+
+/** @brief Encode FileAck command request data
+ *
+ *  @param[in] instance_id - Message's instance id
+ *  @param[in] file_type - Type of the file
+ *  @param[in] file_handle - A handle to the file
+ *  @param[out] msg - Message will be written to this
+ *  @return pldm_completion_codes
+ */
+int encode_file_ack_req(uint8_t instance_id, uint16_t file_type,
+			uint32_t file_handle, struct pldm_msg *msg);
+
+/** @brief Decode FileAck command response data
+ *
+ *  @param[in] msg - pointer to PLDM response message
+ *  @param[in] payload_length - Length of response payload
+ *  @param[out] completion_code - PLDM completion code
+ *  @return pldm_completion_codes
+ */
+int decode_file_ack_resp(const struct pldm_msg *msg, size_t payload_length,
 			 uint8_t *completion_code);
 
 #ifdef __cplusplus
