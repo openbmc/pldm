83409573d6f6303e717382d0428c55eda8b571e1
George Liu <liuxiwei@inspur.com>
2019-12-24 18:42:54 +0800

    pldm: Moving utils function into the common directory
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I4fbd0da4bc6f27e9a9ed711962bd8737d8b55ebf

--- utils.cpp
+++ utils.cpp
@@ -1,24 +1,94 @@
 #include "utils.hpp"
 
-#include "xyz/openbmc_project/Common/error.hpp"
-
 #include <array>
 #include <ctime>
 #include <iostream>
 #include <map>
+#include <phosphor-logging/log.hpp>
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <xyz/openbmc_project/Common/error.hpp>
 
 namespace pldm
+{
+namespace utils
 {
 
 constexpr auto mapperBusName = "xyz.openbmc_project.ObjectMapper";
 constexpr auto mapperPath = "/xyz/openbmc_project/object_mapper";
 constexpr auto mapperInterface = "xyz.openbmc_project.ObjectMapper";
 
-namespace responder
+uint8_t getNumPadBytes(uint32_t data)
 {
+    uint8_t pad;
+    pad = ((data % 4) ? (4 - data % 4) : 0);
+    return pad;
+} // end getNumPadBytes
+
+bool uintToDate(uint64_t data, uint16_t* year, uint8_t* month, uint8_t* day,
+                uint8_t* hour, uint8_t* min, uint8_t* sec)
+{
+    constexpr uint64_t max_data = 29991231115959;
+    constexpr uint64_t min_data = 19700101000000;
+    if (data < min_data || data > max_data)
+    {
+        return false;
+    }
+
+    *year = data / 10000000000;
+    data = data % 10000000000;
+    *month = data / 100000000;
+    data = data % 100000000;
+    *day = data / 1000000;
+    data = data % 1000000;
+    *hour = data / 10000;
+    data = data % 10000;
+    *min = data / 100;
+    *sec = data % 100;
+
+    return true;
+}
+
+bool decodeEffecterData(const std::vector<uint8_t>& effecterData,
+                        uint16_t& effecter_id,
+                        std::vector<set_effecter_state_field>& stateField)
+{
+    int flag = 0;
+    for (auto data : effecterData)
+    {
+        switch (flag)
+        {
+            case 0:
+                effecter_id = data;
+                flag = 1;
+                break;
+            case 1:
+                if (data == PLDM_REQUEST_SET)
+                {
+                    flag = 2;
+                }
+                else
+                {
+                    stateField.push_back({PLDM_NO_CHANGE, 0});
+                }
+                break;
+            case 2:
+                stateField.push_back({PLDM_REQUEST_SET, data});
+                flag = 1;
+                break;
+            default:
+                break;
+        }
+    }
+
+    if (stateField.size() < 1 || stateField.size() > 8)
+    {
+        return false;
+    }
+
+    return true;
+}
 
 std::string getService(sdbusplus::bus::bus& bus, const std::string& path,
                        const std::string& interface)
@@ -37,9 +107,8 @@ std::string getService(sdbusplus::bus::bus& bus, const std::string& path,
     }
     catch (std::exception& e)
     {
-        std::cerr << "Error in mapper call, ERROR=" << e.what()
-                  << " PATH=" << path.c_str()
-                  << " INTERFACE=" << interface.c_str() << "\n";
+        std::cerr << "gete dbus service, PATH=" << path
+                  << " INTERFACE=" << interface << e.what() << "\n";
         throw;
     }
     return mapperResponse.begin()->first;
@@ -73,16 +142,5 @@ void reportError(const char* errorMsg)
     }
 }
 
-namespace utils
-{
-
-uint8_t getNumPadBytes(uint32_t data)
-{
-    uint8_t pad;
-    pad = ((data % 4) ? (4 - data % 4) : 0);
-    return pad;
-} // end getNumPadBytes
-
-} // end namespace utils
-} // namespace responder
+} // namespace utils
 } // namespace pldm
