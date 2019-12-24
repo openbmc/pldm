62f9fa1b91c38ee01f81758ee7d64112ecf0a3d9
George Liu <liuxiwei@inspur.com>
2019-12-24 18:42:54 +0800

    pldm: Moving utils function into the common directory
    Moving libpldmresponder/utils function into the common root directory.
    build and make that as a library for ues.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I4fbd0da4bc6f27e9a9ed711962bd8737d8b55ebf

--- oem/ibm/libpldmresponder/file_io.cpp
+++ oem/ibm/libpldmresponder/file_io.cpp
@@ -4,7 +4,7 @@
 
 #include "file_io_by_type.hpp"
 #include "file_table.hpp"
-#include "libpldmresponder/utils.hpp"
+#include "utils.hpp"
 #include "xyz/openbmc_project/Common/error.hpp"
 
 #include <fcntl.h>
@@ -75,7 +75,7 @@ int DMA::transferDataHost(const fs::path& path, uint32_t offset,
         return rc;
     }
 
-    utils::CustomFD xdmaFd(fd);
+    pldm::utils::CustomFD xdmaFd(fd);
 
     void* vgaMem;
     vgaMem = mmap(nullptr, pageAlignedLength, upstream ? PROT_WRITE : PROT_READ,
