63b8bee1aa4ec0af5a566e41c5074daf9120616f
George Liu <liuxiwei@inspur.com>
2020-01-13 11:36:22 +0800

    pldm: Add parsing JSON for Numeric Effecter PDR
    According to spec DSP0248_1.2.0: 28.11 and effecter_pdr.json, parse JSON and generate PDR
    structure.
    
    In addition, move the parsing pldm_numeric_effecter_pdr method to pdr_numeric_effecter_pdr.hpp.
    
    Tested with JSON files:
     https://gist.github.com/lxwinspur/2c3fd68cdb35e06480c4a5f7890e3a06#file-effecter_pdr-json.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I4254a14f0fd5e33b312f65c15c9a23437e28d341

--- test/libpldmresponder_pdr_effecter_test.cpp
+++ test/libpldmresponder_pdr_effecter_test.cpp
@@ -8,7 +8,7 @@
 
 using namespace pldm::responder;
 
-TEST(GeneratePDR, testGoodJson)
+TEST(GeneratePDRByStateEffecter, testGoodJson)
 {
     using namespace effecter::dbus_mapping;
     pdr_utils::Repo pdrRepo = pdr::getRepoByType(
@@ -91,6 +91,39 @@ TEST(GeneratePDR, testGoodJson)
     ASSERT_EQ(dbusObj2[1].objectPath, "/foo/bar/baz");
 
     ASSERT_THROW(getDbusObjs(0xDEAD), std::exception);
+}
+
+TEST(GeneratePDRByNumericEffecter, testGoodJson)
+{
+    using namespace effecter::dbus_mapping;
+    pdr_utils::Repo pdrRepo = pdr::getRepoByType(
+        "./pdr_jsons/state_effecter/good", PLDM_NUMERIC_EFFECTER_PDR);
+
+    // 1 entries
+    ASSERT_EQ(pdrRepo.getRecordCount(), 1);
+
+    // Check first PDR
+    pdr_utils::PdrEntry e;
+    auto record = pdr::getRecordByHandle(pdrRepo, 3, e);
+    ASSERT_NE(record, nullptr);
+
+    pldm_numeric_effecter_value_pdr* pdr =
+        reinterpret_cast<pldm_numeric_effecter_value_pdr*>(e.data);
+    EXPECT_EQ(pdr->hdr.record_handle, 3);
+    EXPECT_EQ(pdr->hdr.version, 1);
+    EXPECT_EQ(pdr->hdr.type, PLDM_NUMERIC_EFFECTER_PDR);
+    EXPECT_EQ(pdr->hdr.record_change_num, 0);
+    EXPECT_EQ(pdr->hdr.length,
+              sizeof(pldm_numeric_effecter_value_pdr) - sizeof(pldm_pdr_hdr));
+
+    EXPECT_EQ(pdr->effecter_id, 3);
+    EXPECT_EQ(pdr->effecter_data_size, 4);
+
+    const auto& dbusObjs = getDbusObjs(pdr->effecter_id);
+    EXPECT_EQ(dbusObjs[0].objectPath, "/foo/bar");
+    EXPECT_EQ(dbusObjs[0].interface, "xyz.openbmc_project.Foo.Bar");
+    EXPECT_EQ(dbusObjs[0].propertyName, "propertyName");
+    EXPECT_EQ(dbusObjs[0].propertyType, "string");
 }
 
 TEST(GeneratePDR, testNoJson)
