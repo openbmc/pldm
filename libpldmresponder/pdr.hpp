b5ad8ac06b88a5a006f566cdd10550fbf137d98d
George Liu <liuxiwei@inspur.com>
2020-01-13 11:36:22 +0800

    pldm: Add parsing JSON for Numeric Effecter PDR
    According to spec DSP0248_1.2.0: 28.11 and 9.json, parse JSON and generate PDR
    structure.
    
    In addition, move the parsing pldm_state_effecter_pdr method to pdr.cpp.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I4254a14f0fd5e33b312f65c15c9a23437e28d341

--- libpldmresponder/pdr.hpp
+++ libpldmresponder/pdr.hpp
@@ -177,6 +177,12 @@ class IndexedRepo : public Repo
     Pdr repo{};
 };
 
+template <typename T>
+void generate_pldm_state_effecter_pdr(const Json& json, T& repo);
+
+template <typename T>
+void generate_pldm_numeric_effecter_pdr(const Json& json, T& repo);
+
 /** @brief Parse PDR JSONs and build PDR repository
  *
  *  @param[in] dir - directory housing platform specific PDR JSON files
@@ -191,89 +197,12 @@ void generate(const std::string& dir, T& repo)
     // generate the PDR structures. This function iterates through the map to
     // invoke all lambdas, so that all PDR types can be created.
     std::map<Type, std::function<void(const Json& json, T& repo)>> generators =
-        {{PLDM_STATE_EFFECTER_PDR, [](const auto& json, T& repo) {
-              static const std::vector<Json> emptyList{};
-              static const Json empty{};
-              auto entries = json.value("entries", emptyList);
-              for (const auto& e : entries)
-              {
-                  size_t pdrSize = 0;
-                  auto effecters = e.value("effecters", emptyList);
-                  static const Json empty{};
-                  for (const auto& effecter : effecters)
-                  {
-                      auto set = effecter.value("set", empty);
-                      auto statesSize = set.value("size", 0);
-                      if (!statesSize)
-                      {
-                          std::cerr
-                              << "Malformed PDR JSON - no state set info, TYPE="
-                              << PLDM_STATE_EFFECTER_PDR << "\n";
-                          throw InternalFailure();
-                      }
-                      pdrSize += sizeof(state_effecter_possible_states) -
-                                 sizeof(bitfield8_t) +
-                                 (sizeof(bitfield8_t) * statesSize);
-                  }
-                  pdrSize += sizeof(pldm_state_effecter_pdr) - sizeof(uint8_t);
-
-                  Entry pdrEntry{};
-                  pdrEntry.resize(pdrSize);
-
-                  pldm_state_effecter_pdr* pdr =
-                      reinterpret_cast<pldm_state_effecter_pdr*>(
-                          pdrEntry.data());
-                  pdr->hdr.record_handle = repo.getNextRecordHandle();
-                  pdr->hdr.version = 1;
-                  pdr->hdr.type = PLDM_STATE_EFFECTER_PDR;
-                  pdr->hdr.record_change_num = 0;
-                  pdr->hdr.length = pdrSize - sizeof(pldm_pdr_hdr);
-
-                  pdr->terminus_handle = 0;
-                  pdr->effecter_id = effecter::nextId();
-                  pdr->entity_type = e.value("type", 0);
-                  pdr->entity_instance = e.value("instance", 0);
-                  pdr->container_id = e.value("container", 0);
-                  pdr->effecter_semantic_id = 0;
-                  pdr->effecter_init = PLDM_NO_INIT;
-                  pdr->has_description_pdr = false;
-                  pdr->composite_effecter_count = effecters.size();
-
-                  using namespace effecter::dbus_mapping;
-                  Paths paths{};
-                  uint8_t* start = pdrEntry.data() +
-                                   sizeof(pldm_state_effecter_pdr) -
-                                   sizeof(uint8_t);
-                  for (const auto& effecter : effecters)
-                  {
-                      auto set = effecter.value("set", empty);
-                      state_effecter_possible_states* possibleStates =
-                          reinterpret_cast<state_effecter_possible_states*>(
-                              start);
-                      possibleStates->state_set_id = set.value("id", 0);
-                      possibleStates->possible_states_size =
-                          set.value("size", 0);
-
-                      start += sizeof(possibleStates->state_set_id) +
-                               sizeof(possibleStates->possible_states_size);
-                      static const std::vector<uint8_t> emptyStates{};
-                      auto states = set.value("states", emptyStates);
-                      for (const auto& state : states)
-                      {
-                          auto index = state / 8;
-                          auto bit = state - (index * 8);
-                          bitfield8_t* bf =
-                              reinterpret_cast<bitfield8_t*>(start + index);
-                          bf->byte |= 1 << bit;
-                      }
-                      start += possibleStates->possible_states_size;
-
-                      auto dbus = effecter.value("dbus", empty);
-                      paths.emplace_back(std::move(dbus));
-                  }
-                  add(pdr->effecter_id, std::move(paths));
-                  repo.add(std::move(pdrEntry));
-              }
+        {{PLDM_STATE_EFFECTER_PDR,
+          [](const auto& json, T& repo) {
+              generate_pldm_state_effecter_pdr(json, repo);
+          }},
+         {PLDM_NUMERIC_EFFECTER_PDR, [](const auto& json, T& repo) {
+              generate_pldm_numeric_effecter_pdr(json, repo);
           }}};
 
     auto eraseLen = strlen(".json");
