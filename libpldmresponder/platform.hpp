af9306509bc0a1728fcc8ebc6bfef12b6a5e91a2
George Liu <liuxiwei@inspur.com>
2020-01-14 11:09:56 +0800

    pldm: Implement SetNumericEffecterValue function
    Register the setNumericEffecterValue method and get the PDR structure
    according to the EffecterId attribute.
    Verify the TPO time and update the Dbus interface.
    
    In addition, move the setStateEffecterStatesHandler method to
    platform_11.hpp.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I7eb56331813d7777a67d6c50f15b244f57e8492c

--- libpldmresponder/platform.hpp
+++ libpldmresponder/platform.hpp
@@ -1,17 +1,7 @@
 #pragma once
 
-#include "config.h"
-
-#include "handler.hpp"
-#include "libpldmresponder/pdr.hpp"
-#include "utils.hpp"
-
-#include <stdint.h>
-
-#include <map>
-
-#include "libpldm/platform.h"
-#include "libpldm/states.h"
+#include "libpldmresponder/platform_11.hpp"
+#include "libpldmresponder/platform_9.hpp"
 
 namespace pldm
 {
@@ -29,6 +19,11 @@ class Handler : public CmdHandler
                          [this](const pldm_msg* request, size_t payloadLength) {
                              return this->getPDR(request, payloadLength);
                          });
+        handlers.emplace(PLDM_SET_NUMERIC_EFFECTER_VALUE,
+                         [this](const pldm_msg* request, size_t payloadLength) {
+                             return this->setNumericEffecterValue(
+                                 request, payloadLength);
+                         });
         handlers.emplace(PLDM_SET_STATE_EFFECTER_STATES,
                          [this](const pldm_msg* request, size_t payloadLength) {
                              return this->setStateEffecterStates(request,
@@ -44,6 +39,15 @@ class Handler : public CmdHandler
      */
     Response getPDR(const pldm_msg* request, size_t payloadLength);
 
+    /** @brief Handler for setNumericEffecterValue
+     *
+     *  @param[in] request - Request message
+     *  @param[in] payloadLength - Request payload length
+     *  @return Response - PLDM Response message
+     */
+    Response setNumericEffecterValue(const pldm_msg* request,
+                                     size_t payloadLength);
+
     /** @brief Handler for setStateEffecterStates
      *
      *  @param[in] request - Request message
@@ -52,226 +56,6 @@ class Handler : public CmdHandler
      */
     Response setStateEffecterStates(const pldm_msg* request,
                                     size_t payloadLength);
-
-    /** @brief Function to set the effecter requested by pldm requester
-     *  @param[in] dBusIntf - The interface object
-     *  @param[in] effecterId - Effecter ID sent by the requester to act on
-     *  @param[in] stateField - The state field data for each of the states,
-     * equal to composite effecter count in number
-     *  @return - Success or failure in setting the states. Returns failure in
-     * terms of PLDM completion codes if atleast one state fails to be set
-     */
-    template <class DBusInterface>
-    int setStateEffecterStatesHandler(
-        const DBusInterface& dBusIntf, effecter::Id effecterId,
-        const std::vector<set_effecter_state_field>& stateField)
-    {
-        using namespace std::string_literals;
-        using DBusProperty = std::variant<std::string, bool>;
-        using StateSetId = uint16_t;
-        using StateSetNum = uint8_t;
-        using PropertyMap =
-            std::map<StateSetId, std::map<StateSetNum, DBusProperty>>;
-        static const PropertyMap stateNumToDbusProp = {
-            {PLDM_BOOT_PROGRESS_STATE,
-             {{PLDM_BOOT_NOT_ACTIVE,
-               "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus."
-               "Standby"s},
-              {PLDM_BOOT_COMPLETED,
-               "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus."
-               "BootComplete"s}}},
-            {PLDM_SYSTEM_POWER_STATE,
-             {{PLDM_OFF_SOFT_GRACEFUL,
-               "xyz.openbmc_project.State.Chassis.Transition.Off"s}}}};
-        using namespace pldm::responder::pdr;
-        using namespace pldm::responder::effecter::dbus_mapping;
-
-        state_effecter_possible_states* states = nullptr;
-        pldm_state_effecter_pdr* pdr = nullptr;
-        uint8_t compEffecterCnt = stateField.size();
-        uint32_t recordHndl{};
-        Repo& pdrRepo = get(PDR_JSONS_DIR);
-        pdr::Entry pdrEntry{};
-
-        while (!pdr)
-        {
-            pdrEntry = pdrRepo.at(recordHndl);
-            pldm_pdr_hdr* header =
-                reinterpret_cast<pldm_pdr_hdr*>(pdrEntry.data());
-            if (header->type != PLDM_STATE_EFFECTER_PDR)
-            {
-                recordHndl = pdrRepo.getNextRecordHandle(recordHndl);
-                if (recordHndl)
-                {
-                    continue;
-                }
-                return PLDM_PLATFORM_INVALID_EFFECTER_ID;
-            }
-            pdr = reinterpret_cast<pldm_state_effecter_pdr*>(pdrEntry.data());
-            recordHndl = pdr->hdr.record_handle;
-            if (pdr->effecter_id == effecterId)
-            {
-                states = reinterpret_cast<state_effecter_possible_states*>(
-                    pdr->possible_states);
-                if (compEffecterCnt > pdr->composite_effecter_count)
-                {
-                    std::cerr
-                        << "The requester sent wrong composite effecter"
-                        << " count for the effecter, EFFECTER_ID=" << effecterId
-                        << "COMP_EFF_CNT=" << compEffecterCnt << "\n";
-                    return PLDM_ERROR_INVALID_DATA;
-                }
-                break;
-            }
-            recordHndl = pdrRepo.getNextRecordHandle(recordHndl);
-            if (!recordHndl)
-            {
-                return PLDM_PLATFORM_INVALID_EFFECTER_ID;
-            }
-            pdr = nullptr;
-        }
-
-        std::map<StateSetId, std::function<int(const std::string& objPath,
-                                               const uint8_t currState)>>
-            effecterToDbusEntries = {
-                {PLDM_BOOT_PROGRESS_STATE,
-                 [&](const std::string& objPath, const uint8_t currState) {
-                     auto stateSet =
-                         stateNumToDbusProp.find(PLDM_BOOT_PROGRESS_STATE);
-                     if (stateSet == stateNumToDbusProp.end())
-                     {
-                         std::cerr << "Couldn't find D-Bus mapping for "
-                                   << "PLDM_BOOT_PROGRESS_STATE, EFFECTER_ID="
-                                   << effecterId << "\n";
-                         return PLDM_ERROR;
-                     }
-                     auto iter = stateSet->second.find(
-                         stateField[currState].effecter_state);
-                     if (iter == stateSet->second.end())
-                     {
-                         std::cerr << "Invalid state field passed or field not "
-                                   << "found for PLDM_BOOT_PROGRESS_STATE, "
-                                      "EFFECTER_ID="
-                                   << effecterId << " FIELD="
-                                   << stateField[currState].effecter_state
-                                   << " OBJECT_PATH=" << objPath.c_str()
-                                   << "\n";
-                         return PLDM_ERROR_INVALID_DATA;
-                     }
-                     auto dbusProp = "OperatingSystemState";
-                     std::variant<std::string> value{
-                         std::get<std::string>(iter->second)};
-                     auto dbusInterface =
-                         "xyz.openbmc_project.State.OperatingSystem.Status";
-                     try
-                     {
-                         dBusIntf.setDbusProperty(objPath.c_str(), dbusProp,
-                                                  dbusInterface, value);
-                     }
-                     catch (const std::exception& e)
-                     {
-                         std::cerr
-                             << "Error setting property, ERROR=" << e.what()
-                             << " PROPERTY=" << dbusProp
-                             << " INTERFACE=" << dbusInterface
-                             << " PATH=" << objPath.c_str() << "\n";
-                         return PLDM_ERROR;
-                     }
-                     return PLDM_SUCCESS;
-                 }},
-                {PLDM_SYSTEM_POWER_STATE,
-                 [&](const std::string& objPath, const uint8_t currState) {
-                     auto stateSet =
-                         stateNumToDbusProp.find(PLDM_SYSTEM_POWER_STATE);
-                     if (stateSet == stateNumToDbusProp.end())
-                     {
-                         std::cerr << "Couldn't find D-Bus mapping for "
-                                   << "PLDM_SYSTEM_POWER_STATE, EFFECTER_ID="
-                                   << effecterId << "\n";
-                         return PLDM_ERROR;
-                     }
-                     auto iter = stateSet->second.find(
-                         stateField[currState].effecter_state);
-                     if (iter == stateSet->second.end())
-                     {
-                         std::cerr << "Invalid state field passed or field not "
-                                   << "found for PLDM_SYSTEM_POWER_STATE, "
-                                      "EFFECTER_ID="
-                                   << effecterId << " FIELD="
-                                   << stateField[currState].effecter_state
-                                   << " OBJECT_PATH=" << objPath.c_str()
-                                   << "\n";
-                         return PLDM_ERROR_INVALID_DATA;
-                     }
-                     auto dbusProp = "RequestedPowerTransition";
-                     std::variant<std::string> value{
-                         std::get<std::string>(iter->second)};
-                     auto dbusInterface = "xyz.openbmc_project.State.Chassis";
-                     try
-                     {
-                         dBusIntf.setDbusProperty(objPath.c_str(), dbusProp,
-                                                  dbusInterface, value);
-                     }
-                     catch (const std::exception& e)
-                     {
-                         std::cerr
-                             << "Error setting property, ERROR=" << e.what()
-                             << " PROPERTY=" << dbusProp
-                             << " INTERFACE=" << dbusInterface
-                             << " PATH=" << objPath.c_str() << "\n";
-                         return PLDM_ERROR;
-                     }
-                     return PLDM_SUCCESS;
-                 }}};
-
-        int rc = PLDM_SUCCESS;
-        auto paths = get(effecterId);
-        for (uint8_t currState = 0; currState < compEffecterCnt; ++currState)
-        {
-            std::vector<StateSetNum> allowed{};
-            // computation is based on table 79 from DSP0248 v1.1.1
-            uint8_t bitfieldIndex = stateField[currState].effecter_state / 8;
-            uint8_t bit =
-                stateField[currState].effecter_state - (8 * bitfieldIndex);
-            if (states->possible_states_size < bitfieldIndex ||
-                !(states->states[bitfieldIndex].byte & (1 << bit)))
-            {
-                std::cerr << "Invalid state set value, EFFECTER_ID="
-                          << effecterId
-                          << " VALUE=" << stateField[currState].effecter_state
-                          << " COMPOSITE_EFFECTER_ID=" << currState
-                          << " DBUS_PATH=" << paths[currState].c_str() << "\n";
-                rc = PLDM_PLATFORM_SET_EFFECTER_UNSUPPORTED_SENSORSTATE;
-                break;
-            }
-            auto iter = effecterToDbusEntries.find(states->state_set_id);
-            if (iter == effecterToDbusEntries.end())
-            {
-                uint16_t setId = states->state_set_id;
-                std::cerr << "Did not find the state set for the"
-                          << " state effecter pdr, STATE=" << setId
-                          << " EFFECTER_ID=" << effecterId << "\n";
-                rc = PLDM_PLATFORM_INVALID_STATE_VALUE;
-                break;
-            }
-            if (stateField[currState].set_request == PLDM_REQUEST_SET)
-            {
-                rc = iter->second(paths[currState], currState);
-                if (rc != PLDM_SUCCESS)
-                {
-                    break;
-                }
-            }
-            uint8_t* nextState =
-                reinterpret_cast<uint8_t*>(states) +
-                sizeof(state_effecter_possible_states) -
-                sizeof(states->states) +
-                (states->possible_states_size * sizeof(states->states));
-            states =
-                reinterpret_cast<state_effecter_possible_states*>(nextState);
-        }
-        return rc;
-    }
 };
 
 } // namespace platform
---
+++ libpldmresponder/platform_9.hpp
@@ -0,0 +1,160 @@
+#pragma once
+
+#include "config.h"
+
+#include "handler.hpp"
+#include "libpldmresponder/pdr.hpp"
+#include "utils.hpp"
+
+#include <stdint.h>
+
+#include <map>
+
+#include "libpldm/platform.h"
+#include "libpldm/states.h"
+
+namespace pldm
+{
+namespace responder
+{
+namespace platform_9
+{
+
+template <class DBusInterface>
+int setTimedPowerOnValue(const DBusInterface& dBusIntf,
+                         const std::string& objPath,
+                         const std::string& dbusInterface,
+                         uint8_t* effecterValue)
+{
+    auto dbusProp = "PowerOnTime";
+    uint64_t timeNow = time(0);
+    uint64_t powerOnTime = *(reinterpret_cast<uint64_t*>(&effecterValue[0]));
+
+    // When the powerOnTime is 0, it means the function TPO is disabled.
+    if (powerOnTime < timeNow && 0 != powerOnTime)
+    {
+        std::cerr << "Error setting TPO time, effecterValue=" << powerOnTime
+                  << "\n";
+        return PLDM_ERROR;
+    }
+
+    // Update Dbus interfaces
+    std::variant<uint64_t> value{powerOnTime};
+    try
+    {
+        dBusIntf.setDbusProperty(objPath.c_str(), dbusProp,
+                                 dbusInterface.c_str(), value);
+    }
+    catch (const sdbusplus::exception::SdBusError& e)
+    {
+        std::cerr << "Error setting TPO property, ERROR=" << e.what()
+                  << " PROPERTY=" << dbusProp << " INTERFACE=" << dbusInterface
+                  << " PATH=" << objPath.c_str() << "\n";
+        return PLDM_ERROR;
+    }
+
+    return PLDM_SUCCESS;
+}
+
+/** @brief Function to set the effecter value requested by pldm requester
+ *  @param[in] dBusIntf - The interface object
+ *  @param[in] effecterId - Effecter ID sent by the requester to act on
+ *  @param[in] effecterDataSize - The bit width and format of the setting
+ * 				value for the effecter
+ *  @param[in] effecter_value - The setting value of numeric effecter being
+ * 				requested.
+ *  @param[in] effecterValueLength - The setting value length of numeric
+ *              effecter being requested.
+ *  @return - Success or failure in setting the states. Returns failure in
+ * terms of PLDM completion codes if atleast one state fails to be set
+ */
+template <class DBusInterface>
+int setNumericEffecterValueHandler(const DBusInterface& dBusIntf,
+                                   const uint16_t effecterId,
+                                   const uint8_t effecterDataSize,
+                                   uint8_t* effecterValue,
+                                   size_t effecterValueLength)
+{
+    constexpr auto uint32_length = 4;
+    using PropertyMap = std::map<uint16_t, std::string>;
+    static const PropertyMap setNumToDbusProp = {
+        {PLDM_TIMED_POWER_ON_STATE,
+         "xyz.openbmc_project.Control.TimedPowerOn"}};
+    using namespace pldm::responder::pdr;
+    using namespace pldm::responder::effecter::dbus_mapping;
+
+    // Get the pdr structure of pldm_numeric_effecter_valuer_pdr according
+    // to the effecterId
+    pldm_numeric_effecter_valuer_pdr* pdr = nullptr;
+    uint32_t recordHndl{};
+    Repo& pdrRepo = get(PDR_JSONS_DIR);
+    pdr::Entry pdrEntry{};
+
+    while (!pdr)
+    {
+        pdrEntry = pdrRepo.at(recordHndl);
+        pldm_pdr_hdr* header = reinterpret_cast<pldm_pdr_hdr*>(pdrEntry.data());
+        if (header->type != PLDM_NUMERIC_EFFECTER_PDR)
+        {
+            recordHndl = pdrRepo.getNextRecordHandle(recordHndl);
+            if (recordHndl)
+            {
+                continue;
+            }
+            return PLDM_PLATFORM_INVALID_EFFECTER_ID;
+        }
+        pdr = reinterpret_cast<pldm_numeric_effecter_valuer_pdr*>(
+            pdrEntry.data());
+        recordHndl = pdr->hdr.record_handle;
+        if (pdr->effecter_id == effecterId)
+        {
+            if (pdr->effecter_data_size != effecterDataSize ||
+                effecterValueLength != uint32_length)
+            {
+                std::cerr << "The requester sent wrong effecter"
+                          << " data size for the effecter, EFFECTER_ID="
+                          << effecterId
+                          << "EFFECTER_DATA_SIZE=" << effecterDataSize << "\n";
+                return PLDM_ERROR_INVALID_DATA;
+            }
+            break;
+        }
+        recordHndl = pdrRepo.getNextRecordHandle(recordHndl);
+        if (!recordHndl)
+        {
+            return PLDM_PLATFORM_INVALID_EFFECTER_ID;
+        }
+        pdr = nullptr;
+    }
+
+    // Get the value of effecterValue according to the value of
+    // effecterDataSize
+    std::map<uint16_t, std::function<int(const std::string& objPath,
+                                         const std::string& dbusInterface,
+                                         uint8_t* effecterValue)>>
+        effecterToDbusEntries = {
+            {PLDM_TIMED_POWER_ON_STATE,
+             [&](const std::string& objPath, const std::string& dbusInterface,
+                 uint8_t* effecterValue) {
+                 return setTimedPowerOnValue(dBusIntf, objPath, dbusInterface,
+                                             effecterValue);
+             }}};
+
+    auto iter = effecterToDbusEntries.find(effecterId);
+    if (iter == effecterToDbusEntries.end())
+    {
+        std::cerr << "Did not find the state set for the"
+                  << " effecterToDbusEntries, EFFECTER_ID=" << effecterId
+                  << "\n";
+        return PLDM_PLATFORM_INVALID_STATE_VALUE;
+    }
+    auto paths = get(effecterId);
+    auto dbusInterface = setNumToDbusProp.at(effecterId);
+
+    auto rc = iter->second(paths[0], dbusInterface, effecterValue);
+    return rc;
+}
+
+} // namespace platform_9
+} // namespace responder
+} // namespace pldm
