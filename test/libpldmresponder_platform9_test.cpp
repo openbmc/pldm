9953b8041d14d1c9010a5b23bd774836a1f22914
George Liu <liuxiwei@inspur.com>
2020-01-15 10:58:10 +0800

    pldm: Add gmock test for the setNumericEffecterValue method
    Add the setNumericEffecterValueHandler test method to the
    test/libpldmresponder_platform9_test.cpp.
    
    In addition, move the setStateEffecterStatesHandler test method to the
    test/libpldmresponder_platform11_test.cpp.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: Iccc7e672ddd5044eb23f5e61202a5a51a1ef17f3

---
+++ test/libpldmresponder_platform9_test.cpp
@@ -0,0 +1,79 @@
+#include "libpldmresponder/platform.hpp"
+
+#include <gmock/gmock-matchers.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace pldm::responder;
+using namespace pldm::responder::pdr;
+
+namespace pldm
+{
+
+namespace responder
+{
+
+class MockdBusHandler9
+{
+  public:
+    MOCK_CONST_METHOD4(setDbusProperty,
+                       int(const std::string&, const std::string&,
+                           const std::string&, const std::variant<uint64_t>&));
+};
+} // namespace responder
+} // namespace pldm
+
+using ::testing::_;
+using ::testing::Return;
+
+TEST(setNumericEffecterValueHandler, testGoodRequest)
+{
+    Repo& pdrRepo = get("./pdr_jsons/state_effecter/good");
+    pdr::Entry e = pdrRepo.at(1);
+    pldm_numeric_effecter_valuer_pdr* pdr =
+        reinterpret_cast<pldm_numeric_effecter_valuer_pdr*>(e.data());
+    EXPECT_EQ(pdr->hdr.type, PLDM_NUMERIC_EFFECTER_PDR);
+
+    uint16_t effecterId = PLDM_TIMED_POWER_ON_STATE;
+    uint64_t effecterValue = 9876543210;
+
+    auto bootProgressInf = "xyz.openbmc_project.Control.TimedPowerOn";
+    auto bootProgressProp = "PowerOnTime";
+    std::string objPath = "/xyz/openbmc_project/control/timedPowerOn";
+    std::variant<uint64_t> value{effecterValue};
+
+    MockdBusHandler9 handlerObj;
+    EXPECT_CALL(handlerObj, setDbusProperty(objPath, bootProgressProp,
+                                            bootProgressInf, value))
+        .Times(1);
+    auto rc = pldm::responder::platform_9::setNumericEffecterValueHandler<
+        MockdBusHandler9>(handlerObj, effecterId,
+                          PLDM_EFFECTER_DATA_SIZE_UINT32,
+                          reinterpret_cast<uint8_t*>(&effecterValue), 4);
+    ASSERT_EQ(rc, 0);
+}
+
+TEST(setNumericEffecterValueHandler, testBadRequest)
+{
+    Repo& pdrRepo = get("./pdr_jsons/state_effecter/good");
+    pdr::Entry e = pdrRepo.at(1);
+    pldm_numeric_effecter_valuer_pdr* pdr =
+        reinterpret_cast<pldm_numeric_effecter_valuer_pdr*>(e.data());
+    EXPECT_EQ(pdr->hdr.type, PLDM_NUMERIC_EFFECTER_PDR);
+
+    uint16_t effecterId = PLDM_TIMED_POWER_ON_STATE;
+    uint64_t effecterValue = 9876543210;
+    MockdBusHandler9 handlerObj;
+    auto rc = pldm::responder::platform_9::setNumericEffecterValueHandler<
+        MockdBusHandler9>(handlerObj, effecterId,
+                          PLDM_EFFECTER_DATA_SIZE_SINT32,
+                          reinterpret_cast<uint8_t*>(&effecterValue), 4);
+    ASSERT_EQ(rc, PLDM_ERROR_INVALID_DATA);
+
+    effecterValue = 123456789;
+    rc = pldm::responder::platform_9::setNumericEffecterValueHandler<
+        MockdBusHandler9>(handlerObj, effecterId,
+                          PLDM_EFFECTER_DATA_SIZE_UINT32,
+                          reinterpret_cast<uint8_t*>(&effecterValue), 4);
+    ASSERT_EQ(rc, PLDM_ERROR);
+}
