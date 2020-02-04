1d45d1c470d66ec415f0efdc4b8d6bdfa5435403
Pavithra Barithaya <pbaritha@in.ibm.com>
2020-02-04 02:46:15 -0600

    Validation of BIOS String attributes: JSON config file
    This is a schema to validate the String attributes. This
    validates the different properties of the object such as
    "attribute_name","string_type","minimum_string_length",
    "maximum_string_length","default_string_length",
    "default_string","dbus".This also validates the dbus
    properties such as "object_path","interface",
    "property_name","property_type".Validates thestring
    pattern and the required and optional properties.
    
    JSON schema supports some of the keywords based on the
    drafts.Consideration of two or more conditional statements
    without the anyOf,allOf,oneOf keyword is not possible.
    Based on the minimum string length, the default string length
    must be considered. Based on the string length the default string
    must be considered.
    
    Tested: The schema is tested against the bios json string
    attributes(string_attrs.json).
    When the property "attribute_name" which has a pattern to be
    followed was changed, an error message will be displayed as
     'Str_example3' does not match '^[a-z]{3}_[a-z]+[0-9]$'
    
    Change-Id: I1978d5f03bdc0c996729029bba6b5d72cf1a979d
    Signed-off-by: Pavithra Barithaya <pbaritha@in.ibm.com>

---
+++ libpldmresponder/examples/bios/main.py
@@ -0,0 +1,14 @@
+import jsonschema
+import json
+import sys
+with open('string_schema.json','r') as f:
+    schema=json.loads(f.read()) 
+input_file=sys.argv[1]
+with open(input_file,'r') as file:
+    json_data=json.loads(file.read())
+ try:
+    jsonschema.validate(json_data,schema)
+except jsonschema.ValidationError as e:
+    print(e.message)
+except jsonschema.SchemaError as e:
+    print (e)
