b8563a085d5b80c22410c1991aa3530549c083a7
Deepak Kodihalli <dkodihal@in.ibm.com>
2020-02-06 01:18:25 -0600

    WIP: libpldm PDR lib
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>
    Change-Id: I17acef32c3168deceab28965919b6d596d0e2b61

---
+++ libpldm/pdr.h
@@ -0,0 +1,50 @@
+#ifndef PDR_H
+#define PDR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+/** @struct pldm_pdr
+ *  structure representing handle to main PDR repository
+ */
+typedef struct pldm_pdr pldm_pdr;
+typedef struct pldm_pdr_record pldm_pdr_record;
+
+/* Common PDR access APIs */
+pldm_pdr *pldm_pdr_init();
+
+void pldm_pdr_destroy(pldm_pdr **repo);
+
+uint32_t pldm_pdr_get_record_count(const pldm_pdr *repo);
+
+uint32_t pldm_pdr_get_repo_size(const pldm_pdr *repo);
+
+uint32_t pldm_pdr_add(pldm_pdr *repo, const uint8_t *data, uint32_t size,
+		      uint32_t record_handle);
+
+uint32_t pldm_pdr_get_record_handle(const pldm_pdr *repo,
+				    const pldm_pdr_record *record);
+
+const pldm_pdr_record *pldm_pdr_get_record(const pldm_pdr *repo,
+					   uint32_t record_handle,
+					   uint8_t **data, uint32_t *size,
+					   uint32_t *next_record_handle);
+
+const pldm_pdr_record *
+pldm_pdr_get_next_record(const pldm_pdr *repo,
+			 const pldm_pdr_record *curr_record, uint8_t **data,
+			 uint32_t *size, uint32_t *next_record_handle);
+
+const pldm_pdr_record *
+pldm_pdr_find_record_by_type(const pldm_pdr *repo, uint8_t pdr_type,
+			     const pldm_pdr_record *curr_record, uint8_t **data,
+			     uint32_t *size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PDR_H */
