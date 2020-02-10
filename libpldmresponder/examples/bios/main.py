9467617dd3375f726821765dcc62cd3ae32cb4c1
Pavithra Barithaya <pbaritha@in.ibm.com>
2020-02-09 22:29:21 -0600

    Validation of BIOS Enum attributes:JSON config files
    A schema that validates the json file is written.
    This schema validates the enum attributes of BIOS
    related JSON config files. It validates different
    properties of the object such as "attribute_name",
    "possible_value","default_value","dbus".This also
    validates the dbus properties such as "object-path",
    "interface","property_type","property_name",
    "property_value".Validation of required and optional
    properties.Checks for the number of items present.
    
    There are few aspects that json does not validate:
    1.The value present in the default value property
      should be one among the possible value property.
    2.The type of the values present in the property
      values should be same as the type present in the
      property type.
    
    Tested: The schema is tested against the Bios enum
    attributes(enum_attrs.json). When the property
    "attribute name" was changed to an integer, an error
    message displayed- 2 is not of type 'string'
    
    Change-Id: Id06c413e9e776ec187c348bdaca6d8a589b1307d
    Signed-off-by: Pavithra Barithaya <pbaritha@in.ibm.com>

--- libpldmresponder/examples/bios/main.py
+++ libpldmresponder/examples/bios/main.py
@@ -1,15 +1,12 @@
 import jsonschema
 import json
+import sys
 
 with open('schema.json','r') as f:
-    schema=json.loads(f.read()) 
-		
-
-input_file=input('Enter the data_file:')
+    schema=json.loads(f.read())
+input_file=sys.argv[1]
 with open(input_file,'r') as file:
     json_data=json.loads(file.read())
- 
-
 try:
     jsonschema.validate(json_data,schema)
 except jsonschema.ValidationError as e:
