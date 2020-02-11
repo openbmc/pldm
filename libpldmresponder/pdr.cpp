64a16f982b98c8c72a8bdbe30c8717c80154c6d6
George Liu <liuxiwei@inspur.com>
2020-02-11 11:09:30 +0800

    pldm: Optimized parsing of PDR JSON files
    Move the pldm_state_effecter_pdr method of parse JSON to pdr11.hpp.
    Move the pldm_state_effecter_pdr test method to
    libpldmresponder_platform11_test.cpp.
    In test/libpldmresponder_pdr_state_effecter_test.cpp, since the GetPDR
    method is only for pldm_state_effecter_pdr test, should be renamed
    libpldmresponder_pdr11_test.cpp.
    
    In addition, pdr.hpp: 184 line: @tparam replaces @param.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I8428ed042fb0a90ecc448073f059f66360066306

--- libpldmresponder/pdr.cpp
+++ libpldmresponder/pdr.cpp
@@ -21,6 +21,36 @@ Repo& get(const std::string& dir)
     return repo;
 }
 
+Repo& getRepoFromType(const std::string& dir, const Type type)
+{
+    using namespace internal;
+    static IndexedRepo pdrRepo;
+    pdrRepo.makeEmpty();
+
+    Repo& repo = get(dir);
+    uint32_t recordHndl{};
+    pdr::Entry pdrEntry{};
+    while (true)
+    {
+        pdrEntry = repo.at(recordHndl);
+        pldm_pdr_hdr* header = reinterpret_cast<pldm_pdr_hdr*>(pdrEntry.data());
+        if (header->type == type)
+        {
+            pdrRepo.add(std::move(pdrEntry));
+        }
+
+        recordHndl = repo.getNextRecordHandle(recordHndl);
+        if (recordHndl)
+        {
+            continue;
+        }
+
+        break;
+    }
+
+    return pdrRepo;
+}
+
 } // namespace pdr
 } // namespace responder
 } // namespace pldm
