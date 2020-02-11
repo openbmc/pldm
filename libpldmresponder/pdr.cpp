2968c98565384baededecb78b78355f936ee2260
George Liu <liuxiwei@inspur.com>
2020-02-11 11:09:30 +0800

    pldm: Optimized parsing of PDR JSON files
    Move the pldm_state_effecter_pdr method of parse JSON to
    pdr_state_effecter.hpp.
    Move the pldm_state_effecter_pdr test method to
    libpldmresponder_platform_state_effecter_test.cpp.
    
    Tested with JSON files:
     https://gist.github.com/lxwinspur/2c3fd68cdb35e06480c4a5f7890e3a06#file-effecter_pdr-json.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I8428ed042fb0a90ecc448073f059f66360066306

--- libpldmresponder/pdr.cpp
+++ libpldmresponder/pdr.cpp
@@ -1,5 +1,7 @@
 #include "pdr.hpp"
 
+#include "pdr_state_effecter.hpp"
+
 namespace pldm
 {
 
@@ -16,116 +18,10 @@ void generate(const std::string& dir, RepoInterface& repo)
     // The lambda essentially would parse the platform specific PDR JSONs to
     // generate the PDR structures. This function iterates through the map to
     // invoke all lambdas, so that all PDR types can be created.
-    std::map<Type, std::function<void(const Json& json, RepoInterface& repo)>>
-        generators = {
-            {PLDM_STATE_EFFECTER_PDR,
-             [](const auto& json, RepoInterface& repo) {
-                 static const std::vector<Json> emptyList{};
-                 static const Json empty{};
-                 auto entries = json.value("entries", emptyList);
-                 for (const auto& e : entries)
-                 {
-                     size_t pdrSize = 0;
-                     auto effecters = e.value("effecters", emptyList);
-                     static const Json empty{};
-                     for (const auto& effecter : effecters)
-                     {
-                         auto set = effecter.value("set", empty);
-                         auto statesSize = set.value("size", 0);
-                         if (!statesSize)
-                         {
-                             std::cerr << "Malformed PDR JSON return "
-                                          "pdrEntry;- no state set "
-                                          "info, TYPE="
-                                       << PLDM_STATE_EFFECTER_PDR << "\n";
-                             throw InternalFailure();
-                         }
-                         pdrSize += sizeof(state_effecter_possible_states) -
-                                    sizeof(bitfield8_t) +
-                                    (sizeof(bitfield8_t) * statesSize);
-                     }
-                     pdrSize +=
-                         sizeof(pldm_state_effecter_pdr) - sizeof(uint8_t);
-
-                     std::vector<uint8_t> entry{};
-                     entry.resize(pdrSize);
-
-                     pldm_state_effecter_pdr* pdr =
-                         reinterpret_cast<pldm_state_effecter_pdr*>(
-                             entry.data());
-                     pdr->hdr.record_handle = 0;
-                     pdr->hdr.version = 1;
-                     pdr->hdr.type = PLDM_STATE_EFFECTER_PDR;
-                     pdr->hdr.record_change_num = 0;
-                     pdr->hdr.length = pdrSize - sizeof(pldm_pdr_hdr);
-
-                     pdr->terminus_handle = 0;
-                     pdr->effecter_id = effecter::nextId();
-                     pdr->entity_type = e.value("type", 0);
-                     pdr->entity_instance = e.value("instance", 0);
-                     pdr->container_id = e.value("container", 0);
-                     pdr->effecter_semantic_id = 0;
-                     pdr->effecter_init = PLDM_NO_INIT;
-                     pdr->has_description_pdr = false;
-                     pdr->composite_effecter_count = effecters.size();
-
-                     using namespace effecter::dbus_mapping;
-                     DbusObjs dbusObjs{};
-                     DbusValMaps dbusValMaps{};
-                     uint8_t* start = entry.data() +
-                                      sizeof(pldm_state_effecter_pdr) -
-                                      sizeof(uint8_t);
-                     for (const auto& effecter : effecters)
-                     {
-                         auto set = effecter.value("set", empty);
-                         state_effecter_possible_states* possibleStates =
-                             reinterpret_cast<state_effecter_possible_states*>(
-                                 start);
-                         possibleStates->state_set_id = set.value("id", 0);
-                         possibleStates->possible_states_size =
-                             set.value("size", 0);
-
-                         start += sizeof(possibleStates->state_set_id) +
-                                  sizeof(possibleStates->possible_states_size);
-                         static const std::vector<uint8_t> emptyStates{};
-                         PossibleValues stateValues;
-                         auto states = set.value("states", emptyStates);
-                         for (const auto& state : states)
-                         {
-                             auto index = state / 8;
-                             auto bit = state - (index * 8);
-                             bitfield8_t* bf =
-                                 reinterpret_cast<bitfield8_t*>(start + index);
-                             bf->byte |= 1 << bit;
-                             stateValues.emplace_back(std::move(state));
-                         }
-                         start += possibleStates->possible_states_size;
-
-                         auto dbusEntry = effecter.value("dbus", empty);
-                         auto objectPath = dbusEntry.value("path", "");
-                         auto interface = dbusEntry.value("interface", "");
-                         auto propertyName =
-                             dbusEntry.value("property_name", "");
-                         auto propertyType =
-                             dbusEntry.value("property_type", "");
-                         DBusMapping dbusMapping{objectPath, interface,
-                                                 propertyName, propertyType};
-                         dbusObjs.emplace_back(std::move(dbusMapping));
-
-                         DbusIdToValMap dbusIdToValMap;
-                         Json propValues = dbusEntry["property_values"];
-                         dbusIdToValMap = populateMapping(
-                             propertyType, propValues, stateValues);
-                         dbusValMaps.emplace_back(std::move(dbusIdToValMap));
-                     }
-                     addDbusObjs(pdr->effecter_id, std::move(dbusObjs));
-                     addDbusValMaps(pdr->effecter_id, std::move(dbusValMaps));
-                     PdrEntry pdrEntry{};
-                     pdrEntry.data = entry.data();
-                     pdrEntry.size = pdrSize;
-                     repo.addRecord(pdrEntry);
-                 }
-             }}};
+    std::map<Type, generateHandler> generateHandlers = {
+        {PLDM_STATE_EFFECTER_PDR, [](const auto& json, RepoInterface& repo) {
+             pdr_state_effecter::generateStateEffecterHandler(json, repo);
+         }}};
 
     Type pdrType{};
     static const Json empty{};
@@ -140,7 +36,7 @@ void generate(const std::string& dir, RepoInterface& repo)
                 for (const auto& effecter : effecterPDRs)
                 {
                     pdrType = effecter.value("pdrType", 0);
-                    generators.at(pdrType)(effecter, repo);
+                    generateHandlers.at(pdrType)(effecter, repo);
                 }
             }
         }
