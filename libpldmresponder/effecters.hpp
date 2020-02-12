7f6460aba95807a52fedb3dbe4ecde3ad20fae9d
George Liu <liuxiwei@inspur.com>
2020-02-12 16:05:32 +0800

    pdr: Update Dbus mapping structure
    For the most PDR types, the Dbus object structure(paths, interfaces, and
    properties) should be defined in the JSON file instead of hard-coded.
    
    Tested with JSON files:
     https://gist.github.com/lxwinspur/2c3fd68cdb35e06480c4a5f7890e3a06#file-effecter_pdr-json.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: Ifc0cd1540b08e9b73e03d99d71a0980ef6353e72

--- libpldmresponder/effecters.hpp
+++ libpldmresponder/effecters.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "pdr_utils.hpp"
+#include "utils.hpp"
+
 #include <stdint.h>
 
 #include <string>
@@ -13,6 +16,8 @@ namespace responder
 
 namespace effecter
 {
+using namespace pldm::utils;
+using namespace pldm::responder::pdr_utils;
 
 using Id = uint16_t;
 
@@ -24,22 +29,40 @@ Id nextId();
 
 namespace dbus_mapping
 {
-using Paths = std::vector<std::string>;
+
+using DbusObjs = std::vector<DBusMapping>;
+using DbusValMaps = std::vector<DbusIdToValMap>;
 
 /** @brief Add an effecter id -> D-Bus objects mapping
  *
  *  @param[in] id - effecter id
- *  @param[in] paths - list of D-Bus object paths
+ *  @param[in] dbusObj - list of D-Bus object structure
  */
-void add(Id id, Paths&& paths);
+void addDbusObjs(Id id, DbusObjs&& dbusObjs);
 
 /** @brief Retrieve an effecter id -> D-Bus objects mapping
  *
  *  @param[in] id - effecter id
  *
- *  @return Paths - list of D-Bus object paths
+ *  @return DbusObjs - list of D-Bus object structure
  */
-Paths get(Id id);
+const DbusObjs& getDbusObjs(Id id);
+
+/** @brief Add an effecter id -> D-Bus value mapping
+ *
+ *  @param[in] id - effecter id
+ *  @param[in] dbusValMap - list of DBus property value to attribute value
+ */
+void addDbusValMaps(Id id, DbusValMaps&& dbusValMap);
+
+/** @brief Retrieve an effecter id -> D-Bus value mapping
+ *
+ *  @param[in] id - effecter id
+ *
+ *  @return DbusValMaps - list of DBus property value to attribute value
+ */
+const DbusValMaps& getDbusValMaps(Id id);
+
 } // namespace dbus_mapping
 
 } // namespace effecter
