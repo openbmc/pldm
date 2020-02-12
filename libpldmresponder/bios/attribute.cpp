abf7cc2af8efe35d974d9d4efc42000ed249f22e
John Wang <wangzqbj@inspur.com>
2020-02-12 17:02:06 +0800

    bios: Implement BIOSAttribute
    bios tables are built based on json entry. when a bios attribute
    is set by pldm, the corresponding dbus backend should be synchronized.
    
    then, the BIOSAttribute class is abstracted, and it provides the
    following two interfaces
    
    1. constructEntry Construct the entry of the attribute/attribute-value table
    2. setAttrValueOnDbus Update the corresponding dbus backend
    
    Signed-off-by: John Wang <wangzqbj@inspur.com>
    Change-Id: I11e304c470360ca5fde23e4969494bb03de475c0

---
+++ libpldmresponder/bios/attribute.cpp
@@ -0,0 +1,31 @@
+#include "attribute.hpp"
+
+#include "utils.hpp"
+
+#include <iostream>
+#include <variant>
+
+namespace pldm
+{
+namespace responder
+{
+namespace bios
+{
+
+BIOSAttribute::BIOSAttribute(const Json& entry) :
+    name(entry.at("attribute_name")), readOnly(!entry.contains("dbus"))
+{
+    if (!readOnly)
+    {
+        std::string objectPath = entry.at("dbus").at("object_path");
+        std::string interface = entry.at("dbus").at("interface");
+        std::string propertyName = entry.at("dbus").at("property_name");
+        std::string propertyType = entry.at("dbus").at("property_type");
+
+        dBusMap = {objectPath, interface, propertyName, propertyType};
+    }
+}
+
+} // namespace bios
+} // namespace responder
+} // namespace pldm
