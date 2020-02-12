984c556f6cc56d0155886ef3bd89591f6a4e8c04
John Wang <wangzqbj@inspur.com>
2020-02-12 17:02:06 +0800

    Add bios_attribute
    Signed-off-by: John Wang <wangzqbj@inspur.com>
    Change-Id: I11e304c470360ca5fde23e4969494bb03de475c0

---
+++ libpldmresponder/bios/bios_attribute.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "bios_table.hpp"
+
+#include <cstdint>
+#include <filesystem>
+#include <memory>
+#include <nlohmann/json.hpp>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include "bios_table.h"
+
+namespace pldm
+{
+namespace responder
+{
+namespace bios
+{
+namespace wip
+{
+
+using Json = nlohmann::json;
+using namespace pldm::responder::bios;
+
+struct DBusMapping
+{
+    std::string objectPath;   //!< D-Bus object path
+    std::string interface;    //!< D-Bus interface
+    std::string propertyName; //!< D-Bus property name
+    std::string propertyType; //!< D-Bus property type
+};
+
+class BIOSAttribute
+{
+  public:
+    BIOSAttribute(const Json& entry);
+
+    virtual ~BIOSAttribute()
+    {
+    }
+
+    virtual int
+        setAttrValueOnDbus(const pldm_bios_attr_val_table_entry* attrValueEntry,
+                           const pldm_bios_attr_table_entry* attrEntry,
+                           const BIOSStringTable& stringTable) = 0;
+
+    virtual void constructEntry(const BIOSStringTable& stringTable,
+                                Table& attrTable, Table& attrValueTable) = 0;
+
+    const std::string name;
+    const bool readOnly;
+
+  protected:
+    std::optional<DBusMapping> dBusMap;
+};
+
+} // namespace wip
+} // namespace bios
+} // namespace responder
+} // namespace pldm
