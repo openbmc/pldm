70a0aa55426923400004e96851255ece00b75e98
George Liu <liuxiwei@inspur.com>
2020-02-12 16:05:32 +0800

    pdr: Update Dbus mapping structure
    For the most PDR types, the Dbus object structure(paths, interfaces, and
    properties) should be defined in the JSON file instead of hard-coded.
    
    Tested with JSON files:
     https://gist.github.com/lxwinspur/2c3fd68cdb35e06480c4a5f7890e3a06#file-effecter_pdr-json.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: Ifc0cd1540b08e9b73e03d99d71a0980ef6353e72

--- libpldmresponder/pdr.cpp
+++ libpldmresponder/pdr.cpp
@@ -70,7 +70,8 @@ void generate(const std::string& dir, RepoInterface& repo)
                      pdr->composite_effecter_count = effecters.size();
 
                      using namespace effecter::dbus_mapping;
-                     Paths paths{};
+                     DbusObjs dbusObjs{};
+                     DbusValMaps dbusValMaps{};
                      uint8_t* start = entry.data() +
                                       sizeof(pldm_state_effecter_pdr) -
                                       sizeof(uint8_t);
@@ -87,6 +88,7 @@ void generate(const std::string& dir, RepoInterface& repo)
                          start += sizeof(possibleStates->state_set_id) +
                                   sizeof(possibleStates->possible_states_size);
                          static const std::vector<uint8_t> emptyStates{};
+                         PossibleValues stateValues;
                          auto states = set.value("states", emptyStates);
                          for (const auto& state : states)
                          {
@@ -95,13 +97,29 @@ void generate(const std::string& dir, RepoInterface& repo)
                              bitfield8_t* bf =
                                  reinterpret_cast<bitfield8_t*>(start + index);
                              bf->byte |= 1 << bit;
+                             stateValues.emplace_back(std::move(state));
                          }
                          start += possibleStates->possible_states_size;
 
-                         auto dbus = effecter.value("dbus", empty);
-                         paths.emplace_back(std::move(dbus));
+                         auto dbusEntry = effecter.value("dbus", empty);
+                         auto objectPath = dbusEntry.value("path", "");
+                         auto interface = dbusEntry.value("interface", "");
+                         auto propertyName =
+                             dbusEntry.value("property_name", "");
+                         auto propertyType =
+                             dbusEntry.value("property_type", "");
+                         DBusMapping dbusMapping{objectPath, interface,
+                                                 propertyName, propertyType};
+                         dbusObjs.emplace_back(std::move(dbusMapping));
+
+                         DbusIdToValMap dbusIdToValMap;
+                         Json propValues = dbusEntry["property_values"];
+                         dbusIdToValMap = populateMapping(
+                             propertyType, propValues, stateValues);
+                         dbusValMaps.emplace_back(std::move(dbusIdToValMap));
                      }
-                     add(pdr->effecter_id, std::move(paths));
+                     addDbusObjs(pdr->effecter_id, std::move(dbusObjs));
+                     addDbusValMaps(pdr->effecter_id, std::move(dbusValMaps));
                      PdrEntry pdrEntry{};
                      pdrEntry.data = entry.data();
                      pdrEntry.size = pdrSize;
@@ -110,6 +128,7 @@ void generate(const std::string& dir, RepoInterface& repo)
              }}};
 
     Type pdrType{};
+    static const Json empty{};
     for (const auto& dirEntry : fs::directory_iterator(dir))
     {
         try
@@ -117,8 +136,12 @@ void generate(const std::string& dir, RepoInterface& repo)
             auto json = readJson(dirEntry.path().string());
             if (!json.empty())
             {
-                pdrType = json.value("pdrType", 0);
-                generators.at(pdrType)(json, repo);
+                auto effecterPDRs = json.value("effecterPDRs", empty);
+                for (const auto& effecter : effecterPDRs)
+                {
+                    pdrType = effecter.value("pdrType", 0);
+                    generators.at(pdrType)(effecter, repo);
+                }
             }
         }
         catch (const InternalFailure& e)
