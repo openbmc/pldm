e3573a44b21cf9f1bfdc22d8220ecbabe5594a63
John Wang <wangzqbj@inspur.com>
2020-02-13 09:43:24 +0800

    bios: Implement BIOSIntegerAttribute
    Implement BIOSIntegerAttribute, most of the code is copied from
    bios/bios_parser.cpp.
    
    Implement SetAttrValueOnDbus and constructEntry for integer attribute
    
    Signed-off-by: John Wang <wangzqbj@inspur.com>
    Change-Id: I57d5b5dbcb74f9a404f5133426208f4c6851dea2

--- libpldmresponder/bios_config.cpp
+++ libpldmresponder/bios_config.cpp
@@ -1,5 +1,6 @@
 #include "bios_config.hpp"
 
+#include "bios_integer_attribute.hpp"
 #include "bios_string_attribute.hpp"
 
 #include <fstream>
@@ -64,6 +65,9 @@ void BIOSConfig::buildAttrTables(const Table& stringTable)
     BIOSStringTable biosStringTable(stringTable);
     load(jsonDir / stringJsonFile, [&biosStringTable, this](const Json& entry) {
         constructAttributes<BIOSStringAttribute>(entry);
+    });
+    load(jsonDir / stringJsonFile, [&biosStringTable, this](const Json& entry) {
+        constructAttributes<BIOSIntegerAttribute>(entry);
     });
 
     if (biosAttributes.empty())
