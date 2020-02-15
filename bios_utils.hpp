bc5cb4f8c26004065b7dcfb72a5dfbad6d31b671
John Wang <wangzqbj@inspur.com>
2020-02-15 16:10:25 +0800

    pldmtool: Implement GetBIOSTable for attr/attrValue Table
    Implement GetBIOSTable for the attribute table and attribute
    value table.
    
    Implement an iterator for bios table, it may be used in
    libpldmresponder, at least in unit tests. So it's placed
    in the utils namespace.
    
    Signed-off-by: John Wang <wangzqbj@inspur.com>
    Change-Id: Iaf79af8267fc0afc5c53704537dd7b6dfc36a6a7

---
+++ bios_utils.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <cstring>
+#include <memory>
+#include <type_traits>
+#include <vector>
+
+#include "libpldm/bios_table.h"
+
+namespace pldm
+{
+namespace bios
+{
+namespace utils
+{
+
+using Table = std::vector<uint8_t>;
+
+template <pldm_bios_table_types N>
+class BIOSTableIter
+{
+  public:
+    struct EndSentinel
+    {
+    };
+    class iterator
+    {
+      public:
+        using T = typename std::conditional<
+            N == PLDM_BIOS_STRING_TABLE, pldm_bios_string_table_entry,
+            typename std::conditional<
+                N == PLDM_BIOS_ATTR_TABLE, pldm_bios_attr_table_entry,
+                typename std::conditional<N == PLDM_BIOS_ATTR_VAL_TABLE,
+                                 pldm_bios_attr_val_table_entry, void>::type>::type>::type;
+
+        explicit iterator(const void* data, size_t length) noexcept :
+            iter(pldm_bios_table_iter_create(
+                     data, length, static_cast<pldm_bios_table_types>(N)),
+                 pldm_bios_table_iter_free)
+        {
+        }
+
+        const T* operator*()
+        {
+            return (const T*)pldm_bios_table_iter_value(iter.get());
+        }
+
+        iterator& operator++()
+        {
+            pldm_bios_table_iter_next(iter.get());
+            return *this;
+        }
+
+        bool operator==(const EndSentinel&)
+        {
+            return pldm_bios_table_iter_is_end(iter.get());
+        }
+
+        bool operator!=(const EndSentinel& endSentinel)
+        {
+            return !operator==(endSentinel);
+        }
+
+      private:
+        std::unique_ptr<pldm_bios_table_iter,
+                        decltype(&pldm_bios_table_iter_free)>
+            iter;
+    };
+
+    BIOSTableIter(const void* data, size_t length) noexcept :
+        tableData(data), tableSize(length)
+    {
+    }
+
+    iterator begin()
+    {
+        return iterator(tableData, tableSize);
+    }
+
+    EndSentinel end()
+    {
+        return {};
+    }
+
+  private:
+    const void* tableData;
+    size_t tableSize;
+};
+
+} // namespace utils
+} // namespace bios
+} // namespace pldm
