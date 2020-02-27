d34e24df4314dd468087539eea7e2fdcd428ae8e
John Wang <wangzqbj@inspur.com>
2020-02-27 16:46:05 +0800

    bios: Implement BIOSConfig
    Load the parsed json configs into memory.
    And provid APIs to get/set on bios tables.
    
    Signed-off-by: John Wang <wangzqbj@inspur.com>
    Change-Id: Ida1fedc923d31afc61dd2d4aec70d81bb6a90ae9

--- libpldmresponder/bios_table.hpp
+++ libpldmresponder/bios_table.hpp
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 #include <filesystem>
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -68,6 +69,12 @@ class BIOSTable
      */
     void load(Response& response) const;
 
+    /** @brief Append Pad and Checksum
+     *
+     *  @param[in,out] table - table to be appended with pad and checksum
+     */
+    static void appendPadAndChecksum(Table& table);
+
   private:
     // file storing PLDM BIOS table
     fs::path filePath;
@@ -132,6 +139,15 @@ class BIOSStringTable : public BIOSStringTableInterface
      */
     static std::string decodeString(const pldm_bios_string_table_entry* entry);
 
+    /** @brief construct entry of string table at the end of the given
+     *         table
+     *  @param[in,out] table - The given table
+     *  @param[in] str - string itself
+     *  @return pointer to the constructed entry
+     */
+    static const pldm_bios_string_table_entry*
+        constructEntry(Table& table, const std::string& str);
+
   private:
     Table stringTable;
 };
@@ -157,6 +173,14 @@ class BIOSAttrTable
      *  @return Attribute table header
      */
     static TableHeader decodeHeader(const pldm_bios_attr_table_entry* entry);
+
+    /** @brief Find attribute entry by handle
+     *  @param[in] table - attribute table
+     *  @param[in] handle - attribute handle
+     *  @return Pointer to the attribute table entry
+     */
+    static const pldm_bios_attr_table_entry* findByHandle(const Table& table,
+                                                          uint16_t handle);
 
     /** @struct StringField
      *  @brief String field of attribute table
@@ -228,6 +252,9 @@ class BIOSAttrValTable
     static const pldm_bios_attr_val_table_entry*
         constructStringEntry(Table& table, uint16_t attrHandle,
                              uint8_t attrType, const std::string& str);
+
+    static std::optional<Table> updateTable(Table& table, const void* entry,
+                                            size_t size);
 };
 
 } // namespace bios
