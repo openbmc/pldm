8a9301c5060ad813f675683ef78a53035f4d9d6c
John Wang <wangzqbj@inspur.com>
2020-02-27 16:41:44 +0800

    bios: Implement BIOSStringAttribute
    Implement BIOSStringAttribute, most of the code is copied from
    bios/bios_parser.cpp.
    
    Implement SetAttrValueOnDbus and constructEntry for string attribute
    
    Signed-off-by: John Wang <wangzqbj@inspur.com>
    Change-Id: Ic7c6b35d32738d698d7649f97cb7843606b8a2ba

---
+++ test/libpldmresponder_bios_string_attribute_test.cpp
@@ -0,0 +1,132 @@
+#include "libpldmresponder/bios_string_attribute.hpp"
+
+#include <memory>
+#include <nlohmann/json.hpp>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace pldm::responder::bios;
+
+class TestBIOSStringAttribute : public ::testing::Test
+{
+  public:
+    const std::string&
+        getDefaultString(const BIOSStringAttribute& biosStringAttribute)
+    {
+        return biosStringAttribute.defaultString;
+    }
+
+    uint8_t getStringType(const BIOSStringAttribute& biosStringAttribute)
+    {
+        return biosStringAttribute.stringType;
+    }
+    uint16_t getMinLength(const BIOSStringAttribute& biosStringAttribute)
+    {
+        return biosStringAttribute.minStringLength;
+    }
+    uint16_t getMaxLength(const BIOSStringAttribute& biosStringAttribute)
+    {
+        return biosStringAttribute.maxStringLength;
+    }
+    uint16_t getDefLength(const BIOSStringAttribute& biosStringAttribute)
+    {
+        return biosStringAttribute.defaultStringLength;
+    }
+};
+
+TEST_F(TestBIOSStringAttribute, CtorTest)
+{
+    auto jsonStringReadOnly = R"(  {
+            "attribute_name" : "str_example3",
+            "string_type" : "ASCII",
+            "minimum_string_length" : 1,
+            "maximum_string_length" : 100,
+            "default_string_length" : 2,
+            "default_string" : "ef"
+        })"_json;
+    BIOSStringAttribute stringReadOnly{jsonStringReadOnly, nullptr};
+    EXPECT_EQ(stringReadOnly.name, "str_example3");
+    EXPECT_TRUE(stringReadOnly.readOnly);
+    EXPECT_EQ(getStringType(stringReadOnly), BIOSStringAttribute::ASCII);
+    EXPECT_EQ(getMinLength(stringReadOnly), 1);
+    EXPECT_EQ(getMaxLength(stringReadOnly), 100);
+    EXPECT_EQ(getDefLength(stringReadOnly), 2);
+    EXPECT_EQ(getDefaultString(stringReadOnly), "ef");
+
+    auto jsonStringReadOnlyError = R"(  {
+            "attribute_name" : "str_example3",
+            "string_type" : "ASCII",
+            "minimum_string_length" : 1,
+            "maximum_string_length" : 100,
+            "default_string" : "ef"
+        })"_json; // missing default_string_length
+
+    EXPECT_THROW((BIOSStringAttribute{jsonStringReadOnlyError, nullptr}),
+                 Json::exception);
+
+    auto jsonStringReadWrite = R"({
+            "attribute_name" : "str_example1",
+            "string_type" : "ASCII",
+            "minimum_string_length" : 1,
+            "maximum_string_length" : 100,
+            "default_string_length" : 3,
+            "default_string" : "abc",
+            "dbus" : {
+                "object_path" : "/xyz/abc/def",
+                "interface" : "xyz.openbmc_project.str_example1.value",
+                "property_name" : "Str_example1",
+                "property_type" : "string"
+            }
+        })"_json;
+    BIOSStringAttribute stringReadWrite{jsonStringReadWrite, nullptr};
+
+    EXPECT_EQ(stringReadWrite.name, "str_example1");
+    EXPECT_TRUE(!stringReadWrite.readOnly);
+}
+
+class MockBIOSStringTable : public BIOSStringTable
+{
+  public:
+    MockBIOSStringTable() : BIOSStringTable({})
+    {
+    }
+    MOCK_CONST_METHOD1(findString, std::string(uint16_t));
+    MOCK_CONST_METHOD1(findHandle, uint16_t(const std::string&));
+};
+
+using ::testing::_;
+using ::testing::Return;
+
+TEST_F(TestBIOSStringAttribute, ConstructEntry)
+{
+    auto jsonStringReadWrite = R"({
+            "attribute_name" : "str_example1",
+            "string_type" : "ASCII",
+            "minimum_string_length" : 1,
+            "maximum_string_length" : 100,
+            "default_string_length" : 3,
+            "default_string" : "abc",
+            "dbus" : {
+                "object_path" : "/xyz/abc/def",
+                "interface" : "xyz.openbmc_project.str_example1.value",
+                "property_name" : "Str_example1",
+                "property_type" : "string"
+            }
+        })"_json;
+    BIOSStringAttribute stringReadWrite{jsonStringReadWrite, nullptr};
+
+    MockBIOSStringTable biosStringTable;
+
+    ON_CALL(biosStringTable, findHandle(_)).WillByDefault(Return(5));
+    EXPECT_CALL(biosStringTable, findHandle(_)).WillOnce(Return(5));
+
+    Table attrEntry, attrValueEntry;
+
+    stringReadWrite.constructEntry(biosStringTable, attrEntry, attrValueEntry);
+
+    for (auto v : attrEntry)
+    {
+        std::cerr << (int)v << std::endl;
+    }
+}
