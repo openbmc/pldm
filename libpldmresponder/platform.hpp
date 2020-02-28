7f1741abaac1800892b660202d7d7974356ceb7e
George Liu <liuxiwei@inspur.com>
2020-02-28 20:20:06 +0800

    pldm: Move the updateDbusProperty method to the common utils
    The updateDbusProperty method defined
    https://github.com/openbmc/pldm/blob/master/libpldmresponder/bios_parser.cpp#L177
    and need to moved pldm/utils.hpp.
    Add the updateDbusProperty method to Mock test class and remove the
    setDbusProperty method in Mock test class.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: Ib1c339af41df7f625d6976c192a3b72ce06b04a2
    Signed-off-by: George Liu <liuxiwei@inspur.com>

--- libpldmresponder/platform.hpp
+++ libpldmresponder/platform.hpp
@@ -68,6 +68,7 @@ class Handler : public CmdHandler
         const std::vector<set_effecter_state_field>& stateField)
     {
         using namespace pldm::responder::pdr;
+        using namespace pldm::utils;
         using namespace std::string_literals;
         using DBusProperty = std::variant<std::string, bool>;
         using StateSetId = uint16_t;
@@ -156,23 +157,24 @@ class Handler : public CmdHandler
                                    << "\n";
                          return PLDM_ERROR_INVALID_DATA;
                      }
-                     auto dbusProp = "OperatingSystemState";
-                     std::variant<std::string> value{
+                     const PropertyValue value{
                          std::get<std::string>(iter->second)};
-                     auto dbusInterface =
-                         "xyz.openbmc_project.State.OperatingSystem.Status";
+                     DBusMapping dbusMapping{
+                         objPath,
+                         "xyz.openbmc_project.State.OperatingSystem.Status",
+                         "OperatingSystemState", "string"};
                      try
                      {
-                         dBusIntf.setDbusProperty(objPath.c_str(), dbusProp,
-                                                  dbusInterface, value);
+                         dBusIntf.updateDbusProperty(dbusMapping, value);
                      }
                      catch (const std::exception& e)
                      {
                          std::cerr
                              << "Error setting property, ERROR=" << e.what()
-                             << " PROPERTY=" << dbusProp
-                             << " INTERFACE=" << dbusInterface
-                             << " PATH=" << objPath.c_str() << "\n";
+                             << " PROPERTY=" << dbusMapping.propertyName
+                             << " INTERFACE="
+                             << dbusMapping.interface << " PATH="
+                             << dbusMapping.objectPath << "\n";
                          return PLDM_ERROR;
                      }
                      return PLDM_SUCCESS;
@@ -201,22 +203,23 @@ class Handler : public CmdHandler
                                    << "\n";
                          return PLDM_ERROR_INVALID_DATA;
                      }
-                     auto dbusProp = "RequestedPowerTransition";
-                     std::variant<std::string> value{
+                     const PropertyValue value{
                          std::get<std::string>(iter->second)};
-                     auto dbusInterface = "xyz.openbmc_project.State.Chassis";
+                     DBusMapping dbusMapping{
+                         objPath, "xyz.openbmc_project.State.Chassis",
+                         "RequestedPowerTransition", "string"};
                      try
                      {
-                         dBusIntf.setDbusProperty(objPath.c_str(), dbusProp,
-                                                  dbusInterface, value);
+                         dBusIntf.updateDbusProperty(dbusMapping, value);
                      }
                      catch (const std::exception& e)
                      {
                          std::cerr
                              << "Error setting property, ERROR=" << e.what()
-                             << " PROPERTY=" << dbusProp
-                             << " INTERFACE=" << dbusInterface
-                             << " PATH=" << objPath.c_str() << "\n";
+                             << " PROPERTY=" << dbusMapping.propertyName
+                             << " INTERFACE="
+                             << dbusMapping.interface << " PATH="
+                             << dbusMapping.objectPath << "\n";
                          return PLDM_ERROR;
                      }
                      return PLDM_SUCCESS;
---
+++ test/mocked_utils.hpp
@@ -0,0 +1,15 @@
+#include "utils.hpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace pldm::utils;
+
+class MockdBusHandler : public DBusHandler
+{
+  public:
+    virtual ~MockdBusHandler() = default;
+
+    MOCK_CONST_METHOD2(updateDbusProperty,
+                       void(const DBusMapping&, const PropertyValue&));
+};
