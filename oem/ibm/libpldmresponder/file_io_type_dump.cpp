e30139d2a8f5443b3767b9ac3ecb75769f023085
Deepak Kodihalli <dkodihal@in.ibm.com>
2020-03-05 11:14:05 -0600

    oem-ibm: inband dump transfer
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>
    Change-Id: Ieb6afac6d596093b1963f1761f694419afacf9e2

--- oem/ibm/libpldmresponder/file_io_type_dump.cpp
+++ oem/ibm/libpldmresponder/file_io_type_dump.cpp
@@ -21,6 +21,8 @@ namespace pldm
 namespace responder
 {
 
+static constexpr auto nbdInterface = "/dev/nbd1";
+
 int DumpHandler::fd = -1;
 
 int DumpHandler::newFileAvailable(uint64_t length)
@@ -59,7 +61,6 @@ int DumpHandler::newFileAvailable(uint64_t length)
 int DumpHandler::writeFromMemory(uint32_t offset, uint32_t length,
                                  uint64_t address)
 {
-    static constexpr auto nbdInterface = "/dev/nbd1";
     int flags = O_WRONLY | O_CREAT | O_TRUNC | O_LARGEFILE;
 
     if (DumpHandler::fd == -1)
@@ -80,34 +81,40 @@ int DumpHandler::fileAck(uint8_t /*fileStatus*/)
     if (DumpHandler::fd >= 0)
     {
         static constexpr auto dumpObjPath = "/xyz/openbmc_project/dump";
-        static constexpr auto MAPPER_BUSNAME = "xyz.openbmc_project.ObjectMapper";
-        static constexpr auto MAPPER_PATH = "/xyz/openbmc_project/object_mapper";
-        static constexpr auto MAPPER_INTERFACE = "xyz.openbmc_project.ObjectMapper";
-        static constexpr auto systemDumpEntry = "xyz.openbmc_project.Dump.Entry.System";
+        static constexpr auto MAPPER_BUSNAME =
+            "xyz.openbmc_project.ObjectMapper";
+        static constexpr auto MAPPER_PATH =
+            "/xyz/openbmc_project/object_mapper";
+        static constexpr auto MAPPER_INTERFACE =
+            "xyz.openbmc_project.ObjectMapper";
+        static constexpr auto systemDumpEntry =
+            "xyz.openbmc_project.Dump.Entry.System";
         static constexpr auto dumpEntry = "xyz.openbmc_project.Dump.Entry";
         auto& bus = pldm::utils::DBusHandler::getBus();
 
         try
         {
             std::vector<std::string> paths;
-            auto method = bus.new_method_call(MAPPER_BUSNAME, MAPPER_PATH,
-                                              MAPPER_INTERFACE, "GetSubTreePaths");
+            auto method =
+                bus.new_method_call(MAPPER_BUSNAME, MAPPER_PATH,
+                                    MAPPER_INTERFACE, "GetSubTreePaths");
             method.append(dumpObjPath);
             method.append(0);
             method.append(std::vector<std::string>({systemDumpEntry}));
             auto reply = bus.call(method);
             reply.read(paths);
-            for (auto path: paths)
+            for (auto path : paths)
             {
                 std::cout << "found dump path " << path << std::endl;
-                uint32_t dumpId = pldm::utils::DBusHandler().getDbusProperty<uint32_t>(
-                    path.c_str(), "SourceDumpId", systemDumpEntry);
+                uint32_t dumpId =
+                    pldm::utils::DBusHandler().getDbusProperty<uint32_t>(
+                        path.c_str(), "SourceDumpId", systemDumpEntry);
                 if (dumpId == fileHandle)
                 {
                     std::cout << "found dump id " << dumpId << std::endl;
                     std::variant<bool> value{true};
-                    pldm::utils::DBusHandler().setDbusProperty(path.c_str(), "Offloaded",
-                                                       dumpEntry, value);
+                    pldm::utils::DBusHandler().setDbusProperty(
+                        path.c_str(), "Offloaded", dumpEntry, value);
                     close(DumpHandler::fd);
                     DumpHandler::fd = -1;
                     return PLDM_SUCCESS;
@@ -122,6 +129,39 @@ int DumpHandler::fileAck(uint8_t /*fileStatus*/)
     }
 
     return PLDM_ERROR;
+}
+
+int DumpHandler::write(const char* buffer, uint32_t offset, uint32_t& length)
+{
+    int flags = O_WRONLY | O_CREAT | O_TRUNC | O_LARGEFILE;
+    if (DumpHandler::fd == -1)
+    {
+        DumpHandler::fd = open(nbdInterface, flags);
+        if (DumpHandler::fd == -1)
+        {
+            std::cerr << "NBD file does not exist at " << nbdInterface
+                      << " ERROR=" << errno << "\n";
+            return PLDM_ERROR;
+        }
+    }
+
+    int rc = lseek(DumpHandler::fd, offset, SEEK_SET);
+    if (rc == -1)
+    {
+        std::cerr << "lseek failed, ERROR=" << errno << ", OFFSET=" << offset
+                  << "\n";
+        return PLDM_ERROR;
+    }
+    rc = ::write(DumpHandler::fd, buffer, length);
+    if (rc == -1)
+    {
+        std::cerr << "file write failed, ERROR=" << errno
+                  << ", LENGTH=" << length << ", OFFSET=" << offset << "\n";
+        return PLDM_ERROR;
+    }
+    length = rc;
+
+    return PLDM_SUCCESS;
 }
 
 } // namespace responder
