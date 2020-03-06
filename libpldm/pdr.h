974d1c64fe4a0fd665070090b2cbe708f84bf4f5
Deepak Kodihalli <dkodihal@in.ibm.com>
2020-03-06 06:56:16 -0600

    libpldm: add API to create entity association PDR
    This commit adds an API to make entity association PDR records out of an
    input entity association tree (implemented in the previous commit).
    
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>
    Change-Id: Ida7fa5a3040ee14f46da98fff021b6cb8e40b4c4

--- libpldm/pdr.h
+++ libpldm/pdr.h
@@ -188,7 +188,7 @@ typedef struct pldm_entity {
 
 enum entity_association_containment_type {
 	PLDM_ENTITY_ASSOCIAION_PHYSICAL = 0x0,
-	PLDM_ENTITY_ASSOCIAION_LOGICAL = 0x1
+	PLDM_ENTITY_ASSOCIAION_LOGICAL = 0x1,
 };
 
 /** @struct pldm_entity_association_tree
@@ -246,6 +246,24 @@ void pldm_entity_association_tree_destroy(pldm_entity_association_tree *tree);
  *  @return bool true if node is a parent, false otherwise
  */
 bool pldm_entity_is_node_parent(pldm_entity_node *node);
+
+/** @brief Convert entity association tree to PDR
+ *
+ *  @param[in] tree - opaque pointer to entity association tree
+ *  @param[in] repo - PDR repo where entity association records should be added
+ */
+void pldm_entity_association_pdr_add(pldm_entity_association_tree *tree,
+				     pldm_pdr *repo);
+
+/** @brief Get number of children of entity
+ *
+ *  @param[in] node - opaque pointer acting as a handle to an entity node
+ *  @param[in] association_type - relation type filter : logical or physical
+ *
+ *  @return uint8_t number of children
+ */
+uint8_t pldm_entity_get_num_children(pldm_entity_node *node,
+				     uint8_t association_type);
 
 #ifdef __cplusplus
 }
