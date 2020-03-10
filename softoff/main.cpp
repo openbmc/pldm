c67df5c6a0a89b0f57c7662a20640d28487d6ccb
George Liu <liuxiwei@inspur.com>
2020-03-10 08:25:21 +0800

    pldm : pldm host soft off:set soft off state effecter to host
    Since the phosphor-softpoweroff is too tied up to IPMI,
    we need to implement a new pldm host soft off requester
    application in parallel in the pldm repo.
    
    phosphor-softpoweroff will send a setStateEffecterStates
    command to host, then the host will soft off.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I47534bb25c976a0004a1cd9f07d7590e605ce243

---
+++ softoff/main.cpp
@@ -0,0 +1,43 @@
+#include "config.h"
+
+#include "softoff.hpp"
+
+#include <unistd.h>
+
+#include <array>
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+#include "libpldm/platform.h"
+#include "libpldm/requester/pldm.h"
+
+#include <sdbusplus/bus.hpp>
+#include <sdeventplus/event.hpp>
+
+
+int main()
+{
+    using namespace phosphor::logging;
+    using namespace pldm;
+
+    // Get a default event loop
+    auto event = sdeventplus::Event::get_default();
+
+    // Get a handle to system dbus.
+    auto bus = sdbusplus::bus::new_default();
+
+    // Attach the bus to sd_event to service user requests
+    bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
+
+    // pldm::PldmSoftPowerOff  SoftPowerOff;
+    pldm::PldmSoftPowerOff softPower = pldm::PldmSoftPowerOff(bus, event.get());
+
+    return 0;
+}
+
