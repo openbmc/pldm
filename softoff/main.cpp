936c175d16c3190f85f06113bcf15172941db517
Chicago Duan <duanzhijia01@inspur.com>
2020-04-16 19:54:56 +0800

    pldm : Pldm host soft off : Wait the host soft off responds and set chassis off
    After start a timer,pldm-softpowerroff application need
    Subscribe to a event Msg, which is pldm host soft off property transition.
    When the host soft off is complete, , it sends a pldm event Msg to BMC.
    After pldm receive this event Msg and set the pldm host soft off property transition,
    pldm-softpowerroff will stop the timer and send chassis off command.
    
    pldm-softpowerroff uses guardChassisOffTarget@.service and
    guardChassisOffTarget.sh to guard the chassis off target. Prevent
    hard power off during host soft off.
    
    Todo: The pldm host soft off Dbus PATH and interface need to be creat.
    
    test:
    Tested ok on fp5280g2.
    1、Execute pldm-softpowerroff on bmc. Leave the host running.  When the timer is expired,
    the host will chassis off.
    2、Execute pldm-softpowerroff on bmc. Use set-proprety to modify pldm host soft off property,
    the host will chassis off.
    3、Execute pldm-softpowerroff on bmc. We can't use obmcutil chassisoff/poweroff to
    shut down the host before the timer expired and the property transition. So the guard chassis
    off  function is OK.
    
    Signed-off-by: Chicago Duan <duanzhijia01@inspur.com>
    Change-Id: I4cff86b81e24efea55d10f6a1c93630b43a22bdc

--- softoff/main.cpp
+++ softoff/main.cpp
@@ -12,14 +12,12 @@
 #include <nlohmann/json.hpp>
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/elog.hpp>
+#include <sdbusplus/bus.hpp>
+#include <sdeventplus/event.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
 
 #include "libpldm/platform.h"
 #include "libpldm/requester/pldm.h"
-
-#include <sdbusplus/bus.hpp>
-#include <sdeventplus/event.hpp>
-
 
 int main()
 {
@@ -38,6 +36,29 @@ int main()
     // pldm::PldmSoftPowerOff  SoftPowerOff;
     pldm::PldmSoftPowerOff softPower = pldm::PldmSoftPowerOff(bus, event.get());
 
+    // Block chassis off target
+    softPower.guardChassisOff(ENABLE_GUARD);
+
+    // Time out or soft off complete
+    while (!softPower.iscompleted() && !softPower.isTimerExpired())
+    {
+        try
+        {
+            event.run(std::nullopt);
+        }
+        catch (const sdeventplus::SdEventError& e)
+        {
+            log<level::ERR>("PLDM host soft off: Failure in processing request",
+                            entry("ERROR=%s", e.what()));
+            return 1;
+        }
+    }
+
+    // Don't block chassis off target
+    softPower.guardChassisOff(DISABLE_GUARD);
+
+    // Now request the shutdown
+    softPower.sendChassisOffcommand();
+
     return 0;
 }
-
