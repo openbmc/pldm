d76d2b074fc36420a3b9e7d5e351129c8d731d81
Chicago Duan <duanzhijia01@inspur.com>
2020-04-17 10:46:53 +0800

    pldm : Pldm host soft off : Start a timer and wait for the host to soft off
    After send a set effecter to host to trigger host soft off and get the timeout
    seconds, the pldm-softpoweroff application need start a timer. When the
    timer runs out, pldm-softpoweroff will set chassis off.
    
    Tested:
    On fp5280g2, execute the pldm-softpoweroff .The timer starts and stops
    normally.
    
    Signed-off-by: Chicago Duan <duanzhijia01@inspur.com>
    Change-Id: I84824e839b26e66db3c6996ad9bb663cbfb1b7e8

--- softoff/main.cpp
+++ softoff/main.cpp
@@ -3,10 +3,20 @@
 #include "softoff.hpp"
 
 #include "libpldm/base.h"
+#include <sdeventplus/event.hpp>
 
 int main()
 {
-    pldm::PldmSoftPowerOff softPower = pldm::PldmSoftPowerOff();
+    // Get a default event loop
+    auto event = sdeventplus::Event::get_default();
+
+    // Get a handle to system dbus.
+    auto bus = sdbusplus::bus::new_default();
+
+    // Attach the bus to sd_event to service user requests
+    bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
+
+    pldm::PldmSoftPowerOff softPower = pldm::PldmSoftPowerOff(bus, event.get());
 
     if (softPower.isHasError() == true)
     {
