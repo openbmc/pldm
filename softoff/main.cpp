123f3d4b04e82c99ba35102731edb822b4a19520
Chicago Duan <duanzhijia01@inspur.com>
2020-04-17 10:46:53 +0800

    soft-off : Start a timer and wait for the host to soft off
    After send a set effecter to host to trigger host soft off and get the timeout
    seconds, the pldm-softpoweroff application need start a timer. if the host
    shutdown response is not received within the timeout then log an error and
    exit the application.
    
    Tested:
    On fp5280g2, execute the pldm-softpoweroff .The timer starts and stops
    normally.
    
    Signed-off-by: Chicago Duan <duanzhijia01@inspur.com>
    Change-Id: I84824e839b26e66db3c6996ad9bb663cbfb1b7e8

--- softoff/main.cpp
+++ softoff/main.cpp
@@ -2,11 +2,22 @@
 
 #include "softoff.hpp"
 
+#include <sdeventplus/event.hpp>
+
 #include <iostream>
 
 int main()
 {
-    pldm::SoftPowerOff softPower;
+    // Get a default event loop
+    auto event = sdeventplus::Event::get_default();
+
+    // Get a handle to system D-Bus.
+    auto bus = sdbusplus::bus::new_default();
+
+    // Attach the bus to sd_event to service user requests
+    bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
+
+    pldm::SoftPowerOff softPower(bus, event.get());
 
     if (softPower.isError() == true)
     {
