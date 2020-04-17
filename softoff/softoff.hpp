672d5daf877ae7be26fdd0737a0078509c44ce95
Chicago Duan <duanzhijia01@inspur.com>
2020-04-17 09:42:24 +0800

    soft-off: Send a set effecter to host to trigger host soft off
    pldm-softpoweroff will send a set state effecter states command to
    host, then the host will soft off.
    
    Signed-off-by: Chicago Duan <duanzhijia01@inspur.com>
    Change-Id: Ibfb56f81c160d21e111361cb977cecdd1f6ad6ef

---
+++ softoff/softoff.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "libpldm/requester/pldm.h"
+
+#include "common/types.hpp"
+
+#include <sdeventplus/event.hpp>
+namespace pldm
+{
+
+/** @class SoftPowerOff
+ *  @brief Responsible for coordinating Host SoftPowerOff operation
+ */
+class SoftPowerOff
+{
+  public:
+    /** @brief Constructs SoftPowerOff object.
+     */
+    SoftPowerOff();
+
+    /** @brief Is the pldm-softpoweroff has error.
+     * if hasError is true, that means the pldm-softpoweroff failed to
+     * trigger the host soft off,so the pldm-softpoweroff will exit.
+     */
+    inline auto isError()
+    {
+        return hasError;
+    }
+
+    /** @brief Is the host soft off completed.
+     */
+    inline auto isCompleted()
+    {
+        return completed;
+    }
+
+    /** @brief Send PLDM Set State Effecter States command and
+     * wait the host gracefully shutdown.
+     */
+    int hostSoftOff(sdeventplus::Event& event);
+
+  private:
+    /** @brief Getting the host current state.
+     */
+    int getHostState();
+
+    /** @brief Get effecterID from PDRs.
+     */
+    int getEffecterID();
+
+    /** @brief effecterID
+     */
+    uint16_t effecterID;
+
+    /** @brief Failed to send host soft off command flag.
+     */
+    bool hasError = false;
+
+    /** @brief Host soft off completed flag.
+     */
+    bool completed = false;
+
+    /** @brief The response for the PLDM request msg is received flag.
+     */
+    bool responseReceived = false;
+
+    /** @brief Is the Virtual Machine Manager/VMM state effecter available.
+     */
+    bool VMMPdrExist = true;
+};
+
+} // namespace pldm
