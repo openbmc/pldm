006df755155a6b7fd057466a7c49288b44a19eac
Chicago Duan <duanzhijia01@inspur.com>
2020-04-17 09:42:24 +0800

    soft-off: Send a set effecter to host to trigger host soft off
    pldm-softpoweroff will send a setStateEffecterStates command to
    host, then the host will soft off.
    
    Signed-off-by: Chicago Duan <duanzhijia01@inspur.com>
    Change-Id: Ibfb56f81c160d21e111361cb977cecdd1f6ad6ef

--- softoff/main.cpp
+++ softoff/main.cpp
@@ -1,8 +1,24 @@
-#include <iostream>
+#include "config.h"
 
-#include "libpldm/platform.h"
+#include "softoff.hpp"
+
+#include <iostream>
 
 int main()
 {
+    pldm::SoftPowerOff softPower;
+
+    if (softPower.isError() == true)
+    {
+        std::cerr << "Host failed to gracefully shutdown, exiting "
+                     "pldm-softpoweroff app\n";
+        return -1;
+    }
+
+    if (softPower.isCompleted() == true)
+    {
+        return 0;
+    }
+
     return 0;
 }
