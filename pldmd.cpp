11672615f11e74f7ad5846bf5627feb234c2d5b2
Pavithra Barithaya <pbaritha@in.ibm.com>
2020-04-27 02:17:10 -0500

    Implement PLDM.Pdr DBus API
    This commit implements a DBus API defined at
    https://gerrit.openbmc-project.xyz/c/openbmc/phosphor-dbus-interfaces/+/31774
    to find the stateEffecterPDR based on the entity information
    and stateStateSetID.
    
    Change-Id: I5c15be5303b511465c36914f5b60a0957cd3857e
    Signed-off-by: Pavithra Barithaya <pbaritha@in.ibm.com>

--- pldmd.cpp
+++ pldmd.cpp
@@ -1,3 +1,4 @@
+#include "dbus_impl_pdr.hpp"
 #include "dbus_impl_requester.hpp"
 #include "invoker.hpp"
 #include "libpldmresponder/base.hpp"
@@ -205,6 +206,7 @@ int main(int argc, char** argv)
 
     auto& bus = pldm::utils::DBusHandler::getBus();
     dbus_api::Requester dbusImplReq(bus, "/xyz/openbmc_project/pldm");
+    dbus_api::Pdr dbusImplPdr(bus, "/xyz/openbmc_project/pldm", pdrRepo.get());
     auto callback = [verbose, &invoker, &dbusImplReq](IO& /*io*/, int fd,
                                                       uint32_t revents) {
         if (!(revents & EPOLLIN))
