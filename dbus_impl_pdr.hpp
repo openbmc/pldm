edbe09ff161f48e1729336c04ccc6679fac53fd6
Pavithra Barithaya <pbaritha@in.ibm.com>
2020-04-27 02:17:10 -0500

    PLDM: Implement PLDM.Pdr DBus API
    This commit adds a DBus API to find the
    stateEffecterPDR based on the entity information
    and stateStateSetID.
    
    Change-Id: I5c15be5303b511465c36914f5b60a0957cd3857e
    Signed-off-by: Pavithra Barithaya <pbaritha@in.ibm.com>

---
+++ dbus_impl_pdr.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "xyz/openbmc_project/PLDM/Pdr/server.hpp"
+
+#include <iostream>
+#include <map>
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <vector>
+
+#include "libpldm/pdr.h"
+#include "libpldm/platform.h"
+
+namespace pldm
+{
+namespace dbus_api
+{
+using PdrIntf = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::PLDM::server::Pdr>;
+
+class Pdr : public PdrIntf
+{
+  public:
+    Pdr() = delete;
+    Pdr(const Pdr&) = delete;
+    Pdr& operator=(const Pdr&) = delete;
+    Pdr(Pdr&&) = delete;
+    Pdr& operator=(Pdr&&) = delete;
+    virtual ~Pdr() = default;
+
+    Pdr(sdbusplus::bus::bus& bus, const std::string& path) :
+        PdrIntf(bus, path.c_str())
+    {
+        std::cerr << "constructor" << std::endl;
+    };
+
+    std::vector<uint8_t> findStateEffecterPDR(uint8_t /*tid*/,
+                                              uint16_t containerId,
+                                              uint16_t entityType,
+                                              uint16_t entityInstance,
+                                              uint16_t stateSetId) override;
+
+  private:
+    pldm_pdr* repo;
+};
+
+} // namespace dbus_api
+} // namespace pldm
