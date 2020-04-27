9849e9ec4c1a017a66bd2410c65bf68cba6a83ff
Pavithra Barithaya <pbaritha@in.ibm.com>
2020-04-27 02:17:10 -0500

    Implement PLDM.Pdr DBus API
    This commit implements a DBus API defined at
    https://gerrit.openbmc-project.xyz/c/openbmc/phosphor-dbus-interfaces/+/31774
    to find the stateEffecterPDR based on the entity information
    and stateStateSetID.
    
    Change-Id: I5c15be5303b511465c36914f5b60a0957cd3857e
    Signed-off-by: Pavithra Barithaya <pbaritha@in.ibm.com>

---
+++ dbus_impl_pdr.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "xyz/openbmc_project/PLDM/PDR/server.hpp"
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <vector>
+
+#include "libpldm/pdr.h"
+#include "libpldm/platform.h"
+
+namespace pldm
+{
+namespace dbus_api
+{
+
+using PdrIntf = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::PLDM::server::PDR>;
+
+class Pdr : public PdrIntf
+{
+  public:
+    Pdr() = delete;
+    Pdr(const Pdr&) = delete;
+    Pdr& operator=(const Pdr&) = delete;
+    Pdr(Pdr&&) = delete;
+    Pdr& operator=(Pdr&&) = delete;
+    virtual ~Pdr() = default;
+
+    Pdr(sdbusplus::bus::bus& bus, const std::string& path,
+        const pldm_pdr* repo) :
+        PdrIntf(bus, path.c_str(), repo),
+        pdrRepo(repo){};
+
+    std::vector<std::vector<uint8_t>>
+        findStateEffecterPDR(uint8_t tid, uint16_t entityType,
+                             uint16_t stateSetId) override;
+
+  private:
+    const pldm_pdr* pdrRepo;
+};
+
+} // namespace dbus_api
+} // namespace pldm
