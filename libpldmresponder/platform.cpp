ad012562544ab411ef1d721acab31cbbef21bfc0
George Liu <liuxiwei@inspur.com>
2020-05-09 19:20:19 +0800

    Add parsing JSON for State Sensor PDR
    According to spec DSP0248_1.2.0: 28.6 and effecter_pdr.json, parse JSON
    and generate PDR structure.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I654913b6fa07f34f405f7dd41a5f1ac0ae2706fb

--- libpldmresponder/platform.cpp
+++ libpldmresponder/platform.cpp
@@ -2,6 +2,7 @@
 #include "platform.hpp"
 
 #include "pdr_state_effecter.hpp"
+#include "pdr_state_sensor.hpp"
 #include "utils.hpp"
 
 namespace pldm
@@ -55,10 +56,22 @@ void Handler::addDbusObjMaps(
     dbusObjMaps.emplace(effecterId, dbusObj);
 }
 
+void Handler::addDbusObjPropertyMaps(uint16_t sensorId,
+                                     pdr_utils::DbusPropertyMaps dbusObj)
+{
+    dbusObjPropertyMaps.emplace(sensorId, dbusObj);
+}
+
 const std::tuple<pdr_utils::DbusMappings, pdr_utils::DbusValMaps>&
     Handler::getDbusObjMaps(uint16_t effecterId) const
 {
     return dbusObjMaps.at(effecterId);
+}
+
+const pdr_utils::DbusPropertyMaps&
+    Handler::getDbusObjPropertyMaps(uint16_t sensorId) const
+{
+    return dbusObjPropertyMaps.at(sensorId);
 }
 
 void Handler::generate(const std::string& dir, Repo& repo)
@@ -73,6 +86,10 @@ void Handler::generate(const std::string& dir, Repo& repo)
          [this](const auto& json, RepoInterface& repo) {
              pdr_state_effecter::generateStateEffecterPDR<Handler>(json, *this,
                                                                    repo);
+         }},
+        {PLDM_STATE_SENSOR_PDR, [this](const auto& json, RepoInterface& repo) {
+             pdr_state_sensor::generateStateSensorPDR<Handler>(json, *this,
+                                                               repo);
          }}};
 
     Type pdrType{};
