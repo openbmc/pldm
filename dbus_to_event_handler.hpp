970395f424984a21e86c88aa996bd2df21fff2f8
George Liu <liuxiwei@inspur.com>
2020-05-15 09:32:57 +0800

    send PLDM event msg when state sensor changes
    Add the D-Bus to event handler interface to listen all of the state
    sensor PDRs and pldm should be able to send up a PLDM event msg when a
    D-Bus property changes. the PlatformEventMessage command format is
    defined in Table 15 in DSP0248 v1.2.0.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I9b6d5c1403bfcaa00dbbf478f7d797cc4a40f20d

---
+++ dbus_to_event_handler.hpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "dbus_impl_requester.hpp"
+#include "libpldmresponder/pdr_utils.hpp"
+
+#include <map>
+
+#include "libpldm/platform.h"
+
+using namespace pldm::dbus_api;
+using namespace pldm::responder;
+
+namespace pldm
+{
+
+using SensorId = uint16_t;
+using DbusObjMaps =
+    std::map<SensorId,
+             std::tuple<pdr_utils::DbusMappings, pdr_utils::DbusValMaps>>;
+using sensorEvent = std::function<void(const uint8_t& sensorId)>;
+
+/** @class DbusToSensorEventHandler
+ *  @brief This class can listen to the state sensor PDRs and send PLDM event
+ *         msg when a D-Bus property changes
+ */
+class DbusToSensorEventHandler
+{
+  public:
+    DbusToSensorEventHandler() = delete;
+    DbusToSensorEventHandler(const DbusToSensorEventHandler&) = delete;
+    DbusToSensorEventHandler(DbusToSensorEventHandler&&) = delete;
+    DbusToSensorEventHandler&
+        operator=(const DbusToSensorEventHandler&) = delete;
+    DbusToSensorEventHandler& operator=(DbusToSensorEventHandler&&) = delete;
+    ~DbusToSensorEventHandler() = default;
+
+    /** @brief Constructor
+     *  @param[in] mctp_fd - fd of MCTP communications socket
+     *  @param[in] mctp_eid - MCTP EID of host firmware
+     *  @param[in] requester - reference to Requester object
+     */
+    explicit DbusToSensorEventHandler(int mctp_fd, uint8_t mctp_eid,
+                                      Requester& requester);
+
+    /** @brief Listen all of the state sensor PDRs
+     *  @param[in] repo - pdr utils repo object
+     *  @param[in] sensorDbusObjMaps - The map of D-Bus mapping and value
+     */
+    void listenSensorEvent(pdr_utils::Repo repo, DbusObjMaps sensorDbusObjMaps);
+
+  private:
+    /** @brief Send state sensor event msg when a D-Bus property changes
+     *  @param[in] sensorId - sensor id
+     */
+    void sendStateSensorEvent(const uint8_t sensorId);
+
+    /** @brief Send all of sensor event
+     *  @param[in] eventType - PLDM Event types
+     *  @param[in] eventData - send event data
+     *  @param[in] eventSize - send event data size
+     */
+    void sendEventMsg(const uint8_t eventType, const uint8_t* eventData,
+                      size_t eventSize);
+
+    /** @brief fd of MCTP communications socket */
+    int mctp_fd;
+
+    /** @brief MCTP EID of host firmware */
+    uint8_t mctp_eid;
+
+    /** @brief pointer to BMC's primary PDR repo, host PDRs are added here */
+    pldm_pdr* repo;
+
+    /** @brief reference to Requester object, primarily used to access API to
+     *  obtain PLDM instance id.
+     */
+    Requester& requester;
+
+    /** @brief D-Bus property changed signal match */
+    std::unique_ptr<sdbusplus::bus::match::match> stateSensorMatch;
+
+    /** @brief Sensor D-Bus Maps */
+    DbusObjMaps sensorDbusMaps;
+};
+
+} // namespace pldm
