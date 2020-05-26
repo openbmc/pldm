a651663169097b69749094000b738e1b0e255064
Pavithra Barithaya <pbaritha@in.ibm.com>
2020-05-26 07:00:26 -0500

    pldmtool : File table parsing
    This commit implements parsing of the file attribute
    table by issuing the GetFileTable command with the
    -t option which specifies the pldm file table type.
    
    Tested:
     ./pldmtool oem-ibm-fileIO -h
    oem type command
    Usage: ./pldmtool oem-ibm-fileIO [OPTIONS] SUBCOMMAND
    
    Options:
      -h,--help                   Print this help message and exit
    
    Subcommands:
      GetFileTable                get file table
    
    ./pldmtool oem-ibm-fileIO GetFileTable -t 0
    Request Message:
    08 01 80 3f 01 00 00 00 00 01 00
    Success in creating the socket : RC = 3
    Success in connecting to socket : RC = 0
    Success in sending message type as pldm to mctp : RC = 0
    Received Msg
    Buffer Data: 08 01 80 3f 01 00 00 00 00 01 00
    Sending Msg
    Buffer Data: 00 3f 01 00 00 00 00 00 05 00 00 00 00 0a 00 50 48 59 50 2d 4e 56 52 41 4d 00 b0 10 01 01 00 00 00 01 00 00 00 10 00 50 48 59 50 2d 4e 56 52 41 4d 2d 43 4b 53 55 4d 10 00 00 00 04 00 00 00 00 00 87 55 62 3e
    Received Msg
    Buffer Data: 08 01 00 3f 01 00 00 00 00 00 05 00 00 00 00 0a 00 50 48 59 50 2d 4e 56 52 41 4d 00 b0 10 01 01 00 00 00 01 00 00 00 10 00 50 48 59 50 2d 4e 56 52 41 4d 2d 43 4b 53 55 4d 10 00 00 00 04 00 00 00 00 00 87 55 62 3e
    Write to socket successful : RC = 11
    Total length:71
    Shutdown Socket successful :  RC = 0
    Response Message:
    08 01 00 3f 01 00 00 00 00 00 05 00 00 00 00 0a 00 50 48 59 50 2d 4e 56 52 41 4d 00 b0 10 01 01 00 00 00 01 00 00 00 10 00 50 48 59 50 2d 4e 56 52 41 4d 2d 43 4b 53 55 4d 10 00 00 00 04 00 00 00 00 00 87 55 62 3e
    FileHandle[0]:0
    FileNameLength[0]:10
    FileName[0]:PHYP-NVRAM
    FileSize[0]:17870848
    FileTraits[0]:1
    FileHandle[1]:1
    FileNameLength[1]:16
    FileName[1]:PHYP-NVRAM-CKSUM
    FileSize[1]:16
    FileTraits[1]:4
    
    Change-Id: I44de4dd33fee42df7cdc67eaaddf259ea83346ae
    Signed-off-by: Pavithra Barithaya <pbaritha@in.ibm.com>

---
+++ tool/oem/ibm/pldm_file_cmd.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <CLI/CLI.hpp>
+
+namespace pldmtool
+{
+
+namespace oem_ibm
+{
+namespace file_io
+{
+
+void registerCommand(CLI::App& app);
+
+}
+
+} // namespace oem_ibm
+
+} // namespace pldmtool
