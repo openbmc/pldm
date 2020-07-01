36e813520e3f6141dc2c574d7d75ee7d4803f6b3
George Liu <liuxiwei@inspur.com>
2020-07-01 14:40:30 +0800

    libpldmresponder: create PDR when the D-Bus object path exists
    On a system where this D-Bus object path does not exist, it should
    not create a PDR when the D-Bus object specified here is not found.
    
    Tested: In the PDR config files, the PDR is successfully created when
    the objectPath of the D-Bus exists, otherwise, skipped the current
    config and the PDR is not created.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: Ib59fa79cc28d5712161bfb971c37c86c363bafb0

--- test/mocked_utils.hpp
+++ test/mocked_utils.hpp
@@ -28,6 +28,9 @@ using namespace pldm::utils;
 class MockdBusHandler : public DBusHandler
 {
   public:
+    MOCK_METHOD(std::string, getService, (const char*, const char*),
+                (const override));
+
     MOCK_METHOD(void, setDbusProperty,
                 (const DBusMapping&, const PropertyValue&), (const override));
 
