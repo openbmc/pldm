e1a50018a3a32bea197e5a39b2be72de02ebd6e0
Sagar Srinivas <sagar.srinivas@ibm.com>
2020-07-08 05:16:04 -0500

    FRU: simplify BMC FRU table construction
    We should be able to map standard D-Bus FRU inventory properties to
    standard FRU properties from the PLDM FRU spec.
    This commit with simplify things by removing JSONs for standard FRUs.
    
    Tested on rainier with the pldmtool.
    
    Signed-off-by: Sagar Srinivas <sagar.srinivas@ibm.com>
    Change-Id: I53cd3cd98a0552e3faa42d50c9ae93280218ad28

--- test/libpldmresponder_fru_test.cpp
+++ test/libpldmresponder_fru_test.cpp
@@ -6,26 +6,62 @@ TEST(FruParser, allScenarios)
 {
     using namespace pldm::responder::fru_parser;
 
-    // No master FRU JSON
-    ASSERT_THROW(FruParser("./fru_jsons/malformed1"), std::exception);
-    // Malformed master FRU JSON
-    ASSERT_THROW(FruParser("./fru_jsons/malformed2"), std::exception);
     // Malformed FRU JSON
-    ASSERT_THROW(FruParser("./fru_jsons/malformed3"), std::exception);
-
+    ASSERT_THROW(FruParser("./fru_jsons/malformed"), std::exception);
     FruParser parser{"./fru_jsons/good"};
 
     // Get an item with a single PLDM FRU record
-    FruRecordInfos cpu{{1,
-                        1,
-                        {{"xyz.openbmc_project.Inventory.Decorator.Asset",
-                          "PartNumber", "string", 3},
-                         {"xyz.openbmc_project.Inventory.Decorator.Asset",
-                          "SerialNumber", "string", 4}}}};
-    auto cpuInfos =
-        parser.getRecordInfo("xyz.openbmc_project.Inventory.Item.Cpu");
-    ASSERT_EQ(cpuInfos.size(), 1);
-    ASSERT_EQ(cpu == cpuInfos, true);
+    FruRecordInfos motherBoard{
+        {1,
+         1,
+         {{"xyz.openbmc_project.Inventory.Decorator.Asset", "Model", "string",
+           2},
+          {"xyz.openbmc_project.Inventory.Decorator.Asset", "PartNumber",
+           "string", 3},
+          {"xyz.openbmc_project.Inventory.Decorator.Asset", "SerialNumber",
+           "string", 4},
+          {"xyz.openbmc_project.Inventory.Decorator.Asset", "Manufacturer",
+           "string", 5},
+          {"xyz.openbmc_project.Inventory.Item", "PrettyName", "string", 8},
+          {"xyz.openbmc_project.Inventory.Decorator.AssetTag", "AssetTag",
+           "string", 11},
+          {"xyz.openbmc_project.Inventory.Decorator.Revision", "Version",
+           "string", 10}}},
+
+        {254,
+         1,
+         {{"com.ibm.ipzvpd.VSYS", "RT", "bytearray", 2},
+          {"com.ibm.ipzvpd.VSYS", "BR", "bytearray", 3}}}};
+    auto motherBoardInfos = parser.getRecordInfo(
+        "xyz.openbmc_project.Inventory.Item.Board.Motherboard");
+    ASSERT_EQ(motherBoardInfos.size(), 2);
+    ASSERT_EQ(motherBoard == motherBoardInfos, true);
+
+    FruRecordInfos bmc{
+        {1,
+         1,
+         {{"xyz.openbmc_project.Inventory.Decorator.Asset", "Model", "string",
+           2},
+          {"xyz.openbmc_project.Inventory.Decorator.Asset", "PartNumber",
+           "string", 3},
+          {"xyz.openbmc_project.Inventory.Decorator.Asset", "SerialNumber",
+           "string", 4},
+          {"xyz.openbmc_project.Inventory.Decorator.Asset", "Manufacturer",
+           "string", 5},
+          {"xyz.openbmc_project.Inventory.Item", "PrettyName", "string", 8},
+          {"xyz.openbmc_project.Inventory.Decorator.AssetTag", "AssetTag",
+           "string", 11},
+          {"xyz.openbmc_project.Inventory.Decorator.Revision", "Version",
+           "string", 10}}},
+
+        {254,
+         1,
+         {{"com.ibm.ipzvpd.VINI", "RT", "bytearray", 2},
+          {"com.ibm.ipzvpd.VINI", "B3", "bytearray", 3}}}};
+    auto bmcInfos =
+        parser.getRecordInfo("xyz.openbmc_project.Inventory.Item.Bmc");
+    ASSERT_EQ(bmcInfos.size(), 2);
+    ASSERT_EQ(bmc == bmcInfos, true);
 
     // Get an item type with 2 PLDM FRU records
     auto boardInfos =
@@ -33,10 +69,20 @@ TEST(FruParser, allScenarios)
     ASSERT_EQ(boardInfos.size(), 2);
 
     // D-Bus lookup info for FRU information
-    DBusLookupInfo lookupInfo{"xyz.openbmc_project.Inventory.Manager",
-                              "/xyz/openbmc_project/inventory/system/",
-                              {"xyz.openbmc_project.Inventory.Item.Board",
-                               "xyz.openbmc_project.Inventory.Item.Cpu"}};
+    DBusLookupInfo lookupInfo{
+        "xyz.openbmc_project.Inventory.Manager",
+        "/xyz/openbmc_project/inventory",
+        {"xyz.openbmc_project.Inventory.Item.Chassis",
+         "xyz.openbmc_project.Inventory.Item.Board",
+         "xyz.openbmc_project.Inventory.Item.Board.Motherboard",
+         "xyz.openbmc_project.Inventory.Item.Panel",
+         "xyz.openbmc_project.Inventory.Item.PowerSupply",
+         "xyz.openbmc_project.Inventory.Item.Vrm",
+         "xyz.openbmc_project.Inventory.Item.Cpu",
+         "xyz.openbmc_project.Inventory.Item.Bmc",
+         "xyz.openbmc_project.Inventory.Item.Dimm",
+         "xyz.openbmc_project.Inventory.Item.Tpm",
+         "xyz.openbmc_project.Inventory.Item.System"}};
 
     auto dbusInfo = parser.inventoryLookup();
     ASSERT_EQ(dbusInfo == lookupInfo, true);
@@ -46,3 +92,4 @@ TEST(FruParser, allScenarios)
         parser.getRecordInfo("xyz.openbmc_project.Inventory.Item.DIMM"),
         std::exception);
 }
+
