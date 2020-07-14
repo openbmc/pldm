c6d9dc49d5d83b6533fc1156cd837c7959e86cfe
George Liu <liuxiwei@inspur.com>
2020-07-14 15:48:44 +0800

    Mechanism to associate sensor/effecter to FRU entity
    The "entity_path" key is defined in PDR JSONs and will have an FRU D-Bus object
    path as the value, if this is present, the entity type, instance, and container
    id should be associate with the FRU entity.
    
    Tested: test JSON with https://gist.github.com/lxwinspur/7225a9301ccfcbff1f6549203405b4fa
    after executing the following command, get the entity information from FruHandler
    entity_type = 120
    entity_instance_num = 1
    entity_container_id = 0
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I019f9a0b9d4dbec20a71dfd797f30ac8f57f67b8

--- libpldmresponder/platform.hpp
+++ libpldmresponder/platform.hpp
@@ -2,9 +2,9 @@
 
 #include "config.h"
 
-#include "libpldm/pdr.h"
 #include "libpldm/platform.h"
 #include "libpldm/states.h"
+#include "pdr.h"
 
 #include "common/utils.hpp"
 #include "event_parser.hpp"
@@ -46,6 +46,7 @@ using EventHandler = std::function<int(
     uint8_t tid, size_t eventDataOffset)>;
 using EventHandlers = std::vector<EventHandler>;
 using EventMap = std::map<EventType, EventHandlers>;
+using AssociatedEntityMap = std::map<DbusPath, pldm_entity>;
 
 // EventEntry = <uint8_t> - EventState <uint8_t> - SensorOffset <uint16_t> -
 // SensorID
@@ -436,6 +437,17 @@ class Handler : public CmdHandler
      */
     void generateTerminusLocatorPDR(Repo& repo);
 
+    /** @brief Get std::map associated with the entity
+     *         key: object path
+     *         value: pldm_entity
+     *
+     *  @return std::map<ObjectPath, pldm_entity>
+     */
+    inline const AssociatedEntityMap& getAssociateEntityMap() const
+    {
+        return associatedEntityMap;
+    }
+
   private:
     pdr_utils::Repo pdrRepo;
     uint16_t nextEffecterId{};
@@ -449,6 +461,7 @@ class Handler : public CmdHandler
     const pldm::utils::DBusHandler* dBusIntf;
     std::string pdrJsonsDir;
     bool pdrCreated;
+    AssociatedEntityMap associatedEntityMap;
 };
 
 } // namespace platform
