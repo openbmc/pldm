07df6182c89394996cb1fa8c18c1c13df7e0caf9
Sagar Srinivas <sagar.srinivas@ibm.com>
2020-08-27 00:52:20 -0500

    Platform: create OEM PDRs for all OEM state sensors and effecters
    Created Sensors and effecters for Code update for the Boot side
    and Firmware update State indication and control.
    root@rainier:/tmp# ./pldmtool platform GetPDR -d 22
    nextRecordHandle: 23
    responseCount: 30
    recordHandle: 22
    PDRHeaderVersion: 1
    PDRType: State Effecter PDR(11)
    recordChangeNumber: 0
    dataLength: 16
    
    PLDMTerminusHandle: 1
    effecterID: 0
    entityType: Virtual Machine Manager
    entityInstanceNumber: 0
    containerID: 0
    effecterSemanticID: 0
    effecterInit: noInit
    effecterDescriptionPDR: false
    compositeEffecterCount: 1
    stateSetID: 32769
    possibleStatesSize: 2
    possibleStates: 1 2
    root@rainier:/tmp# ./pldmtool platform GetPDR -d 23
    nextRecordHandle: 24
    responseCount: 30
    recordHandle: 23
    PDRHeaderVersion: 1
    PDRType: State Effecter PDR(11)
    recordChangeNumber: 0
    dataLength: 16
    
    PLDMTerminusHandle: 1
    effecterID: 0
    entityType: Virtual Machine Manager
    entityInstanceNumber: 1
    containerID: 0
    effecterSemanticID: 0
    effecterInit: noInit
    effecterDescriptionPDR: false
    compositeEffecterCount: 1
    stateSetID: 32769
    possibleStatesSize: 2
    possibleStates: 1 2
    root@rainier:/tmp# ./pldmtool platform GetPDR -d 24
    nextRecordHandle: 25
    responseCount: 30
    recordHandle: 24
    PDRHeaderVersion: 1
    PDRType: State Effecter PDR(11)
    recordChangeNumber: 0
    dataLength: 16
    
    PLDMTerminusHandle: 1
    effecterID: 0
    entityType: Virtual Machine Manager
    entityInstanceNumber: 0
    containerID: 0
    effecterSemanticID: 0
    effecterInit: noInit
    effecterDescriptionPDR: false
    compositeEffecterCount: 1
    stateSetID: 32768
    possibleStatesSize: 2
    possibleStates: 1 2 3 4 5 6
    root@rainier:/tmp# ./pldmtool platform GetPDR -d 25
    nextRecordHandle: 26
    responseCount: 28
    recordHandle: 25
    PDRHeaderVersion: 1
    PDRType: State Sensor PDR(4)
    recordChangeNumber: 0
    dataLength: 14
    
    PLDMTerminusHandle: 1
    sensorID: 0
    entityType: Virtual Machine Manager
    entityInstanceNumber: 0
    containerID: 0
    sensorInit: noInit
    sensorAuxiliaryNamesPDR: false
    compositeSensorCount: 1
    stateSetID: 32769
    possibleStatesSize: 2
    possibleStates: 1 2
    root@rainier:/tmp# ./pldmtool platform GetPDR -d 26
    nextRecordHandle: 27
    responseCount: 28
    recordHandle: 26
    PDRHeaderVersion: 1
    PDRType: State Sensor PDR(4)
    recordChangeNumber: 0
    dataLength: 14
    
    PLDMTerminusHandle: 1
    sensorID: 0
    entityType: Virtual Machine Manager
    entityInstanceNumber: 1
    containerID: 0
    sensorInit: noInit
    sensorAuxiliaryNamesPDR: false
    compositeSensorCount: 1
    stateSetID: 32769
    possibleStatesSize: 2
    possibleStates: 1 2
    root@rainier:/tmp# ./pldmtool platform GetPDR -d 27
    nextRecordHandle: 0
    responseCount: 28
    recordHandle: 27
    PDRHeaderVersion: 1
    PDRType: State Sensor PDR(4)
    recordChangeNumber: 0
    dataLength: 14
    
    PLDMTerminusHandle: 1
    sensorID: 0
    entityType: Virtual Machine Manager
    entityInstanceNumber: 0
    containerID: 0
    sensorInit: noInit
    sensorAuxiliaryNamesPDR: false
    compositeSensorCount: 1
    stateSetID: 32768
    possibleStatesSize: 2
    possibleStates: 1 2 3 4 5 6
    
    Signed-off-by: Sagar Srinivas <sagar.srinivas@ibm.com>
    Change-Id: I1dc39a549297241792cebc7812722447cce5762d

--- oem/ibm/libpldmresponder/oem_ibm_handler.cpp
+++ oem/ibm/libpldmresponder/oem_ibm_handler.cpp
@@ -1,4 +1,10 @@
 #include "oem_ibm_handler.hpp"
+#include "libpldmresponder/oem_handler.hpp"
+#include "../../../libpldmresponder/pdr_utils.hpp"
+#include "../../../libpldm/pdr.h"
+#include "../../../libpldm/platform.h"
+#include "libpldmresponder/platform.hpp"
+#include "libpldmresponder/pdr.hpp"
 
 namespace pldm
 {
@@ -98,6 +104,110 @@ int pldm::responder::oem_ibm_platform::Handler::
         }
     }
     return rc;
+}
+
+void generateStateEffecterOEMPDR(pldm::responder::platform::Handler* handler,
+                                uint16_t entityInstance, uint16_t stateSetID,
+                                 pdr_utils::RepoInterface& repo)
+{
+
+    size_t pdrSize = 0;
+    pdrSize = sizeof(pldm_state_effecter_pdr) +
+              sizeof(state_effecter_possible_states);
+    std::vector<uint8_t> entry{};
+    entry.resize(pdrSize);
+    pldm_state_effecter_pdr* pdr =
+        reinterpret_cast<pldm_state_effecter_pdr*>(entry.data());
+
+    pdr->hdr.record_handle = 0;
+    pdr->hdr.version = 1;
+    pdr->hdr.type = PLDM_STATE_EFFECTER_PDR;
+    pdr->hdr.record_change_num = 0;
+    pdr->hdr.length = sizeof(pldm_state_effecter_pdr) - sizeof(pldm_pdr_hdr);
+    pdr->terminus_handle = pdr::BmcPldmTerminusHandle;
+    pdr->effecter_id = handler->getNextEffecterId();
+    pdr->entity_type = 33;
+    pdr->entity_instance = entityInstance;
+    pdr->container_id = 0;
+    pdr->effecter_semantic_id = 0;
+    pdr->effecter_init = PLDM_NO_INIT;
+    pdr->has_description_pdr = false;
+    pdr->composite_effecter_count = 1;
+
+    auto* possibleStatesPtr = pdr->possible_states;
+    auto possibleStates =
+        reinterpret_cast<state_effecter_possible_states*>(possibleStatesPtr);
+    possibleStates->state_set_id = stateSetID;
+    possibleStates->possible_states_size = 2;
+    auto state =
+        reinterpret_cast<state_effecter_possible_states*>(possibleStates);
+    if (stateSetID == 32769)
+        state->states[0].byte = 6;
+    else if (stateSetID == 32768)
+        state->states[0].byte = 126;
+    pldm::responder::pdr_utils::PdrEntry pdrEntry{};
+    pdrEntry.data = entry.data();
+    pdrEntry.size = pdrSize;
+    repo.addRecord(pdrEntry);
+}
+
+void generateStateSensorOEMPDR(pldm::responder::platform::Handler* handler,
+                              uint16_t entityInstance, uint16_t stateSetID,
+                               pdr_utils::RepoInterface& repo)
+{
+    size_t pdrSize = 0;
+    pdrSize =
+        sizeof(pldm_state_sensor_pdr) + sizeof(state_sensor_possible_states);
+    std::vector<uint8_t> entry{};
+    entry.resize(pdrSize);
+    pldm_state_sensor_pdr* pdr =
+        reinterpret_cast<pldm_state_sensor_pdr*>(entry.data());
+
+    pdr->hdr.record_handle = 0;
+    pdr->hdr.version = 1;
+    pdr->hdr.type = PLDM_STATE_SENSOR_PDR;
+    pdr->hdr.record_change_num = 0;
+    pdr->hdr.length = sizeof(pldm_state_sensor_pdr) - sizeof(pldm_pdr_hdr);
+    pdr->terminus_handle = pdr::BmcPldmTerminusHandle;
+    pdr->sensor_id = handler->getNextSensorId();
+    pdr->entity_type = 33;
+    pdr->entity_instance = entityInstance;
+    pdr->container_id = 0;
+    pdr->sensor_init = PLDM_NO_INIT;
+    pdr->sensor_auxiliary_names_pdr = false;
+    pdr->composite_sensor_count = 1;
+
+    auto* possibleStatesPtr = pdr->possible_states;
+    auto possibleStates =
+        reinterpret_cast<state_sensor_possible_states*>(possibleStatesPtr);
+    possibleStates->state_set_id = stateSetID;
+    possibleStates->possible_states_size = 2;
+    auto state =
+        reinterpret_cast<state_sensor_possible_states*>(possibleStates);
+    if (stateSetID == 32769)
+        state->states[0].byte = 6;
+    else if (stateSetID == 32768)
+        state->states[0].byte = 126;
+    pldm::responder::pdr_utils::PdrEntry pdrEntry{};
+    pdrEntry.data = entry.data();
+    pdrEntry.size = pdrSize;
+    repo.addRecord(pdrEntry);
+}
+
+void pldm::responder::oem_ibm_platform::Handler::buildOEMPDR(pdr_utils::RepoInterface& repo)
+{
+    generateStateEffecterOEMPDR(platformHandler,0, 32769, repo);
+
+    generateStateEffecterOEMPDR(platformHandler,1, 32769, repo);
+
+    generateStateEffecterOEMPDR(platformHandler,0, 32768, repo);
+
+    generateStateSensorOEMPDR(platformHandler,0, 32769, repo);
+
+    generateStateSensorOEMPDR(platformHandler,1, 32769, repo);
+
+    generateStateSensorOEMPDR(platformHandler,0, 32768, repo);
+
 }
 
 void pldm::responder::oem_ibm_platform::Handler::setPlatformHandler(
