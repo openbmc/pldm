5cc41d064dee81e87aa3ffe031e41a872a666b50
Sampa Misra <sampmisr@in.ibm.com>
2020-08-31 08:33:47 -0500

    pldm: oem: infrastructure for oem handlers
    This commit adds the framework for an oem handler
    which can be used by specific oem use-cases
    for implementing various commands.
    This commit adds implementation for getStateSensorReadings
    and setStateEffecterStates commands.
    Also adds implementation for inband code update.
    
    Change-Id: Ib38a66ee381dd06b93f6a9313d51de1c23e6ee65
    Signed-off-by: Sampa Misra <sampmisr@in.ibm.com>

--- oem/ibm/libpldmresponder/file_io.hpp
+++ oem/ibm/libpldmresponder/file_io.hpp
@@ -7,6 +7,7 @@
 #include "oem/ibm/libpldm/host.h"
 
 #include "common/utils.hpp"
+#include "oem_ibm_handler.hpp"
 #include "pldmd/handler.hpp"
 
 #include <fcntl.h>
@@ -147,7 +148,8 @@ namespace oem_ibm
 class Handler : public CmdHandler
 {
   public:
-    Handler()
+    Handler(oem_platform::Handler* oemPlatformHandler) :
+        oemPlatformHandler(oemPlatformHandler)
     {
         handlers.emplace(PLDM_READ_FILE_INTO_MEMORY,
                          [this](const pldm_msg* request, size_t payloadLength) {
@@ -302,6 +304,9 @@ class Handler : public CmdHandler
      *  @return PLDM response message
      */
     Response newFileAvailable(const pldm_msg* request, size_t payloadLength);
+
+  private:
+    oem_platform::Handler* oemPlatformHandler; // will be used by read LID
 };
 
 } // namespace oem_ibm
---
+++ oem/ibm/libpldmresponder/inband_code_update.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "common/utils.hpp"
+
+#include <string>
+
+namespace pldm
+{
+namespace responder
+{
+
+static constexpr auto P_SIDE = 1;
+static constexpr auto T_SIDE = 2;
+
+class CodeUpdate
+{
+  public:
+    CodeUpdate(const pldm::utils::DBusHandler* dBusIntf) : dBusIntf(dBusIntf)
+    {
+        currBootSide = "T";
+        nextBootSide = "T";
+        runningVersion = "";
+        nonRunningVersion = "";
+        setVersions();
+    }
+
+    std::string fetchCurrentBootSide();
+    std::string fetchNextBootSide();
+    bool persistBootSideInfo(); // may be not needed default is T when pldm
+                                // starts
+    int setCurrentBootSide(std::string currSide);
+    int setNextBootSide(std::string nextSide);
+
+    // subscribe here for dbus prop change notification for nonRunningVersion
+
+  private:
+    void setVersions();
+    std::string currBootSide;
+    std::string nextBootSide;
+    std::string runningVersion; // verify whether really needed to be maintained
+    std::string nonRunningVersion; // empty means single image is installed
+    inline static bool codeUpdateInProgress = false;
+    const pldm::utils::DBusHandler* dBusIntf;
+};
+
+} // namespace responder
+} // namespace pldm
