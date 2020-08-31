a61554c1ebfed3f4c89d2bd1621eafe94e7fa688
Sampa Misra <sampmisr@in.ibm.com>
2020-08-31 08:33:47 -0500

    pldm: oem: infrastructure for oem handlers
    This commit adds the framework for an oem handler
    which can be used by specific oem use-cases
    for implementing various commands.
    This commit adds implementation for getStateSensorReadings
    and setStateEffecterStates commands.
    Also adds implementation for inband code update.
    
    Change-Id: Ib38a66ee381dd06b93f6a9313d51de1c23e6ee65
    Signed-off-by: Sampa Misra <sampmisr@in.ibm.com>

---
+++ libpldmresponder/oem_handler.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "common/utils.hpp"
+#include "pldmd/handler.hpp"
+
+namespace pldm
+{
+
+namespace responder
+{
+
+namespace oem_platform
+{
+
+class Handler : public CmdHandler
+{
+  public:
+    Handler(const pldm::utils::DBusHandler* dBusIntf) : dBusIntf(dBusIntf)
+    {}
+
+    virtual int getOemStateSensorReadingsHandler(
+        uint16_t sensorId, /*uint8_t sensorRearmCnt,*/ uint16_t entityType,
+        uint16_t entityInstance, uint16_t stateSetId, uint8_t compSensorCnt,
+        std::vector<get_sensor_state_field>& stateField) = 0;
+
+    virtual int OemSetStateEffecterStatesHandler(
+        uint16_t effecterId, uint16_t entityType, uint16_t entityInstance,
+        uint16_t stateSetId, uint8_t compEffecterCnt,
+        const std::vector<set_effecter_state_field>& stateField) = 0;
+
+    virtual ~Handler()
+    {}
+
+  protected:
+    const pldm::utils::DBusHandler* dBusIntf;
+};
+
+} // namespace oem_platform
+
+} // namespace responder
+
+} // namespace pldm
---
+++ oem/ibm/libpldmresponder/oem_ibm_handler.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "inband_code_update.hpp"
+#include "libpldmresponder/oem_handler.hpp"
+#include "libpldmresponder/platform.hpp"
+
+namespace pldm
+{
+
+namespace responder
+{
+
+namespace oem_ibm_platform
+{
+
+class Handler : public oem_platform::Handler
+{
+  public:
+    Handler(const pldm::utils::DBusHandler* dBusIntf) :
+        oem_platform::Handler(dBusIntf), codeUpdate(dBusIntf),
+        platformHandler(nullptr)
+    {}
+
+    int getOemStateSensorReadingsHandler(
+        uint16_t sensorId, /*uint8_t sensorRearmCnt,*/ uint16_t entityType,
+        uint16_t entityInstance, uint16_t stateSetId, uint8_t compSensorCnt,
+        std::vector<get_sensor_state_field>& stateField);
+
+    int OemSetStateEffecterStatesHandler(
+        uint16_t effecterId, uint16_t entityType, uint16_t entityInstance,
+        uint16_t stateSetId, uint8_t compEffecterCnt,
+        const std::vector<set_effecter_state_field>& stateField);
+    void setPlatformHandler(pldm::responder::platform::Handler* handler);
+
+    ~Handler()
+    {}
+
+  private:
+    pldm::responder::CodeUpdate codeUpdate;
+    pldm::responder::platform::Handler* platformHandler;
+};
+
+} // namespace oem_ibm_platform
+
+} // namespace responder
+
+} // namespace pldm
