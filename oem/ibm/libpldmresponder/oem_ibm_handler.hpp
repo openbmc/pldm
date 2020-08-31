348f423af3c6105f32d5248e384e2af7472c267c
Sampa Misra <sampmisr@in.ibm.com>
2020-08-31 08:33:47 -0500

    pldm: oem: infrastructure for oem handlers
    This commit adds the framework for an oem handler
    which can be used by specific oem use-cases
    for implementing various commands.
    This commit adds implementation for getStateSensorReadings
    and setStateEffecterStates commands for oem state sets.
    
    Also adds implementation for inband code update.
    
    Change-Id: Ib38a66ee381dd06b93f6a9313d51de1c23e6ee65
    Signed-off-by: Sampa Misra <sampmisr@in.ibm.com>

---
+++ oem/ibm/libpldmresponder/oem_ibm_handler.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "inband_code_update.hpp"
+#include "libpldmresponder/oem_handler.hpp"
+#include "libpldmresponder/platform.hpp"
+
+namespace pldm
+{
+
+namespace responder
+{
+
+namespace oem_ibm_platform
+{
+
+static constexpr auto PLDM_OEM_IBM_BOOT_STATE = 32769;
+static constexpr auto PLDM_OEM_IBM_FIRMWARE_UPDATE_STATE = 32768;
+
+class Handler : public oem_platform::Handler
+{
+  public:
+    Handler(const pldm::utils::DBusHandler* dBusIntf,
+            pldm::responder::CodeUpdate* codeUpdate) :
+        oem_platform::Handler(dBusIntf),
+        codeUpdate(codeUpdate), platformHandler(nullptr)
+    {
+        codeUpdate->setVersions();
+    }
+
+    int getOemStateSensorReadingsHandler(
+        uint16_t entityType, uint16_t entityInstance, uint16_t stateSetId,
+        uint8_t compSensorCnt, std::vector<get_sensor_state_field>& stateField);
+
+    int OemSetStateEffecterStatesHandler(
+        uint16_t entityType, uint16_t entityInstance, uint16_t stateSetId,
+        uint8_t compEffecterCnt,
+        const std::vector<set_effecter_state_field>& stateField);
+
+    /** @brief Method to set the platform handler in the
+     *         oem_ibm_handler class
+     *  @param[in] handler - pointer to PLDM platform handler
+     */
+    void setPlatformHandler(pldm::responder::platform::Handler* handler);
+
+    ~Handler()
+    {}
+
+    pldm::responder::CodeUpdate* codeUpdate; //!< pointer to CodeUpdate object
+    pldm::responder::platform::Handler*
+        platformHandler; //!< pointer to PLDM platform handler
+};
+
+} // namespace oem_ibm_platform
+
+} // namespace responder
+
+} // namespace pldm
