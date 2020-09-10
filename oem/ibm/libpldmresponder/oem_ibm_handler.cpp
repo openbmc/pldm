91caf2a9f5ccf68341118156794c873ce0b9e282
Varsha Kaverappa <vkaverap@in.ibm.com>
2020-09-10 06:15:42 -0500

    PLDM: Platform: event framework to send events to PHYP
    This commit consts of APIs that will be used to send code update
    event change/update PlatformEventMessage to Phyp for different
    stages of inband code update progress.
    The event is a state sensor event.
    
    Signed-off-by: Varsha Kaverappa <vkaverap@in.ibm.com>
    Change-Id: I9b4b0493dd6886805044011d994df58b31f80a6e

--- oem/ibm/libpldmresponder/oem_ibm_handler.cpp
+++ oem/ibm/libpldmresponder/oem_ibm_handler.cpp
@@ -1,5 +1,7 @@
 #include "oem_ibm_handler.hpp"
 
+#include "libpldm/requester/pldm.h"
+
 #include "libpldmresponder/pdr_utils.hpp"
 
 namespace pldm
@@ -81,6 +83,97 @@ void pldm::responder::oem_ibm_platform::Handler::setPlatformHandler(
     pldm::responder::platform::Handler* handler)
 {
     platformHandler = handler;
+}
+
+int pldm::responder::oem_ibm_platform::Handler::sendEventToHost(
+    std::vector<uint8_t>& requestMsg)
+{
+    uint8_t* responseMsg = nullptr;
+    size_t responseMsgSize{};
+
+    auto requesterRc =
+        pldm_send_recv(mctp_eid, mctp_fd, requestMsg.data(), requestMsg.size(),
+                       &responseMsg, &responseMsgSize);
+    std::unique_ptr<uint8_t, decltype(std::free)*> responseMsgPtr{responseMsg,
+                                                                  std::free};
+    if (requesterRc != PLDM_REQUESTER_SUCCESS)
+    {
+        return requesterRc;
+    }
+    uint8_t completionCode{};
+    uint8_t status{};
+    auto responsePtr = reinterpret_cast<struct pldm_msg*>(responseMsgPtr.get());
+    auto rc = decode_platform_event_message_resp(
+        responsePtr, responseMsgSize - sizeof(pldm_msg_hdr), &completionCode,
+        &status);
+
+    if (completionCode != PLDM_SUCCESS)
+    {
+        return PLDM_ERROR;
+    }
+
+    return rc;
+}
+
+void pldm::responder::oem_ibm_platform::Handler::sendCodeUpdateEvent(
+    EffecterId effecterId, codeUpdateStateValues opState,
+    codeUpdateStateValues previousOpState)
+{
+    std::vector<uint8_t> effecterEventDataVec{};
+    size_t effecterEventSize = PLDM_EFFECTER_EVENT_DATA_MIN_LENGTH + 1;
+    effecterEventDataVec.resize(effecterEventSize);
+
+    auto eventData = reinterpret_cast<struct pldm_effecter_event_data*>(
+        effecterEventDataVec.data());
+    eventData->effecter_id = effecterId;
+    eventData->effecter_event_class_type = PLDM_EFFECTER_OP_STATE;
+
+    auto opStateEffecterEventData =
+        reinterpret_cast<struct pldm_effecter_event_effecter_op_state*>(
+            effecterEventDataVec.data());
+    opStateEffecterEventData->present_op_state = opState;
+    opStateEffecterEventData->previous_op_state = previousOpState;
+
+    std::vector<uint8_t> requestMsg(sizeof(pldm_msg_hdr) +
+                                    PLDM_PLATFORM_EVENT_MESSAGE_MIN_REQ_BYTES +
+                                    effecterEventDataVec.size());
+
+    auto instanceId = requester.getInstanceId(mctp_eid);
+
+    auto rc = encodeEventMsg(PLDM_EFFECTER_EVENT, effecterEventDataVec,
+                             requestMsg, instanceId);
+
+    if (rc != PLDM_SUCCESS)
+    {
+        std::cerr << "Failed to encode state sensor event, rc = " << rc
+                  << std::endl;
+        return;
+    }
+
+    rc = sendEventToHost(requestMsg);
+
+    if (rc != PLDM_SUCCESS)
+    {
+        std::cerr << "Failed to send event to host: "
+                  << "rc=" << rc << std::endl;
+    }
+
+    requester.markFree(mctp_eid, instanceId);
+
+    return;
+}
+
+int encodeEventMsg(uint8_t eventType, const std::vector<uint8_t>& eventDataVec,
+                   std::vector<uint8_t>& requestMsg, uint8_t instanceId)
+{
+    auto request = reinterpret_cast<pldm_msg*>(requestMsg.data());
+
+    auto rc = encode_platform_event_message_req(
+        instanceId, 1 /*formatVersion*/, 0 /*tId*/, eventType,
+        eventDataVec.data(), eventDataVec.size(), request,
+        eventDataVec.size() + PLDM_PLATFORM_EVENT_MESSAGE_MIN_REQ_BYTES);
+
+    return rc;
 }
 
 } // namespace oem_ibm_platform
