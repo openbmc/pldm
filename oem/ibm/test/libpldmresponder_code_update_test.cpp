0385f7853d80a5e80843f5d4ef723d0ec380afbd
Varsha Kaverappa <vkaverap@in.ibm.com>
2020-09-10 06:15:42 -0500

    PLDM: Platform: event framework to send events to PHYP
    This commit consts of APIs that will be used to send code update
    event change/update PlatformEventMessage to Phyp for different
    stages of inband code update progress.
    The event is a state sensor event.
    
    Signed-off-by: Varsha Kaverappa <vkaverap@in.ibm.com>
    Change-Id: I9b4b0493dd6886805044011d994df58b31f80a6e

---
+++ oem/ibm/test/libpldmresponder_code_update_test.cpp
@@ -0,0 +1,51 @@
+#include "libpldmresponder/code_update_event.hpp"
+
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+using namespace pldm::code_update_event;
+
+TEST(EncodeCodeUpdate, testGoodRequest)
+{
+    size_t sensorEventSize = PLDM_SENSOR_EVENT_DATA_MIN_LENGTH + 1;
+    std::vector<uint8_t> sensorEventDataVec{};
+    sensorEventDataVec.resize(sensorEventSize);
+
+    auto eventData = reinterpret_cast<struct pldm_sensor_event_data*>(
+        sensorEventDataVec.data());
+    eventData->sensor_id = 32768;
+    eventData->sensor_event_class_type = PLDM_STATE_SENSOR_STATE;
+    uint8_t stateSenserEventPtr = 0x0;
+
+    std::cerr << "[          ] random here2 = " << std::endl;
+
+    auto stateSensorEventData =
+        reinterpret_cast<struct pldm_sensor_event_state_sensor_state*>(
+            sensorEventDataVec.data());
+    stateSensorEventData->sensor_offset = stateSenserEventPtr;
+    stateSensorEventData->event_state = END;
+    stateSensorEventData->previous_event_state = START;
+
+    std::vector<uint8_t> requestMsg(sizeof(pldm_msg_hdr) +
+                                    PLDM_PLATFORM_EVENT_MESSAGE_MIN_REQ_BYTES +
+                                    sensorEventDataVec.size());
+    std::cerr << "[          ] random here = " << std::endl;
+
+    auto rc =
+        encodeEventMsg(PLDM_SENSOR_EVENT, sensorEventDataVec, requestMsg, 0x1);
+
+    EXPECT_EQ(rc, PLDM_SUCCESS);
+}
+
+TEST(EncodeCodeUpdate, testBadRequest)
+{
+    std::vector<uint8_t> requestMsg;
+
+    std::vector<uint8_t> sensorEventDataVec{};
+
+    auto rc =
+        encodeEventMsg(PLDM_SENSOR_EVENT, sensorEventDataVec, requestMsg, 0x1);
+
+    EXPECT_EQ(rc, PLDM_ERROR_INVALID_DATA);
+}
