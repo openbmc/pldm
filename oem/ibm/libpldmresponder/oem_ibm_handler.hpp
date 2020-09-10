91f7f6a69da268bbc10498dddb02c9a467efb92c
Varsha Kaverappa <vkaverap@in.ibm.com>
2020-09-10 06:15:42 -0500

    PLDM: Platform: event framework to send events to PHYP
    This commit consts of APIs that will be used to send code update
    event change/update PlatformEventMessage to Phyp for different
    stages of inband code update progress.
    The event is a state sensor event.
    
    Signed-off-by: Varsha Kaverappa <vkaverap@in.ibm.com>
    Change-Id: I9b4b0493dd6886805044011d994df58b31f80a6e

--- oem/ibm/libpldmresponder/oem_ibm_handler.hpp
+++ oem/ibm/libpldmresponder/oem_ibm_handler.hpp
@@ -1,9 +1,14 @@
 #pragma once
 
+#include "libpldm/platform.h"
+
 #include "inband_code_update.hpp"
 #include "libpldmresponder/oem_handler.hpp"
+#include "libpldmresponder/pdr_utils.hpp"
 #include "libpldmresponder/platform.hpp"
 
+#include <memory>
+#include <vector>
 namespace pldm
 {
 
@@ -17,13 +22,28 @@ static constexpr auto PLDM_OEM_IBM_BOOT_STATE = 32769;
 static constexpr auto PLDM_OEM_IBM_FIRMWARE_UPDATE_STATE = 32768;
 constexpr uint16_t ENTITY_INSTANCE_0 = 0;
 constexpr uint16_t ENTITY_INSTANCE_1 = 1;
+
+enum codeUpdateStateValues
+{
+    START = 0x1,
+    END = 0x2,
+    FAIL = 0x3,
+    ABORT = 0x4,
+    ACCEPT = 0x5,
+    REJECT = 0x6,
+};
+
+using EffecterId = uint16_t;
+
 class Handler : public oem_platform::Handler
 {
   public:
     Handler(const pldm::utils::DBusHandler* dBusIntf,
-            pldm::responder::CodeUpdate* codeUpdate) :
+            pldm::responder::CodeUpdate* codeUpdate, int mctp_fd,
+            uint8_t mctp_eid, Requester& requester) :
         oem_platform::Handler(dBusIntf),
-        codeUpdate(codeUpdate), platformHandler(nullptr)
+        codeUpdate(codeUpdate), platformHandler(nullptr), mctp_fd(mctp_fd),
+        mctp_eid(mctp_eid), requester(requester)
     {
         codeUpdate->setVersions();
     }
@@ -59,13 +79,50 @@ class Handler : public oem_platform::Handler
      */
     void buildOEMPDR(pdr_utils::Repo& repo);
 
+    /** @brief Method to send code update event to host
+     *  @param[in] sensorId - sendor ID
+     *  @param[in] opState - new code update operational state
+     *  @param[in] previousOpState - previous code update operational state
+     *  @return void
+     */
+    void sendCodeUpdateEvent(EffecterId effecterId,
+                             codeUpdateStateValues opState,
+                             codeUpdateStateValues previousOpState);
+
+    /** @brief Method to send encoded request msg of code update event to host
+     *  @param[in] requestMsg - encoded request msg
+     *  @return PLDM status code
+     */
+    int sendEventToHost(std::vector<uint8_t>& requestMsg);
+
     ~Handler()
     {}
 
     pldm::responder::CodeUpdate* codeUpdate; //!< pointer to CodeUpdate object
     pldm::responder::platform::Handler*
         platformHandler; //!< pointer to PLDM platform handler
+
+    /** @brief fd of MCTP communications socket */
+    int mctp_fd;
+
+    /** @brief MCTP EID of host firmware */
+    uint8_t mctp_eid;
+
+    /** @brief reference to Requester object, primarily used to access API to
+     *  obtain PLDM instance id.
+     */
+    Requester& requester;
 };
+
+/** @brief Method to encode code update event msg
+ *  @param[in] eventType - type of event
+ *  @param[in] eventDataVec - vector of event data to be sent to host
+ *  @param[in] instanceId - instance ID
+ *  @param[in/out] requestMsg - request msg to be encoded
+ *  @return PLDM status code
+ */
+int encodeEventMsg(uint8_t eventType, const std::vector<uint8_t>& eventDataVec,
+                   std::vector<uint8_t>& requestMsg, uint8_t instanceId);
 
 } // namespace oem_ibm_platform
 
