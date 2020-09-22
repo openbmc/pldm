6ee5fa2b48ca4e3600b9430de7943d20891c21d7
Sagar Srinivas <sagar.srinivas@ibm.com>
2020-09-22 10:03:35 -0500

    Platform: implement SetEffecter corresponding to code update actions
    Signed-off-by: Sagar Srinivas <sagar.srinivas@ibm.com>
    Change-Id: I2e86177efe4c8924069cc400b79e4fc4b8983df4

--- oem/ibm/libpldmresponder/oem_ibm_handler.cpp
+++ oem/ibm/libpldmresponder/oem_ibm_handler.cpp
@@ -1,7 +1,6 @@
 #include "oem_ibm_handler.hpp"
 
 #include "libpldm/requester/pldm.h"
-
 #include "libpldmresponder/pdr_utils.hpp"
 
 namespace pldm
@@ -57,6 +56,36 @@ int pldm::responder::oem_ibm_platform::Handler::
             {
                 rc = setBootSide(entityInstance, currState, stateField,
                                  codeUpdate);
+            }
+            else if (entityType == 33 && stateSetId == 32768)
+            {
+                if (stateField[currState].effecter_state == START)
+                {
+                    rc = codeUpdate->setRequestedApplyTime();
+                }
+                else if (stateField[currState].effecter_state == END)
+                {
+                    // int  retc = assembleImage(LID_STAGING_DIR);
+                    // if (retc == PLDM_SUCCESS)
+                    rc = codeUpdate->setRequestedActivation();
+                    // else
+                    // std::cerr << "Image assembly Failed ERROR:" << retc
+                    //        << "\n";
+                }
+                else if (stateField[currState].effecter_state == FAIL)
+                {}
+                else if (stateField[currState].effecter_state == ABORT)
+                {
+                    // Call API to clear lids
+                }
+                else if (stateField[currState].effecter_state == ACCEPT)
+                {
+                    // TODO Set new Dbus property provided by code update app
+                }
+                else if (stateField[currState].effecter_state == REJECT)
+                {
+                    // TODO Set new Dbus property provided by code update app
+                }
             }
             else
             {
