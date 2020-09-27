e8c0a33e7950a33076601f461cdb1099b677a39d
Varsha Kaverappa <vkaverap@in.ibm.com>
2020-09-27 12:39:22 -0500

    pldm: inband code update: clear the image LIDs when pldm daemon comes up
    Clear the LID_STAGING_DIR path when pldm daemon comes up. This function
    will also be used during code update process.
    
    Signed-off-by: Varsha Kaverappa <vkaverap@in.ibm.com>
    Change-Id: I2a757caaa3e204e4089235d9e3baf6d425f5bec5

--- oem/ibm/test/libpldmresponder_fileio_test.cpp
+++ oem/ibm/test/libpldmresponder_fileio_test.cpp
@@ -10,6 +10,8 @@
 #include "libpldmresponder/file_table.hpp"
 #include "xyz/openbmc_project/Common/error.hpp"
 
+#include <sys/stat.h>
+
 #include <nlohmann/json.hpp>
 #include <phosphor-logging/elog-errors.hpp>
 
@@ -924,4 +926,23 @@ TEST(readFileByType, testReadFile)
     ASSERT_EQ(length, in.size());
     ASSERT_EQ(response.size(), in.size());
     ASSERT_EQ(std::equal(in.begin(), in.end(), response.begin()), true);
+}
+
+TEST(clearDirPath, testClearDirPath)
+{
+    char dirPath[] = "/temp/testClearDir/";
+    fs::path dir(dirPath);
+    fs::create_directories(dir);
+    struct stat buffer;
+    ASSERT_EQ(stat(dirPath, &buffer), 0);
+    char filePath[] = "/temp/testClearDir/file.txt";
+    std::ofstream file(filePath);
+    ASSERT_EQ(stat(filePath, &buffer), 0);
+
+    std::unique_ptr<oem_platform::Handler> oemPlatformHandler{};
+    oem_ibm::Handler handler(oemPlatformHandler.get());
+    auto rc = handler.clearDirPath(dirPath);
+    ASSERT_EQ(rc, PLDM_SUCCESS);
+    ASSERT_EQ(stat(filePath, &buffer), -1);
+    ASSERT_EQ(stat(dirPath, &buffer), 0);
 }
