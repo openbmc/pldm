478e3c33635da45c57c49680309450cb15ac8e8f
Varsha Kaverappa <vkaverap@in.ibm.com>
2020-09-28 01:40:22 -0500

    Inband code update: send PLDM events up for all code update states
    send pldm events to Host to respond to the code update commands.
    
    Signed-off-by: Varsha Kaverappa <vkaverap@in.ibm.com>
    Change-Id: Iae90a4663d4c5ed20b3a8a6222edb6055a533589

--- oem/ibm/libpldmresponder/oem_ibm_handler.cpp
+++ oem/ibm/libpldmresponder/oem_ibm_handler.cpp
@@ -45,7 +45,7 @@ int pldm::responder::oem_ibm_platform::Handler::
     OemSetStateEffecterStatesHandler(
         uint16_t entityType, uint16_t entityInstance, uint16_t stateSetId,
         uint8_t compEffecterCnt,
-        const std::vector<set_effecter_state_field>& stateField)
+        std::vector<set_effecter_state_field>& stateField)
 {
     int rc = PLDM_SUCCESS;
 
@@ -75,6 +75,11 @@ int pldm::responder::oem_ibm_platform::Handler::
                     // else
                     // std::cerr << "Image assembly Failed ERROR:" << retc
                     //        << "\n";
+                    /*auto return = call adriana API here << to be added by
+                    varsha /if(return = A)
+                    {
+                        stateField[currState].effecter_state = ABORT;
+                    }*/
                 }
                 else if (stateField[currState].effecter_state == ABORT)
                 {
@@ -82,7 +87,6 @@ int pldm::responder::oem_ibm_platform::Handler::
                     std::unique_ptr<oem_platform::Handler> oemPlatformHandler{};
                     oem_ibm::Handler handler(oemPlatformHandler.get());
                     rc = handler.clearDirPath(LID_STAGING_DIR);
-                    std::cout << "Property Set" << std::endl;
                     // rc = codeUpdate->clearLids(platformHandler);
                 }
                 else if (stateField[currState].effecter_state == ACCEPT)
@@ -93,6 +97,8 @@ int pldm::responder::oem_ibm_platform::Handler::
                 {
                     // TODO Set new Dbus property provided by code update app
                 }
+                auto effecterId = getNextEffecterId();
+                sendCodeUpdateEvent(effecterId, stateField, compEffecterCnt);
             }
             else
             {
