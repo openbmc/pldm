3d29c0693235c2a4d29a726b12dafd430a28c338
Sampa Misra <sampmisr@in.ibm.com>
2020-10-21 08:34:01 -0500

    pldm inband codeupdate: misc fixes
    Change-Id: I8e5d7353a1499e47af66ade64bbfcf08abe31513
    Signed-off-by: Sampa Misra <sampmisr@in.ibm.com>

--- oem/ibm/libpldmresponder/file_io_type_lid.hpp
+++ oem/ibm/libpldmresponder/file_io_type_lid.hpp
@@ -32,6 +32,7 @@ class LidHandler : public FileHandler
         FileHandler(fileHandle), lidType(lidType)
     {
         sideToRead = permSide ? Pside : Tside;
+        isPatchDir = false;
         std::string dir = permSide ? LID_ALTERNATE_DIR : LID_RUNNING_DIR;
         std::stringstream stream;
         stream << std::hex << fileHandle;
@@ -42,6 +43,7 @@ class LidHandler : public FileHandler
         if (fs::is_regular_file(patch))
         {
             lidPath = patch;
+            isPatchDir = true;
         }
         else
         {
@@ -61,10 +63,22 @@ class LidHandler : public FileHandler
                 dynamic_cast<pldm::responder::oem_ibm_platform::Handler*>(
                     oemPlatformHandler);
             std::string dir = LID_ALTERNATE_DIR;
+            if(isPatchDir)
+            {
+                dir = LID_ALTERNATE_PATCH_DIR;
+            }
+
             if (oemIbmPlatformHandler->codeUpdate->fetchCurrentBootSide() ==
                 sideToRead)
             {
-                dir = LID_RUNNING_DIR;
+                if(isPatchDir)
+                {
+                    dir = LID_RUNNING_PATCH_DIR;
+                }
+                else
+                {
+                    dir = LID_RUNNING_DIR;
+                }
             }
             else if (oemIbmPlatformHandler->codeUpdate
                          ->isCodeUpdateInProgress())
@@ -102,9 +116,33 @@ class LidHandler : public FileHandler
                 lidPath = std::move(dir) + '/' + lidName;
             }
         }
+        std::cout << "got writeFromMemory() for LID " << lidPath.c_str() 
+                  << " and offset " << offset << " and length " << length 
+                  << "\n";
+        bool fileExists = fs::exists(lidPath);
+        int flags{};
+        if (fileExists)
+        {
+            flags = O_RDWR;
+        }
+        else
+        {
+            flags = O_WRONLY | O_CREAT | O_TRUNC | O_SYNC;
+        }
+        auto fd = open(lidPath.c_str(),flags);
+        if (fd == -1)
+        {
+            std::cerr << "Could not open file for writing  " 
+                      << lidPath.c_str() << "\n";
+            return PLDM_ERROR;
+        }
+        close(fd);
         rc = transferFileData(lidPath, false, offset, length, address);
+        //rc = transferFileData(fd,false, offset, length, address);
+        //close(fd);
         if (rc != PLDM_SUCCESS)
         {
+            std::cout << "writeFileFromMemory failed with rc= " << rc << " \n";
             return rc;
         }
         if (lidType == PLDM_FILE_TYPE_LID_MARKER)
@@ -126,7 +164,9 @@ class LidHandler : public FileHandler
         }
         else if (codeUpdateInProgress)
         {
-            rc = assembleImage(lidPath);
+            std::cout << "calling processCodeUpdateLid from writeFromMemory \n";
+            rc = processCodeUpdateLid(lidPath);
+            std::cout << "processCodeUpdateLid returned " << (uint32_t)rc << "\n";
         }
         return rc;
     }
@@ -163,7 +203,56 @@ class LidHandler : public FileHandler
                 lidPath = std::move(dir) + '/' + lidName;
             }
         }
-        std::ios_base::openmode flags =
+        std::cout << "got write() call for LID " << lidPath.c_str() 
+                  << " and offset " << offset << " and length " << length 
+                  << "\n";
+
+        bool fileExists = fs::exists(lidPath);
+        int flags{};
+        if(fileExists)
+        {
+            flags = O_RDWR;
+            size_t fileSize = fs::file_size(lidPath);
+            if (offset > fileSize)
+            {
+                std::cerr << "Offset exceeds file size, OFFSET=" << offset
+                          << " FILE_SIZE=" << fileSize << "\n";
+                return PLDM_DATA_OUT_OF_RANGE;
+            }
+        }
+        else
+        {
+            flags = O_WRONLY | O_CREAT | O_TRUNC | O_SYNC;
+            if(offset>0)
+            {
+                std::cerr << "Offset is non zero in a new file \n";
+                return PLDM_DATA_OUT_OF_RANGE;
+            }
+        }
+        auto fd = open(lidPath.c_str(),flags);
+        if (fd == -1)
+        {
+            std::cerr << "could not open file " << lidPath.c_str() << "\n";
+            return PLDM_ERROR;
+        }
+        rc = lseek(fd,offset, SEEK_SET);
+        if (rc == -1)
+        {
+            std::cerr << "lseek failed, ERROR=" << errno << ", OFFSET=" << offset
+                  << "\n";
+            return PLDM_ERROR;
+        }
+        std::cout << "lseek returned " << rc << "\n";
+        rc = ::write(fd,buffer,length);
+        if(rc == -1)
+        {
+            std::cerr << "file write failed, ERROR=" << errno
+               << ", LENGTH=" << length << ", OFFSET=" << offset << "\n";
+            return PLDM_ERROR;
+        }
+        close(fd);
+
+       /* std::ios_base::openmode flags =
             std::ios::in | std::ios::out | std::ios::binary;
         if (!fs::exists(lidPath))
         {
@@ -172,6 +261,7 @@ class LidHandler : public FileHandler
         else
         {
             size_t fileSize = fs::file_size(lidPath);
+        
             if (offset > fileSize)
             {
                 std::cerr << "Offset exceeds file size, OFFSET=" << offset
@@ -183,7 +273,7 @@ class LidHandler : public FileHandler
         std::ofstream stream(lidPath, flags);
         stream.seekp(offset);
         stream.write(buffer, length);
-        stream.close();
+        stream.close();*/
 
         if (lidType == PLDM_FILE_TYPE_LID_MARKER)
         {
@@ -204,9 +294,10 @@ class LidHandler : public FileHandler
         }
         else if (codeUpdateInProgress)
         {
-            rc = assembleImage(lidPath);
+            std::cout << "calling processCodeUpdateLid from write \n";
+            rc = processCodeUpdateLid(lidPath);
+            std::cout << "processCodeUpdateLid returned " << (uint32_t)rc << "\n";
         }
-
         return rc;
     }
 
@@ -236,12 +327,6 @@ class LidHandler : public FileHandler
         return PLDM_ERROR_UNSUPPORTED_PLDM_CMD;
     }
 
-    /** @brief Method to assemble the code update tarball and trigger the
-     *         phosphor software manager to create a version interface.
-     *  @return PLDM status code
-     */
-    int assembleFinalImage();
-
     /** @brief LidHandler destructor
      */
     ~LidHandler()
@@ -252,54 +337,7 @@ class LidHandler : public FileHandler
     std::string sideToRead;
     static inline MarkerLIDremainingSize markerLIDremainingSize;
     uint8_t lidType;
-
-  private:
-    /** @brief Method to assemble code update images from LID files.
-     *  @param[in] filePath - Path to the file to use for the image creation.
-     *  @return PLDM status code
-     */
-    int assembleImage(const std::string& filePath);
-
-    /** @brief Directory where the image files are stored as they are built */
-    std::string imageDirPath = fs::path(LID_STAGING_DIR) / "image";
-
-    /** @brief Directory where the lid files without a header are stored */
-    std::string lidDirPath = fs::path(LID_STAGING_DIR) / "lid";
-
-    /** @brief Directory where the code update tarball files are stored */
-    std::string updateDirPath = fs::path(LID_STAGING_DIR) / "update";
-
-    /** @brief The file name of the code update tarball */
-    std::string tarImageName = "image.tar";
-
-    /** @brief The path to the code update tarball file */
-    std::string tarImagePath = fs::path(imageDirPath) / tarImageName;
-
-    /** @brief The file name of the hostfw image */
-    std::string hostfwImageName = "image-host-fw";
-
-    /** @brief The path to the hostfw image */
-    std::string hostfwImagePath = fs::path(imageDirPath) / hostfwImageName;
-
-    /** @brief The path to the tarball file expected by the phosphor software
-     *         manager */
-    std::string updateImagePath = fs::path("/tmp/images") / tarImageName;
-
-    /** @struct lidHeader
-     *  @brief LID header structure
-     */
-    struct lidHeader
-    {
-        uint16_t magicNumber;
-        uint16_t headerVersion;
-        uint32_t lidNumber;
-        uint32_t lidDate;
-        uint16_t lidTime;
-        uint16_t lidClass;
-        uint32_t lidCrc;
-        uint32_t lidSize;
-        uint32_t headerSize;
-    };
+    bool isPatchDir;
 };
 
 } // namespace responder
