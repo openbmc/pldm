9d272d4e84cfd9b4ef971ee3963083302c2157b9
George Liu <liuxiwei@inspur.com>
2020-12-22 16:27:16 +0800

    Handle entity association records from host
    - Get the PDRs from host, traverse the entity association structure
      and construct the D-Bus object path according to the entity type ID
      and container type ID.
    
    - eg:
     /xyz/openbmc_project/inventory/system1/chassis1/io_board1/powersupply1
     /xyz/openbmc_project/inventory/system1/chassis1/io_board1/powersupply2
     /xyz/openbmc_project/inventory/system1/chassis2/io_board2/powersupply1
     /xyz/openbmc_project/inventory/system1/chassis2/io_board2/powersupply2
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I1c606ea943624f2601be4f5f6b5c30d1bf71cd11

---
+++ host-bmc/test/utils_test.cpp
@@ -0,0 +1,96 @@
+#include "libpldm/pdr.h"
+
+#include "../utils.hpp"
+
+#include <filesystem>
+
+#include <gtest/gtest.h>
+
+namespace fs = std::filesystem;
+using namespace pldm;
+using namespace pldm::hostbmc::utils;
+
+TEST(EntityAssociation, addObjectPathEntityAssociations)
+{
+    ObjectPathMaps retObjectMaps = {
+        {"/xyz/openbmc_project/inventory/system1/chassis1/io_board1/"
+         "powersupply1",
+         {120, 1, 1}},
+        {"/xyz/openbmc_project/inventory/system1/chassis1/io_board2/"
+         "powersupply2",
+         {120, 2, 1}},
+        {"/xyz/openbmc_project/inventory/system1/chassis2/motherboard1/dimm1",
+         {66, 1, 1}},
+        {"/xyz/openbmc_project/inventory/system1/chassis2/motherboard2/dimm2",
+         {66, 2, 1}}};
+
+    const EntityAssociations entityAssociations = {
+        {{45, 1, 1}, {60, 1, 1}, {60, 2, 1}},
+        {{60, 1, 1}, {120, 1, 1}},
+        {{60, 2, 1}, {120, 2, 1}},
+        {{45, 2, 1}, {64, 1, 1}, {64, 2, 1}},
+        {{64, 1, 1}, {66, 1, 1}},
+        {{64, 2, 1}, {66, 2, 1}}};
+
+    pldm_entity entities[11]{};
+
+    entities[0].entity_type = 11521;
+    entities[1].entity_type = 45;
+    entities[2].entity_type = 45;
+    entities[3].entity_type = 60;
+    entities[4].entity_type = 60;
+    entities[5].entity_type = 120;
+    entities[6].entity_type = 120;
+    entities[7].entity_type = 64;
+    entities[8].entity_type = 64;
+    entities[9].entity_type = 66;
+    entities[10].entity_type = 66;
+
+    auto tree = pldm_entity_association_tree_init();
+
+    auto l1 = pldm_entity_association_tree_add(tree, &entities[0], nullptr,
+                                               PLDM_ENTITY_ASSOCIAION_PHYSICAL);
+
+    auto l2a = pldm_entity_association_tree_add(
+        tree, &entities[1], l1, PLDM_ENTITY_ASSOCIAION_PHYSICAL);
+    auto l2b = pldm_entity_association_tree_add(
+        tree, &entities[2], l1, PLDM_ENTITY_ASSOCIAION_PHYSICAL);
+
+    auto l3a = pldm_entity_association_tree_add(
+        tree, &entities[3], l2a, PLDM_ENTITY_ASSOCIAION_PHYSICAL);
+    auto l3b = pldm_entity_association_tree_add(
+        tree, &entities[4], l2a, PLDM_ENTITY_ASSOCIAION_PHYSICAL);
+
+    pldm_entity_association_tree_add(tree, &entities[5], l3a,
+                                     PLDM_ENTITY_ASSOCIAION_PHYSICAL);
+    pldm_entity_association_tree_add(tree, &entities[6], l3b,
+                                     PLDM_ENTITY_ASSOCIAION_PHYSICAL);
+
+    auto l5a = pldm_entity_association_tree_add(
+        tree, &entities[7], l2b, PLDM_ENTITY_ASSOCIAION_PHYSICAL);
+    auto l5b = pldm_entity_association_tree_add(
+        tree, &entities[8], l2b, PLDM_ENTITY_ASSOCIAION_PHYSICAL);
+
+    pldm_entity_association_tree_add(tree, &entities[9], l5a,
+                                     PLDM_ENTITY_ASSOCIAION_PHYSICAL);
+    pldm_entity_association_tree_add(tree, &entities[10], l5b,
+                                     PLDM_ENTITY_ASSOCIAION_PHYSICAL);
+
+    ObjectPathMaps objPathMap;
+    updateEntityAssociation(entityAssociations, tree, objPathMap);
+
+    EXPECT_EQ(objPathMap.size(), 4);
+    EXPECT_EQ(retObjectMaps.size(), 4);
+
+    for (auto& obj : objPathMap)
+    {
+        EXPECT_NE(retObjectMaps.find(obj.first), retObjectMaps.end());
+        EXPECT_EQ(obj.second.entity_type, retObjectMaps[obj.first].entity_type);
+        EXPECT_EQ(obj.second.entity_instance_num,
+                  retObjectMaps[obj.first].entity_instance_num);
+        EXPECT_EQ(obj.second.entity_container_id,
+                  retObjectMaps[obj.first].entity_container_id);
+    }
+
+    pldm_entity_association_tree_destroy(tree);
+}
