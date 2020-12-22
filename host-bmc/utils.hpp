08f94b9772b83e8c7904e16a519e45b15b9b7227
George Liu <liuxiwei@inspur.com>
2020-12-22 16:27:16 +0800

    Handle entity association records from host
    - Get the PDRs from host, traverse the entity association structure
      and construct the D-Bus object path according to the entity type ID
      and container type ID.
    
    - eg:
     /xyz/openbmc_project/inventory/system0/chassis0/io_board0/powersupply0
     /xyz/openbmc_project/inventory/system0/chassis0/io_board0/powersupply1
     /xyz/openbmc_project/inventory/system0/chassis1/io_board1/powersupply0
     /xyz/openbmc_project/inventory/system0/chassis1/io_board1/powersupply1
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I1c606ea943624f2601be4f5f6b5c30d1bf71cd11

---
+++ host-bmc/utils.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "libpldm/entity.h"
+#include "libpldm/pdr.h"
+
+#include <filesystem>
+#include <map>
+#include <queue>
+#include <string>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+namespace pldm
+{
+using ObjectPath = fs::path;
+using EntityName = std::string;
+using EntityType = uint16_t;
+
+using Entities = std::vector<pldm_entity>;
+using EntityAssociations = std::vector<Entities>;
+using ObjectPathMaps = std::map<ObjectPath, pldm_entity>;
+
+const std::map<EntityType, EntityName> entityMaps = {
+    {PLDM_ENTITY_SYSTEM_CHASSIS, "chassis"},
+    {PLDM_ENTITY_BOARD, "io_board"},
+    {PLDM_ENTITY_SYS_BOARD, "motherboard"},
+    {PLDM_ENTITY_POWER_SUPPLY, "powersupply"},
+    {PLDM_ENTITY_MEMORY_MODULE, "dimm"},
+    {PLDM_ENTITY_PROC, "cpu"},
+    {PLDM_ENTITY_SYSTEM_LOGICAL, "system"}};
+
+namespace hostbmc
+{
+namespace utils
+{
+
+/** @brief Vector a entity name to pldm_entity from entity association tree
+ *  @param[in]  entityAssoc    - Vector of associated pldm entities
+ *  @param[in]  entityTree     - entity association tree
+ *  @param[out] objPathMap     - maps an object path to pldm_entity from the
+ *                               BMC's entity association tree
+ *  @return
+ */
+void updateEntityAssociation(const EntityAssociations& entityAssoc,
+                             pldm_entity_association_tree* entityTree,
+                             ObjectPathMaps& objPathMap);
+
+} // namespace utils
+} // namespace hostbmc
+} // namespace pldm
