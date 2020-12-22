d07113e48081ba2d98e3f37e95cbc0db24197b4c
George Liu <liuxiwei@inspur.com>
2020-12-22 16:27:16 +0800

    Handle entity association records from host
    - Get the PDRs from host, traverse the entity association structure
      and construct the D-Bus object path according to the entity type ID
      and container type ID.
    
    - eg:
     /xyz/openbmc_project/inventory/system1/chassis1/io_board1/powersupply1
     /xyz/openbmc_project/inventory/system1/chassis1/io_board1/powersupply2
     /xyz/openbmc_project/inventory/system1/chassis2/io_board2/powersupply1
     /xyz/openbmc_project/inventory/system1/chassis2/io_board2/powersupply2
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I1c606ea943624f2601be4f5f6b5c30d1bf71cd11

--- host-bmc/host_pdr_handler.cpp
+++ host-bmc/host_pdr_handler.cpp
@@ -204,14 +204,21 @@ void HostPDRHandler::mergeEntityAssociations(const std::vector<uint8_t>& pdr)
             return;
         }
 
+        Entities entityAssoc;
+        entityAssoc.push_back(pNode);
         for (size_t i = 1; i < numEntities; ++i)
         {
-            pldm_entity_association_tree_add(
+            auto node = pldm_entity_association_tree_add(
                 entityTree, &entities[i], entities[i].entity_instance_num,
                 pNode, entityPdr->association_type, true);
             merged = true;
+            entityAssoc.push_back(node);
         }
         mergedHostParents = true;
+        if (merged)
+        {
+            entityAssociations.push_back(entityAssoc);
+        }
     }
 
     if (merged)
@@ -495,6 +502,8 @@ void HostPDRHandler::processHostPDRs(mctp_eid_t /*eid*/,
     }
     if (!nextRecordHandle)
     {
+        updateEntityAssociation(entityAssociations, entityTree, objPathMap);
+
         /*received last record*/
         this->parseStateSensorPDRs(stateSensorPDRs, tlpdrInfo);
         if (isHostUp())
@@ -503,6 +512,9 @@ void HostPDRHandler::processHostPDRs(mctp_eid_t /*eid*/,
         }
         stateSensorPDRs.clear();
         tlpdrInfo.clear();
+
+        entityAssociations.clear();
+
         if (merged)
         {
             merged = false;
