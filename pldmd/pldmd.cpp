234916cf6b31723c940047baae6581dd2866e15f
George Liu <liuxiwei@inspur.com>
2020-12-25 15:24:33 +0800

    Handle FRU records from host
    - Get FRU record table from host and traverse the entity structure
      from the FRU record set PDR, and create D-Bus object.
    
    - When the FRU field type is a PLDM_OEM_FRU_FIELD_TYPE_LOCATION_CODE
      , the location code is used to  populate the
      xyz.openbmc_project.Inventory.Decorator.LocationCode D-Bus
      interface for the corresponding FRU.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I482c4d371f76b4881109ef420dfd9543e1aa810b

--- pldmd/pldmd.cpp
+++ pldmd/pldmd.cpp
@@ -167,6 +167,8 @@ int main(int argc, char** argv)
 
     auto event = Event::get_default();
     auto& bus = pldm::utils::DBusHandler::getBus();
+    sdbusplus::server::manager::manager objManager(
+        bus, "/xyz/openbmc_project/inventory");
     dbus_api::Requester dbusImplReq(bus, "/xyz/openbmc_project/pldm");
 
     Invoker invoker{};
