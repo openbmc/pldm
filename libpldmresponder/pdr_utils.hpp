7f7c90598908456eb8fc9dbf8ba71c72c2427dc0
George Liu <liuxiwei@inspur.com>
2020-12-25 15:24:33 +0800

    Handle FRU records from host
    - Get FRU record table from host and traverse the entity structure
      from the FRU record set PDR, and create D-Bus object.
    
    - When the FRU field type is a PLDM_OEM_FRU_FIELD_TYPE_LOCATION_CODE
      , the location code is used to  populate the
      xyz.openbmc_project.Inventory.Decorator.LocationCode D-Bus
      interface for the corresponding FRU.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I482c4d371f76b4881109ef420dfd9543e1aa810b

--- libpldmresponder/pdr_utils.hpp
+++ libpldmresponder/pdr_utils.hpp
@@ -22,13 +22,10 @@ namespace fs = std::filesystem;
 
 namespace pldm
 {
-
 namespace responder
 {
-
 namespace pdr_utils
 {
-
 /** @struct Type ID associated with pdr
  *
  */
@@ -36,6 +33,22 @@ enum class TypeId
 {
     PLDM_EFFECTER_ID,
     PLDM_SENSOR_ID
+};
+
+struct FruTLV
+{
+    uint8_t fruFieldType;
+    uint8_t fruFieldLen;
+    std::vector<uint8_t> fruFieldValue;
+};
+
+struct FruRecordDataFormat
+{
+    uint16_t fruRSI;
+    uint8_t fruRecType;
+    uint8_t fruNum;
+    uint8_t fruEncodeType;
+    std::vector<FruTLV> fruTLV;
 };
 
 /** @struct PdrEntry
@@ -214,6 +227,18 @@ class Repo : public RepoInterface
 std::tuple<pldm::pdr::TerminusHandle, pldm::pdr::SensorID,
            pldm::pdr::SensorInfo>
     parseStateSensorPDR(const std::vector<uint8_t>& stateSensorPdr);
+
+/** @brief Parse FRU record table and return the vector of the FRU record data
+ *         format structure
+ *
+ *  @param[in] fruData - fru data
+ *  @param[in] fruLen  - fru len
+ *
+ *  @return std::vector<FruRecordDataFormat> - the vector of the FRU record data
+ *          format structure
+ */
+std::vector<FruRecordDataFormat> parseFruRecordTable(const uint8_t* fruData,
+                                                     size_t fruLen);
 
 } // namespace pdr_utils
 } // namespace responder
