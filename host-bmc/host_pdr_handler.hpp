5e4db579e3ddbf8a4609ce25f7bb267f67ff139a
George Liu <liuxiwei@inspur.com>
2020-12-25 15:24:33 +0800

    Handle FRU records from host
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I482c4d371f76b4881109ef420dfd9543e1aa810b

--- host-bmc/host_pdr_handler.hpp
+++ host-bmc/host_pdr_handler.hpp
@@ -145,6 +145,13 @@ class HostPDRHandler
     void parseStateSensorPDRs(const PDRList& stateSensorPDRs,
                               const TLPDRMap& tlpdrInfo);
 
+    /** @brief Parse FRU record set PDRs
+     *
+     *  @param[in] fruRecordSetPDRs - host fru record set PDRs
+     *
+     */
+    void parseFruRecordSetPDRs(const PDRList& fruRecordSetPDRs);
+
   private:
     /** @brief fetchPDR schedules work on the event loop, this method does the
      *  actual work. This is so that the PDR exchg with the host is async.
@@ -175,6 +182,25 @@ class HostPDRHandler
     void addObjectPathEntityAssociationMap(
         const std::map<std::string, pldm_entity>& entityMaps,
         std::string& path);
+
+    /** @brief Get FRU record table metadata by host
+     *
+     *  @return uint16_t    - total table records
+     */
+    uint16_t getFRURecordTableMetadataByHost();
+
+    /** @brief Get FRU record table by host
+     *
+     *  @return
+     */
+    void getFRURecordTableByHost();
+
+    /** @brief Get FRU Record Set Identifier from FRU Record data Format
+     *  @param[in] fruRecordSetPDRs - fru record set pdr
+     *  @param[in] entity           - PLDM entity information
+     *  @return
+     */
+    uint16_t getRSI(const PDRList& fruRecordSetPDRs, const pldm_entity& entity);
 
     /** @brief fd of MCTP communications socket */
     int mctp_fd;
@@ -220,6 +246,10 @@ class HostPDRHandler
      */
     std::map<EntityName, std::map<EntityName, pldm_entity>>
         entityAssociationMap;
+
+    /** @brief the vector of FRU Record Data Format
+     */
+    std::vector<responder::pdr_utils::FruRecordDataFormat> fruRecordData;
 
     /** @brief Object path and entity association and is only loaded once
      */
