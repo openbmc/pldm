ddc451f99191065342e9738e72ab50cf73bc2980
George Liu <liuxiwei@inspur.com>
2020-12-25 15:24:33 +0800

    Handle FRU records from host
    - Get FRU record table from host and traverse the entity structure
      from the FRU record set PDR, and create D-Bus object.
    
    - When the FRU field type is a PLDM_OEM_FRU_FIELD_TYPE_LOCATION_CODE
      , the location code is used to  populate the
      xyz.openbmc_project.Inventory.Decorator.LocationCode D-Bus
      interface for the corresponding FRU.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I482c4d371f76b4881109ef420dfd9543e1aa810b

--- host-bmc/host_pdr_handler.hpp
+++ host-bmc/host_pdr_handler.hpp
@@ -144,6 +144,13 @@ class HostPDRHandler
     void parseStateSensorPDRs(const PDRList& stateSensorPDRs,
                               const TLPDRMap& tlpdrInfo);
 
+    /** @brief Parse FRU record set PDRs
+     *
+     *  @param[in] fruRecordSetPDRs - host fru record set PDRs
+     *
+     */
+    void parseFruRecordSetPDRs(const PDRList& fruRecordSetPDRs);
+
   private:
     /** @brief fetchPDR schedules work on the event loop, this method does the
      *  actual work. This is so that the PDR exchg with the host is async.
@@ -157,6 +164,25 @@ class HostPDRHandler
      *  @param[in] pdr - entity association pdr
      */
     void mergeEntityAssociations(const std::vector<uint8_t>& pdr);
+
+    /** @brief Get FRU record table metadata by host
+     *
+     *  @return uint16_t    - total table records
+     */
+    uint16_t getFRURecordTableMetadataByHost();
+
+    /** @brief Get FRU record table by host
+     *
+     *  @return
+     */
+    void getFRURecordTableByHost();
+
+    /** @brief Get FRU Record Set Identifier from FRU Record data Format
+     *  @param[in] fruRecordSetPDRs - fru record set pdr
+     *  @param[in] entity           - PLDM entity information
+     *  @return
+     */
+    uint16_t getRSI(const PDRList& fruRecordSetPDRs, const pldm_entity& entity);
 
     /** @brief fd of MCTP communications socket */
     int mctp_fd;
@@ -206,6 +232,14 @@ class HostPDRHandler
     /** @brief maps an entity name to map, maps to entity name to pldm_entity
      */
     EntityAssociations entityAssociations;
+
+    /** @brief the vector of FRU Record Data Format
+     */
+    std::vector<responder::pdr_utils::FruRecordDataFormat> fruRecordData;
+
+    /** @brief Object path and entity association and is only loaded once
+     */
+    bool objPathEntityAssociation;
 };
 
 } // namespace pldm
