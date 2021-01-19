327c368e76135344b65ae68e1cea2d7af55cf3df
George Liu <liuxiwei@inspur.com>
2021-01-19 17:32:07 +0800

    Process state sensors PDRs and update Operational Fault Status
    - When processing the FRU record set PDRs lookup for state sensors
      with state set ID OperationFaultStatus and matching entity
      information (entity type, container ID and entity InstanceNumber).
    
    - Implement xyz.openbmc_project.State.Decorator.OperationalStatus
      interface and initialize Functional property by reading the sensor
      value(the GetStateSensorReadings command).
    
    - Also, Need to update the Functional property for the corresponding
      FRU D-Bus object when the functional status changes on the host.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: If4cc6b6fc293c4eaf035373145d81f21f0537e06

--- host-bmc/custom_dbus.cpp
+++ host-bmc/custom_dbus.cpp
@@ -17,5 +17,25 @@ void CustomDBus::updateLocation(const std::string& path, std::string value)
     location.at(path)->locationCode(value);
 }
 
+void CustomDBus::updateOperationalStatus(const std::string& path,
+                                         uint8_t status)
+{
+    if (operationalStatus.find(path) == operationalStatus.end())
+    {
+        operationalStatus.emplace(
+            path, std::make_unique<OperationalStatusIntf>(
+                      pldm::utils::DBusHandler::getBus(), path.c_str()));
+    }
+
+    if (status == PLDM_OPERATIONAL_NORMAL)
+    {
+        operationalStatus.at(path)->functional(true);
+    }
+    else
+    {
+        operationalStatus.at(path)->functional(false);
+    }
+}
+
 } // namespace dbus
 } // namespace pldm
