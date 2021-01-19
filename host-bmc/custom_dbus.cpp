1eb4706993fa63ba29c25b2b390738d88884c3e8
George Liu <liuxiwei@inspur.com>
2021-01-19 17:32:07 +0800

    Process state sensors PDRs and update Operational Fault Status
    - When processing the FRU record set PDRs lookup for state sensors
      with state set ID OperationFaultStatus and matching entity
      information (entity type, container ID and entity InstanceNumber).
    
    - Implement xyz.openbmc_project.State.Decorator.OperationalStatus
      interface and initialize Functional property by reading the sensor
      value(the GetStateSensorReadings command).
    
    - Also, Need to update the Functional property for the corresponding
      FRU D-Bus object when the functional status changes on the host.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: If4cc6b6fc293c4eaf035373145d81f21f0537e06

--- host-bmc/custom_dbus.cpp
+++ host-bmc/custom_dbus.cpp
@@ -27,5 +27,34 @@ std::string CustomDBus::getLocationCode(const std::string& path) const
     return {};
 }
 
+void CustomDBus::setOperationalStatus(const std::string& path, uint8_t status)
+{
+    if (operationalStatus.find(path) == operationalStatus.end())
+    {
+        operationalStatus.emplace(
+            path, std::make_unique<OperationalStatusIntf>(
+                      pldm::utils::DBusHandler::getBus(), path.c_str()));
+    }
+
+    if (status == PLDM_OPERATIONAL_NORMAL)
+    {
+        operationalStatus.at(path)->functional(true);
+    }
+    else
+    {
+        operationalStatus.at(path)->functional(false);
+    }
+}
+
+bool CustomDBus::getOperationalStatus(const std::string& path) const
+{
+    if (operationalStatus.find(path) != operationalStatus.end())
+    {
+        return operationalStatus.at(path)->functional();
+    }
+
+    return false;
+}
+
 } // namespace dbus
 } // namespace pldm
--- host-bmc/host_pdr_handler.hpp
+++ host-bmc/host_pdr_handler.hpp
@@ -183,6 +183,20 @@ class HostPDRHandler
      */
     uint16_t getRSI(const PDRList& fruRecordSetPDRs, const pldm_entity& entity);
 
+    /** @brief Get present state from state sensor readings
+     *  @param[in] sensorId - state sensor Id
+     *
+     *  @return uint8_t     - pldm operational fault status
+     */
+    uint8_t getPresentStateBySensorReadigs(uint16_t sensorId);
+
+    /** @brief Set the OperationalStatus interface
+     *  @param[in] path     - object path
+     *  @param[in] entity   - PLDM entity information
+     *  @return
+     */
+    void setOperationStatus(const std::string& path, const pldm_entity& entity);
+
     /** @brief fd of MCTP communications socket */
     int mctp_fd;
     /** @brief MCTP EID of host firmware */
