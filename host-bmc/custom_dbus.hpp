e72f33679b97dfddde6f93893127483ce630ba42
George Liu <liuxiwei@inspur.com>
2021-01-19 17:32:07 +0800

    Process state sensors PDRs and update Operational Fault Status
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: If4cc6b6fc293c4eaf035373145d81f21f0537e06

--- host-bmc/custom_dbus.hpp
+++ host-bmc/custom_dbus.hpp
@@ -4,6 +4,7 @@
 
 #include <com/ibm/ipzvpd/Location/server.hpp>
 #include <sdbusplus/server.hpp>
+#include <xyz/openbmc_project/State/Decorator/OperationalStatus/server.hpp>
 
 #include <map>
 #include <memory>
@@ -17,6 +18,8 @@ namespace dbus
 using OjectPath = std::string;
 
 using LocationIntf = sdbusplus::com::ibm::ipzvpd::server::Location;
+using OperationalStatusIntf = sdbusplus::xyz::openbmc_project::State::
+    Decorator::server::OperationalStatus;
 
 /** @class CustomDBus
  *  @brief This is a custom D-Bus object, used to add D-Bus interface and update
@@ -48,8 +51,17 @@ class CustomDBus
      */
     void updateLocation(const std::string& path, std::string value);
 
+    /** @brief Update the Functional property
+     *  @param[in] path  - The object path
+     *  @param[in] value - The value of the Functional property
+     */
+    void updateOperationalStatus(const std::string& path, bool value);
+
   private:
     std::map<OjectPath, std::unique_ptr<LocationIntf>> location;
+
+    std::map<OjectPath, std::unique_ptr<OperationalStatusIntf>>
+        operationalStatus;
 };
 
 } // namespace dbus
