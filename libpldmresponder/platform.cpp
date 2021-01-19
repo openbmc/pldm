9b8524522f577d4478da5640928697bdadc59b61
George Liu <liuxiwei@inspur.com>
2021-01-19 17:32:07 +0800

    Process state sensors PDRs and update Operational Fault Status
    - When processing the FRU record set PDRs lookup for state sensors
      with state set ID OperationFaultStatus and matching entity
      information (entity type, container ID and entity InstanceNumber).
    
    - Implement xyz.openbmc_project.State.Decorator.OperationalStatus
      interface and initialize Functional property by reading the sensor
      value(the GetStateSensorReadings command).
    
    - Also, Need to update the Functional property for the corresponding
      FRU D-Bus object when the functional status changes on the host.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: If4cc6b6fc293c4eaf035373145d81f21f0537e06

--- libpldmresponder/platform.cpp
+++ libpldmresponder/platform.cpp
@@ -426,10 +426,11 @@ int Handler::sensorEvent(const pldm_msg* request, size_t payloadLength,
 
         pldm::pdr::EntityInfo entityInfo{};
         pldm::pdr::CompositeSensorStates compositeSensorStates{};
+        std::vector<pldm::pdr::StateSetId> stateSetIds{};
 
         try
         {
-            std::tie(entityInfo, compositeSensorStates) =
+            std::tie(entityInfo, compositeSensorStates, stateSetIds) =
                 hostPDRHandler->lookupSensorInfo(sensorEntry);
         }
         catch (const std::out_of_range& e)
@@ -440,7 +441,7 @@ int Handler::sensorEvent(const pldm_msg* request, size_t payloadLength,
             try
             {
                 sensorEntry.terminusID = PLDM_TID_RESERVED;
-                std::tie(entityInfo, compositeSensorStates) =
+                std::tie(entityInfo, compositeSensorStates, stateSetIds) =
                     hostPDRHandler->lookupSensorInfo(sensorEntry);
             }
             // If there is no mapping for events return PLDM_SUCCESS
@@ -464,8 +465,8 @@ int Handler::sensorEvent(const pldm_msg* request, size_t payloadLength,
         const auto& [containerId, entityType, entityInstance] = entityInfo;
         events::StateSensorEntry stateSensorEntry{containerId, entityType,
                                                   entityInstance, sensorOffset};
-        return hostPDRHandler->handleStateSensorEvent(stateSensorEntry,
-                                                      eventState);
+        return hostPDRHandler->handleStateSensorEvent(
+            stateSetIds, stateSensorEntry, eventState);
     }
     else
     {
