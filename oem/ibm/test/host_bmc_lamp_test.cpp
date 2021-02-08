49fdd36a511c3d2911e4e182b130dfbda7e4d492
George Liu <liuxiwei@inspur.com>
2021-02-08 14:10:17 +0800

    oem-ibm: Implement Host lamp test interface
    IBM has a feature called LampTest and what it does is this:
      - BMC would set the state of LEDs to [ON] on all the BMC accessible
        LEDs.
      - For LEDs that are not accessible by BMC, a message is sent to the
        Host, so that the Host can exercise all the LEDs on the connected
        drawers and anything else that the BMC does not have access to.
    
    This commit adds support to send message to Host when BMC has to tell
    the Host to exercise LEDs accessible by host.
    
    When Host sees the effector is set, it goes ahead and turns [ON] all
    the LEDs for a specified time and automatically restores the states
    at timer expiration. BMC does not have to tell the Host to stop the
    test and it is part of Host's lamp test requirement.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I990dce28d3017f20c73ff9029fced0e7ac84868f

---
+++ oem/ibm/test/host_bmc_lamp_test.cpp
@@ -0,0 +1,47 @@
+#include "oem/ibm/host-bmc/host_lamp_test.hpp"
+#include "pldmd/dbus_impl_requester.hpp"
+
+#include <string.h>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace pldm::led;
+using ::testing::_;
+using ::testing::Return;
+
+class MockLampTest : public HostLampTest
+{
+  public:
+    MockLampTest(sdbusplus::bus::bus& bus, const std::string& objPath,
+                 int mctp_fd, uint8_t mctp_eid, Requester& requester,
+                 const pldm_pdr* repo) :
+        HostLampTest(bus, objPath, mctp_fd, mctp_eid, requester, repo)
+    {}
+
+    MOCK_METHOD(uint16_t, getEffecterID, (), (override));
+    MOCK_METHOD(int, setHostStateEffecter, (uint16_t effecterId), (override));
+};
+
+TEST(TestLamp, Asserted)
+{
+    sdbusplus::bus::bus bus = sdbusplus::bus::new_default();
+    pldm::dbus_api::Requester dbusImplReq(bus, "/abc/def/pldm");
+
+    MockLampTest lampTest(bus, "/xyz/openbmc_project/led/groups/host_lamp_test",
+                          0, 0, dbusImplReq, nullptr);
+
+    EXPECT_CALL(lampTest, getEffecterID())
+        .Times(2)
+        .WillOnce(Return(0))
+        .WillOnce(Return(1));
+    EXPECT_CALL(lampTest, setHostStateEffecter(1)).WillOnce(Return(0));
+
+    lampTest.asserted(false);
+    EXPECT_EQ(lampTest.asserted(), false);
+
+    ASSERT_THROW(lampTest.asserted(true), std::exception::SdBusError);
+
+    lampTest.asserted(true);
+    EXPECT_EQ(lampTest.asserted(), true);
+}
