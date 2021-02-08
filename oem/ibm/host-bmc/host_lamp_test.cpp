ef18f88fd71d4a9dace72a2f980a497b2e42aeb4
George Liu <liuxiwei@inspur.com>
2021-02-08 14:10:17 +0800

    oem-ibm: Implement LEDs lamp test interfaces
    There is a lamp test function on the IBM system, when BMC starts the
    lamp test, it needs to notify PHYP to start the lamp test on the HOST
    by the PLDM command.
    
    PLDM will not do anything when the BMC stoped the lamp test, and PHYP
    will auto stop the timer.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I990dce28d3017f20c73ff9029fced0e7ac84868f

---
+++ oem/ibm/host-bmc/host_lamp_test.cpp
@@ -0,0 +1,130 @@
+
+#include "host_lamp_test.hpp"
+
+#include "entity.h"
+#include "platform.h"
+#include "pldm.h"
+#include "state_set.h"
+
+#include "common/types.hpp"
+
+#include <iostream>
+
+namespace pldm
+{
+namespace led
+{
+
+/** @brief Overloaded Property Setter function */
+bool HostLampTest::asserted(bool value)
+{
+    // If the value is already what is before, return right away
+    if (value ==
+        sdbusplus::xyz::openbmc_project::Led::server::Group::asserted())
+    {
+        return value;
+    }
+
+    uint16_t effecterID = getEffecterID();
+    if (effecterID == 0)
+    {
+        return value;
+    }
+
+    sendHostStateEffecter(effecterID);
+
+    // Set the base class's asserted to 'true' since the getter
+    // operation is handled there.
+    return sdbusplus::xyz::openbmc_project::Led::server::Group::asserted(value);
+}
+
+uint16_t HostLampTest::getEffecterID()
+{
+    uint16_t effecterID = 0;
+
+    // INDICATOR is a logical entity, so the bit 15 in entity type is set.
+    pdr::EntityType entityType = PLDM_ENTITY_INDICATOR | 0x8000;
+
+    try
+    {
+        std::vector<std::vector<uint8_t>> stateEffecterPDRs{};
+        auto method = bus.new_method_call(
+            "xyz.openbmc_project.PLDM", "/xyz/openbmc_project/pldm",
+            "xyz.openbmc_project.PLDM.PDR", "FindStateEffecterPDR");
+        method.append(mctp_eid, entityType,
+                      (uint16_t)PLDM_STATE_SET_IDENTIFY_STATE);
+
+        auto reply = bus.call(method);
+        reply.read(stateEffecterPDRs);
+
+        for (auto& rep : stateEffecterPDRs)
+        {
+            auto pdr = reinterpret_cast<pldm_state_effecter_pdr*>(rep.data());
+            effecterID = pdr->effecter_id;
+            break;
+        }
+    }
+    catch (const sdbusplus::exception::SdBusError& e)
+    {
+        std::cerr << "host lamp test: Error get effecterID,ERROR=" << e.what()
+                  << "\n";
+    }
+
+    return effecterID;
+}
+
+int HostLampTest::sendHostStateEffecter(uint16_t effecterID)
+{
+    constexpr uint8_t effecterCount = 1;
+    auto instanceId = requester.getInstanceId(mctp_eid);
+
+    std::array<uint8_t, sizeof(pldm_msg_hdr) + sizeof(effecterID) +
+                            sizeof(effecterCount) +
+                            sizeof(set_effecter_state_field)>
+        requestMsg{};
+    auto request = reinterpret_cast<pldm_msg*>(requestMsg.data());
+    set_effecter_state_field stateField{PLDM_REQUEST_SET,
+                                        PLDM_STATE_SET_IDENTIFY_STATE_ASSERTED};
+    auto rc = encode_set_state_effecter_states_req(
+        instanceId, effecterID, effecterCount, &stateField, request);
+    if (rc != PLDM_SUCCESS)
+    {
+        requester.markFree(mctp_eid, instanceId);
+        std::cerr << "Failed to encode_set_state_effecter_states_req, rc = "
+                  << rc << std::endl;
+        return PLDM_ERROR;
+    }
+
+    uint8_t* responseMsg = nullptr;
+    size_t responseMsgSize{};
+    auto requesterRc =
+        pldm_send_recv(mctp_eid, mctp_fd, requestMsg.data(), requestMsg.size(),
+                       &responseMsg, &responseMsgSize);
+    std::unique_ptr<uint8_t, decltype(std::free)*> responseMsgPtr{responseMsg,
+                                                                  std::free};
+    requester.markFree(mctp_eid, instanceId);
+    if (requesterRc)
+    {
+        std::cerr << "Failed to send msg to report state effecter states, rc = "
+                  << requesterRc << std::endl;
+        return PLDM_ERROR;
+    }
+
+    uint8_t completionCode{};
+    auto responsePtr = reinterpret_cast<struct pldm_msg*>(responseMsgPtr.get());
+    rc = decode_set_state_effecter_states_resp(
+        responsePtr, responseMsgSize - sizeof(pldm_msg_hdr), &completionCode);
+
+    if (rc != PLDM_SUCCESS || completionCode != PLDM_SUCCESS)
+    {
+        std::cerr << "Failed to decode_set_state_effecter_states_resp: "
+                  << "rc=" << rc
+                  << ", cc=" << static_cast<unsigned>(completionCode)
+                  << std::endl;
+    }
+
+    return rc;
+}
+
+} // namespace led
+} // namespace pldm
