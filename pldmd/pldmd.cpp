6415e2e9e6ce252f1c9499659c60c0a66002f970
sampmisr <sampmisr@gmail.com>
2021-03-02 11:07:36 +0530

    BMC RR: check whether the Host is running
    This commit checks whether Host is up by sending a request to
    Host when pldmd starts up. This does not differentiate the case
    when there is a bmc RR or pldmd restarted.
    
    Updates the Host condition accordingly.
    
    Initiates a PDR exchange if Host is running.
    
    Change-Id: I630b342f5f9cc62530e0274f46357d238e92d0c9
    Signed-off-by: sampmisr <sampmisr@gmail.com>

--- pldmd/pldmd.cpp
+++ pldmd/pldmd.cpp
@@ -360,6 +360,19 @@ int main(int argc, char** argv)
     bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
     bus.request_name("xyz.openbmc_project.PLDM");
     IO io(event, socketFd(), EPOLLIN, std::move(callback));
+    hostPDRHandler->setHostState();
+    if (hostPDRHandler->isHostUp())
+    {
+        hostPDRHandler->fetchPDRsOnStart();
+    }
+    else
+    {
+        if (verbose)
+        {
+            std::cout << "Host is not running\n";
+        }
+    }
+
     event.loop();
 
     result = shutdown(sockfd, SHUT_RDWR);
