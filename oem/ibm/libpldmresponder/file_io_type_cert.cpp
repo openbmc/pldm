abfa94fbb8803389d04d7a65c567e6fb44a85875
Varsha Kaverappa <vkaverap@in.ibm.com>
2021-04-01 02:50:11 -0500

    PLDM : Multiple vmi certificate exchange
    This story includes the dbus property change that signals VMI certificate
    added which means we dont need to use so that the previously hardcoded
    path to store VMI certificate.
    
    Mutltiple certifucate exchange is also supported in this story where
    VMI for different HMC are stored in separate directories.
    
    Signed-off-by: Varsha Kaverappa <vkaverap@in.ibm.com>
    Change-Id: I63afb15190ae9c21eb86421d75f51618b358c074

--- oem/ibm/libpldmresponder/file_io_type_cert.cpp
+++ oem/ibm/libpldmresponder/file_io_type_cert.cpp
@@ -27,7 +27,6 @@ int CertHandler::writeFromMemory(uint32_t offset, uint32_t length,
     auto it = certMap.find(certType);
     if (it == certMap.end())
     {
-        std::cerr << "file for type " << certType << " doesn't exist\n";
         return PLDM_ERROR;
     }
 
@@ -106,6 +105,7 @@ int CertHandler::write(const char* buffer, uint32_t offset, uint32_t& length,
 int CertHandler::newFileAvailable(uint64_t length)
 {
     static constexpr auto vmiCertPath = "/var/lib/bmcweb";
+
     fs::create_directories(vmiCertPath);
     int fileFd = -1;
     int flags = O_WRONLY | O_CREAT | O_TRUNC;
@@ -117,6 +117,47 @@ int CertHandler::newFileAvailable(uint64_t length)
     if (certType == PLDM_FILE_TYPE_SIGNED_CERT)
     {
         fileFd = open(clientCertPath, flags, S_IRUSR | S_IWUSR);
+
+        static constexpr auto certObjPath = "/xyz/openbmc_project/certs/ca/entry";
+        static constexpr auto certEntryIntf = "xyz.openbmc_project.Certs.Entry";
+
+        std::fstream inFile;
+        inFile.open(clientCertPath);
+        std::stringstream strStream;
+        strStream << inFile.rdbuf();
+        std::string str = strStream.str();
+
+        PropertyValue value{str};
+        DBusMapping dbusMapping{certObjPath, certEntryIntf, "ClientCertificate",
+                                "string"};
+        try
+        {
+            pldm::utils::DBusHandler().setDbusProperty(dbusMapping, value);
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "failed to set Client certificate, "
+                         "ERROR="
+                      << e.what() << "\n";
+        }
+        if (!str.empty())
+        {
+            PropertyValue value{
+                "xyz.openbmc_project.Certs.Entry.State.Complete"};
+            DBusMapping dbusMapping{certObjPath, certEntryIntf, "Status",
+                                    "string"};
+            try
+            {
+                pldm::utils::DBusHandler().setDbusProperty(dbusMapping, value);
+            }
+            catch (const std::exception& e)
+            {
+                std::cerr
+                    << "failed to set status property of certicate entry, "
+                       "ERROR="
+                    << e.what() << "\n";
+            }
+        }
     }
     else if (certType == PLDM_FILE_TYPE_ROOT_CERT)
     {
