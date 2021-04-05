da50ab40e816aaf141c1780ddbae1a8aaca35e2a
gokulsanker <gokul.sanker.v.g@intel.com>
2021-04-05 16:36:04 +0530

    libpldm: Added encode API for RequestUpdate cmd request
    RequestUpdate command is the command to initiate
    firmware update for a firmware device.
    
    its defined in DSP0267 Version 1.1.0 sec:11.1
    
    Tested:
    Unit test case passed
    [==========] Running 7 tests from 4 test suites.
    [----------] Global test environment set-up.
    
    [----------] 2 tests from RequestUpdate
    [ RUN      ] RequestUpdate.testGoodEncodeRequest
    [       OK ] RequestUpdate.testGoodEncodeRequest (0 ms)
    [ RUN      ] RequestUpdate.testBadEncodeRequest
    [       OK ] RequestUpdate.testBadEncodeRequest (0 ms)
    [----------] 2 tests from RequestUpdate (1 ms total)
    
    [----------] Global test environment tear-down
    [==========] 7 tests from 4 test suites ran. (7 ms total)
    [  PASSED  ] 7 tests.
    
    Signed-off-by: gokulsanker <gokul.sanker.v.g@intel.com>
    Change-Id: Id74a65aa5975839c89ca433c16cfc67dcf4890c1
    Signed-off-by: gokulsanker <gokul.sanker.v.g@intel.com>

--- libpldm/firmware_update.c
+++ libpldm/firmware_update.c
@@ -228,3 +228,104 @@ int decode_get_firmware_parameters_comp_img_set_resp(
 
 	return PLDM_SUCCESS;
 }
+
+/** @brief Check whether Component Version String Type or Component Image Set
+ * Version String Type is valid
+ *
+ *  @return true if is from below mentioned values, false if not
+ */
+static bool check_comp_ver_str_type_valid(const uint8_t comp_type)
+{
+	switch (comp_type) {
+	case PLDM_COMP_VER_STR_TYPE_UNKNOWN:
+	case PLDM_COMP_ASCII:
+	case PLDM_COMP_UTF_8:
+	case PLDM_COMP_UTF_16:
+	case PLDM_COMP_UTF_16LE:
+	case PLDM_COMP_UTF_16BE:
+		return true;
+
+	default:
+		return false;
+	}
+}
+
+/** @brief Check whether Request Update Request is valid
+ *
+ *  @return true if request is valid,false if not
+ */
+static bool
+validate_request_update_req(const struct pldm_request_update_req *data)
+{
+	if (data->max_transfer_size < PLDM_FWU_BASELINE_TRANSFER_SIZE) {
+		return false;
+	}
+	if (data->max_outstand_transfer_req < PLDM_MIN_OUTSTANDING_REQ) {
+		return false;
+	}
+	if (!check_comp_ver_str_type_valid(data->comp_image_set_ver_str_type)) {
+		return false;
+	}
+	return true;
+}
+
+/** @brief Fill the Request Update Request
+ *
+ */
+static void fill_request_update_req(const struct pldm_request_update_req *data,
+				    struct pldm_request_update_req *request)
+{
+	request->max_transfer_size = htole32(data->max_transfer_size);
+	request->no_of_comp = htole16(data->no_of_comp);
+	request->max_outstand_transfer_req = data->max_outstand_transfer_req;
+	request->pkg_data_len = htole16(data->pkg_data_len);
+	request->comp_image_set_ver_str_type =
+	    data->comp_image_set_ver_str_type;
+	request->comp_image_set_ver_str_len = data->comp_image_set_ver_str_len;
+}
+
+int encode_request_update_req(const uint8_t instance_id, struct pldm_msg *msg,
+			      const size_t payload_length,
+			      const struct pldm_request_update_req *data,
+			      struct variable_field *comp_img_set_ver_str)
+{
+	if (msg == NULL || data == NULL || comp_img_set_ver_str == NULL ||
+	    comp_img_set_ver_str->ptr == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	if (payload_length < sizeof(struct pldm_request_update_req)) {
+		return PLDM_ERROR_INVALID_LENGTH;
+	}
+	if (payload_length != sizeof(struct pldm_request_update_req) +
+				  comp_img_set_ver_str->length) {
+		return PLDM_ERROR_INVALID_LENGTH;
+	}
+	if (data->comp_image_set_ver_str_len != comp_img_set_ver_str->length) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+	int rc = encode_pldm_header_only(
+	    instance_id, PLDM_FWUP, PLDM_REQUEST_UPDATE, PLDM_REQUEST, msg);
+
+	if (PLDM_SUCCESS != rc) {
+		return rc;
+	}
+
+	struct pldm_request_update_req *request =
+	    (struct pldm_request_update_req *)msg->payload;
+
+	if (request == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	if (!validate_request_update_req(data)) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	fill_request_update_req(data, request);
+
+	memcpy(msg->payload + sizeof(struct pldm_request_update_req),
+	       comp_img_set_ver_str->ptr, comp_img_set_ver_str->length);
+
+	return PLDM_SUCCESS;
+}
