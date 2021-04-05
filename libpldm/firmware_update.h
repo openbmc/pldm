4c6a8dfea06612d8c84329be0db2ee9e5b03de40
gokulsanker <gokul.sanker.v.g@intel.com>
2021-04-05 13:25:25 +0530

    libpldm : Added encode API for QueryDeviceIdentifiers cmd
    QueryDeviceIdentifiers command is used by update agent to
    obtain firmware identifiers for the firmware device and
    its defined in DSP0267 Version 1.1.0 sec:10.1
    
    Tested:
    Unit test case passed
    [==========] Running 1 test from 1 test suite.
    [----------] Global test environment set-up.
    [----------] 1 test from QueryDeviceIdentifiers
    [ RUN      ] QueryDeviceIdentifiers.testGoodEncodeRequest
    [       OK ] QueryDeviceIdentifiers.testGoodEncodeRequest (0 ms)
    [----------] 1 test from QueryDeviceIdentifiers (6 ms total)
    
    [----------] Global test environment tear-down
    [==========] 1 test from 1 test suite ran. (7 ms total)
    [  PASSED  ] 1 test.
    
    Signed-off-by: gokulsanker <gokul.sanker.v.g@intel.com>
    Change-Id: I472e85414b1a04b550b88c3e6058b86efb9b89b1
    Signed-off-by: gokulsanker <gokul.sanker.v.g@intel.com>

---
+++ libpldm/firmware_update.h
@@ -0,0 +1,40 @@
+#ifndef FW_UPDATE_H
+#define FW_UPDATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "base.h"
+#include "utils.h"
+
+/* inventory commands */
+#define PLDM_QUERY_DEVICE_IDENTIFIERS 0x01
+#define PLDM_QUERY_DEVICE_IDENTIFIERS_REQ_BYTES 0
+/* QueryDeviceIdentifiers */
+
+/** @brief Return code for encode-decode API
+ */
+enum encode_decode_rc {
+ENCODE_SUCCESS=0xF0,
+ENCODE_FAILURE=0xF1,
+DECODE_SUCCESS=0xF2,
+DECODE_FAILURE=0xF3
+};
+
+/** @brief Create a PLDM request message for QueryDeviceIdentifiers
+ *
+ *  @param[in] instance_id - Message's instance id
+ *  @param[in,out] msg - Message will be written to this
+ *  @param[in] payload_length - Length of the request message payload
+ *  @return pldm_completion_codes
+ *  @note  Caller is responsible for memory alloc and dealloc of param
+ *         'msg.payload'
+ */
+int encode_query_device_identifiers_req(const uint8_t instance_id,
+					struct pldm_msg *msg,
+					const size_t payload_length);
+#ifdef __cplusplus
+}
+#endif
+
+#endif // End of FW_UPDATE_H
