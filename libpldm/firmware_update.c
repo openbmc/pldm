f5252d3ead86d5c3bb3790a8a1ac9f0ad8d309c6
gokulsanker <gokul.sanker.v.g@intel.com>
2021-04-05 14:57:41 +0530

    libpldm : Added decode API for QueryDeviceIdentifiers cmd
    QueryDeviceIdentifiers command is used by update agent to
    obtain firmware identifiers for the firmware device and
    its defined in DSP0267 Version 1.1.0 sec:10.1
    
    Tested:
    Unit test case passed
    [==========] Running 2 tests from 1 test suite.
    [----------] Global test environment set-up.
    [----------] 2 tests from QueryDeviceIdentifiers
    
    [ RUN      ] QueryDeviceIdentifiers.testGoodDecodeResponse
    [       OK ] QueryDeviceIdentifiers.testGoodDecodeResponse (0 ms)
    [----------] 2 tests from QueryDeviceIdentifiers (1 ms total)
    
    [----------] Global test environment tear-down
    [==========] 2 tests from 1 test suite ran. (2 ms total)
    [  PASSED  ] 2 tests.
    
    Signed-off-by: gokulsanker <gokul.sanker.v.g@intel.com>
    Change-Id: I9ef81ce75aaedaae2fff9f5632ce16aa952cda17
    Signed-off-by: gokulsanker <gokul.sanker.v.g@intel.com>

--- libpldm/firmware_update.c
+++ libpldm/firmware_update.c
@@ -1,4 +1,5 @@
 #include "firmware_update.h"
+#include <endian.h>
 
 int encode_query_device_identifiers_req(const uint8_t instance_id,
 					struct pldm_msg *msg,
@@ -23,4 +24,50 @@ int encode_query_device_identifiers_req(const uint8_t instance_id,
 	}
 
 	return ENCODE_SUCCESS;
+}
+
+int decode_query_device_identifiers_resp(const struct pldm_msg *msg,
+					 const size_t payload_length,
+					 uint8_t *completion_code,
+					 uint32_t *device_identifiers_len,
+					 uint8_t *descriptor_count,
+					 uint8_t **descriptor_data)
+{
+	if (msg == NULL || completion_code == NULL ||
+	    device_identifiers_len == NULL || descriptor_count == NULL ||
+	    descriptor_data == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	*completion_code = msg->payload[0];
+	if (PLDM_SUCCESS != *completion_code) {
+		return DECODE_SUCCESS;
+	}
+
+	if (payload_length < sizeof(struct query_device_identifiers_resp)) {
+		return PLDM_ERROR_INVALID_LENGTH;
+	}
+
+	struct query_device_identifiers_resp *response =
+	    (struct query_device_identifiers_resp *)msg->payload;
+	*device_identifiers_len = le32toh(response->device_identifiers_len);
+
+	if (*device_identifiers_len < PLDM_FWU_MIN_DESCRIPTOR_IDENTIFIERS_LEN) {
+		return PLDM_ERROR_INVALID_LENGTH;
+	}
+
+	if (payload_length != sizeof(struct query_device_identifiers_resp) +
+				  *device_identifiers_len) {
+		return PLDM_ERROR_INVALID_LENGTH;
+	}
+	*descriptor_count = response->descriptor_count;
+
+	if (*descriptor_count == 0) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	*descriptor_data =
+	    (uint8_t *)(msg->payload +
+			sizeof(struct query_device_identifiers_resp));
+	return DECODE_SUCCESS;
 }
