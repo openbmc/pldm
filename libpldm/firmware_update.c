0cf6dadca8a3b7d9f7e6085bec3dfd0318c92103
gokulsanker <gokul.sanker.v.g@intel.com>
2021-04-05 13:25:25 +0530

    libpldm : Added encode API for QueryDeviceIdentifiers cmd
    QueryDeviceIdentifiers command is used by update agent to
    obtain firmware identifiers for the firmware device and
    its defined in DSP0267 Version 1.1.0 sec:10.1
    
    Tested:
    Unit test case passed
    [==========] Running 1 test from 1 test suite.
    [----------] Global test environment set-up.
    [----------] 1 test from QueryDeviceIdentifiers
    [ RUN      ] QueryDeviceIdentifiers.testGoodEncodeRequest
    [       OK ] QueryDeviceIdentifiers.testGoodEncodeRequest (0 ms)
    [----------] 1 test from QueryDeviceIdentifiers (6 ms total)
    
    [----------] Global test environment tear-down
    [==========] 1 test from 1 test suite ran. (7 ms total)
    [  PASSED  ] 1 test.
    
    Signed-off-by: gokulsanker <gokul.sanker.v.g@intel.com>
    Change-Id: I472e85414b1a04b550b88c3e6058b86efb9b89b1
    Signed-off-by: gokulsanker <gokul.sanker.v.g@intel.com>

---
+++ libpldm/firmware_update.c
@@ -0,0 +1,26 @@
+#include "firmware_update.h"
+
+int encode_query_device_identifiers_req(const uint8_t instance_id,
+					struct pldm_msg *msg,
+					const size_t payload_length)
+{
+	if (msg == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	if (payload_length != PLDM_QUERY_DEVICE_IDENTIFIERS_REQ_BYTES) {
+		return PLDM_ERROR_INVALID_LENGTH;
+	}
+
+	struct pldm_header_info header = {0};
+	header.msg_type = PLDM_REQUEST;
+	header.instance = instance_id;
+	header.pldm_type = PLDM_FWUP;
+	header.command = PLDM_QUERY_DEVICE_IDENTIFIERS;
+	int rc = pack_pldm_header(&header, &(msg->hdr));
+	if (PLDM_SUCCESS != rc) {
+		return rc;
+	}
+
+	return ENCODE_SUCCESS;
+}
