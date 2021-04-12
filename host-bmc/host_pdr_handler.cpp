d06aeded60c217dcae348e8a4f230b6d92ecede7
Manojkiran Eda <manojkiran.eda@gmail.com>
2021-04-12 15:56:46 +0530

    Add the support for Inventory Item dbus Interface
    - This commit would add the xyz.openbmc_project.Inventory.Item
      interface onto the dbus objects present in the entity association
      map
    
    - This commit would support two properties
      - PrettyName : This would be set with the filename from the dbus
                     object path.
      - Present : This is hardcoded to true when ever the PDR is present.
    
    Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>
    Change-Id: Ie2cdd2bb3b427d703a6ce1144b13df33f5a332e8

--- host-bmc/host_pdr_handler.cpp
+++ host-bmc/host_pdr_handler.cpp
@@ -1004,6 +1004,10 @@ void HostPDRHandler::getFRURecordTableByHost(uint16_t& total_table_records,
 
             // update xyz.openbmc_project.State.Decorator.OperationalStatus
             setOperationStatus(entity.first, node);
+
+            // update the Present Property
+            setPresentPropertyStatus(entity.first);
+
             for (auto& data : fruRecordData)
             {
                 if (fruRSI != data.fruRSI)
@@ -1154,6 +1158,11 @@ void HostPDRHandler::setOperationStatus(const std::string& path,
         // Get sensorOpState property by the getStateSensorReadings command.
         getPresentStateBySensorReadigs(sensor.first.sensorID, state, path);
     }
+}
+
+void HostPDRHandler::setPresentPropertyStatus(const std::string& path)
+{
+    CustomDBus::getCustomDBus().updateItemPresentStatus(path);
 }
 
 void HostPDRHandler::parseFruRecordSetPDRs(const PDRList& fruRecordSetPDRs)
