c530e4ac65cd5da950b0c2189057298bac18fc9a
Manojkiran Eda <manojkiran.eda@gmail.com>
2021-04-12 11:02:46 +0530

    Add new core entity in the entity association map
    - This commit wold add the core entity (135 with logical bit set)
      to the entity association map to generate the core dbus object paths
      under the proc.
    
    Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>
    Change-Id: Ib2db827003782b3c20afedcc492dec8d22f86597

--- host-bmc/test/utils_test.cpp
+++ host-bmc/test/utils_test.cpp
@@ -10,6 +10,7 @@ using namespace pldm::hostbmc::utils;
 
 TEST(EntityAssociation, addObjectPathEntityAssociations)
 {
+
     ObjectPathMaps retObjectMaps = {
         {"/xyz/openbmc_project/inventory/system/chassis0/io_board0/"
          "powersupply0",
@@ -17,39 +18,50 @@ TEST(EntityAssociation, addObjectPathEntityAssociations)
         {"/xyz/openbmc_project/inventory/system/chassis0/io_board1/"
          "powersupply1",
          {120, 1, 1}},
-        {"/xyz/openbmc_project/inventory/system/chassis1/motherboard0/dimm0",
+        {"/xyz/openbmc_project/inventory/system/chassis0/motherboard0/dimm0",
          {142, 0, 1}},
         {"/xyz/openbmc_project/inventory/system/chassis1/motherboard1/dimm1",
          {142, 1, 1}}};
-
-    const EntityAssociations entityAssociations = {
-        {{45, 0, 1}, {60, 0, 1}, {60, 1, 1}},
-        {{60, 0, 1}, {120, 0, 1}},
-        {{60, 1, 1}, {120, 1, 1}},
-        {{45, 1, 1}, {64, 0, 1}, {64, 1, 1}},
-        {{64, 0, 1}, {142, 0, 1}},
-        {{64, 1, 1}, {142, 1, 1}}};
-
-    fs::path path{"/xyz/openbmc_project/inventory/system"};
-    ObjectPathMaps objPathMap;
-
-    Entities parentsEntity = getParentEntites(entityAssociations);
-    for (auto& p : parentsEntity)
+    {"/xyz/openbmc_project/inventory/system/chassis0/motherboard0/cpu0/core0",
+     {33903, 0, 1}},
     {
-        addObjectPathEntityAssociations(entityAssociations, p, path,
-                                        objPathMap);
+        "/xyz/openbmc_project/inventory/system/chassis0/motherboard0/cpu1/"
+        "core1",
+        {
+            33903, 1, 1
+        }
     }
+};
 
-    EXPECT_EQ(objPathMap.size(), 4);
-    EXPECT_EQ(retObjectMaps.size(), 4);
+const EntityAssociations entityAssociations = {
+    {{45, 0, 1}, {60, 0, 1}, {60, 1, 1}, {64, 0, 1}},
+    {{60, 0, 1}, {120, 0, 1}},
+    {{60, 1, 1}, {120, 1, 1}},
+    {{45, 1, 1}, {64, 1, 1}},
+    {{64, 0, 1}, {142, 0, 1}, {135, 0, 1}, {135, 1, 1}},
+    {{64, 1, 1}, {142, 1, 1}},
+    {{135, 0, 1}, {33903, 0, 1}},
+    {{135, 1, 1}, {33903, 1, 1}}};
 
-    for (auto& obj : objPathMap)
-    {
-        EXPECT_NE(retObjectMaps.find(obj.first), retObjectMaps.end());
-        EXPECT_EQ(obj.second.entity_type, retObjectMaps[obj.first].entity_type);
-        EXPECT_EQ(obj.second.entity_instance_num,
-                  retObjectMaps[obj.first].entity_instance_num);
-        EXPECT_EQ(obj.second.entity_container_id,
-                  retObjectMaps[obj.first].entity_container_id);
-    }
+fs::path path{"/xyz/openbmc_project/inventory/system"};
+ObjectPathMaps objPathMap;
+
+Entities parentsEntity = getParentEntites(entityAssociations);
+for (auto& p : parentsEntity)
+{
+    addObjectPathEntityAssociations(entityAssociations, p, path, objPathMap);
+}
+
+EXPECT_EQ(objPathMap.size(), 6);
+EXPECT_EQ(retObjectMaps.size(), 6);
+
+for (auto& obj : objPathMap)
+{
+    EXPECT_NE(retObjectMaps.find(obj.first), retObjectMaps.end());
+    EXPECT_EQ(obj.second.entity_type, retObjectMaps[obj.first].entity_type);
+    EXPECT_EQ(obj.second.entity_instance_num,
+              retObjectMaps[obj.first].entity_instance_num);
+    EXPECT_EQ(obj.second.entity_container_id,
+              retObjectMaps[obj.first].entity_container_id);
+}
 }
