b0641bf7673245409eb44e4a3947111dcbf512c7
gokulsanker <gokul.sanker.v.g@intel.com>
2021-04-26 20:18:13 +0530

    libpldm: Added encode API for CancelUpdate cmd request
    Update agent send this command  to FD/FDP to exit
    from update mode. On receiving Cancel update FD
    should return back to IDLE state.Its defined in
    defined in DSP0267 Version 1.1.0 sec:11.14
    
    Tested:
    Unit test case passed
    [==========] Running 23 tests from 8 test suites.
    [----------] Global test environment set-up.
    
    [----------] 2 tests from CancelUpdate
    [ RUN      ] CancelUpdate.testGoodEncodeRequest
    [       OK ] CancelUpdate.testGoodEncodeRequest (1 ms)
    [ RUN      ] CancelUpdate.testBadEncodeRequest
    [       OK ] CancelUpdate.testBadEncodeRequest (0 ms)
    [----------] 2 tests from CancelUpdate (9 ms total)
    
    [----------] Global test environment tear-down
    [==========] 23 tests from 8 test suites ran. (35 ms total)
    [  PASSED  ] 23 tests.
    
    Signed-off-by: gokulsanker <gokul.sanker.v.g@intel.com>
    Change-Id: Iab9c2f078c216c20c2432ac5ec0438fdae8da27d
    Signed-off-by: gokulsanker <gokul.sanker.v.g@intel.com>

--- libpldm/firmware_update.c
+++ libpldm/firmware_update.c
@@ -910,3 +910,12 @@ int decode_get_status_resp(const struct pldm_msg *msg,
 
 	return DECODE_SUCCESS;
 }
+
+int encode_cancel_update_req(const uint8_t instance_id, struct pldm_msg *msg)
+{
+	if (msg == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+	return (encode_header_only_request(instance_id, PLDM_FWUP,
+					   PLDM_CANCEL_UPDATE, msg));
+}
