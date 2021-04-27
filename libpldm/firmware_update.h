fda09f9ddb2f156dd3034e8d0dab5dde57b97227
gokulsanker <gokul.sanker.v.g@intel.com>
2021-04-27 12:29:03 +0530

    libpldm: Added encode API for CancelUpdateComponent cmd request
    Update agent send this command to FD/FDP during firmware
    component trasnfer process.The FD/FDP, upon receiving this
    command shall stop sending RequestFirmwareData commands to
    the UA,and cancel the current component update procedure.
    The FD/FDP controller shall transition to the READY XFER
    state of update mode and be ready to accept another
    UpdateComponent command.Its defined  in DSP0267
    Version 1.1.0 sec:11.13
    
    Tested:
    Unit test case passed
    [==========] Running 27 tests from 9 test suites.
    [----------] Global test environment set-up.
    
    [----------] 2 tests from CancelUpdateComponent
    [ RUN      ] CancelUpdateComponent.testGoodEncodeRequest
    [       OK ] CancelUpdateComponent.testGoodEncodeRequest (0 ms)
    [ RUN      ] CancelUpdateComponent.testBadEncodeRequest
    [       OK ] CancelUpdateComponent.testBadEncodeRequest (0 ms)
    [----------] 2 tests from CancelUpdateComponent (2 ms total)
    
    [----------] Global test environment tear-down
    [==========] 27 tests from 9 test suites ran. (108 ms total)
    [  PASSED  ] 27 tests.
    
    Signed-off-by: gokulsanker <gokul.sanker.v.g@intel.com>
    Change-Id: I95563734c9d22c435a59b697fb784b7acfa897a7
    Signed-off-by: gokulsanker <gokul.sanker.v.g@intel.com>

--- libpldm/firmware_update.h
+++ libpldm/firmware_update.h
@@ -16,6 +16,7 @@ extern "C" {
 #define PLDM_PASS_COMPONENT_TABLE 0x13
 #define PLDM_UPDATE_COMPONENT 0x14
 #define PLDM_GET_STATUS 0x1B
+#define PLDM_CANCEL_UPDATE_COMPONENT 0x1C
 #define PLDM_CANCEL_UPDATE 0x1D
 
 #define PLDM_FWU_BASELINE_TRANSFER_SIZE 32
@@ -584,6 +585,17 @@ int decode_cancel_update_resp(const struct pldm_msg *msg,
 			      uint8_t *completion_code,
 			      bool8_t *non_functioning_component_indication,
 			      bitfield64_t *non_functioning_component_bitmap);
+
+/** @brief Create a PLDM request message for CancelUpdateComponent
+ *
+ *  @param[in] instance_id - Message's instance id
+ *  @param[in,out] msg - Message will be written to this
+ *  @return pldm_completion_codes
+ *  @note  Caller is responsible for memory alloc and dealloc of param
+ *         'msg.payload'
+ */
+int encode_cancel_update_component_req(const uint8_t instance_id,
+				       struct pldm_msg *msg);
 
 #ifdef __cplusplus
 }
