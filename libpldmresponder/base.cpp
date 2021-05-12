ff77bce3d8a707fde1fc3b109efc98df1ca698cc
Sagar Srinivas <sagar.srinivas@ibm.com>
2021-05-12 07:46:56 -0500

    oem_ibm: Disable Watchdog Timer
    This commit is to disable the watchdog timer after
    host poweron completion.
    
    Signed-off-by: Sagar Srinivas <sagar.srinivas@ibm.com>
    Change-Id: I734e6e98f6c2936aebd04c0b56bdfebba4cfd40b

--- libpldmresponder/base.cpp
+++ libpldmresponder/base.cpp
@@ -227,6 +227,13 @@ void Handler::_processSetEventReceiver(
         std::cerr << "Failed to send the setEventReceiver request"
                   << "\n";
     }
+#ifdef OEM_IBM
+    if (oemPlatformHandler)
+    {
+        oemPlatformHandler->countSetEventReceiver();
+        oemPlatformHandler->checkAndDisableWatchDog();
+    }
+#endif
 }
 
 Response Handler::getTID(const pldm_msg* request, size_t /*payloadLength*/)
