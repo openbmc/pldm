743a7f64a838439d835e5e168a717fc9269def8c
Jayashankar Padath <jayashankar.padath@in.ibm.com>
2021-06-04 07:16:11 -0500

    fru: Add new inventory items in the fru parser
    Add new inventory items to the FRU list like Drive backplane,
    System internal expansion board, Fan, Real Time Clock, Connector
    and Slot. This generates new FRU Record Set PDRs and
    Entity Association PDRs and updates the existing ones.
    
    Test Results:
    
    1. RTC FRU Record Set PDR o/p
    
    pldmtool platform GetPDR -d 23
    {
        "nextRecordHandle": 24,
        "responseCount": 20,
        "recordHandle": 23,
        "PDRHeaderVersion": 1,
        "PDRType": "FRU Record Set PDR",
        "recordChangeNumber": 0,
        "dataLength": 10,
        "PLDMTerminusHandle": 0,
        "FRURecordSetIdentifier": 23,
        "entityType": "Real Time Clock (RTC)",
        "entityInstanceNumber": 1,
        "containerID": 3
    }
    
    2. Entity Association PDR for System internal expansion board
    
    pldmtool platform GetPDR -d 33
    {
        "nextRecordHandle": 34,
        "responseCount": 32,
        "recordHandle": 33,
        "PDRHeaderVersion": 1,
        "PDRType": "Entity Association PDR",
        "recordChangeNumber": 0,
        "dataLength": 22,
        "containerID": 7,
        "associationType": "Physical",
        "containerEntityType": "System internal expansion board",
        "containerEntityInstanceNumber": 3,
        "containerEntityContainerID": 3,
        "containedEntityCount": 2,
        "containedEntityType[1]": "Connector",
        "containedEntityInstanceNumber[1]": 1,
        "containedEntityContainerID[1]": 7,
        "containedEntityType[2]": "Connector",
        "containedEntityInstanceNumber[2]": 2,
        "containedEntityContainerID[2]": 7
    }
    
    Signed-off-by: Jayashankar Padath <jayashankar.padath@in.ibm.com>
    Change-Id: I66aff87f00aa9eb17ea845a71710253cea7c6d83

--- libpldmresponder/test/libpldmresponder_fru_test.cpp
+++ libpldmresponder/test/libpldmresponder_fru_test.cpp
@@ -47,14 +47,20 @@ TEST(FruParser, allScenarios)
         {"xyz.openbmc_project.Inventory.Item.Chassis",
          "xyz.openbmc_project.Inventory.Item.Board",
          "xyz.openbmc_project.Inventory.Item.Board.Motherboard",
+         "xyz.openbmc_project.Inventory.Item.Dimm",
          "xyz.openbmc_project.Inventory.Item.Panel",
+         "xyz.openbmc_project.Inventory.Item.DiskBackplane",
+         "xyz.openbmc_project.Inventory.Item.PCIeDevice",
+         "xyz.openbmc_project.Inventory.Item.Fan",
          "xyz.openbmc_project.Inventory.Item.PowerSupply",
          "xyz.openbmc_project.Inventory.Item.Vrm",
          "xyz.openbmc_project.Inventory.Item.Cpu",
          "xyz.openbmc_project.Inventory.Item.Bmc",
-         "xyz.openbmc_project.Inventory.Item.Dimm",
-         "xyz.openbmc_project.Inventory.Item.Tpm",
-         "xyz.openbmc_project.Inventory.Item.System"}};
+         "xyz.openbmc_project.Inventory.Item.Battery",
+         "xyz.openbmc_project.Inventory.Item.Connector",
+         "xyz.openbmc_project.Inventory.Item.PCIeSlot",
+         "xyz.openbmc_project.Inventory.Item.System",
+         "xyz.openbmc_project.Inventory.Item.Tpm"}};
     auto dbusInfo = parser.inventoryLookup();
     ASSERT_EQ(dbusInfo == lookupInfo, true);
 
