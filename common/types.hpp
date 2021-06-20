9069e8761790ce6d373ad0c3734bb85f0037e331
Tom Joseph <rushtotom@gmail.com>
2021-06-20 03:52:40 -0700

    fw-update: Implement inventory manager
    This patch implements the firmware update inventory manager and
    exposes the Software.Version D-Bus interface with the firmware
    details.
    
    1. Discover firmware devices that implement fw update specification
    2. Implements QueryDeviceIdentifier command
    3. Implements GetFirmwareParameters command
    4. Creates D-Bus objects which implements Software.Version interface
    5. Enumerates device identifiers and component information to be used
       for fw-update
    
    Signed-off-by: Tom Joseph <rushtotom@gmail.com>
    Change-Id: Ifa035c801a7c62bac9a7e947ed4a43d48f85a4ed

--- common/types.hpp
+++ common/types.hpp
@@ -2,6 +2,9 @@
 
 #include <stdint.h>
 
+#include <bitset>
+#include <map>
+#include <unordered_map>
 #include <set>
 #include <string>
 #include <variant>
@@ -10,6 +13,7 @@
 namespace pldm
 {
 
+using eid = uint8_t;
 using Request = std::vector<uint8_t>;
 using Response = std::vector<uint8_t>;
 
@@ -26,6 +30,30 @@ using Value = std::variant<bool, uint8_t, int16_t, uint16_t, int32_t, uint32_t,
                            int64_t, uint64_t, double, std::string>;
 
 } // namespace dbus
+
+namespace fw_update
+{
+
+using DescriptorType = uint16_t;
+using DescriptorData = std::vector<uint8_t>;
+using VendorDefinedDescriptorTitle = std::string;
+using VendorDefinedDescriptorData = std::vector<uint8_t>;
+using VendorDefinedDescriptorInfo =
+    std::tuple<VendorDefinedDescriptorTitle, VendorDefinedDescriptorData>;
+
+using Descriptors =
+    std::map<DescriptorType,
+             std::variant<DescriptorData, VendorDefinedDescriptorInfo>>;
+using DescriptorMap = std::unordered_map<eid, Descriptors>;
+
+using CompClassification = uint16_t;
+using CompIdentifier = uint16_t;
+using CompKey = std::pair<CompClassification, CompIdentifier>;
+using CompClassificationIndex = uint8_t;
+using ComponentInfo  = std::map<CompKey, CompClassificationIndex>;
+using ComponentInfoMap = std::unordered_map<eid, ComponentInfo>;
+
+} // namespace fw_update
 
 namespace pdr
 {
