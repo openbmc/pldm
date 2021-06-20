aa5fd7462db95f75457cd33345d25685625933e8
Tom Joseph <rushtotom@gmail.com>
2021-06-20 03:52:40 -0700

    fw-update: Implement inventory manager
    This patch implements the firmware update inventory manager and
    exposes the Software.Version D-Bus interface with the firmware
    details.
    
    1. Discover firmware devices that implement fw update specification
    2. Implements QueryDeviceIdentifier command
    3. Implements GetFirmwareParameters command
    4. Creates D-Bus objects which implements Software.Version interface
    5. Enumerates device identifiers and component information to be used
       for fw-update
    
    Signed-off-by: Tom Joseph <rushtotom@gmail.com>
    Change-Id: Ifa035c801a7c62bac9a7e947ed4a43d48f85a4ed

---
+++ fw-update/version.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "xyz/openbmc_project/Software/Version/server.hpp"
+
+#include <sdbusplus/bus.hpp>
+
+#include <string>
+
+namespace pldm
+{
+
+namespace fw_update
+{
+
+using VersionIntf = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Software::server::Version>;
+
+/** @class Version
+ *
+ *  Concrete implementation of the xyz.openbmc_project.Software.Version
+ *  D-Bus interface to represent the ActiveComponentVersionString of the
+ *  components discovered by the PLDM FW update inventory commands.
+ */
+class Version : public VersionIntf
+{
+  public:
+    Version() = delete;
+    ~Version() = default;
+    Version(const Version&) = delete;
+    Version& operator=(const Version&) = delete;
+    Version(Version&&) = default;
+    Version& operator=(Version&&) = default;
+
+    /** @brief Constructs Version Software Manager
+     *
+     * @param[in] bus - reference to the system bus
+     * @param[in] objPath - D-Bus object path
+     * @param[in] version - version string
+     * @param[in] purpose - version purpose
+     */
+    Version(sdbusplus::bus::bus& bus, const std::string& objPath,
+            const std::string& version, VersionPurpose purpose) :
+        VersionIntf(bus, (objPath).c_str(), true)
+    {
+        VersionIntf::version(version, true);
+        VersionIntf::purpose(purpose, true);
+        VersionIntf::emit_object_added();
+    }
+};
+
+} // namespace fw_update
+
+} // namespace pldm
