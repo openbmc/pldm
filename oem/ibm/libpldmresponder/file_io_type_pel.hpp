60edb7636818b7d62c1b9aaa3a9a8f34f2c53346
George Liu <liuxiwei@inspur.com>
2021-06-28 15:14:29 +0800

    Support inband write for PELS from HB to bmc
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I2d38fc42a2d7127eff22983ca83091d323736e2c

--- oem/ibm/libpldmresponder/file_io_type_pel.hpp
+++ oem/ibm/libpldmresponder/file_io_type_pel.hpp
@@ -35,10 +35,7 @@ class PelHandler : public FileHandler
 
     virtual int write(const char* /*buffer*/, uint32_t /*offset*/,
                       uint32_t& /*length*/,
-                      oem_platform::Handler* /*oemPlatformHandler*/)
-    {
-        return PLDM_ERROR_UNSUPPORTED_PLDM_CMD;
-    }
+                      oem_platform::Handler* /*oemPlatformHandler*/);
 
     virtual int fileAck(uint8_t fileStatus);
 
