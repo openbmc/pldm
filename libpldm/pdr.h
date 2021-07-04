725bc89083deaf66ad1fca3c9dfaf84195440c56
Manojkiran Eda <manojkiran.eda@gmail.com>
2021-07-04 21:46:43 +0530

    libpldm : Enhance the find & add pdr utilities
    In the current state, the tree_find () and tree_add()
    utilities work by replacing the existing container ID
    that comes from host. But we need the host container ID
    when we do the PDR normalization in BMC.
    
    This commit would add a new host_container_id file in the
    pdr node structure and also enhances the existing tree_find()
    and tree_add() utilities so that those can work for BMC-HOST
    normalization.
    
    Also, Add the pldm_extract_host_container_id method to extract
    host container id.
    
    Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>
    Change-Id: Ib721c496314c963759a1d51eed481356cb296006

--- libpldm/pdr.h
+++ libpldm/pdr.h
@@ -244,12 +244,15 @@ pldm_entity_association_tree *pldm_entity_association_tree_init();
  *                      entity. If this is NULL, then the entity is the root
  *  @param[in] association_type - relation with the parent : logical or physical
  *
+ *  @param[in] is_remote - used to denote whether we are adding a BMC entity to
+ *                        the tree or a host entity
+ *
  *  @return pldm_entity_node* - opaque pointer to added entity
  */
 pldm_entity_node *pldm_entity_association_tree_add(
     pldm_entity_association_tree *tree, pldm_entity *entity,
     uint16_t entity_instance_number, pldm_entity_node *parent,
-    uint8_t association_type);
+    uint8_t association_type, bool is_remote);
 
 /** @brief Visit and note each entity in the entity association tree
  *
@@ -268,6 +271,14 @@ void pldm_entity_association_tree_visit(pldm_entity_association_tree *tree,
  *  @return pldm_entity - pldm entity
  */
 pldm_entity pldm_entity_extract(pldm_entity_node *node);
+
+/** @brief Extract host container id by the pldm_entity_node
+ *
+ *  @param[in] entity         - opaque pointer to added entity
+ *
+ *  @return host container id - host container id
+ */
+uint16_t pldm_extract_host_container_id(const pldm_entity_node *entity);
 
 /** @brief Destroy entity association tree
  *
@@ -350,12 +361,14 @@ bool pldm_is_current_parent_child(pldm_entity_node *parent, pldm_entity *node);
  *  @param[in] tree - pointer to entity association tree
  *  @param[in/out] entity - entity type and instance id set on input, container
  *                 id set on output
+ *  @param[in] is_remote - variable to denote whether we are finding a host
+ *                         entity or a BMC entity
  *
  *  @return pldm_entity_node* pointer to entity if found, NULL otherwise
  */
 pldm_entity_node *
 pldm_entity_association_tree_find(pldm_entity_association_tree *tree,
-				  pldm_entity *entity);
+				  pldm_entity *entity, bool is_remote);
 
 /** @brief Create a copy of an existing entity association tree
  *
