9acff0ee8080e56f34a730cc089296a5b2a1470f
Manojkiran Eda <manojkiran.eda@gmail.com>
2021-07-04 21:46:43 +0530

    libpldm : Enhance the find & add pdr utilities
    In the current state, the tree_find () and tree_add()
    utilities work by replacing the existing container ID
    that comes from host. But we need the host container ID
    when we do the PDR normalization in BMC.
    
    This commit would add a new host_container_id file in the
    pdr node structure and also enhances the existing tree_find()
    and tree_add() utilities so that those can work for BMC-HOST
    normalization.
    
    Also, Add the pldm_extract_host_container_id method to extract
    host container id.
    
    Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>
    Change-Id: Ib721c496314c963759a1d51eed481356cb296006

--- host-bmc/host_pdr_handler.cpp
+++ host-bmc/host_pdr_handler.cpp
@@ -198,12 +198,13 @@ void HostPDRHandler::mergeEntityAssociations(const std::vector<uint8_t>& pdr)
         pldm_entity parent{};
         if (getParent(entities[i].entity_type, parent))
         {
-            auto node = pldm_entity_association_tree_find(entityTree, &parent);
+            auto node =
+                pldm_entity_association_tree_find(entityTree, &parent, true);
             if (node)
             {
-                pldm_entity_association_tree_add(entityTree, &entities[i],
-                                                 0xFFFF, node,
-                                                 entityPdr->association_type);
+                pldm_entity_association_tree_add(
+                    entityTree, &entities[i], 0xFFFF, node,
+                    entityPdr->association_type, false);
                 merged = true;
             }
         }
