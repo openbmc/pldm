7f14240ca071e7ff91f18757f96e8507b316ce09
Sridevi Ramesh <sridevra@in.ibm.com>
2021-07-16 04:22:02 -0500

    OEM-IBM: Implement encode response & decode request API for FileAckWithMetadata
    This commit implements requester and responder flow for
    FileAckWithMetadata.
    
    PLDM SPEC: PLDM File IO Table 15
    
    Tested: Unit tests passed
    
    Signed-off-by: Sridevi Ramesh <sridevra@in.ibm.com>
    Change-Id: I6bf5fba7030e42a5501165530a3ffc23a9ff7b8b

--- oem/ibm/libpldm/file_io.h
+++ oem/ibm/libpldm/file_io.h
@@ -24,6 +24,8 @@ enum pldm_fileio_commands {
 	PLDM_READ_FILE_BY_TYPE = 0xB,
 	PLDM_WRITE_FILE_BY_TYPE = 0xC,
 	PLDM_FILE_ACK = 0xD,
+	PLDM_NEW_FILE_AVAILABLE_WITH_META_DATA = 0xE,
+	PLDM_FILE_ACK_WITH_META_DATA = 0xF,
 };
 
 /** @brief PLDM Command specific codes
@@ -57,6 +59,38 @@ enum pldm_fileio_file_type {
 	PLDM_FILE_TYPE_RESOURCE_DUMP_PARMS = 0x8,
 	PLDM_FILE_TYPE_RESOURCE_DUMP = 0x9,
 	PLDM_FILE_TYPE_PROGRESS_SRC = 0xA,
+	PLDM_FILE_TYPE_COD_LICENSE_KEY = 0xD,
+	PLDM_FILE_TYPE_COD_LICENSED_RESOURCES = 0xE,
+	PLDM_FILE_TYPE_UNDEFINED = 0xFFFF,
+};
+
+enum pldm_fileio_resdump_status_codes {
+	PLDM_RES_DUMP_SECURITY_VALIDATED_SUCCESS = 0,
+	PLDM_RES_DUMP_ACF_FILE_INVALID = 1,
+	PLDM_RES_DUMP_PWD_INVALID = 2,
+	PLDM_RES_DUMP_PERMISSION_DENIED = 3,
+	PLDM_RES_DUMP_RESOURCE_SELECTOR_INVALID = 4,
+};
+
+enum pldm_fileio_res_selector_invalid_status_codes {
+	PLDM_RES_SELEC_INVALID_PARAM_LENGTH = 0x00000001,
+	PLDM_RES_SELEC_UNKWN_PARAM_VERSION = 0x00000002,
+	PLDM_RES_SELEC_UNKWN_CLIENT_TYPE = 0x00000004,
+	PLDM_RES_SELEC_INVALID_CMND_STRING = 0x00000005,
+	PLDM_RES_SELEC_INVALID_STATE = 0x00000006,
+	PLDM_RES_SELEC_BUSY = 0x00000007,
+	PLDM_RES_SELEC_UNKWN_DUMP = 0x00000008,
+	PLDM_RES_SELEC_INVALID_OFFSET = 0x00000009,
+	PLDM_RES_SELEC_EOF_REACHED = 0x00000010,
+	PLDM_RES_SELEC_KEY_UNUSED = 0xFFFFFFFF,
+};
+
+enum pldm_fileio_cod_license_key_status_codes {
+	PLDM_COD_LICENSE_KEY_INSTALL_SUCCESS = 0x00000000,
+	PLDM_COD_LICENSE_KEY_INVALID_LICENSE = 0x00000001,
+	PLDM_COD_LICENSE_KEY_INVALID_SYSTEM = 0x00000002,
+	PLDM_COD_LICENSE_KEY_INVALID_SEQUENCE = 0x00000003,
+	PLDM_COD_LICENSE_KEY_UNUSED = 0xFFFFFFFF,
 };
 
 #define PLDM_RW_FILE_MEM_REQ_BYTES 20
@@ -75,6 +109,8 @@ enum pldm_fileio_file_type {
 #define PLDM_RW_FILE_BY_TYPE_RESP_BYTES 5
 #define PLDM_FILE_ACK_REQ_BYTES 7
 #define PLDM_FILE_ACK_RESP_BYTES 1
+#define PLDM_FILE_ACK_WITH_META_DATA_REQ_BYTES 26
+#define PLDM_FILE_ACK_WITH_META_DATA_RESP_BYTES 1
 
 /** @struct pldm_read_write_file_memory_req
  *
@@ -717,6 +753,89 @@ int encode_file_ack_req(uint8_t instance_id, uint16_t file_type,
  */
 int decode_file_ack_resp(const struct pldm_msg *msg, size_t payload_length,
 			 uint8_t *completion_code);
+
+/* FileAckWithMetadata */
+
+/** @struct pldm_file_ack_with_meta_data_req
+ *
+ *  Structure representing FileAckWithMetadata request
+ */
+struct pldm_file_ack_with_meta_data_req {
+	uint16_t file_type;	   //!< Type of file
+	uint32_t file_handle;	   //!< Handle to file
+	uint8_t file_status;	   //!< Status of file processing
+	uint32_t file_meta_data_1; //!< meta data specific to file type
+	uint32_t file_meta_data_2; //!< meta data specific to file type
+	uint32_t file_meta_data_3; //!< meta data specific to file type
+	uint32_t file_meta_data_4; //!< meta data specific to file type
+} __attribute__((packed));
+
+/** @struct pldm_file_ack_with_meta_data_resp
+ *
+ *  Structure representing FileAckWithMetadata response
+ */
+struct pldm_file_ack_with_meta_data_resp {
+	uint8_t completion_code; //!< Completion code
+} __attribute__((packed));
+
+/** @brief Encode FileAckWithMetadata request data
+ *
+ *  @param[in] instance_id - Message's instance id
+ *  @param[in] file_type - Type of the file
+ *  @param[in] file_handle - A handle to the file
+ *  @param[in] file_status - Status of file processing
+ *  @param[in] file_meta_data_1 - File type specific meta data
+ *  @param[in] file_meta_data_2 - File type specific meta data
+ *  @param[in] file_meta_data_3 - File type specific meta data
+ *  @param[in] file_meta_data_4 - File type specific meta data
+ *  @param[out] msg - Message will be written to this
+ *  @return pldm_completion_codes
+ */
+int encode_file_ack_with_meta_data_req(
+    uint8_t instance_id, uint16_t file_type, uint32_t file_handle,
+    uint8_t file_status, uint32_t file_meta_data_1, uint32_t file_meta_data_2,
+    uint32_t file_meta_data_3, uint32_t file_meta_data_4, struct pldm_msg *msg);
+
+/** @brief Decode FileAckWithMetadata command response data
+ *
+ *  @param[in] msg - pointer to PLDM response message
+ *  @param[in] payload_length - Length of response payload
+ *  @param[out] completion_code - PLDM completion code
+ *  @return pldm_completion_codes
+ */
+int decode_file_ack_with_meta_data_resp(const struct pldm_msg *msg,
+					size_t payload_length,
+					uint8_t *completion_code);
+
+/** @brief Decode FileAckWithMetadata request data
+ *
+ *  @param[in] msg - Pointer to PLDM request message
+ *  @param[in] payload_length - Length of request payload
+ *  @param[out] file_type - Type of the file
+ *  @param[out] file_handle - A handle to the file
+ *  @param[out] file_status - Status of file processing
+ *  @param[in] file_meta_data_1 - File type specific meta data
+ *  @param[in] file_meta_data_2 - File type specific meta data
+ *  @param[in] file_meta_data_3 - File type specific meta data
+ *  @param[in] file_meta_data_4 - File type specific meta data
+ *  @return pldm_completion_codes
+ */
+int decode_file_ack_with_meta_data_req(
+    const struct pldm_msg *msg, size_t payload_length, uint16_t *file_type,
+    uint32_t *file_handle, uint8_t *file_status, uint32_t *file_meta_data_1,
+    uint32_t *file_meta_data_2, uint32_t *file_meta_data_3,
+    uint32_t *file_meta_data_4);
+
+/** @brief Create a PLDM response message for FileAckWithMetadata
+ *
+ *  @param[in] instance_id - Message's instance id
+ *  @param[in] completion_code - PLDM completion code
+ *  @param[in,out] msg - Message will be written to this
+ *  @return pldm_completion_codes
+ */
+int encode_file_ack_with_meta_data_resp(uint8_t instance_id,
+					uint8_t completion_code,
+					struct pldm_msg *msg);
 
 #ifdef __cplusplus
 }
