69e7673a59da38110d530a761ec12614007a4c7a
Pavithra Barithaya <pavithra.b@ibm.com>
2021-07-22 01:32:47 -0500

    PLDM: Implement SetFruRecordTable and CM
    This commit implements the setFruRecordTable command
    and also updates the DBus property for the cable cards
    for which the host sends a setFrurecordTable command.
    
    Tested: using pldmtool
    
    Signed-off-by: Pavithra Barithaya <pavithra.b@ibm.com>
    Change-Id: I70e4f85f627577d8ca1bc90447b10e9e2e8e7ccd

--- libpldmresponder/fru.hpp
+++ libpldmresponder/fru.hpp
@@ -6,6 +6,8 @@
 #include "libpldm/pdr.h"
 
 #include "fru_parser.hpp"
+#include "libpldmresponder/pdr_utils.hpp"
+#include "oem_handler.hpp"
 #include "pldmd/handler.hpp"
 
 #include <sdbusplus/message.hpp>
@@ -61,13 +63,16 @@ class FruImpl
      *  @param[in] entityTree - opaque pointer to the entity association tree
      *  @param[in] bmcEntityTree - opaque pointer to bmc's entity association
      *                             tree
+     *  @param[in] oemFruHandler - OEM fru handler
      */
     FruImpl(const std::string& configPath,
             const std::filesystem::path& fruMasterJsonPath, pldm_pdr* pdrRepo,
             pldm_entity_association_tree* entityTree,
-            pldm_entity_association_tree* bmcEntityTree) :
+            pldm_entity_association_tree* bmcEntityTree,
+            pldm::responder::oem_fru::Handler* oemFruHandler) :
         parser(configPath, fruMasterJsonPath),
-        pdrRepo(pdrRepo), entityTree(entityTree), bmcEntityTree(bmcEntityTree)
+        pdrRepo(pdrRepo), entityTree(entityTree), bmcEntityTree(bmcEntityTree),
+        oemFruHandler(oemFruHandler)
     {}
 
     /** @brief Total length of the FRU table in bytes, this excludes the pad
@@ -168,6 +173,13 @@ class FruImpl
      */
     std::string populatefwVersion();
 
+    /* @brief set FRU Record Table
+     *
+     * @param[in] fruData - the data of the fru
+     *
+     */
+    int setFRUTable(const std::vector<uint8_t>& fruData);
+
   private:
     uint16_t nextRSI()
     {
@@ -191,6 +203,7 @@ class FruImpl
     pldm_pdr* pdrRepo;
     pldm_entity_association_tree* entityTree;
     pldm_entity_association_tree* bmcEntityTree;
+    pldm::responder::oem_fru::Handler* oemFruHandler;
 
     std::map<dbus::ObjectPath, pldm_entity_node*> objToEntityNode{};
 
@@ -221,8 +234,10 @@ class Handler : public CmdHandler
     Handler(const std::string& configPath,
             const std::filesystem::path& fruMasterJsonPath, pldm_pdr* pdrRepo,
             pldm_entity_association_tree* entityTree,
-            pldm_entity_association_tree* bmcEntityTree) :
-        impl(configPath, fruMasterJsonPath, pdrRepo, entityTree, bmcEntityTree)
+            pldm_entity_association_tree* bmcEntityTree,
+            pldm::responder::oem_fru::Handler* oemFruHandler) :
+        impl(configPath, fruMasterJsonPath, pdrRepo, entityTree, bmcEntityTree,
+             oemFruHandler)
     {
         handlers.emplace(PLDM_GET_FRU_RECORD_TABLE_METADATA,
                          [this](const pldm_msg* request, size_t payloadLength) {
@@ -239,6 +254,11 @@ class Handler : public CmdHandler
                          [this](const pldm_msg* request, size_t payloadLength) {
                              return this->getFRURecordByOption(request,
                                                                payloadLength);
+                         });
+        handlers.emplace(PLDM_SET_FRU_RECORD_TABLE,
+                         [this](const pldm_msg* request, size_t payloadLength) {
+                             return this->setFRURecordTable(request,
+                                                            payloadLength);
                          });
     }
 
@@ -290,6 +310,18 @@ class Handler : public CmdHandler
      */
     Response getFRURecordByOption(const pldm_msg* request,
                                   size_t payloadLength);
+
+    /** @brief Handler for SetFRURecordTable
+     *
+     *  @param[in] request - Request message
+     *  @param[in] payloadLength - Request payload length
+     *
+     *  @return PLDM response message
+     */
+    Response setFRURecordTable(const pldm_msg* request, size_t payloadLength);
+
+    // std::vector<uint8_t> table;
+    using Table = std::vector<uint8_t>;
 
   private:
     FruImpl impl;
