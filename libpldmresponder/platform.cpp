c92e014f1a9f23013d6f160341dc76add449404e
Manojkiran Eda <manojkiran.eda@gmail.com>
2021-07-22 09:19:02 +0530

    oem_ibm: Add Slot enable infrastructure in BMC
    This commit adds the necessary sensors & effecters for all
    the PCIe Slots & adapters present on BMC. PDRs are created
    for each of these PCIE slots and adapters which have a
    corresponding entry in the entity associate map. These PDRs
    will monitor slot sensor/effector states which will allow
    enabling of a slot, process property change of a slot, etc.
    
    Tested By: SIMICS power on/off and reset reload.
    
    Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>
    Change-Id: I51b79b75c909ddf2cc29872fec6aa01c2d56b418

--- libpldmresponder/platform.cpp
+++ libpldmresponder/platform.cpp
@@ -789,13 +789,15 @@ Response Handler::getStateSensorReadings(const pldm_msg* request,
     uint16_t entityType{};
     uint16_t entityInstance{};
     uint16_t stateSetId{};
+    uint16_t containerId{};
 
     if (isOemStateSensor(*this, sensorId, sensorRearmCount, comSensorCnt,
-                         entityType, entityInstance, stateSetId) &&
-        oemPlatformHandler != nullptr && !sensorDbusObjMaps.contains(sensorId))
+                         entityType, entityInstance, stateSetId, containerId) &&
+        oemPlatformHandler)
     {
         rc = oemPlatformHandler->getOemStateSensorReadingsHandler(
-            entityType, entityInstance, stateSetId, comSensorCnt, stateField);
+            entityType, entityInstance, containerId, stateSetId, comSensorCnt,
+            sensorId, stateField);
     }
     else
     {
@@ -835,7 +837,7 @@ void Handler::_processPostGetPDRActions(sdeventplus::source::EventBase&
 bool isOemStateSensor(Handler& handler, uint16_t sensorId,
                       uint8_t sensorRearmCount, uint8_t& compSensorCnt,
                       uint16_t& entityType, uint16_t& entityInstance,
-                      uint16_t& stateSetId)
+                      uint16_t& stateSetId, uint16_t& containerId)
 {
     pldm_state_sensor_pdr* pdr = nullptr;
 
@@ -868,6 +870,7 @@ bool isOemStateSensor(Handler& handler, uint16_t sensorId,
         }
         auto tmpEntityType = pdr->entity_type;
         auto tmpEntityInstance = pdr->entity_instance;
+        auto tmpEntityContainerId = pdr->container_id;
         auto tmpCompSensorCnt = pdr->composite_sensor_count;
         auto tmpPossibleStates =
             reinterpret_cast<state_sensor_possible_states*>(
@@ -892,6 +895,7 @@ bool isOemStateSensor(Handler& handler, uint16_t sensorId,
             entityInstance = tmpEntityInstance;
             stateSetId = tmpStateSetId;
             compSensorCnt = tmpCompSensorCnt;
+            containerId = tmpEntityContainerId;
             return true;
         }
         else
