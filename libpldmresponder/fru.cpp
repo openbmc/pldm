abc52da99e5e269972b5dc45194ef411363b4969
Sampa Misra <sampmisr@in.ibm.com>
2021-07-26 01:59:41 -0500

    hotplug: create fru records only for present frus
    This commit builds the fru records only for the present
    frus. A fru is considered as present if it does not implement
    the "Present" property.
    
    Change-Id: I02edb2096c688a2e147ccd31fbf3c155878adadf
    Signed-off-by: Sampa Misra <sampmisr@in.ibm.com>

--- libpldmresponder/fru.cpp
+++ libpldmresponder/fru.cpp
@@ -160,6 +160,11 @@ void FruImpl::buildFRUTable()
     for (const auto& object : objects)
     {
         const auto& interfaces = object.second;
+        auto isPresent = checkFruPresence(object.first.str.c_str());
+        if (!isPresent)
+        {
+            continue;
+        }
 
         for (const auto& interface : interfaces)
         {
@@ -257,7 +262,6 @@ void FruImpl::populateRecords(
         uint8_t numFRUFields = 0;
         for (auto const& [intf, prop, propType, fieldTypeNum] : fieldInfos)
         {
-
             try
             {
                 pldm::responder::dbus::Value propValue;
