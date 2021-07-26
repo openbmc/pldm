bfbe2865fe8e16cd886e58e98ee9292216ae2f66
gokulsanker <gokul.sanker.v.g@intel.com>
2021-07-26 20:23:32 +0530

    libpldm: Added decode API for GetMetaData request
    The FD sends this command to transfer the data
    that was originally obtained by the UA through the
    GetDeviceMetaData command. This command shall only
    be used if the FD indicated in the RequestUpdate
    response that it had device metadata that needed
    to be obtained by the UA.Its defined  in DSP0267
    Version 1.1.0 sec:11.10
    
    Signed-off-by: gokulsanker <gokul.sanker.v.g@intel.com>
    Change-Id: I4ac32f4bba650aa616074b14d1025348a44e2818

--- libpldm/firmware_update.c
+++ libpldm/firmware_update.c
@@ -1677,3 +1677,14 @@ int encode_get_meta_data_resp(const uint8_t instance_id,
 						 msg, PLDM_GET_META_DATA, data,
 						 portion_of_meta_data));
 }
+
+int decode_get_meta_data_req(const struct pldm_msg *msg,
+			     const size_t payload_length,
+			     uint32_t *data_transfer_handle,
+			     uint8_t *transfer_operation_flag)
+{
+
+	return (decode_firmware_device_data_req(msg, payload_length,
+						data_transfer_handle,
+						transfer_operation_flag));
+}
