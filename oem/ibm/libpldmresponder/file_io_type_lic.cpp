fea5933c64aa8bf8edc5296a1a6ac30c7592246b
Varsha Kaverappa <vkaverap@in.ibm.com>
2021-07-27 00:20:06 -0500

    OEM: Create a new file in oem-ibm file io to serve COD
    This commit introduces a new LicenseHandler class that is inherits
    the FileHandler class. This class contains functions to read the
    the license and create a new license file in path /var/lib/ibm/cod
    that will be read and sent to the phyp.
    
    Signed-off-by: Varsha Kaverappa <vkaverap@in.ibm.com>
    Change-Id: Ibabeb812795fad1be2e12cd3bb9b740f1f3112b6

---
+++ oem/ibm/libpldmresponder/file_io_type_lic.cpp
@@ -0,0 +1,39 @@
+#include "file_io_type_lic.hpp"
+
+#include "libpldm/base.h"
+#include "oem/ibm/libpldm/file_io.h"
+
+#include "common/utils.hpp"
+
+#include <stdint.h>
+
+#include <iostream>
+
+namespace pldm
+{
+namespace responder
+{
+
+static constexpr auto codFilePath = "/var/lib/ibm/cod/";
+
+int LicenseHandler::read(uint32_t offset, uint32_t& length, Response& response,
+                         oem_platform::Handler* /*oemPlatformHandler*/)
+{
+    std::string filePath = codFilePath;
+    filePath += "licFile"
+
+        if (licType != PLDM_FILE_TYPE_COD_LICENSE_KEY)
+    {
+        return PLDM_ERROR_INVALID_DATA;
+    }
+    auto rc = readFile(filePath.c_str(), offset, length, response);
+    fs::remove(filePath);
+    if (rc)
+    {
+        return PLDM_ERROR;
+    }
+    return PLDM_SUCCESS;
+}
+
+} // namespace responder
+} // namespace pldm
