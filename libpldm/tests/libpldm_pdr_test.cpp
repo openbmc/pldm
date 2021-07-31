6c799512d18dfd6310b0067c41a2236d3fe66115
George Liu <liuxiwei@inspur.com>
2021-07-31 15:23:45 +0800

    Normalization fru record set and state sensor/effecter PDRs from Host
    When Host sends the entity association PDR to BMC, BMC will generate
    a new container Id and put it into Host entity association PDR, and
    then add it to BMC entity association tree.
    
    As the container Id in the corresponding Host fru record set PDR and
    state sensor/effecter PDR has not been updated, when we get a
    fru record set PDR or state sensor/effecter PDR, we will check the
    entity association tree, pick up the changed container Id and update
    the fru record and add to our PDR repo.
    
    Also, In order to maintain the consistency of variable names, change
    the entity_instance_num of pldm_pdr_fru_record_set to entity_instance
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I48cab631ab4a50a8ba654c114792993cb5f418a7

--- libpldm/tests/libpldm_pdr_test.cpp
+++ libpldm/tests/libpldm_pdr_test.cpp
@@ -418,7 +418,7 @@ TEST(PDRUpdate, testAddFruRecordSet)
     EXPECT_EQ(fru->terminus_handle, htole16(1));
     EXPECT_EQ(fru->fru_rsi, htole16(10));
     EXPECT_EQ(fru->entity_type, htole16(1));
-    EXPECT_EQ(fru->entity_instance_num, htole16(0));
+    EXPECT_EQ(fru->entity_instance, htole16(0));
     EXPECT_EQ(fru->container_id, htole16(100));
     outData = nullptr;
 
@@ -441,7 +441,7 @@ TEST(PDRUpdate, testAddFruRecordSet)
     EXPECT_EQ(fru->terminus_handle, htole16(2));
     EXPECT_EQ(fru->fru_rsi, htole16(11));
     EXPECT_EQ(fru->entity_type, htole16(2));
-    EXPECT_EQ(fru->entity_instance_num, htole16(1));
+    EXPECT_EQ(fru->entity_instance, htole16(1));
     EXPECT_EQ(fru->container_id, htole16(101));
     outData = nullptr;
 
@@ -459,7 +459,7 @@ TEST(PDRUpdate, testAddFruRecordSet)
     EXPECT_EQ(fru->terminus_handle, htole16(1));
     EXPECT_EQ(fru->fru_rsi, htole16(10));
     EXPECT_EQ(fru->entity_type, htole16(1));
-    EXPECT_EQ(fru->entity_instance_num, htole16(0));
+    EXPECT_EQ(fru->entity_instance, htole16(0));
     EXPECT_EQ(fru->container_id, htole16(100));
     outData = nullptr;
 
