90525633fe24ea9c5579e9d76798eb07d5589bec
Manojkiran Eda <manojkiran.eda@gmail.com>
2021-08-07 18:06:28 +0530

    Customise the DbusObject paths as per IBM
    Per IBM systems, the dbus paths are assumed that we
    have a single system/chassis/motherboard so to align
    with that, we are changing the generated object path
    for ibm systems.
    
    Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>
    Change-Id: I2370a4dea00b85b42199d2cd569f8b284b5f2101

--- host-bmc/host_pdr_handler.hpp
+++ host-bmc/host_pdr_handler.hpp
@@ -6,8 +6,8 @@
 #include "common/types.hpp"
 #include "common/utils.hpp"
 #include "libpldmresponder/event_parser.hpp"
+#include "libpldmresponder/oem_handler.hpp"
 #include "libpldmresponder/pdr_utils.hpp"
-#include "pldmd/dbus_impl_requester.hpp"
 #include "requester/handler.hpp"
 #include "utils.hpp"
 
@@ -106,6 +106,7 @@ class HostPDRHandler
         pldm_entity_association_tree* bmcEntityTree,
         pldm::dbus_api::Requester& requester,
         pldm::requester::Handler<pldm::requester::Request>* handler,
+        pldm::responder::oem_platform::Handler* oemPlatformHandler,
         bool verbose = false);
 
     /** @brief fetch PDRs from host firmware. See @class.
@@ -337,6 +338,9 @@ class HostPDRHandler
     /** @brief the vector of FRU Record Data Format
      */
     std::vector<responder::pdr_utils::FruRecordDataFormat> fruRecordData;
+
+    /** @OEM platform handler */
+    pldm::responder::oem_platform::Handler* oemPlatformHandler;
 
     /** @brief Object path and entity association and is only loaded once
      */
