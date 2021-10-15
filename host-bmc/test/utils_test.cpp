f3d64bd4e0d57f2fb7a1d7ce046f58c7d3b979fc
George Liu <liuxiwei@inspur.com>
2021-10-15 17:34:35 +0800

    Update pldm_entity_association_tree_add method
    MEX Fru has a requirement that bmc should not change the container ID
    of MEX PDR, so a bool type parameter is added to the
    pldm_entity_association_tree_add method to determine whether
    the container ID needs to be updated.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: Iaf233da29d4250c9139d2dd7e630fe796a62719c

--- host-bmc/test/utils_test.cpp
+++ host-bmc/test/utils_test.cpp
@@ -37,40 +37,163 @@ TEST(EntityAssociation, addObjectPathEntityAssociations1)
 
     auto tree = pldm_entity_association_tree_init();
 
-    auto l1 = pldm_entity_association_tree_add(
-        tree, &entities[0], 1, nullptr, PLDM_ENTITY_ASSOCIAION_PHYSICAL, true);
+    auto l1 = pldm_entity_association_tree_add(tree, &entities[0], 1, nullptr,
+                                               PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                               true, true);
 
-    auto l2 = pldm_entity_association_tree_add(
-        tree, &entities[1], 1, l1, PLDM_ENTITY_ASSOCIAION_PHYSICAL, true);
+    auto l2a = pldm_entity_association_tree_add(
+        tree, &entities[1], 0, l1, PLDM_ENTITY_ASSOCIAION_PHYSICAL, true, true);
+    auto l2b = pldm_entity_association_tree_add(
+        tree, &entities[2], 1, l1, PLDM_ENTITY_ASSOCIAION_PHYSICAL, true, true);
 
-    auto l3a = pldm_entity_association_tree_add(
-        tree, &entities[2], 0, l2, PLDM_ENTITY_ASSOCIAION_PHYSICAL, true);
-    auto l3b = pldm_entity_association_tree_add(
-        tree, &entities[3], 1, l2, PLDM_ENTITY_ASSOCIAION_PHYSICAL, true);
+    auto l3a = pldm_entity_association_tree_add(tree, &entities[7], 0, l2a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l3b = pldm_entity_association_tree_add(tree, &entities[8], 1, l2a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
 
-    auto l4a = pldm_entity_association_tree_add(
-        tree, &entities[4], 0, l3a, PLDM_ENTITY_ASSOCIAION_PHYSICAL, true);
-    auto l4b = pldm_entity_association_tree_add(
-        tree, &entities[5], 1, l3a, PLDM_ENTITY_ASSOCIAION_PHYSICAL, true);
+    auto l4a = pldm_entity_association_tree_add(tree, &entities[9], 0, l2b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l4b = pldm_entity_association_tree_add(tree, &entities[10], 1, l2b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
 
-    auto l5a = pldm_entity_association_tree_add(
-        tree, &entities[6], 0, l3b, PLDM_ENTITY_ASSOCIAION_PHYSICAL, true);
-    auto l5b = pldm_entity_association_tree_add(
-        tree, &entities[7], 1, l3b, PLDM_ENTITY_ASSOCIAION_PHYSICAL, true);
+    auto l5a = pldm_entity_association_tree_add(tree, &entities[11], 0, l4a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l5b = pldm_entity_association_tree_add(tree, &entities[12], 2, l4a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l5c = pldm_entity_association_tree_add(tree, &entities[13], 3, l4a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l5d = pldm_entity_association_tree_add(tree, &entities[14], 5, l4a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l5e = pldm_entity_association_tree_add(tree, &entities[15], 6, l4a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l5f = pldm_entity_association_tree_add(tree, &entities[16], 7, l4a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l5g = pldm_entity_association_tree_add(tree, &entities[17], 8, l4a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l5h = pldm_entity_association_tree_add(tree, &entities[18], 9, l4a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l5i = pldm_entity_association_tree_add(tree, &entities[19], 15, l4a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+
+    auto l6a = pldm_entity_association_tree_add(tree, &entities[20], 1, l4b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l6b = pldm_entity_association_tree_add(tree, &entities[21], 2, l4b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l6c = pldm_entity_association_tree_add(tree, &entities[22], 4, l4b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l6d = pldm_entity_association_tree_add(tree, &entities[23], 8, l4b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l6e = pldm_entity_association_tree_add(tree, &entities[24], 10, l4b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l6f = pldm_entity_association_tree_add(tree, &entities[25], 14, l4b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+
+    auto l7a = pldm_entity_association_tree_add(tree, &entities[26], 5, l3a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l7b = pldm_entity_association_tree_add(tree, &entities[27], 6, l3a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l7c = pldm_entity_association_tree_add(tree, &entities[28], 8, l3a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l7d = pldm_entity_association_tree_add(tree, &entities[29], 9, l3a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l7e = pldm_entity_association_tree_add(tree, &entities[30], 11, l3a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l7f = pldm_entity_association_tree_add(tree, &entities[31], 12, l3a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l7g = pldm_entity_association_tree_add(tree, &entities[32], 13, l3a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l7h = pldm_entity_association_tree_add(tree, &entities[33], 14, l3a,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+
+    auto l8a = pldm_entity_association_tree_add(tree, &entities[34], 1, l3b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l8b = pldm_entity_association_tree_add(tree, &entities[35], 2, l3b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l8c = pldm_entity_association_tree_add(tree, &entities[36], 4, l3b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l8d = pldm_entity_association_tree_add(tree, &entities[37], 10, l3b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l8e = pldm_entity_association_tree_add(tree, &entities[38], 12, l3b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l8f = pldm_entity_association_tree_add(tree, &entities[39], 13, l3b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
+    auto l8g = pldm_entity_association_tree_add(tree, &entities[40], 14, l3b,
+                                                PLDM_ENTITY_ASSOCIAION_PHYSICAL,
+                                                true, true);
 
     EntityAssociations entityAssociations = {
-        {l1, l2}, {l2, l3a, l3b}, {l3a, l4a, l4b}, {l3b, l5a, l5b}};
+        {l1, l2a, l2b},
+        {l2a, l3a, l3b},
+        {l2b, l4a, l4b},
+        {l4a, l5a, l5b, l5c, l5d, l5e, l5f, l5g, l5h, l5i},
+        {l4b, l6a, l6b, l6c, l6d, l6e, l6f},
+        {l3a, l7a, l7b, l7c, l7d, l7e, l7f, l7g, l7h},
+        {l3b, l8a, l8b, l8c, l8d, l8e, l8f, l8g}};
 
     ObjectPathMaps retObjectMaps = {
-        {"/xyz/openbmc_project/inventory/chassis1", l1},
-        {"/xyz/openbmc_project/inventory/chassis1/motherboard1", l2},
-        {"/xyz/openbmc_project/inventory/chassis1/motherboard1/dcm0", l3a},
-        {"/xyz/openbmc_project/inventory/chassis1/motherboard1/dcm1", l3b},
-        {"/xyz/openbmc_project/inventory/chassis1/motherboard1/dcm0/cpu0", l4a},
-        {"/xyz/openbmc_project/inventory/chassis1/motherboard1/dcm0/cpu1", l4b},
-        {"/xyz/openbmc_project/inventory/chassis1/motherboard1/dcm1/cpu0", l5a},
-        {"/xyz/openbmc_project/inventory/chassis1/motherboard1/dcm1/cpu1", l5b},
-    };
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu0/core5", l7a},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu0/core6", l7b},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu0/core8", l7c},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu0/core9", l7d},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu0/core11", l7e},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu0/core12", l7f},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu0/core13", l7g},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu0/core14", l7h},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu1/core1", l8a},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu1/core2", l8b},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu1/core4", l8c},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu1/core10", l8d},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu1/core12", l8e},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu1/core13", l8f},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm0/cpu1/core14", l8g},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu0/core0", l5a},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu0/core2", l5b},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu0/core3", l5c},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu0/core5", l5d},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu0/core6", l5e},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu0/core7", l5f},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu0/core8", l5g},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu0/core9", l5h},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu0/core15", l5i},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu1/core1", l6a},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu1/core2", l6b},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu1/core4", l6c},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu1/core8", l6d},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu1/core10", l6e},
+        {"/xyz/openbmc_project/inventory/motherboard1/dcm1/cpu1/core14", l6f}};
 
     ObjectPathMaps objPathMap;
     pldm::hostbmc::utils::updateEntityAssociation(entityAssociations, tree,
