44b8624ec48e85d233c88759181f7aa86d427af5
Tom Joseph <rushtotom@gmail.com>
2021-10-25 19:30:32 +0530

    pldmtool: Add GetStatus firmware update command
    Tested:
    
    pldmtool fw_update GetStatus -m 44
    {     "CurrentState": "IDLE",
          "PreviousState": "IDLE",
          "AuxState": "Not applicable in current state",
          "AuxStateStatus": "AuxState is In Progress or Success",
          "ProgressPercent": 101,
          "ReasonCode": "Initialization of firmware device has occurred",
          "UpdateOptionFlagsEnabled": 0
    }
    
    Signed-off-by: Tom Joseph <rushtotom@gmail.com>
    Change-Id: I2eb41996ab0dcb1d00ac5b8b0e55846b0d4b28a5

--- pldmtool/pldmtool.cpp
+++ pldmtool/pldmtool.cpp
@@ -2,6 +2,7 @@
 #include "pldm_bios_cmd.hpp"
 #include "pldm_cmd_helper.hpp"
 #include "pldm_fru_cmd.hpp"
+#include "pldm_fw_update_cmd.hpp"
 #include "pldm_platform_cmd.hpp"
 #include "pldmtool/oem/ibm/pldm_oem_ibm.hpp"
 
@@ -72,6 +73,7 @@ int main(int argc, char** argv)
     pldmtool::bios::registerCommand(app);
     pldmtool::platform::registerCommand(app);
     pldmtool::fru::registerCommand(app);
+    pldmtool::fw_update::registerCommand(app);
 
 #ifdef OEM_IBM
     pldmtool::oem_ibm::registerCommand(app);
