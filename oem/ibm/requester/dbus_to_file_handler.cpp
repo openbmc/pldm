3378d44076aee6aebf40804371f707061cada842
Jayashankar Padath <jayashankar.padath@in.ibm.com>
2021-11-10 09:45:06 -0600

    oem: Fix to remove extra system dump
    An unwanted system dump is being generated if the resource dump is
    initiated from hypervisor with an empty vsp string. So the fix here
    is to additionally check the resource dump progress status and block
    sending the command to hypervisor if the status is not in progress.
    
    Tested by initiating a resource dump from hypervisor with an empty
    string and observed that extra system dump was not getting generated
    after the fix.
    
    Signed-off-by: Jayashankar Padath <jayashankar.padath@in.ibm.com>
    Change-Id: I25c1db3510d6f44b354881e4065aa148fe618464

--- oem/ibm/requester/dbus_to_file_handler.cpp
+++ oem/ibm/requester/dbus_to_file_handler.cpp
@@ -112,6 +112,26 @@ void DbusToFileHandler::reportResourceDumpFailure()
 void DbusToFileHandler::processNewResourceDump(
     const std::string& vspString, const std::string& resDumpReqPass)
 {
+    try
+    {
+        std::string objPath = resDumpCurrentObjPath;
+        auto propVal = pldm::utils::DBusHandler().getDbusPropertyVariant(
+            objPath.c_str(), "Status", resDumpProgressIntf);
+        const auto& curResDumpStatus = std::get<std::string>(propVal);
+
+        if (curResDumpStatus !=
+            "xyz.openbmc_project.Common.Progress.OperationStatus.InProgress")
+        {
+            return;
+        }
+    }
+    catch (const sdbusplus::exception_t& e)
+    {
+        std::cerr << "Error in getting current resource dump status, "
+                     "ERROR=" << e.what() << std::endl;
+        return;
+    }
+
     namespace fs = std::filesystem;
     const fs::path resDumpDirPath = "/var/lib/pldm/resourcedump";
 
