df6594191c4928c275eae95db5f1824e613728b6
Gilbert Chen <gilbert.chen@arm.com>
2022-02-14 12:12:25 +0000

    requester: Modified MctpDiscovery class
    Modified MctpDiscovery class to take list of managers instead of single
    fwManager. The change is for adding platform-mc manager.
    Added loadStaticEndpoints API for MCTP layer which doesn't implement
    /xyz/openbmc_project/MCTP/Endpoint.Interface
    
    The patch is part of implementation of design document below.
    https://gerrit.openbmc-project.xyz/c/openbmc/docs/+/47252
    
    There will be two more patches to follow related to the sensor code.
    One is for discovering PLDM terminus in system. Another is for
    monitoring numeric sensors and exporting sensor status to DBus.
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Change-Id: I1e1673504583a87f2a9bc3adf76fb49c2dc30254

--- requester/mctp_endpoint_discovery.hpp
+++ requester/mctp_endpoint_discovery.hpp
@@ -1,11 +1,33 @@
 #pragma once
 
-#include "fw-update/manager.hpp"
+#include "libpldm/requester/pldm.h"
 
 #include <sdbusplus/bus/match.hpp>
 
+#include <filesystem>
+#include <initializer_list>
+#include <vector>
+
 namespace pldm
 {
+
+using EID = uint8_t;
+using UUID = std::string;
+using MctpInfo = std::pair<EID, UUID>;
+using MctpInfos = std::vector<MctpInfo>;
+
+/** @class IMctpDiscoveryHandlerIntf
+ *
+ * This abstract class defines the APIs for MctpDiscovery class has common
+ * interface to execute function from different Manager Classes
+ */
+class IMctpDiscoveryHandlerIntf
+{
+  public:
+    virtual void handleMCTPEndpoints(const MctpInfos& mctpInfos) = 0;
+    virtual ~IMctpDiscoveryHandlerIntf()
+    {}
+};
 
 class MctpDiscovery
 {
@@ -21,21 +43,26 @@ class MctpDiscovery
      *         MCTP enabled devices
      *
      *  @param[in] bus - reference to systemd bus
-     *  @param[in] fwManager - pointer to the firmware manager
+     *  @param[in] list - initializer list to the IMctpDiscoveryHandlerIntf
      */
-    explicit MctpDiscovery(sdbusplus::bus::bus& bus,
-                           fw_update::Manager* fwManager);
+    explicit MctpDiscovery(
+        sdbusplus::bus::bus& bus,
+        std::initializer_list<IMctpDiscoveryHandlerIntf*> list);
+
+    void loadStaticEndpoints(const std::filesystem::path& jsonPath);
 
   private:
     /** @brief reference to the systemd bus */
     sdbusplus::bus::bus& bus;
 
-    fw_update::Manager* fwManager;
-
     /** @brief Used to watch for new MCTP endpoints */
     sdbusplus::bus::match_t mctpEndpointSignal;
 
+    std::vector<IMctpDiscoveryHandlerIntf*> handlers;
+
     void dicoverEndpoints(sdbusplus::message::message& msg);
+
+    void handleMCTPEndpoints(const MctpInfos& mctpInfos);
 
     static constexpr uint8_t mctpTypePLDM = 1;
 
---
+++ requester/test/mock_mctp_discovery_handler_intf.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "requester/mctp_endpoint_discovery.hpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace pldm
+{
+
+class MockManager : public pldm::IMctpDiscoveryHandlerIntf
+{
+  public:
+    MOCK_METHOD(void, handleMCTPEndpoints, (const MctpInfos& mctpInfos),
+                (override));
+};
+
+} // namespace pldm
