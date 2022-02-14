c250da3f711b49385d7a72204d706a572fafe5d9
Gilbert Chen <gilbert.chen@arm.com>
2022-02-14 12:12:25 +0000

    requester: Modified MctpDiscovery class
    Modified MctpDiscovery class to take list of managers instead of single
    fwManager. The change is for adding platform-mc manager.
    Added loadStaticEndpoints API for MCTP layer which doesn't implement
    /xyz/openbmc_project/MCTP/Endpoint.Interface
    
    The patch is part of implementation of design document below.
    https://gerrit.openbmc-project.xyz/c/openbmc/docs/+/47252
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Change-Id: I1e1673504583a87f2a9bc3adf76fb49c2dc30254

---
+++ requester/test/mock_mctp_discovery_handler_intf.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "requester/mctp_endpoint_discovery.hpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace pldm
+{
+
+class MockManager : public pldm::MctpDiscoveryHandlerIntf
+{
+  public:
+    MOCK_METHOD(void, handleMctpEndpoints, (const MctpInfos& mctpInfos),
+                (override));
+};
+
+} // namespace pldm
