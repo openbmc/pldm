366a5f4f8895aa62f8de616e956fc7e4ebcb4f83
ThuBaNguyen <thu@os.amperecomputing.com>
2022-02-15 09:02:20 +0700

    discovery: Handle interfacesRemoved signal
    Add the code to handle interfacesRemoved signal of MCTP D-Bus
    interfaces. The behavior of pldm services for the removed endpoints
    will be defined later.
    
    Tested:
    1. Unplug the PLDM device. The callback removeEndpoints() should be
    called. And the removed endpoint should be detected.
    
    Signed-off-by: ThuBaNguyen <thu@os.amperecomputing.com>
    Change-Id: Ieef02073b5494ba559185420747e4f85e6f6576e

--- requester/mctp_endpoint_discovery.cpp
+++ requester/mctp_endpoint_discovery.cpp
@@ -18,10 +18,16 @@ MctpDiscovery::MctpDiscovery(sdbusplus::bus::bus& bus,
                              fw_update::Manager* fwManager) :
     bus(bus),
     fwManager(fwManager),
-    mctpEndpointSignal(bus,
-                       sdbusplus::bus::match::rules::interfacesAdded(
-                           "/xyz/openbmc_project/mctp"),
-                       std::bind_front(&MctpDiscovery::dicoverEndpoints, this))
+    mctpEndpointAddedSignal(
+        bus,
+        sdbusplus::bus::match::rules::interfacesAdded(
+            "/xyz/openbmc_project/mctp"),
+        std::bind_front(&MctpDiscovery::discoverEndpoints, this)),
+    mctpEndpointRemovedSignal(
+        bus,
+        sdbusplus::bus::match::rules::interfacesRemoved(
+            "/xyz/openbmc_project/mctp"),
+        std::bind_front(&MctpDiscovery::removeEndpoints, this))
 {
     dbus::ObjectValueTree objects;
 
@@ -62,13 +68,16 @@ MctpDiscovery::MctpDiscovery(sdbusplus::bus::bus& bus,
         }
     }
 
+    /* Initial the listEids with the end points in MCTP D-Bus interface */
+    listEids = eids;
+
     if (eids.size() && fwManager)
     {
         fwManager->handleMCTPEndpoints(eids);
     }
 }
 
-void MctpDiscovery::dicoverEndpoints(sdbusplus::message::message& msg)
+void MctpDiscovery::discoverEndpoints(sdbusplus::message::message& msg)
 {
     constexpr std::string_view mctpEndpointIntfName{
         "xyz.openbmc_project.MCTP.Endpoint"};
@@ -92,6 +101,11 @@ void MctpDiscovery::dicoverEndpoints(sdbusplus::message::message& msg)
                     types.end())
                 {
                     eids.emplace_back(eid);
+                    /* Add eid to list Endpoints */
+                    if (!std::count(listEids.begin(), listEids.end(), eid))
+                    {
+                        listEids.emplace_back(eid);
+                    }
                 }
             }
         }
@@ -101,6 +115,79 @@ void MctpDiscovery::dicoverEndpoints(sdbusplus::message::message& msg)
     {
         fwManager->handleMCTPEndpoints(eids);
     }
+}
+
+void MctpDiscovery::removeEndpoints(sdbusplus::message::message& msg)
+{
+    dbus::ObjectValueTree objects;
+    msg = msg;
+
+    /*
+     * interfaceRemoved signal does not include the removed path in the response
+     * message. Check the remained EID in the MCTP D-Bus interface and compare
+     * with the previous list to find the removed EIDs
+     */
+    try
+    {
+        auto method = bus.new_method_call(
+            "xyz.openbmc_project.MCTP.Control", "/xyz/openbmc_project/mctp",
+            "org.freedesktop.DBus.ObjectManager", "GetManagedObjects");
+        auto reply = bus.call(method);
+        reply.read(objects);
+    }
+    catch (const std::exception& e)
+    {
+        /* Remove all of EID in list when MCTP D-Bus is empty*/
+        listEids = {};
+        /* ToDo: Do somethings with the removed MCTP endpoints */
+
+        return;
+    }
+
+    if (!objects.size())
+    {
+        /* Remove all of EID in list when MCTP D-Bus is empty*/
+        listEids = {};
+        /* ToDo: Do somethings with the removed MCTP endpoints */
+
+        return;
+    }
+
+    /* All of EIDs in MCTP D-Bus is removed */
+    if (!listEids.size())
+    {
+        return;
+    }
+
+    std::vector<mctp_eid_t> eids;
+
+    for (const auto& [objectPath, interfaces] : objects)
+    {
+        for (const auto& [intfName, properties] : interfaces)
+        {
+            if (intfName == mctpEndpointIntfName)
+            {
+                if (properties.contains("EID") &&
+                    properties.contains("SupportedMessageTypes"))
+                {
+                    auto eid = std::get<size_t>(properties.at("EID"));
+                    auto types = std::get<std::vector<uint8_t>>(
+                        properties.at("SupportedMessageTypes"));
+                    if (std::find(types.begin(), types.end(), mctpTypePLDM) !=
+                        types.end())
+                    {
+                        eids.emplace_back(eid);
+                    }
+                }
+            }
+        }
+    }
+
+    /* Update the list EIDs with the current endpoints in MCTP D-Bus */
+    listEids = eids;
+    /* ToDo: Do somethings with the removed MCTP endpoints */
+
+    return;
 }
 
 } // namespace pldm
