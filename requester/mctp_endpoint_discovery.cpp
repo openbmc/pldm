40de41fccad62e2e1d83e7d6b7887b507fe2b0e3
ThuBaNguyen <thu@os.amperecomputing.com>
2022-02-15 09:02:20 +0700

    discovery: Handle interfacesRemoved signal
    Add the code to handle interfacesRemoved signal of MCTP D-Bus
    interfaces. The behavior of pldm services for the removed endpoints
    will be defined later.
    
    Tested:
    1. Unplug the PLDM device. The callback removeEndpoints() should be
    called. And the removed endpoint should be detected.
    
    Signed-off-by: ThuBaNguyen <thu@os.amperecomputing.com>
    Change-Id: Ieef02073b5494ba559185420747e4f85e6f6576e

--- requester/mctp_endpoint_discovery.cpp
+++ requester/mctp_endpoint_discovery.cpp
@@ -18,10 +18,16 @@ MctpDiscovery::MctpDiscovery(sdbusplus::bus::bus& bus,
                              fw_update::Manager* fwManager) :
     bus(bus),
     fwManager(fwManager),
-    mctpEndpointSignal(bus,
-                       sdbusplus::bus::match::rules::interfacesAdded(
-                           "/xyz/openbmc_project/mctp"),
-                       std::bind_front(&MctpDiscovery::dicoverEndpoints, this))
+    mctpEndpointAddedSignal(
+        bus,
+        sdbusplus::bus::match::rules::interfacesAdded(
+            "/xyz/openbmc_project/mctp"),
+        std::bind_front(&MctpDiscovery::dicoverEndpoints, this)),
+    mctpEndpointRemovedSignal(
+        bus,
+        sdbusplus::bus::match::rules::interfacesRemoved(
+            "/xyz/openbmc_project/mctp"),
+        std::bind_front(&MctpDiscovery::removeEndpoints, this))
 {
     dbus::ObjectValueTree objects;
 
@@ -101,6 +107,38 @@ void MctpDiscovery::dicoverEndpoints(sdbusplus::message::message& msg)
     {
         fwManager->handleMCTPEndpoints(eids);
     }
+}
+
+void MctpDiscovery::removeEndpoints(sdbusplus::message::message& msg)
+{
+    constexpr std::string_view mctpEndpointIntfName{
+        "xyz.openbmc_project.MCTP.Endpoint"};
+    std::vector<mctp_eid_t> eids;
+
+    sdbusplus::message::object_path objPath;
+    std::map<std::string, std::map<std::string, dbus::Value>> interfaces;
+    msg.read(objPath, interfaces);
+
+    for (const auto& [intfName, properties] : interfaces)
+    {
+        if (intfName == mctpEndpointIntfName)
+        {
+            if (properties.contains("EID") &&
+                properties.contains("SupportedMessageTypes"))
+            {
+                auto eid = std::get<size_t>(properties.at("EID"));
+                auto types = std::get<std::vector<uint8_t>>(
+                    properties.at("SupportedMessageTypes"));
+                if (std::find(types.begin(), types.end(), mctpTypePLDM) !=
+                    types.end())
+                {
+                    eids.emplace_back(eid);
+                }
+            }
+        }
+    }
+
+    /* Do somethings with the removed MCTP endpoints */
 }
 
 } // namespace pldm
