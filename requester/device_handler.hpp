ff3cbaac897bc82e6986ac3c798b42a170c48678
ThuBaNguyen <thu@os.amperecomputing.com>
2022-02-15 10:10:43 +0700

    device: Add device manager and device handler
    Beside the host, pldmd will manager many others PLDM devices. This
    commit will create the device manager to manager the MCTP devices
    in MCTP D-Bus interface. The device manager then create the device
    handler for each new MCTP device. This device handler will provide the
    generic APIs to manage one PLDM device such as GetDeviceState,
    GetPDRs, CreateSensors, ReadingSensors...
    
    Tested:
    1. The addDevices() should be called when there are new devices in
    MCTP D-Bus interface. The device handler should be created for each
    new device.
    2. When the devices is removed in MCTP D-Bus interface, function
    removeDevices() should be called. The device handler of the removed
    devices should be deleted from the device list.
    
    Signed-off-by: ThuBaNguyen <thu@os.amperecomputing.com>
    Change-Id: Ie6ea13b0fa4128cfd3354d2e79cf0fa9c0c7ea1f

---
+++ requester/device_handler.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "libpldm/base.h"
+#include "libpldm/platform.h"
+
+#include "common/types.hpp"
+#include "common/utils.hpp"
+#include "pldmd/dbus_impl_requester.hpp"
+#include "requester/handler.hpp"
+
+#include <deque>
+#include <functional>
+#include <map>
+#include <memory>
+#include <vector>
+
+namespace pldm
+{
+
+namespace device
+{
+
+/** @class DevHandler
+ *  @brief This class handle the functional of a PLDM device
+ *  @details Provides an APIs to manage one PLDM device. The supported APIs can
+ *  be "GetDevicePDRs", "CreateSensors", "ReadingSensors"
+ */
+class DevHandler
+{
+  public:
+    /** @brief Constructor
+     *  @param[in] mctp_fd - fd of MCTP communications socket
+     *  @param[in] mctp_eid - MCTP EID of host firmware
+     *  @param[in] event - reference of main event loop of pldmd
+     *  @param[in] repo - pointer to BMC's primary PDR repo
+     *  @param[in] eventsJsonDir - directory path which has the config JSONs
+     *  @param[in] entityTree - Pointer to BMC and Host entity association tree
+     *  @param[in] bmcEntityTree - pointer to BMC's entity association tree
+     *  @param[in] requester - reference to Requester object
+     *  @param[in] handler - PLDM request handler
+     */
+    explicit DevHandler(
+        int mctp_fd, uint8_t mctp_eid, sdeventplus::Event& event,
+        sdbusplus::bus::bus& bus, pldm_pdr* repo,
+        pldm_entity_association_tree* entityTree,
+        pldm_entity_association_tree* bmcEntityTree,
+        pldm::dbus_api::Requester& requester,
+        pldm::requester::Handler<pldm::requester::Request>* handler);
+
+    ~DevHandler();
+
+  private:
+    /** @brief fd of MCTP communications socket */
+    int mctp_fd;
+    /** @brief MCTP EID of host firmware */
+    uint8_t mctp_eid;
+    /** @brief reference of main D-bus interface of pldmd devices */
+    sdbusplus::bus::bus& bus;
+
+    /** @brief reference of main event loop of pldmd, primarily used to schedule
+     *  work.
+     */
+    sdeventplus::Event& event;
+    /** @brief pointer to BMC's primary PDR repo, host PDRs are added here */
+    pldm_pdr* repo;
+
+    /** @brief Pointer to BMC's and Host's entity association tree */
+    pldm_entity_association_tree* entityTree;
+
+    /** @brief Pointer to BMC's entity association tree */
+    pldm_entity_association_tree* bmcEntityTree;
+
+    /** @brief reference to Requester object, primarily used to access API to
+     *  obtain PLDM instance id.
+     */
+    pldm::dbus_api::Requester& requester;
+
+    /** @brief PLDM request handler */
+    pldm::requester::Handler<pldm::requester::Request>* handler;
+};
+
+} // namespace device
+
+} // namespace pldm
