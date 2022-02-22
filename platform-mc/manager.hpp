ca29cc0c2994f7e118b475dc76c6d18453325e35
Gilbert Chen <gilbert.chen@arm.com>
2022-02-22 15:40:17 +0000

    platform-mc: Added TerminusManager and Terminus class
    Added requester::sendRecvPldmMsg awaiter type to be able to send and
    receive PLDM message by coroutine.
    Added TerminusManager to discover terminus from EID list updated by
    MCTPDiscovery class. The TerminusManager will initialize TID and
    fetch PDRs to instanciate terminus object for the EID which supports
    PLDM Type2. The TerminusManager doesn't handle the event receiver
    related initialization steps, and either doesn't support primary PDR
    repository to mantain terminus locator PDR information yet.
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Change-Id: Ifa5bdfff50648f1d7fba8710e160de662e8f9e06

---
+++ platform-mc/manager.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "libpldm/requester/pldm.h"
+
+#include "common/types.hpp"
+#include "pldmd/dbus_impl_requester.hpp"
+#include "requester/handler.hpp"
+#include "requester/mctp_endpoint_discovery.hpp"
+#include "terminus_manager.hpp"
+
+#include <vector>
+
+namespace pldm
+{
+namespace platform_mc
+{
+using namespace pldm::dbus_api;
+using namespace pldm::pdr;
+
+/**
+ * @brief Manager
+ *
+ * This class handles all the aspect of the PLDM Platform Monitoring and Control
+ * sepcification for the MCTP devices
+ */
+class Manager : public pldm::MctpDiscoveryHandlerIntf
+{
+  public:
+    Manager() = delete;
+    Manager(const Manager&) = delete;
+    Manager(Manager&&) = delete;
+    Manager& operator=(const Manager&) = delete;
+    Manager& operator=(Manager&&) = delete;
+    ~Manager() = default;
+
+    explicit Manager(sdeventplus::Event& event,
+                     requester::Handler<requester::Request>& handler,
+                     Requester& requester) :
+        terminusManager(event, handler, requester, termini, this)
+    {}
+
+    void handleMCTPEndpoints(const MctpInfos& mctpInfos)
+    {
+        terminusManager.discoverTerminus(mctpInfos);
+    }
+
+    Response handleRequest(mctp_eid_t eid, Command command,
+                           const pldm_msg* request, size_t reqMsgLen)
+    {
+        return terminusManager.handleRequest(eid, command, request, reqMsgLen);
+    }
+
+  private:
+    /** @brief List of discovered termini */
+    std::map<mctp_eid_t, std::shared_ptr<Terminus>> termini{};
+
+    TerminusManager terminusManager;
+};
+} // namespace platform_mc
+} // namespace pldm
---
+++ platform-mc/terminus.cpp
@@ -0,0 +1,21 @@
+#include "terminus.hpp"
+
+#include "platform.h"
+
+#include "terminus_manager.hpp"
+
+namespace pldm
+{
+namespace platform_mc
+{
+
+Terminus::Terminus(mctp_eid_t _eid, uint8_t _tid, uint64_t supportedTypes) :
+    _eid(_eid), _tid(_tid), supportedTypes(supportedTypes)
+{}
+
+bool Terminus::doesSupport(uint8_t type)
+{
+    return (supportedTypes & (1 << type));
+}
+} // namespace platform_mc
+} // namespace pldm
