965f0559cda7daa4d0f2c955b1bbdf198a19b875
Gilbert Chen <gilbert.chen@arm.com>
2022-02-22 15:40:17 +0000

    platform-mc: Added Terminus/TerminusManager class
    Added requester::sendRecvPldmMsg awaiter type to be able to send and
    receive PLDM message by coroutine.
    Added TerminusManager to discover terminus from EID list updated by
    MCTPDiscovery class. The TerminusManager will initialize TID and
    fetch PDRs to instantiate terminus object for the EID which supports
    PLDM Type2. The TerminusManager doesn't handle the event receiver
    related initialization steps, and either doesn't support primary PDR
    repository to maintain terminus locator PDR information yet.
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>
    Change-Id: Ifa5bdfff50648f1d7fba8710e160de662e8f9e06

---
+++ platform-mc/test/terminus_test.cpp
@@ -0,0 +1,24 @@
+#include "platform-mc/terminus.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace pldm::platform_mc;
+
+TEST(TerminusTest, supportedTypeTest)
+{
+    auto t1 = Terminus(1, 1 << PLDM_BASE);
+    auto t2 = Terminus(2, 1 << PLDM_BASE | 1 << PLDM_PLATFORM);
+
+    EXPECT_EQ(true, t1.doesSupport(PLDM_BASE));
+    EXPECT_EQ(false, t1.doesSupport(PLDM_PLATFORM));
+    EXPECT_EQ(true, t2.doesSupport(PLDM_BASE));
+    EXPECT_EQ(true, t2.doesSupport(PLDM_PLATFORM));
+}
+
+TEST(TerminusTest, getTidTest)
+{
+    const pldm::tid_t tid = 1;
+    auto t1 = Terminus(tid, 1 << PLDM_BASE);
+
+    EXPECT_EQ(tid, t1.getTid());
+}
