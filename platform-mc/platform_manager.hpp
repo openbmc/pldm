96b66467a702d498729b38ffe008193e540a7158
Gilbert Chen <gilbert.chen@arm.com>
2022-02-22 15:40:17 +0000

    platform-mc: Added Terminus/TerminusManager class
    Added requester::sendRecvPldmMsg awaiter type to be able to send and
    receive PLDM message by coroutine.
    Added TerminusManager to discover terminus from EID list updated by
    MCTPDiscovery class. The TerminusManager will initialize TID and
    fetch PDRs to instantiate terminus object for the EID which supports
    PLDM Type2. The TerminusManager doesn't handle the event receiver
    related initialization steps, and either doesn't support primary PDR
    repository to maintain terminus locator PDR information yet.
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>
    Change-Id: Ifa5bdfff50648f1d7fba8710e160de662e8f9e06

---
+++ platform-mc/platform_manager.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include "libpldm/platform.h"
+#include "libpldm/pldm.h"
+
+#include "terminus.hpp"
+#include "terminus_manager.hpp"
+
+namespace pldm
+{
+
+namespace platform_mc
+{
+
+/**
+ * @brief PlatformManager
+ *
+ * PlatformManager class manages Terminus for fetching PDRs and initializing
+ * sensors.
+ */
+class PlatformManager
+{
+  public:
+    PlatformManager() = delete;
+    PlatformManager(const PlatformManager&) = delete;
+    PlatformManager(PlatformManager&&) = delete;
+    PlatformManager& operator=(const PlatformManager&) = delete;
+    PlatformManager& operator=(PlatformManager&&) = delete;
+    ~PlatformManager() = default;
+
+    explicit PlatformManager(
+        TerminusManager& terminusManager,
+        std::map<pldm_tid_t, std::shared_ptr<Terminus>>& termini) :
+        terminusManager(terminusManager),
+        termini(termini)
+    {}
+
+    /** @brief Initialize terminus which supports PLDM Type 2
+     *  @return coroutine return_value - PLDM completion code
+     */
+    exec::task<int> initTerminus();
+
+  private:
+    /** @brief Fetch all PDRs from terminus.
+     *
+     *  @param[in] terminus - The terminus object to store fetched PDRs
+     *  @return coroutine return_value - PLDM completion code
+     */
+    exec::task<int> getPDRs(std::shared_ptr<Terminus> terminus);
+
+    /** @brief Fetch PDR from terminus
+     *
+     *  @param[in] tid - Destination TID
+     *  @param[in] recordHndl - Record handle
+     *  @param[in] dataTransferHndl - Data transfer handle
+     *  @param[in] transferOpFlag - Transfer Operation Flag
+     *  @param[in] requstCnt - Request Count of data
+     *  @param[in] recordChgNum - Record change number
+     *  @param[out] nextRecordHndl - Next record handle
+     *  @param[out] nextDataTransferHndl - Next data transfer handle
+     *  @param[out] transferFlag - Transfer flag
+     *  @param[out] responseCnt - Response count of record data
+     *  @param[out] recordData - Returned record data
+     *  @param[out] transferCrc - CRC value when record data is last part of PDR
+     *  @return coroutine return_value - PLDM completion code
+     */
+    exec::task<int>
+        getPDR(const pldm_tid_t tid, const uint32_t recordHndl,
+               const uint32_t dataTransferHndl, const uint8_t transferOpFlag,
+               const uint16_t requestCnt, const uint16_t recordChgNum,
+               uint32_t& nextRecordHndl, uint32_t& nextDataTransferHndl,
+               uint8_t& transferFlag, uint16_t& responseCnt,
+               std::vector<uint8_t>& recordData, uint8_t& transferCrc);
+
+    /** @brief get PDR repository information.
+     *
+     *  @param[in] tid - Destination TID
+     *  @param[out] repositoryState - the state of repository
+     *  @param[out] recordCount - number of records
+     *  @param[out] repositorySize - repository size
+     *  @param[out] largestRecordSize - largest record size
+     * *
+     *  @return coroutine return_value - PLDM completion code
+     */
+    exec::task<int> getPDRRepositoryInfo(const pldm_tid_t tid,
+                                         uint8_t& repositoryState,
+                                         uint32_t& recordCount,
+                                         uint32_t& repositorySize,
+                                         uint32_t& largestRecordSize);
+
+    /** @brief reference of terminusManager */
+    TerminusManager& terminusManager;
+
+    /** @brief Managed termini list */
+    std::map<pldm_tid_t, std::shared_ptr<Terminus>>& termini;
+};
+} // namespace platform_mc
+} // namespace pldm
