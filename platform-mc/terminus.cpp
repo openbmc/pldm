4d6681eb6db1ab535e046992646ab4c6f2cff2cc
Gilbert Chen <gilbert.chen@arm.com>
2022-02-22 15:40:17 +0000

    platform-mc: Added Terminus/TerminusManager class
    Added requester::sendRecvPldmMsg awaiter type to be able to send and
    receive PLDM message by coroutine.
    Added TerminusManager to discover terminus from EID list updated by
    MCTPDiscovery class. The TerminusManager will initialize TID and
    fetch PDRs to instantiate terminus object for the EID which supports
    PLDM Type2. The TerminusManager doesn't handle the event receiver
    related initialization steps, and either doesn't support primary PDR
    repository to maintain terminus locator PDR information yet.
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>
    Change-Id: Ifa5bdfff50648f1d7fba8710e160de662e8f9e06

---
+++ platform-mc/terminus.cpp
@@ -0,0 +1,23 @@
+#include "terminus.hpp"
+
+#include "libpldm/platform.h"
+
+#include "terminus_manager.hpp"
+
+namespace pldm
+{
+namespace platform_mc
+{
+
+Terminus::Terminus(pldm_tid_t tid, uint64_t supportedTypes) :
+    initalized(false), tid(tid), supportedTypes(supportedTypes)
+{
+    isConstructed = true;
+}
+
+bool Terminus::doesSupport(uint8_t type)
+{
+    return supportedTypes.test(type);
+}
+} // namespace platform_mc
+} // namespace pldm
