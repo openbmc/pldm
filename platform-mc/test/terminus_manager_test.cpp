f8e29c859f32392b5b01a1e30081b65873872ea4
Gilbert Chen <gilbert.chen@arm.com>
2022-02-22 15:40:17 +0000

    platform-mc: Added TerminusManager and Terminus class
    Added requester::sendRecvPldmMsg awaiter type to be able to send and
    receive PLDM message by coroutine.
    Added TerminusManager to discover terminus from EID list updated by
    MCTPDiscovery class. The TerminusManager will initialize TID and
    fetch PDRs to instanciate terminus object for the EID which supports
    PLDM Type2. The TerminusManager doesn't handle the event receiver
    related initialization steps, and either doesn't support primary PDR
    repository to mantain terminus locator PDR information yet.
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Change-Id: Ifa5bdfff50648f1d7fba8710e160de662e8f9e06

---
+++ platform-mc/test/terminus_manager_test.cpp
@@ -0,0 +1,93 @@
+#include "libpldm/base.h"
+
+#include "platform-mc/terminus_manager.hpp"
+#include "pldmd/dbus_impl_requester.hpp"
+#include "requester/handler.hpp"
+#include "requester/mctp_endpoint_discovery.hpp"
+#include "requester/request.hpp"
+
+#include <sdbusplus/timer.hpp>
+#include <sdeventplus/event.hpp>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace std::chrono;
+
+using ::testing::AtLeast;
+using ::testing::Between;
+using ::testing::Exactly;
+using ::testing::NiceMock;
+using ::testing::Return;
+
+class TerminusManagerTest : public testing::Test
+{
+  protected:
+    TerminusManagerTest() :
+        bus(pldm::utils::DBusHandler::getBus()),
+        event(sdeventplus::Event::get_default()),
+        dbusImplRequester(bus, "/xyz/openbmc_project/pldm"),
+        reqHandler(fd, event, dbusImplRequester, false, 90000, seconds(1), 2,
+                   milliseconds(100)),
+        terminusManager(event, reqHandler, dbusImplRequester, termini)
+    {}
+
+    int fd = -1;
+    sdbusplus::bus::bus& bus;
+    sdeventplus::Event event;
+    pldm::dbus_api::Requester dbusImplRequester;
+    pldm::requester::Handler<pldm::requester::Request> reqHandler;
+    pldm::platform_mc::TerminusManager terminusManager;
+    std::map<mctp_eid_t, std::shared_ptr<pldm::platform_mc::Terminus>> termini;
+};
+
+TEST_F(TerminusManagerTest, mapTIDTest)
+{
+    uint8_t eid = 1;
+
+    auto mappedTid = terminusManager.toTID(eid);
+    EXPECT_EQ(mappedTid, std::nullopt);
+
+    auto tid = terminusManager.mapToTID(eid);
+    EXPECT_NE(tid, std::nullopt);
+
+    auto mappedEid = terminusManager.toEID(tid.value());
+    EXPECT_EQ(mappedEid.value(), eid);
+
+    mappedTid = terminusManager.toTID(eid);
+    EXPECT_EQ(mappedTid.value(), tid.value());
+
+    tid = terminusManager.mapToTID(eid);
+    EXPECT_EQ(mappedTid.value(), tid.value());
+
+    terminusManager.unmapTID(tid.value());
+    mappedTid = terminusManager.toTID(eid);
+    EXPECT_EQ(mappedTid, std::nullopt);
+}
+
+TEST_F(TerminusManagerTest, negativeMapTIDTest)
+{
+    // lookup a unmapped EID to TID
+    auto mappedTid = terminusManager.toTID(1);
+    EXPECT_EQ(mappedTid, std::nullopt);
+
+    // map null EID(0)
+    mappedTid = terminusManager.mapToTID(0);
+    EXPECT_EQ(mappedTid, std::nullopt);
+
+    // map booadcast EID(0xff)
+    mappedTid = terminusManager.mapToTID(0xff);
+    EXPECT_EQ(mappedTid, std::nullopt);
+
+    // lookup reserved TID(0)
+    auto mappedEid = terminusManager.toEID(0);
+    EXPECT_EQ(mappedEid, std::nullopt);
+
+    // lookup reserved TID(0xff)
+    mappedEid = terminusManager.toEID(0xff);
+    EXPECT_EQ(mappedEid, std::nullopt);
+
+    // lookup unmapped TID
+    mappedEid = terminusManager.toEID(1);
+    EXPECT_EQ(mappedEid, std::nullopt);
+}
