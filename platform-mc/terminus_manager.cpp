34f5715bafd1cbba19a83a0ca5715e0336111b5a
Gilbert Chen <gilbert.chen@arm.com>
2022-02-22 15:40:17 +0000

    platform-mc: Added Terminus/TerminusManager class
    Added requester::sendRecvPldmMsg awaiter type to be able to send and
    receive PLDM message by coroutine.
    Added TerminusManager to discover terminus from EID list updated by
    MCTPDiscovery class. The TerminusManager will initialize TID and
    fetch PDRs to instantiate terminus object for the EID which supports
    PLDM Type2. The TerminusManager doesn't handle the event receiver
    related initialization steps, and either doesn't support primary PDR
    repository to maintain terminus locator PDR information yet.
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>
    Change-Id: Ifa5bdfff50648f1d7fba8710e160de662e8f9e06

---
+++ platform-mc/terminus_manager.cpp
@@ -0,0 +1,418 @@
+#include "terminus_manager.hpp"
+
+#include "manager.hpp"
+
+namespace pldm
+{
+namespace platform_mc
+{
+
+std::optional<MctpInfo> TerminusManager::toMctpInfo(const pldm_tid_t& tid)
+{
+    if (transportLayerTable[tid] != SupportedTransportLayer::MCTP)
+    {
+        return std::nullopt;
+    }
+
+    auto it = mctpInfoTable.find(tid);
+    if (it == mctpInfoTable.end())
+    {
+        return std::nullopt;
+    }
+
+    return it->second;
+}
+
+std::optional<pldm_tid_t> TerminusManager::toTid(const MctpInfo& mctpInfo)
+{
+    auto mctpInfoTableIterator = std::find_if(
+        mctpInfoTable.begin(), mctpInfoTable.end(), [&mctpInfo](auto& v) {
+        return (std::get<0>(v.second) == std::get<0>(mctpInfo)) &&
+               (std::get<3>(v.second) == std::get<3>(mctpInfo));
+    });
+    if (mctpInfoTableIterator == mctpInfoTable.end())
+    {
+        return std::nullopt;
+    }
+    return mctpInfoTableIterator->first;
+}
+
+std::optional<pldm_tid_t> TerminusManager::mapTid(const MctpInfo& mctpInfo,
+                                                  pldm_tid_t tid)
+{
+    if (tidPool[tid])
+    {
+        return std::nullopt;
+    }
+
+    tidPool[tid] = true;
+    transportLayerTable[tid] = SupportedTransportLayer::MCTP;
+    mctpInfoTable[tid] = mctpInfo;
+
+    return tid;
+}
+
+std::optional<pldm_tid_t> TerminusManager::mapTid(const MctpInfo& mctpInfo)
+{
+    if (std::get<0>(mctpInfo) == 0 || std::get<0>(mctpInfo) == 0xff)
+    {
+        return std::nullopt;
+    }
+
+    auto mctpInfoTableIterator = std::find_if(
+        mctpInfoTable.begin(), mctpInfoTable.end(), [&mctpInfo](auto& v) {
+        return (std::get<0>(v.second) == std::get<0>(mctpInfo)) &&
+               (std::get<3>(v.second) == std::get<3>(mctpInfo));
+    });
+    if (mctpInfoTableIterator != mctpInfoTable.end())
+    {
+        return mctpInfoTableIterator->first;
+    }
+
+    auto tidPoolIterator = std::find(tidPool.begin(), tidPool.end(), false);
+    if (tidPoolIterator == tidPool.end())
+    {
+        return std::nullopt;
+    }
+
+    pldm_tid_t tid = std::distance(tidPool.begin(), tidPoolIterator);
+    return mapTid(mctpInfo, tid);
+}
+
+bool TerminusManager::unmapTid(const pldm_tid_t& tid)
+{
+    if (tid == 0 || tid == PLDM_TID_RESERVED)
+    {
+        return false;
+    }
+    tidPool[tid] = false;
+
+    auto transportLayerTableIterator = transportLayerTable.find(tid);
+    transportLayerTable.erase(transportLayerTableIterator);
+
+    auto mctpInfoTableIterator = mctpInfoTable.find(tid);
+    mctpInfoTable.erase(mctpInfoTableIterator);
+    return true;
+}
+
+void TerminusManager::discoverMctpTerminus(const MctpInfos& mctpInfos)
+{
+    queuedMctpInfos.emplace(mctpInfos);
+    auto& [scope, rcOpt] = discoverMctpTerminusTaskHandle;
+    if (!rcOpt.has_value())
+    {
+        return;
+    }
+    rcOpt.reset();
+    scope.spawn(discoverMctpTerminusTask() |
+                    stdexec::then([&](int rc) { rcOpt.emplace(rc); }),
+                exec::default_task_context<void>());
+}
+
+std::map<pldm_tid_t, std::shared_ptr<Terminus>>::iterator
+    TerminusManager::findTeminusPtr(const MctpInfo& mctpInfo)
+{
+    bool found = false;
+    auto it = termini.begin();
+    for (; it != termini.end();)
+    {
+        auto terminusMctpInfo = toMctpInfo(it->first);
+        /* Teminus already initialized and added to temini list */
+        if (terminusMctpInfo &&
+            (std::get<0>(terminusMctpInfo.value()) == std::get<0>(mctpInfo)) &&
+            (std::get<3>(terminusMctpInfo.value()) == std::get<3>(mctpInfo)))
+        {
+            found = true;
+            break;
+        }
+        it++;
+    }
+    if (found)
+    {
+        return it;
+    }
+
+    return termini.end();
+}
+
+exec::task<int> TerminusManager::discoverMctpTerminusTask()
+{
+    while (!queuedMctpInfos.empty())
+    {
+        if (manager)
+        {
+            co_await manager->beforeDiscoverTerminus();
+        }
+
+        const MctpInfos& mctpInfos = queuedMctpInfos.front();
+        for (auto& mctpInfo : mctpInfos)
+        {
+            auto it = findTeminusPtr(mctpInfo);
+            if (it == termini.end())
+            {
+                co_await initMctpTerminus(mctpInfo);
+            }
+        }
+
+        if (manager)
+        {
+            co_await manager->afterDiscoverTerminus();
+        }
+
+        queuedMctpInfos.pop();
+    }
+
+    co_return PLDM_SUCCESS;
+}
+
+void TerminusManager::removeMctpTerminus(const MctpInfos& mctpInfos)
+{
+    // remove terminus
+    for (auto& mctpInfo : mctpInfos)
+    {
+        auto it = findTeminusPtr(mctpInfo);
+        if (it == termini.end())
+        {
+            continue;
+        }
+
+        unmapTid(it->first);
+        termini.erase(it);
+    }
+}
+
+exec::task<int> TerminusManager::initMctpTerminus(const MctpInfo& mctpInfo)
+{
+    mctp_eid_t eid = std::get<0>(mctpInfo);
+    pldm_tid_t tid = 0;
+    bool isMapped = false;
+    auto rc = co_await getTidOverMctp(eid, &tid);
+    if (rc || tid == PLDM_TID_RESERVED)
+    {
+        co_return PLDM_ERROR;
+    }
+
+    /* Terminus already has TID */
+    if (tid != 0x00)
+    {
+        auto it = termini.find(tid);
+        /* TID is used by one discovered terminus */
+        if (it != termini.end())
+        {
+            auto terminusMctpInfo = toMctpInfo(it->first);
+            /* The discovered terminus has the same MCTP Info */
+            if (terminusMctpInfo &&
+                (std::get<0>(terminusMctpInfo.value()) ==
+                 std::get<0>(mctpInfo)) &&
+                (std::get<3>(terminusMctpInfo.value()) ==
+                 std::get<3>(mctpInfo)))
+            {
+                co_return PLDM_SUCCESS;
+            }
+            else
+            {
+                /* ToDo:
+                 * Maybe the terminus supports multiple medium interfaces
+                 * Or the TID is used by other terminus.
+                 * Check the UUID to confirm.
+                 */
+                isMapped = false;
+            }
+        }
+        /* Use the terminus TID for mapping */
+        else
+        {
+            auto mappedTid = mapTid(mctpInfo, tid);
+            if (!mappedTid)
+            {
+                co_return PLDM_ERROR;
+            }
+            isMapped = true;
+        }
+    }
+
+    if (!isMapped)
+    {
+        // Assigning a tid. If it has been mapped, mapTid()
+        // returns the tid assigned before.
+        auto mappedTid = mapTid(mctpInfo);
+        if (!mappedTid)
+        {
+            co_return PLDM_ERROR;
+        }
+
+        tid = mappedTid.value();
+        rc = co_await setTidOverMctp(eid, tid);
+        if (rc != PLDM_SUCCESS && rc != PLDM_ERROR_UNSUPPORTED_PLDM_CMD)
+        {
+            unmapTid(tid);
+            co_return rc;
+        }
+
+        auto it = termini.find(tid);
+        if (it != termini.end())
+        {
+            // the terminus has been discovered before
+            co_return PLDM_SUCCESS;
+        }
+    }
+    /* Discovery the mapped terminus */
+    uint64_t supportedTypes = 0;
+    rc = co_await getPLDMTypes(tid, supportedTypes);
+    if (rc)
+    {
+        std::cerr << "failed to get PLDM Types\n";
+        co_return PLDM_ERROR;
+    }
+
+    termini[tid] = std::make_shared<Terminus>(tid, supportedTypes);
+    co_return PLDM_SUCCESS;
+}
+
+exec::task<int>
+    TerminusManager::SendRecvPldmMsgOverMctp(mctp_eid_t eid, Request& request,
+                                             const pldm_msg** responseMsg,
+                                             size_t* responseLen)
+{
+    try
+    {
+        std::tie(*responseMsg, *responseLen) =
+            co_await handler.sendRecvMsg(eid, std::move(request));
+        co_return PLDM_SUCCESS;
+    }
+    catch (int rc)
+    {
+        std::cerr << "sendRecvPldmMsgOverMctp failed. rc="
+                  << static_cast<unsigned>(rc) << "\n";
+        co_return rc;
+    }
+}
+
+exec::task<int> TerminusManager::getTidOverMctp(mctp_eid_t eid, pldm_tid_t* tid)
+{
+    auto instanceId = instanceIdDb.next(eid);
+    Request request(sizeof(pldm_msg_hdr));
+    auto requestMsg = reinterpret_cast<pldm_msg*>(request.data());
+    auto rc = encode_get_tid_req(instanceId, requestMsg);
+    if (rc)
+    {
+        instanceIdDb.free(eid, instanceId);
+        std::cerr << "encode_get_tid_req failed. rc="
+                  << static_cast<unsigned>(rc) << "\n";
+        co_return rc;
+    }
+
+    const pldm_msg* responseMsg = NULL;
+    size_t responseLen = 0;
+    rc = co_await SendRecvPldmMsgOverMctp(eid, request, &responseMsg,
+                                          &responseLen);
+    if (rc)
+    {
+        co_return rc;
+    }
+
+    uint8_t completionCode = 0;
+    rc = decode_get_tid_resp(responseMsg, responseLen, &completionCode, tid);
+    if (rc)
+    {
+        std::cerr << "decode_get_tid_resp failed. rc="
+                  << static_cast<unsigned>(rc) << "\n";
+        co_return rc;
+    }
+
+    co_return completionCode;
+}
+
+exec::task<int> TerminusManager::setTidOverMctp(mctp_eid_t eid, pldm_tid_t tid)
+{
+    auto instanceId = instanceIdDb.next(eid);
+    Request request(sizeof(pldm_msg_hdr) + sizeof(pldm_set_tid_req));
+    auto requestMsg = reinterpret_cast<pldm_msg*>(request.data());
+    auto rc = encode_set_tid_req(instanceId, tid, requestMsg);
+    if (rc)
+    {
+        instanceIdDb.free(eid, instanceId);
+        co_return rc;
+    }
+
+    const pldm_msg* responseMsg = NULL;
+    size_t responseLen = 0;
+    rc = co_await SendRecvPldmMsgOverMctp(eid, request, &responseMsg,
+                                          &responseLen);
+    if (rc)
+    {
+        co_return rc;
+    }
+
+    if (responseMsg == NULL || responseLen != PLDM_SET_TID_RESP_BYTES)
+    {
+        co_return PLDM_ERROR_INVALID_LENGTH;
+    }
+
+    co_return responseMsg->payload[0];
+}
+
+exec::task<int> TerminusManager::getPLDMTypes(pldm_tid_t tid,
+                                              uint64_t& supportedTypes)
+{
+    Request request(sizeof(pldm_msg_hdr));
+    auto requestMsg = reinterpret_cast<pldm_msg*>(request.data());
+    auto rc = encode_get_types_req(0, requestMsg);
+    if (rc)
+    {
+        std::cerr << "encode_get_types_req failed. rc="
+                  << static_cast<unsigned>(rc) << "\n";
+        co_return rc;
+    }
+
+    const pldm_msg* responseMsg = NULL;
+    size_t responseLen = 0;
+
+    rc = co_await SendRecvPldmMsg(tid, request, &responseMsg, &responseLen);
+    if (rc)
+    {
+        co_return rc;
+    }
+
+    uint8_t completionCode = 0;
+    bitfield8_t* types = reinterpret_cast<bitfield8_t*>(&supportedTypes);
+    rc = decode_get_types_resp(responseMsg, responseLen, &completionCode,
+                               types);
+    if (rc)
+    {
+        std::cerr << "decode_get_types_resp failed. rc="
+                  << static_cast<unsigned>(rc) << "\n";
+        co_return rc;
+    }
+    co_return completionCode;
+}
+
+exec::task<int> TerminusManager::SendRecvPldmMsg(pldm_tid_t tid,
+                                                 Request& request,
+                                                 const pldm_msg** responseMsg,
+                                                 size_t* responseLen)
+{
+    if (tidPool[tid] &&
+        transportLayerTable[tid] == SupportedTransportLayer::MCTP)
+    {
+        auto mctpInfo = toMctpInfo(tid);
+        if (!mctpInfo)
+        {
+            co_return PLDM_ERROR;
+        }
+
+        auto eid = std::get<0>(mctpInfo.value());
+        auto requestMsg = reinterpret_cast<pldm_msg*>(request.data());
+        requestMsg->hdr.instance_id = instanceIdDb.next(eid);
+        auto rc = co_await SendRecvPldmMsgOverMctp(eid, request, responseMsg,
+                                                   responseLen);
+        co_return rc;
+    }
+    else
+    {
+        co_return PLDM_ERROR;
+    }
+}
+
+} // namespace platform_mc
+} // namespace pldm
---
+++ platform-mc/terminus_manager.hpp
@@ -0,0 +1,179 @@
+#pragma once
+
+#include "config.h"
+
+#include "libpldm/platform.h"
+#include "libpldm/pldm.h"
+
+#include "requester/handler.hpp"
+#include "requester/mctp_endpoint_discovery.hpp"
+#include "terminus.hpp"
+
+#include <queue>
+
+namespace pldm
+{
+
+enum SupportedTransportLayer
+{
+    MCTP
+};
+
+namespace platform_mc
+{
+constexpr size_t tidPoolSize = std::numeric_limits<pldm_tid_t>::max() + 1;
+using RequesterHandler = requester::Handler<requester::Request>;
+
+class Manager;
+/**
+ * @brief TerminusManager
+ *
+ * TerminusManager class to discover and initialize PLDM terminus.
+ */
+class TerminusManager
+{
+  public:
+    TerminusManager() = delete;
+    TerminusManager(const TerminusManager&) = delete;
+    TerminusManager(TerminusManager&&) = delete;
+    TerminusManager& operator=(const TerminusManager&) = delete;
+    TerminusManager& operator=(TerminusManager&&) = delete;
+    virtual ~TerminusManager() = default;
+
+    explicit TerminusManager(
+        sdeventplus::Event& event,
+        requester::Handler<requester::Request>& handler,
+        pldm::InstanceIdDb& instanceIdDb,
+        std::map<pldm_tid_t, std::shared_ptr<Terminus>>& termini,
+        Manager* manager) :
+        event(event),
+        handler(handler), instanceIdDb(instanceIdDb), termini(termini),
+        tidPool(tidPoolSize, false), manager(manager)
+    {
+        // DSP0240 v1.1.0 table-8, special value: 0,0xFF = reserved
+        tidPool[0] = true;
+        tidPool[PLDM_TID_RESERVED] = true;
+    }
+
+    /** @brief start a coroutine to discover terminus
+     *
+     *  @param[in] mctpInfos - list of mctpInfo to be checked
+     */
+    void discoverMctpTerminus(const MctpInfos& mctpInfos);
+
+    /** @brief remove MCTP endpoints
+     *
+     *  @param[in] mctpInfos - information of removed MCTP endpoints
+     */
+    void removeMctpTerminus(const MctpInfos& mctpInfos);
+
+    /** @brief Send request PLDM message to tid. The function will
+     *         return when received the response message from terminus.
+     *
+     *  @param[in] tid - tid
+     *  @param[in] request - request PLDM message
+     *  @param[out] responseMsg - response PLDM message
+     *  @param[out] responseLen - length of response PLDM message
+     *  @return coroutine return_value - PLDM completion code
+     */
+    exec::task<int> SendRecvPldmMsg(pldm_tid_t tid, Request& request,
+                                    const pldm_msg** responseMsg,
+                                    size_t* responseLen);
+
+    /** @brief Send request PLDM message to eid. The function will
+     *         return when received the response message from terminus.
+     *
+     *  @param[in] eid - eid
+     *  @param[in] request - request PLDM message
+     *  @param[out] responseMsg - response PLDM message
+     *  @param[out] responseLen - length of response PLDM message
+     *  @return coroutine return_value - PLDM completion code
+     */
+    virtual exec::task<int>
+        SendRecvPldmMsgOverMctp(mctp_eid_t eid, Request& request,
+                                const pldm_msg** responseMsg,
+                                size_t* responseLen);
+
+    /** @brief member functions to map/unmap tid
+     */
+    std::optional<MctpInfo> toMctpInfo(const pldm_tid_t& tid);
+    std::optional<pldm_tid_t> toTid(const MctpInfo& mctpInfo);
+    std::optional<pldm_tid_t> mapTid(const MctpInfo& mctpInfo);
+    std::optional<pldm_tid_t> mapTid(const MctpInfo& mctpInfo, pldm_tid_t tid);
+    bool unmapTid(const pldm_tid_t& tid);
+
+  private:
+    /** @brief The coroutine task execute by discoverMctpTerminus()
+     *
+     *  @return coroutine return_value - PLDM completion code
+     */
+    exec::task<int> discoverMctpTerminusTask();
+
+    /** @brief Initialize terminus and then instantiate terminus object to keeps
+     *         the data fetched from terminus
+     *
+     *  @param[in] mctpInfo - NetworkId, EID and UUID
+     */
+    exec::task<int> initMctpTerminus(const MctpInfo& mctpInfo);
+
+    /** @brief Send getTID PLDM command to destination EID and then return the
+     *         value of tid in reference parameter.
+     *
+     *  @param[in] eid - Destination EID
+     *  @param[out] tid - TID returned from terminus
+     *  @return coroutine return_value - PLDM completion code
+     */
+    exec::task<int> getTidOverMctp(mctp_eid_t eid, pldm_tid_t* tid);
+
+    /** @brief Find the terminus object pointer in termini list.
+     *
+     *  @param[in] mctpInfos - information of removed MCTP endpoints
+     */
+    std::map<pldm_tid_t, std::shared_ptr<Terminus>>::iterator
+        findTeminusPtr(const MctpInfo& mctpInfo);
+
+    /** @brief Send setTID command to destination EID.
+     *
+     *  @param[in] eid - Destination EID
+     *  @param[in] tid - Terminus ID
+     *  @return coroutine return_value - PLDM completion code
+     */
+    exec::task<int> setTidOverMctp(mctp_eid_t eid, pldm_tid_t tid);
+
+    /** @brief Send getPLDMTypes command to destination TID and then return the
+     *         value of supportedTypes in reference parameter.
+     *
+     *  @param[in] tid - Destination TID
+     *  @param[out] supportedTypes - Supported Types returned from terminus
+     *  @return coroutine return_value - PLDM completion code
+     */
+    exec::task<int> getPLDMTypes(pldm_tid_t tid, uint64_t& supportedTypes);
+
+    sdeventplus::Event& event;
+    RequesterHandler& handler;
+    pldm::InstanceIdDb& instanceIdDb;
+
+    /** @brief Managed termini list */
+    std::map<pldm_tid_t, std::shared_ptr<Terminus>>& termini;
+
+    /** @brief tables for maintaining assigned TID */
+    std::vector<bool> tidPool;
+    std::map<pldm_tid_t, SupportedTransportLayer> transportLayerTable;
+    std::map<pldm_tid_t, MctpInfo> mctpInfoTable;
+
+    /** @brief A queue of MctpInfos to be discovered **/
+    std::queue<MctpInfos> queuedMctpInfos{};
+
+    /** @brief coroutine handle of discoverTerminusTask */
+    std::pair<exec::async_scope, std::optional<int>>
+        discoverMctpTerminusTaskHandle{
+            std::piecewise_construct,
+            std::forward_as_tuple(),
+            std::forward_as_tuple((int)PLDM_SUCCESS),
+        };
+
+    /** @brief A Manager interface for calling the hook functions **/
+    Manager* manager;
+};
+} // namespace platform_mc
+} // namespace pldm
