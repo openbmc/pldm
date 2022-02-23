f4f0dc490a6ef59d376ee882c9b123e8031eb2b5
Gilbert Chen <gilbert.chen@arm.com>
2022-02-23 20:56:19 +0000

    platform-mc: Sensor handling
    Added sensor_manager and numeric_sensor class. The sensor_manager
    class manages the timing of sensor polling. The NumericSensor class
    handles sensor status and exports its status to D-Bus interfaces.
    
    tested: Verified on ast2600 EVB which is connected to a PLDM device
    over I2C. bmcweb can display the state of numeric sensor.
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Change-Id: I4257f823ea26d7fdb322cc82d847e94db056258c

--- platform-mc/terminus.cpp
+++ platform-mc/terminus.cpp
@@ -11,7 +11,12 @@ namespace platform_mc
 
 Terminus::Terminus(tid_t tid, uint64_t supportedTypes) :
     initalized(false), tid(tid), supportedTypes(supportedTypes)
-{}
+{
+    inventoryPath = "/xyz/openbmc_project/inventory/Item/Board/PLDM_Device_" +
+                    std::to_string(tid);
+    inventoryItemBoardInft = std::make_unique<InventoryItemBoardIntf>(
+        utils::DBusHandler::getBus(), inventoryPath.c_str());
+}
 
 bool Terminus::doesSupport(uint8_t type)
 {
@@ -23,6 +28,8 @@ bool Terminus::parsePDRs()
     bool rc = true;
     std::vector<std::shared_ptr<pldm_numeric_sensor_value_pdr>>
         numericSensorPdrs{};
+    std::vector<std::shared_ptr<pldm_compact_numeric_sensor_pdr>>
+        compactNumericSensorPdrs{};
 
     for (auto& pdr : pdrs)
     {
@@ -41,6 +48,20 @@ bool Terminus::parsePDRs()
                 numericSensorPdrs.emplace_back(std::move(parsedPdr));
             }
         }
+        else if (pdrHdr->type == PLDM_COMPACT_NUMERIC_SENSOR_PDR)
+        {
+            auto parsedPdr = parseCompactNumericSensorPDR(pdr);
+            if (parsedPdr != nullptr)
+            {
+                compactNumericSensorPdrs.emplace_back(std::move(parsedPdr));
+                auto sensorAuxiliaryNames = parseCompactNumericSensorNames(pdr);
+                if (sensorAuxiliaryNames != nullptr)
+                {
+                    sensorAuxiliaryNamesTbl.emplace_back(
+                        std::move(sensorAuxiliaryNames));
+                }
+            }
+        }
         else
         {
             std::cerr << "parsePDRs() Unsupported PDR, type="
@@ -48,6 +69,17 @@ bool Terminus::parsePDRs()
             rc = false;
         }
     }
+
+    for (auto pdr : numericSensorPdrs)
+    {
+        addNumericSensor(pdr);
+    }
+
+    for (auto pdr : compactNumericSensorPdrs)
+    {
+        addCompactNumericSensor(pdr);
+    }
+
     return rc;
 }
 
@@ -125,6 +157,159 @@ std::shared_ptr<pldm_numeric_sensor_value_pdr>
         return nullptr;
     }
     return parsedPdr;
+}
+
+void Terminus::addNumericSensor(
+    const std::shared_ptr<pldm_numeric_sensor_value_pdr> pdr)
+{
+    uint16_t sensorId = pdr->sensor_id;
+    std::string sensorName = "PLDM_Device_" + std::to_string(sensorId) + "_" +
+                             std::to_string(tid);
+
+    if (pdr->sensor_auxiliary_names_pdr)
+    {
+        auto sensorAuxiliaryNames = getSensorAuxiliaryNames(sensorId);
+        if (sensorAuxiliaryNames)
+        {
+            const auto& [sensorId, sensorCnt,
+                         sensorNames] = *sensorAuxiliaryNames;
+            if (sensorCnt == 1)
+            {
+                for (const auto& [languageTag, name] : sensorNames[0])
+                {
+                    if (languageTag == "en")
+                    {
+                        sensorName = name + "_" + std::to_string(sensorId) +
+                                     "_" + std::to_string(tid);
+                    }
+                }
+            }
+        }
+    }
+
+    try
+    {
+        auto sensor = std::make_shared<NumericSensor>(
+            tid, true, pdr, sensorName, inventoryPath);
+        numericSensors.emplace_back(sensor);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Failed to create NumericSensor. ERROR=" << e.what()
+                  << "sensorName=" << sensorName << "\n";
+    }
+}
+
+std::shared_ptr<SensorAuxiliaryNames>
+    Terminus::parseCompactNumericSensorNames(const std::vector<uint8_t>& sPdr)
+{
+    std::string nameString;
+    std::vector<std::vector<std::pair<NameLanguageTag, SensorName>>>
+        sensorAuxNames{};
+    std::vector<std::pair<NameLanguageTag, SensorName>> nameStrings{};
+    auto pdr =
+        reinterpret_cast<const pldm_compact_numeric_sensor_pdr*>(sPdr.data());
+
+    if (sPdr.size() <
+        (sizeof(pldm_compact_numeric_sensor_pdr) - sizeof(uint8_t)))
+    {
+        return nullptr;
+    }
+
+    if (pdr->sensor_name_length == 0)
+    {
+        return nullptr;
+    }
+
+    if (sPdr.size() < (sizeof(pldm_compact_numeric_sensor_pdr) -
+                       sizeof(uint8_t) + pdr->sensor_name_length))
+    {
+        return nullptr;
+    }
+
+    std::string sTemp(reinterpret_cast<const char*>(pdr->sensor_name),
+                      pdr->sensor_name_length);
+    size_t pos = 0;
+    while ((pos = sTemp.find(" ")) != std::string::npos)
+    {
+        sTemp.replace(pos, 1, "_");
+    }
+    nameString = sTemp;
+
+    nameString.erase(nameString.find('\0'));
+    nameStrings.emplace_back(std::make_pair("en", nameString));
+    sensorAuxNames.emplace_back(nameStrings);
+
+    return std::make_shared<SensorAuxiliaryNames>(pdr->sensor_id, 1,
+                                                  sensorAuxNames);
+}
+
+std::shared_ptr<pldm_compact_numeric_sensor_pdr>
+    Terminus::parseCompactNumericSensorPDR(const std::vector<uint8_t>& sPdr)
+{
+    std::string nameString;
+    std::vector<std::pair<NameLanguageTag, SensorName>> nameStrings{};
+    auto pdr =
+        reinterpret_cast<const pldm_compact_numeric_sensor_pdr*>(sPdr.data());
+    auto parsedPdr = std::make_shared<pldm_compact_numeric_sensor_pdr>();
+
+    parsedPdr->hdr = pdr->hdr;
+    parsedPdr->terminus_handle = pdr->terminus_handle;
+    parsedPdr->sensor_id = pdr->sensor_id;
+    parsedPdr->entity_type = pdr->entity_type;
+    parsedPdr->entity_instance = pdr->entity_instance;
+    parsedPdr->container_id = pdr->container_id;
+    parsedPdr->sensor_name_length = pdr->sensor_name_length;
+    parsedPdr->base_unit = pdr->base_unit;
+    parsedPdr->unit_modifier = pdr->unit_modifier;
+    parsedPdr->occurrence_rate = pdr->occurrence_rate;
+    parsedPdr->range_field_support = pdr->range_field_support;
+    parsedPdr->warning_high = pdr->warning_high;
+    parsedPdr->warning_low = pdr->warning_low;
+    parsedPdr->critical_high = pdr->critical_high;
+    parsedPdr->critical_low = pdr->critical_low;
+    parsedPdr->fatal_high = pdr->fatal_high;
+    parsedPdr->fatal_low = pdr->fatal_low;
+    return parsedPdr;
+}
+
+void Terminus::addCompactNumericSensor(
+    const std::shared_ptr<pldm_compact_numeric_sensor_pdr> pdr)
+{
+    uint16_t sensorId = pdr->sensor_id;
+    std::string sensorName = "PLDM_Device_" + std::to_string(sensorId) + "_" +
+                             std::to_string(tid);
+
+    auto sensorAuxiliaryNames = getSensorAuxiliaryNames(sensorId);
+    if (sensorAuxiliaryNames)
+    {
+        const auto& [sensorId, sensorCnt, sensorNames] = *sensorAuxiliaryNames;
+        if (sensorCnt == 1)
+        {
+            for (const auto& [languageTag, name] : sensorNames[0])
+            {
+                if (languageTag == "en")
+                {
+                    {
+                        sensorName = name + "_" + std::to_string(sensorId) +
+                                     "_" + std::to_string(tid);
+                    }
+                }
+            }
+        }
+    }
+
+    try
+    {
+        auto sensor = std::make_shared<NumericSensor>(
+            tid, true, pdr, sensorName, inventoryPath);
+        numericSensors.emplace_back(sensor);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Failed to create NumericSensor. ERROR=" << e.what()
+                  << "sensorName=" << sensorName << "\n";
+    }
 }
 
 } // namespace platform_mc
