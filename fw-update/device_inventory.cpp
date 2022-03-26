cae112e32fe6add612fe8e77bda9c86972b8713b
Tom Joseph <rushtotom@gmail.com>
2022-03-26 07:30:03 +0530

    fw-update: Add device inventory for FDs
    This patch creates D-Bus device inventory based on the firmware
    update config file and MCTP endpoints discovered by pldmd. The
    details of device inventory creation is documented in
    fw-update/examples/fw_update.json.
    
    This is an optional feature and can be skipped from the firmware
    update config file.
    
    Tested: Unit tests added
    
    Signed-off-by: Tom Joseph <rushtotom@gmail.com>
    Change-Id: Iced3482de85430bca60544597d09a092eaa5ef65

---
+++ fw-update/device_inventory.cpp
@@ -0,0 +1,45 @@
+#include "device_inventory.hpp"
+
+#include <iostream>
+
+namespace pldm::fw_update::device_inventory
+{
+
+Entry::Entry(sdbusplus::bus::bus& bus, const pldm::dbus::ObjectPath& objPath,
+             const pldm::UUID& mctpUUID, const Associations& assocs) :
+    Ifaces(bus, objPath.c_str(), true)
+{
+    Ifaces::type(ChassisType::Component, true);
+    Ifaces::uuid(mctpUUID, true);
+    Ifaces::associations(assocs, true);
+    Ifaces::emit_object_added();
+}
+
+Manager::Manager(sdbusplus::bus::bus& bus,
+                 const DeviceInventoryInfo& deviceInventoryInfo) :
+    bus(bus),
+    deviceInventoryInfo(deviceInventoryInfo)
+{}
+
+sdbusplus::message::object_path Manager::createEntry(const pldm::UUID& uuid)
+{
+    sdbusplus::message::object_path deviceObjPath{};
+    if (deviceInventoryInfo.contains(uuid))
+    {
+        auto search = deviceInventoryInfo.find(uuid);
+        const auto& objPath = std::get<DeviceObjPath>(search->second);
+        const auto& assocs = std::get<Associations>(search->second);
+        deviceEntryMap.emplace(
+            uuid, std::make_unique<Entry>(bus, objPath, uuid, assocs));
+        deviceObjPath = objPath;
+    }
+    else
+    {
+        // Skip if UUID is not present or device inventory information from
+        // firmware update config JSON is empty
+    }
+
+    return deviceObjPath;
+}
+
+} // namespace pldm::fw_update::device_inventory
