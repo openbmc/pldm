ead95d85d0568bcb481c5bccabdafb8ada0d6c34
Gilbert Chen <gilbert.chen@arm.com>
2022-05-24 15:35:21 +0100

    platform-mc: PDR handling
    Added parse PDR member functions to terminus class for parsing Numeric
    sensor PDR and sensor auxiliary names PDR.
    Added sensor auxiliary names PDR and numeric sensor
    PDR struct in libpldm/platform.h
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Change-Id: I30a0cc594a3c08fc17f2dad861b5c5d41c80ebdd

--- platform-mc/terminus_manager.cpp
+++ platform-mc/terminus_manager.cpp
@@ -7,6 +7,19 @@ namespace pldm
 namespace platform_mc
 {
 
+TerminusManager::TerminusManager(
+    sdeventplus::Event& event, requester::Handler<requester::Request>& handler,
+    dbus_api::Requester& requester,
+    std::map<tid_t, std::shared_ptr<Terminus>>& termini, Manager* manager) :
+    event(event),
+    handler(handler), requester(requester), termini(termini),
+    tidPool(tidPoolSize, false), manager(manager)
+{
+    // DSP0240 v1.1.0 table-8, special value: 0,0xFF = reserved
+    tidPool[0] = true;
+    tidPool[PLDM_TID_RESERVED] = true;
+}
+
 std::optional<MctpInfo> TerminusManager::toMctpInfo(const tid_t& tid)
 {
     if (transportLayerTable[tid] != SupportedTransportLayer::MCTP)
