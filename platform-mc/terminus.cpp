198bc35fda1802237738622949e775ed7aed89d3
Gilbert Chen <gilbert.chen@arm.com>
2022-05-24 15:35:21 +0100

    platform-mc: PDR handling
    Added parse PDR member functions to terminus class for parsing Numeric
    sensor PDR and sensor auxiliary names PDR.
    Added sensor auxiliary names PDR and numeric sensor
    PDR struct in libpldm/platform.h
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Change-Id: I30a0cc594a3c08fc17f2dad861b5c5d41c80ebdd

--- platform-mc/terminus.cpp
+++ platform-mc/terminus.cpp
@@ -17,5 +17,115 @@ bool Terminus::doesSupport(uint8_t type)
 {
     return supportedTypes.test(type);
 }
+
+bool Terminus::parsePDRs()
+{
+    bool rc = true;
+    std::vector<std::shared_ptr<pldm_numeric_sensor_value_pdr>>
+        numericSensorPdrs{};
+
+    for (auto& pdr : pdrs)
+    {
+        auto pdrHdr = reinterpret_cast<pldm_pdr_hdr*>(pdr.data());
+        if (pdrHdr->type == PLDM_SENSOR_AUXILIARY_NAMES_PDR)
+        {
+            auto sensorAuxiliaryNames = parseSensorAuxiliaryNamesPDR(pdr);
+            sensorAuxiliaryNamesTbl.emplace_back(
+                std::move(sensorAuxiliaryNames));
+        }
+        else if (pdrHdr->type == PLDM_NUMERIC_SENSOR_PDR)
+        {
+            auto parsedPdr = parseNumericSensorPDR(pdr);
+            if (parsedPdr != nullptr)
+            {
+                numericSensorPdrs.emplace_back(std::move(parsedPdr));
+            }
+        }
+        else
+        {
+            std::cerr << "parsePDRs() Unsupported PDR, type="
+                      << static_cast<unsigned>(pdrHdr->type) << "\n";
+            rc = false;
+        }
+    }
+    return rc;
+}
+
+std::shared_ptr<SensorAuxiliaryNames>
+    Terminus::getSensorAuxiliaryNames(SensorId id)
+{
+    for (auto sensorAuxiliaryNames : sensorAuxiliaryNamesTbl)
+    {
+        const auto& [sensorId, sensorCnt, sensorNames] = *sensorAuxiliaryNames;
+        if (sensorId == id)
+        {
+            return sensorAuxiliaryNames;
+        }
+    }
+    return nullptr;
+}
+
+std::shared_ptr<SensorAuxiliaryNames>
+    Terminus::parseSensorAuxiliaryNamesPDR(const std::vector<uint8_t>& pdrData)
+{
+    constexpr uint8_t nullTerminator = 0;
+    auto pdr = reinterpret_cast<const struct pldm_sensor_auxiliary_names_pdr*>(
+        pdrData.data());
+    const uint8_t* ptr = pdr->names;
+    std::vector<std::vector<std::pair<NameLanguageTag, SensorName>>>
+        sensorAuxNames{};
+    char16_t alignedBuffer[PLDM_STR_UTF_16_MAX_LEN];
+    for (int i = 0; i < pdr->sensor_count; i++)
+    {
+        const uint8_t nameStringCount = static_cast<uint8_t>(*ptr);
+        ptr += sizeof(uint8_t);
+        std::vector<std::pair<NameLanguageTag, SensorName>> nameStrings{};
+        for (int j = 0; j < nameStringCount; j++)
+        {
+            std::string nameLanguageTag(reinterpret_cast<const char*>(ptr), 0,
+                                        PLDM_STR_UTF_8_MAX_LEN);
+            ptr += nameLanguageTag.size() + sizeof(nullTerminator);
+
+            int u16NameStringLen = 0;
+            for (int i = 0; ptr[i] != 0 || ptr[i + 1] != 0; i += 2)
+            {
+                u16NameStringLen++;
+            }
+            memset(alignedBuffer, 0,
+                   PLDM_STR_UTF_16_MAX_LEN * sizeof(uint16_t));
+            memcpy(alignedBuffer, ptr, u16NameStringLen * sizeof(uint16_t));
+            std::u16string u16NameString(alignedBuffer, 0,
+                                         PLDM_STR_UTF_16_MAX_LEN);
+            ptr += (u16NameString.size() + sizeof(nullTerminator)) *
+                   sizeof(uint16_t);
+            std::transform(u16NameString.cbegin(), u16NameString.cend(),
+                           u16NameString.begin(),
+                           [](uint16_t utf16) { return be16toh(utf16); });
+            std::string nameString =
+                std::wstring_convert<std::codecvt_utf8_utf16<char16_t>,
+                                     char16_t>{}
+                    .to_bytes(u16NameString);
+            nameStrings.emplace_back(
+                std::make_pair(nameLanguageTag, nameString));
+        }
+        sensorAuxNames.emplace_back(nameStrings);
+    }
+    return std::make_shared<SensorAuxiliaryNames>(
+        pdr->sensor_id, pdr->sensor_count, sensorAuxNames);
+}
+
+std::shared_ptr<pldm_numeric_sensor_value_pdr>
+    Terminus::parseNumericSensorPDR(const std::vector<uint8_t>& pdr)
+{
+    const uint8_t* ptr = pdr.data();
+    auto parsedPdr = std::make_shared<pldm_numeric_sensor_value_pdr>();
+    auto rc = decode_numeric_sensor_pdr_data(ptr, pdr.size(), parsedPdr.get());
+    if (rc)
+    {
+        return nullptr;
+    }
+    return parsedPdr;
+}
+
 } // namespace platform_mc
 } // namespace pldm
