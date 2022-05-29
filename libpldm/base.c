b5c939163cda97202e8eb2b053aa1c153adf505e
Gilbert Chen <gilbert.chen@arm.com>
2022-05-29 19:38:36 +0100

    libpldm: Added encode API for SetTID cmd
    Added encode API for SetTID command(0x01) which is defined in
    DSP0240 Version 1.1.0 sec:8.1.1.
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Change-Id: I14cf1d9770416ab495a61bc95eec35feef13d71e

--- libpldm/base.c
+++ libpldm/base.c
@@ -398,6 +398,29 @@ int decode_get_tid_resp(const struct pldm_msg *msg, size_t payload_length,
 	return PLDM_SUCCESS;
 }
 
+int encode_set_tid_req(uint8_t instance_id, uint8_t tid, struct pldm_msg *msg)
+{
+	if (msg == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	struct pldm_header_info header = {0};
+	header.instance = instance_id;
+	header.msg_type = PLDM_REQUEST;
+	header.command = PLDM_SET_TID;
+
+	uint8_t rc = pack_pldm_header(&header, &(msg->hdr));
+	if (rc != PLDM_SUCCESS) {
+		return rc;
+	}
+
+	struct pldm_set_tid_req *request =
+	    (struct pldm_set_tid_req *)msg->payload;
+	request->tid = tid;
+
+	return PLDM_SUCCESS;
+}
+
 int encode_cc_only_resp(uint8_t instance_id, uint8_t type, uint8_t command,
 			uint8_t cc, struct pldm_msg *msg)
 {
