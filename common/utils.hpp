9820a4c3641747919d60ee2b37eb0b1a0113eca0
Sridevi Ramesh <sridevra@in.ibm.com>
2022-07-15 02:35:08 -0500

    PLDM:OEM-IBM: Add file type running.
    Change-Id: I4b08647556fc3b2ee7d4ff3ad66ea7dd35a112aa
    Signed-off-by: Sridevi Ramesh <sridevra@in.ibm.com>

--- common/utils.hpp
+++ common/utils.hpp
@@ -26,7 +26,6 @@ namespace pldm
 {
 namespace utils
 {
-
 namespace fs = std::filesystem;
 using Json = nlohmann::json;
 constexpr bool Tx = true;
@@ -407,6 +406,15 @@ std::vector<std::string> split(std::string_view srcStr, std::string_view delim,
  *  @return - std::string equivalent of the system time
  */
 std::string getCurrentSystemTime();
+
+/** @brief Method to get the value from a bios attribute
+ *
+ *  @param[in] dbusAttrName - the bios attribute name from
+ *             which the value must be retrieved
+ *
+ * @return the attribute value
+ */
+std::string getBiosAttrValue(const std::string& dbusAttrName);
 
 } // namespace utils
 } // namespace pldm
