f9a1680186731909ddea382c555aea2ab8a0d555
Rashmica Gupta <rashmica@linux.ibm.com>
2023-04-12 16:32:32 +1000

    pldmd: move to libpldm instance ID alloc/free
    Change-Id: Ia03de8245dfb114e6266ba36dcf26ca4398a4ce0
    Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>

--- pldmd/dbus_impl_requester.cpp
+++ pldmd/dbus_impl_requester.cpp
@@ -2,8 +2,6 @@
 
 #include "xyz/openbmc_project/Common/error.hpp"
 
-#include <iostream>
-
 using namespace sdbusplus::xyz::openbmc_project::Common::Error;
 
 namespace pldm
@@ -11,26 +9,24 @@ namespace pldm
 namespace dbus_api
 {
 
+/* Ideally we would be able to look up the TID for a given EID. We don't
+ * have that infrastructure in place yet. So use the EID value for the TID.
+ * This is an interim step towards the PLDM requester logic moving into
+ * libpldm, and eventually this won't be needed. */
+
 uint8_t Requester::getInstanceId(uint8_t eid)
 {
-    if (ids.find(eid) == ids.end())
-    {
-        InstanceId id;
-        ids.emplace(eid, InstanceId());
-    }
-
-    uint8_t id{};
-    try
-    {
-        id = ids[eid].next();
-    }
-    catch (const std::runtime_error& e)
+    uint8_t id;
+    int rc = pldm_instance_id_alloc(pldmInstanceIdDb, eid, &id);
+    if (rc == -EAGAIN)
     {
         throw TooManyResources();
     }
-
+    else if (rc)
+    {
+        throw std::exception();
+    }
     return id;
 }
-
 } // namespace dbus_api
 } // namespace pldm
