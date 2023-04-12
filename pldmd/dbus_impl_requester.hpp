c3d1b23bb2c08eb729f1d47df6acea8795cded23
Rashmica Gupta <rashmica@linux.ibm.com>
2023-04-12 16:32:32 +1000

    pldmd: move to libpldm instance ID alloc/free
    Change-Id: Ia03de8245dfb114e6266ba36dcf26ca4398a4ce0
    Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>

--- pldmd/dbus_impl_requester.hpp
+++ pldmd/dbus_impl_requester.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
-#include "instance_id.hpp"
+#include "libpldm/instance-id.h"
+#include "libpldm/pldm.h"
+
 #include "xyz/openbmc_project/PLDM/Requester/server.hpp"
 
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/server/object.hpp>
 
 #include <map>
-
 namespace pldm
 {
 namespace dbus_api
@@ -36,7 +37,21 @@ class Requester : public RequesterIntf
      *  @param[in] path - Path to attach at.
      */
     Requester(sdbusplus::bus_t& bus, const std::string& path) :
-        RequesterIntf(bus, path.c_str()){};
+        RequesterIntf(bus, path.c_str())
+    {
+        int rc;
+
+        rc = pldm_instance_id_init_default(&pldmInstID);
+        if (rc < 0)
+        {
+            throw std::exception();
+        }
+    };
+
+    /* Ideally we would be able to look up the TID for a given EID. We don't
+     * have that infrastructure in place yet. So use the EID value for the TID.
+     * This is an interim step towards the PLDM requester logic moving into
+     * libpldm, and eventually this won't be needed. */
 
     /** @brief Implementation for RequesterIntf.GetInstanceId */
     uint8_t getInstanceId(uint8_t eid) override;
@@ -48,12 +63,15 @@ class Requester : public RequesterIntf
      */
     void markFree(uint8_t eid, uint8_t instanceId)
     {
-        ids[eid].markFree(instanceId);
+        int rc = pldm_instance_id_free(pldmInstID, eid, instanceId);
+        if (rc)
+        {
+            throw std::system_category().default_error_condition(errno);
+        }
     }
 
   private:
-    /** @brief EID to PLDM Instance ID map */
-    std::map<uint8_t, InstanceId> ids;
+    struct pldm_instance_id* pldmInstID = NULL;
 };
 
 } // namespace dbus_api
