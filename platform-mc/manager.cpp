8aaa0bc71ae2029be1889f726930a13e2d531826
Gilbert Chen <gilbertc@nvidia.com>
2023-05-02 10:20:10 +0000

    platform-mc: Added EventManager
    Added eventManager to handle sensor event class(00h),
    pldmMessagePollEvent class(05h)  and OEM CPER event class(FAh)
    which are defined in table 11 of DSP0248 v1.2.1. The eventManager
    supports to receive event asynchronously and polls all event
    synchronously if terminus sends pldmMessagePollEvent.
    
    Change-Id: I1b337ccae454067841ffbbd8754631216a995542
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>
    Signed-off-by: Gilbert Chen <gilbertc@nvidia.com>

--- platform-mc/manager.cpp
+++ platform-mc/manager.cpp
@@ -15,5 +15,17 @@ exec::task<int> Manager::afterDiscoverTerminus()
     co_return rc;
 }
 
+exec::task<int> Manager::pollForPlatformEvent(pldm_tid_t tid)
+{
+    auto it = termini.find(tid);
+    if (it != termini.end())
+    {
+        auto& terminus = it->second;
+        co_await eventManager.pollForPlatformEventTask(tid);
+        terminus->pollEvent = false;
+    }
+    co_return PLDM_SUCCESS;
+}
+
 } // namespace platform_mc
 } // namespace pldm
