f59c06ee69521d1a2d3f3b80dee263cf011c0d95
Gilbert Chen <gilbertc@nvidia.com>
2023-05-02 10:20:10 +0000

    platform-mc: Added EventManager
    Added eventManager to handle sensor event class(00h),
    pldmMessagePollEvent class(05h)  and OEM CPER event class(FAh)
    which are defined in table 11 of DSP0248 v1.2.1. The eventManager
    supports to receive event asynchronously and polls all event
    synchronously if terminus sends pldmMessagePollEvent.
    
    Change-Id: I1b337ccae454067841ffbbd8754631216a995542
    Signed-off-by: Gilbert Chen <gilbertc@nvidia.com>

--- platform-mc/test/terminus_manager_test.cpp
+++ platform-mc/test/terminus_manager_test.cpp
@@ -31,7 +31,7 @@ class TerminusManagerTest : public testing::Test
         event(sdeventplus::Event::get_default()), instanceIdDb(),
         reqHandler(fd, event, instanceIdDb, false, 90000, seconds(1), 2,
                    milliseconds(100)),
-        terminusManager(event, reqHandler, instanceIdDb, termini, nullptr),
+        terminusManager(event, reqHandler, instanceIdDb, termini, 0x0, nullptr),
         mockTerminusManager(event, reqHandler, instanceIdDb, termini, nullptr)
     {}
 
