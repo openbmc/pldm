764b57b7d8e8812ea74b8488c3772f102cdbdb0f
Gilbert Chen <gilbertc@nvidia.com>
2023-05-02 10:20:10 +0000

    platform-mc: Added EventManager
    Added eventManager to handle sensor event class(00h),
    pldmMessagePollEvent class(05h)  and OEM CPER event class(FAh)
    which are defined in table 11 of DSP0248 v1.2.1. The eventManager
    supports to receive event asynchronously and polls all event
    synchronously if terminus sends pldmMessagePollEvent.
    
    Change-Id: I1b337ccae454067841ffbbd8754631216a995542
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>
    Signed-off-by: Gilbert Chen <gilbertc@nvidia.com>

--- platform-mc/terminus_manager.hpp
+++ platform-mc/terminus_manager.hpp
@@ -46,10 +46,11 @@ class TerminusManager
     explicit TerminusManager(sdeventplus::Event& event,
                              RequesterHandler& handler,
                              pldm::InstanceIdDb& instanceIdDb,
-                             TerminiMapper& termini, Manager* manager) :
+                             TerminiMapper& termini, mctp_eid_t localEid,
+                             Manager* manager) :
         event(event),
         handler(handler), instanceIdDb(instanceIdDb), termini(termini),
-        tidPool(tidPoolSize, false), manager(manager)
+        localEid(localEid), tidPool(tidPoolSize, false), manager(manager)
     {
         // DSP0240 v1.1.0 table-8, special value: 0,0xFF = reserved
         tidPool[0] = true;
@@ -145,6 +146,15 @@ class TerminusManager
      */
     auto findTeminusPtr(const MctpInfo& mctpInfo);
 
+    /** @brief getter of local EID
+     *
+     *  @return uint8_t - local EID
+     */
+    mctp_eid_t getLocalEid()
+    {
+        return localEid;
+    }
+
   private:
     /** @brief The coroutine task execute by discoverMctpTerminus()
      *
@@ -212,6 +222,9 @@ class TerminusManager
 
     /** @brief Managed termini list */
     TerminiMapper& termini;
+
+    /** @brief local EID */
+    mctp_eid_t localEid;
 
     /** @brief tables for maintaining assigned TID */
     std::vector<bool> tidPool;
