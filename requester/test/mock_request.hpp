f9f1552bafbbaf7fae77f247ea9881f26657f0c4
Rashmica Gupta <rashmica@linux.ibm.com>
2023-05-22 13:56:42 +1000

    pldmd: Convert to using libpldm transport APIs
    We don't currently have the infrastructure in place to get the correct
    TIDs, so to keep everything working as before use the EID as the TID in
    the EID-to-TID mapping.
    
    Change-Id: I366f079082b102cfc0e90db0f62208581eb8693e
    Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>

--- requester/test/mock_request.hpp
+++ requester/test/mock_request.hpp
@@ -2,6 +2,8 @@
 
 #include "requester/request.hpp"
 
+#include <libpldm/transport.h>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
@@ -14,7 +16,8 @@ namespace requester
 class MockRequest : public RequestRetryTimer
 {
   public:
-    MockRequest(int /*fd*/, mctp_eid_t /*eid*/, sdeventplus::Event& event,
+    MockRequest(int /*fd*/, pldm_transport& /*pldmTransport*/,
+                mctp_eid_t /*eid*/, sdeventplus::Event& event,
                 pldm::Request&& /*requestMsg*/, uint8_t numRetries,
                 std::chrono::milliseconds responseTimeOut,
                 int /*currentSendbuffSize*/, bool /*verbose*/) :
