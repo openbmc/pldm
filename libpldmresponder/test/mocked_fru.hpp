85b9e59a14fe8d726ba4f109ff92fac7aaf88e50
Archana Kakani <archana.kakani@ibm.com>
2023-07-23 23:21:11 -0500

    Added testcase for updateAssociationTree
    Added new testcase to test updateAssociationTree,
    Checked if the nodes are getting added to the entity tree.
    
    Tested:
    TesteCase passed succesfully
    
    Change-Id: I47c7a88c139ed80a989d380266e5df69dc870e59
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

---
+++ libpldmresponder/test/mocked_fru.hpp
@@ -0,0 +1,32 @@
+#include "libpldmresponder/fru.hpp"
+
+#include <libpldm/pdr.h>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using testing::ElementsAreArray;
+using namespace pldm::responder;
+class MockFruImpl : public FruImpl
+{
+  public:
+    using FruImpl::FruImpl;
+    MOCK_METHOD1(
+        getEntityByObjectPath,
+        std::optional<pldm_entity>(const pldm::responder::dbus::InterfaceMap&));
+    // MOCK_METHOD((std::optional<pldm_entity>), getEntityByObjectPath, (const
+    // pldm::responder::dbus::InterfaceMap&));
+};
+/*
+class MockFruParser : public pldm::responder::fru_parser::FruParser
+{
+  public:
+
+    MOCK_METHOD(uint16_t, getEntityType, (std::string), (const, override));
+
+};
+    MockFruImpl(const std::string& configPath,
+            const std::filesystem::path& fruMasterJsonPath, pldm_pdr* pdrRepo,
+            pldm_entity_association_tree* entityTree,
+            pldm_entity_association_tree* bmcEntityTree);
+*/
