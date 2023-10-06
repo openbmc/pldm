02b96656518081d29e6b3a7f20ff58282a59e55f
Kamalkumar Patel <kamalkumar.patel@ibm.com>
2023-10-06 03:48:25 -0500

    PLDM:Catching exception precisely and printing it
    Correcting catch block in PLDM repo to print all
    exception precisely so pldm trace can be more
    useful to identify defect easily.
    
    Change-Id: If2e86dcb031ddc2e927e7836d7f4359f5b44cdec
    Signed-off-by: Kamalkumar Patel <kamalkumar.patel@ibm.com>

--- libpldmresponder/bios.cpp
+++ libpldmresponder/bios.cpp
@@ -134,9 +134,9 @@ Response Handler::getDateTime(const pldm_msg* request, size_t /*payloadLength*/)
     catch (const sdbusplus::exception_t& e)
     {
         error(
-            "Error getting time, PATH={BMC_TIME_PATH} TIME INTERACE={TIME_INTERFACE}",
-            "BMC_TIME_PATH", bmcTimePath, "TIME_INTERFACE", timeInterface);
-
+            "Error getting time, PATH={BMC_TIME_PATH} TIME INTERACE={TIME_INTF}  ERROR={ERR_EXCEP}",
+            "BMC_TIME_PATH", bmcTimePath, "TIME_INTF", timeInterface,
+            "ERR_EXCEP", e.what());
         return CmdHandler::ccOnlyResponse(request, PLDM_ERROR);
     }
 
