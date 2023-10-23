791aaa6de6c946953222496975aba2a3a5e5e125
Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
2023-10-23 17:06:54 +0800

    Support OEM-META write file request for post code history
    Summary:
      - Support write file req when BIC sends postcode to BMC.
        Also, set up TID to slot mapping table at the beginning of
        the oem mata handler to correspond to the post code
        belonging to which slot.
      - Extend the handler with TID parameter to know which request is sent from.
    
    Change-Id: I56d303a5bc2f059451ee207a1de591be0d9993ac
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>

---
+++ oem/meta/libpldmresponder/file_io_by_type.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <vector>
+#include <cstdint>
+
+namespace pldm
+{
+namespace responder
+{
+namespace oem_meta
+{
+
+/**
+ *  @class FileHandler
+ *
+ *  Base class to handle read/write of all oem file types
+ */
+class FileHandler
+{
+  public:
+    virtual int write(const std::vector<uint8_t>& data) = 0;
+    virtual int read(const std::vector<uint8_t>& data) = 0;
+    virtual ~FileHandler() {}
+};
+
+} // namespace oem_meta
+
+} // namespace responder
+} // namespace pldm
