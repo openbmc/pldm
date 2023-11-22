7d5b225f76792c798e01ad297993c95b80937782
Dung Cao <dung@os.amperecomputing.com>
2023-11-22 02:30:49 +0000

    oem: ampere: Add Ampere OEM Event handler
    1. Add poll event for normal event
    2. Add OEM response handler to decode CPER data, log IPMI OEM sel,
    and Redish log.
    
    Change-Id: I4ff1cd3c0ba8a2471afb1c8ee61a8caaf30596b8
    Signed-off-by: Dung Cao <dung@os.amperecomputing.com>
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>

---
+++ oem/ampere/event/oem_event_manager.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "libpldm/pldm.h"
+
+#include "common/instance_id.hpp"
+#include "common/types.hpp"
+#include "oem_event_manager.hpp"
+#include "platform-mc/manager.hpp"
+#include "requester/handler.hpp"
+
+namespace pldm
+{
+namespace oem
+{
+using namespace pldm::pdr;
+
+#define NORMAL_EVENT_POLLING_TIME 5000000 // ms
+
+/**
+ * @brief OemEventManager
+ *
+ *
+ */
+class OemEventManager
+{
+  public:
+    OemEventManager() = delete;
+    OemEventManager(const OemEventManager&) = delete;
+    OemEventManager(OemEventManager&&) = delete;
+    OemEventManager& operator=(const OemEventManager&) = delete;
+    OemEventManager& operator=(OemEventManager&&) = delete;
+    virtual ~OemEventManager() = default;
+
+    explicit OemEventManager(
+        sdeventplus::Event& event,
+        [[maybe_unused]] requester::Handler<requester::Request>& handler,
+        [[maybe_unused]] pldm::InstanceIdDb& instanceIdDb,
+        platform_mc::Manager* manager) :
+        event(event), pollingTime(NORMAL_EVENT_POLLING_TIME),
+        manager(manager) {};
+
+    exec::task<int> oemPollForPlatformEvent(pldm_tid_t tid);
+
+    int processOemMsgPollEvent(pldm_tid_t tid, uint16_t eventId,
+                               const uint8_t* eventData, size_t eventDataSize);
+
+  protected:
+    void pausePolling();
+
+    sdeventplus::Event& event;
+    /** @brief sensor polling interval in ms. */
+    uint64_t pollingTime;
+
+    std::map<pldm_tid_t, uint64_t> timeStamp;
+
+    platform_mc::Manager* manager;
+};
+} // namespace oem
+} // namespace pldm
