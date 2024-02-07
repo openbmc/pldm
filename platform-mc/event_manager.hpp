18ab3bb99433c45084050fdcaa1efcbec5a68fb1
Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
2024-02-07 13:52:07 +0800

    platform-mc: Add OEM Meta event handler
    Add OEM Meta handler with event type 0xFB. It will print the logs
    with prefix "BIOS_IPMI_SEL", so the user can utilize it to filter
    the journal logs. It will also send logs on D-Bus.
    
    Tested: tested and verified on yosemite4 system.
    Test Log:
    root@bmc:~# journalctl |grep BIOS_IPMI_SEL
    Feb 07 07:14:34 bmc pldmd[897]: BIOS_IPMI_SEL: Host 4: GeneralInfo:
     MEMORY_ECC_ERR(0x21), DIMM Slot Location: Sled 0/Socket 0, Channel
     0, Slot 0, DIMM A0, DIMM Failure Event: Memory correctable error
    
    Change-Id: I722d8e09e7aea8dfa533e921d41beb505b59fb95
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>

--- platform-mc/event_manager.hpp
+++ platform-mc/event_manager.hpp
@@ -6,6 +6,7 @@
 #include "common/types.hpp"
 #include "numeric_sensor.hpp"
 #include "pldmd/dbus_impl_requester.hpp"
+#include "requester/configuration_discovery_handler.hpp"
 #include "requester/handler.hpp"
 #include "terminus.hpp"
 #include "terminus_manager.hpp"
@@ -54,9 +55,11 @@ class EventManager
 
     explicit EventManager(
         TerminusManager& terminusManager,
-        std::map<mctp_eid_t, std::shared_ptr<Terminus>>& termini) :
+        std::map<mctp_eid_t, std::shared_ptr<Terminus>>& termini,
+        pldm::ConfigurationDiscoveryHandler* configurationDiscovery = nullptr) :
         terminusManager(terminusManager),
-        termini(termini){};
+        termini(termini), configurationDiscovery(configurationDiscovery)
+    {}
 
     /** @brief Handle platform event
      *
@@ -125,6 +128,8 @@ class EventManager
 
     /** @brief List of discovered termini */
     std::map<tid_t, std::shared_ptr<Terminus>>& termini;
+
+    pldm::ConfigurationDiscoveryHandler* configurationDiscovery;
 };
 } // namespace platform_mc
 } // namespace pldm
