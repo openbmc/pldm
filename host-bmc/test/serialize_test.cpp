b58cea1484e3fb290e957aede77e2e0b6c229ac1
Kamalkumar Patel <kamalkumar.patel@ibm.com>
2024-03-25 12:55:02 -0500

    Adding support to store pldm data into file
    - This commit will be add support to store PLDM data(e.g CPUcore,
    Entity paths, Dbus values, etc.) into persistent file which will be
    managed by cereal data store mechanism.
    
    - BMC stores data into persistent file to reuse later.
    
    - When BMC reboots and it need minimum data to boot which will be
    retrieved from this persistent file.
    
    Testing:
    This is basic infrastructure to store data into file, so Unit test are
    added to test the functionality.
    
    Change-Id: Ie4fe13e20dc7a1f23c5a4aad5f2d41f9562d0505
    Signed-off-by: Kamalkumar Patel <kamalkumar.patel@ibm.com>

---
+++ host-bmc/test/serialize_test.cpp
@@ -0,0 +1,104 @@
+#include "../dbus/serialize.hpp"
+
+#include <cereal/archives/binary.hpp>
+#include <cereal/types/map.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/tuple.hpp>
+#include <cereal/types/variant.hpp>
+#include <cereal/types/vector.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+using namespace pldm::serialize;
+
+TEST(Serialize, SerializeGoodPath)
+{
+    // Setting up basic requirement for function to store data
+    std::string filepath = "/tmp/temp";
+    pldm_entity entities;
+    entities.entity_type = 1;
+    entities.entity_instance_num = 2;
+    entities.entity_container_id = 3;
+    auto tree = pldm_entity_association_tree_init();
+    auto l1 = pldm_entity_association_tree_add(tree, &entities, 0xFFFF, nullptr,
+                                               PLDM_ENTITY_ASSOCIAION_PHYSICAL);
+    EXPECT_NE(l1, nullptr);
+    std::set<uint16_t> entitytypes{entities.entity_type};
+    ObjectPathMaps objectMaps;
+    std::string path = "/xyz/local_project/DEF";
+    std::string interface = "com.abc.DEF.TEST";
+    std::string propertyname = "TEST";
+    std::string propValue = "unittest";
+    objectMaps[path] = l1;
+
+    // Changing file location to store data for testing
+    pldm::serialize::Serialize::getSerialize().setfilePathForUnitTest(filepath);
+    pldm::serialize::Serialize::getSerialize().setEntityTypes(entitytypes);
+    pldm::serialize::Serialize::getSerialize().setObjectPathMaps(objectMaps);
+
+    // Storing data as serialize way into file
+    pldm::serialize::Serialize::getSerialize().serialize(
+        path, interface, propertyname, propValue);
+
+    pldm::utils::SavedObjs savedObjs;
+    std::map<std::string, pldm::utils::PropertyValue> savedKeyVal;
+
+    // Reading data as from file to verify
+    std::ifstream is(filepath.c_str(), std::ios::in | std::ios::binary);
+    cereal::BinaryInputArchive iarchive(is);
+    iarchive(savedObjs, savedKeyVal);
+
+    uint16_t type = entities.entity_type, instancenum, cid;
+    std::map<std::string, std::map<std::string, pldm::utils::PropertyValue>>
+        maps;
+    std::string intf, name;
+    pldm::utils::PropertyValue value;
+
+    // Verifying data from original data
+    EXPECT_EQ(true, savedObjs.contains(type));
+    EXPECT_EQ(true, savedObjs[type].contains(path));
+    auto tuple = savedObjs[type][path];
+    std::tie(instancenum, cid, maps) = tuple;
+    auto insideMap = maps[interface];
+
+    EXPECT_EQ(true, insideMap.contains(propertyname));
+    EXPECT_EQ(propValue, std::get<std::string>(insideMap[propertyname]));
+    EXPECT_EQ(instancenum, entities.entity_instance_num);
+    EXPECT_EQ(cid, entities.entity_container_id);
+
+    pldm_entity_association_tree_destroy(tree);
+}
+
+TEST(Serialize, DeserializeGoodPath)
+{
+    // Changing file location which is present but testcase doesn't have access
+    // permission since it's internal system location, so function should return
+    // false.
+    pldm::serialize::Serialize::getSerialize().setfilePathForUnitTest(
+        PERSISTENT_FILE);
+    bool value = pldm::serialize::Serialize::getSerialize().deserialize();
+    EXPECT_EQ(value, false);
+}
+
+TEST(Serialize, DeserializeGoodPath1)
+{
+    // File path "/tmp/temp" not present at first time but 'SerializeGoodPath'
+    // testcase created it, so function should return true.
+    std::string filepath = "/tmp/temp";
+
+    pldm::serialize::Serialize::getSerialize().setfilePathForUnitTest(filepath);
+    bool value1 = pldm::serialize::Serialize::getSerialize().deserialize();
+    EXPECT_EQ(value1, true);
+}
+
+TEST(Serialize, DeserializeBadPath)
+{
+    // Changing file location which does not present, so function should return
+    // false.
+    std::string filepath = "/tmp/temp1";
+
+    pldm::serialize::Serialize::getSerialize().setfilePathForUnitTest(filepath);
+    bool value1 = pldm::serialize::Serialize::getSerialize().deserialize();
+    EXPECT_EQ(value1, false);
+}
