d36c86c12209b4ee4c9bc0019712d09cb509dde9
Kamalkumar Patel <kamalkumar.patel@ibm.com>
2024-03-27 13:01:42 -0500

    Adding CPUcore data store/restore support
    - Adding support for storing and restoring for CPUcore configuration
      using serialization/deserialization mechanism.
    
    - This commit also adds support for custom store, custom restore
      functionality where pldm can serialize and deserialize the dbus
      objects based on a configuration file.
    
    - The custom serialize mechanism found at:
      https://gerrit.openbmc.org/c/openbmc/pldm/+/70260
    
    Change-Id: I535c36ee4a01a3f43942378405e37475bde55181
    Signed-off-by: Kamalkumar Patel <kamalkumar.patel@ibm.com>

---
+++ host-bmc/dbus/deserialize.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "host-bmc/host_pdr_handler.hpp"
+
+#include <filesystem>
+#include <fstream>
+
+namespace pldm
+{
+namespace deserialize
+{
+
+/** @brief Restoring Dbus values from persistent file.
+ *
+ */
+void restoreDbusObj(HostPDRHandler* hostPDRHandler);
+
+} // namespace deserialize
+} // namespace pldm
