ba92bf22f9a456904f9c449b372757e9088e66bf
Chau Ly <chaul@amperecomputing.com>
2024-04-23 10:00:58 +0000

    platform-mc: Add State Sensor support
    Handle the parsing of State Sensor PDRs.
    
    1. The StateSensor object will be created for each sensor PDR, as long as
    terminus name is available. No sensor object path is created for
    State Sensor PDRs as the paths are expected to be published elsewhere
    (by other applications or by PLDM but based on another means).
    
    2. The ComponentStateSensor object will be created for each component
    sensor of the state sensor. D-Bus inventory objects will also be
    published under PLDM service for each component sensor, which holds
    entity information. The entity info between component sensors of the
    same state sensor can be duplicated. This component sensor information
    will also be mapped with state event entries in the JSON file for setting
    the corresponding value to a specific object path when the sensor's state
    changes.
    
    3. The mapping between each sensor and their D-Bus information which the
    state will be written to is defined in
    `configurations/events/stateSensorPdrs.json`.
    libpldmresponder::StateSensorHandler class is reused for this task.
    
    4. Member functions are defined for State Sensor to update the sensor's
    state value and operational state.
    
    5. After the terminus is discovered and added to terminus manager. As
    the numeric sensors start to be polled, requests are also sent to get
    the initial values of all state sensors of the terminus (using
    GetStateSensorReading) to initialize the sensor states. No polling task
    is implemented for state sensor as the states should be updated though
    sensor events.
    
    5. This commit also added unit test for StateSensor class.
    
    Tested: Pass CI check
    
    Change-Id: I4572eea131169b3db46f7d474d354bda1e5bfc19
    Signed-off-by: Chau Ly <chaul@amperecomputing.com>

---
+++ platform-mc/test/state_sensor_test.cpp
@@ -0,0 +1,123 @@
+
+#include "libpldm/entity.h"
+#include "libpldm/platform.h"
+
+#include "platform-mc/state_sensor.hpp"
+#include "platform-mc/terminus.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace pldm::pdr;
+
+TEST(StateSensor, processStateSensorReadings)
+{
+    pldm::pdr::CompositeCount comp_sensor_count = 2, comp_sensor_count_f = 3;
+    pldm::pdr::EventState state = 1, state_f = 0, opState = PLDM_SENSOR_ENABLED;
+
+    std::vector<uint8_t> pdr1{
+        0x1,
+        0x0,
+        0x0,
+        0x0,                   // record handle
+        0x1,                   // PDRHeaderVersion
+        PLDM_STATE_SENSOR_PDR, // PDRType
+        0x0,
+        0x0,                   // recordChangeNumber
+        21,
+        0,                     // dataLength
+        0,
+        0,                     // PLDMTerminusHandle
+        0x2,
+        0x0,                   // sensorID=2
+        PLDM_ENTITY_SYS_FIRMWARE,
+        0,                     // entityType=31
+        20,
+        0,                     // entityInstanceNumber=20
+        0x4,
+        0x0,                   // containerID=4
+        PLDM_NO_INIT,          // sensorInit
+        false,                 // sensorAuxiliaryNamesPDR
+        2,                     // composite_sensor_count
+        1,
+        0,                     // state_set_id=1
+        1,                     // possible_states_size=1
+        0x1e,                  // states[]=[1,2,3,4]
+        2,
+        0,                     // state_set_id=2
+        1,                     // possible_states_size=1
+        0x1e,                  // states[]=[1,2,3,4]
+    };
+
+    pldm_tid_t tid = 0x01;
+    auto pdr1_ptr = std::make_shared<std::vector<uint8_t>>(pdr1);
+    std::vector<std::string> sensorNames = {"Test_Health_Sensor",
+                                            "Test_Availability_Sensor"};
+    std::string inventoryPath{"/xyz/openbmc_project/inventory/board/S0"};
+    pldm::responder::events::StateSensorHandler stateSensorHandler(
+        EVENTS_JSONS_DIR);
+
+    pldm::platform_mc::StateSensor stateSensor = pldm::platform_mc::StateSensor(
+        tid, pdr1_ptr, sensorNames, inventoryPath, stateSensorHandler);
+
+    EXPECT_EQ(stateSensor.compositeCount, comp_sensor_count);
+
+    std::array<get_sensor_state_field,
+               PLDM_STATE_SENSOR_MAX_COMPOSITE_SENSOR_COUNT>
+        stateField = {
+            {{
+                 0, // sensor_op_state=PLDM_SENSOR_ENABLED
+                 2, // present_state
+                 1, // previous_state
+                 1  // event_state
+             },
+             {
+                 0, // sensor_op_state=PLDM_SENSOR_ENABLED
+                 2, // present_state
+                 1, // previous_state
+                 1  // event_state
+             },
+             {},
+             {},
+             {},
+             {},
+             {},
+             {}}};
+
+    // Call process operational state for component sensor index 0
+    auto rc = stateSensor.processOpState(opState, 0);
+    EXPECT_EQ(rc, PLDM_SUCCESS);
+
+    // Call process state for component sensor index 0
+    rc = stateSensor.processSensorState(state_f, 0);
+    EXPECT_EQ(
+        rc,
+        PLDM_ERROR_INVALID_DATA); // because state_f is not in possible states
+
+    // Call process state for component sensor index 0
+    rc = stateSensor.processSensorState(state, 0);
+    EXPECT_EQ(rc, PLDM_ERROR); // because DBus is not available
+
+    // Call process state for component sensor index 1
+    rc = stateSensor.processSensorState(state, 1);
+    EXPECT_EQ(rc,
+              PLDM_ERROR); // because of initial PLDM_SENSOR_DISABLED op state
+
+    // Call process operational state for component sensor index 1
+    rc = stateSensor.processOpState(opState, 1);
+    EXPECT_EQ(rc, PLDM_SUCCESS);
+
+    // Call process state for component sensor index 1
+    rc = stateSensor.processSensorState(state, 1);
+    EXPECT_EQ(rc, PLDM_SUCCESS); // because event is not configured
+
+    // Call process state sensor reading
+    rc = stateSensor.processStateSensorReadings(stateField, comp_sensor_count);
+    EXPECT_EQ(rc, PLDM_SUCCESS);
+
+    // Call process state sensor reading with out-of-bound component sensor
+    // count
+    rc =
+        stateSensor.processStateSensorReadings(stateField, comp_sensor_count_f);
+    EXPECT_EQ(rc,
+              PLDM_ERROR_INVALID_DATA); // comp_sensor_count_f > compositeCount
+}
