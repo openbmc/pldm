03fc02c75548df6e3c293240be9b11e9cc60862d
Kamalkumar Patel <kamalkumar.patel@ibm.com>
2024-05-07 04:39:32 -0500

    Adding Configuration support for EntityMap
    Entity map contains entityID and respective entityname which is used to
    generate dbus path for each entity but there are challenges like each
    company has their own way of path generation using entityID which might
    be different,so making EntityID configurable which community can
    configure as per their own usecase.
    
    Testing:
    Added unit test to read EntityMap structure
    
    Change-Id: I3981fde64bd00940caa5067a9472fd948e74cbf0
    Signed-off-by: Kamalkumar Patel <kamalkumar.patel@ibm.com>

--- common/utils.hpp
+++ common/utils.hpp
@@ -40,40 +40,6 @@ using Json = nlohmann::json;
 constexpr bool Tx = true;
 constexpr bool Rx = false;
 
-using EntityName = std::string;
-using EntityType = uint16_t;
-
-using Entities = std::vector<pldm_entity_node*>;
-using EntityAssociations = std::vector<Entities>;
-using ObjectPathMaps = std::map<fs::path, pldm_entity_node*>;
-
-const std::map<EntityType, EntityName> entityMaps = {
-    {PLDM_ENTITY_SYSTEM_CHASSIS, "chassis"},
-    {PLDM_ENTITY_BOARD, "io_board"},
-    {PLDM_ENTITY_SYS_BOARD, "motherboard"},
-    {PLDM_ENTITY_POWER_SUPPLY, "powersupply"},
-    {PLDM_ENTITY_PROC, "cpu"},
-    {PLDM_ENTITY_SYSTEM_CHASSIS | 0x8000, "system"},
-    {PLDM_ENTITY_PROC_MODULE, "dcm"},
-    {PLDM_ENTITY_PROC | 0x8000, "core"},
-    {PLDM_ENTITY_IO_MODULE, "io_module"},
-    {PLDM_ENTITY_FAN, "fan"},
-    {PLDM_ENTITY_SYS_MGMT_MODULE, "system_management_module"},
-    {PLDM_ENTITY_POWER_CONVERTER, "power_converter"},
-    {PLDM_ENTITY_SLOT, "slot"},
-    {PLDM_ENTITY_CONNECTOR, "connector"}};
-
-/** @brief Vector a entity name to pldm_entity from entity association tree
- *  @param[in]  entityAssoc    - Vector of associated pldm entities
- *  @param[in]  entityTree     - entity association tree
- *  @param[out] objPathMap     - maps an object path to pldm_entity from the
- *                               BMC's entity association tree
- *  @return
- */
-void updateEntityAssociation(const EntityAssociations& entityAssoc,
-                             pldm_entity_association_tree* entityTree,
-                             ObjectPathMaps& objPathMap);
-
 /** @struct CustomFD
  *
  *  RAII wrapper for file descriptor.
