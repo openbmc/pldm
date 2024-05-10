48dd8b7cf674a70b349b727dbe8e6e5283dc7b63
Archana Kakani <archana.kakani@ibm.com>
2024-05-10 13:28:08 -0500

    host-bmc: Support to create/delete interface
    Adding support to create and delete interfaces for Cables, Assets,
    Associations, PCIeSlots and PCIe Devices
    
    Tested:
           Power on/off successful
    
    Change-Id: Iee0c1e6912fb7aabba16a35e85ba192944d63611
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

--- host-bmc/dbus/custom_dbus.cpp
+++ host-bmc/dbus/custom_dbus.cpp
@@ -29,5 +29,122 @@ std::optional<std::string>
     return std::nullopt;
 }
 
+void CustomDBus::implementCableInterface(const std::string& path)
+{
+    if (!cable.contains(path))
+    {
+        cable.emplace(
+            path, std::make_unique<Cable>(pldm::utils::DBusHandler::getBus(),
+                                          path.c_str()));
+    }
+}
+
+void CustomDBus::implementPCIeSlotInterface(const std::string& path)
+{
+    if (pcieSlot.find(path) == pcieSlot.end())
+    {
+        pcieSlot.emplace(
+            path, std::make_unique<PCIeSlot>(pldm::utils::DBusHandler::getBus(),
+                                             path.c_str()));
+    }
+}
+
+void CustomDBus::implementPCIeDeviceInterface(const std::string& path)
+{
+    if (!pcieDevice.contains(path))
+    {
+        pcieDevice.emplace(
+            path, std::make_unique<PCIeDevice>(
+                      pldm::utils::DBusHandler::getBus(), path.c_str()));
+    }
+}
+
+void CustomDBus::implementAssetInterface(const std::string& path)
+{
+    if (!asset.contains(path))
+    {
+        asset.emplace(
+            path, std::make_unique<Asset>(pldm::utils::DBusHandler::getBus(),
+                                          path.c_str()));
+    }
+}
+
+void CustomDBus::setAssociations(const std::string& path, AssociationsObj assoc)
+{
+    using PropVariant = sdbusplus::xyz::openbmc_project::Association::server::
+        Definitions::PropertiesVariant;
+
+    if (associations.find(path) == associations.end())
+    {
+        PropVariant value{std::move(assoc)};
+        std::map<std::string, PropVariant> properties;
+        properties.emplace("Associations", std::move(value));
+
+        associations.emplace(path, std::make_unique<Associations>(
+                                       pldm::utils::DBusHandler::getBus(),
+                                       path.c_str(), properties));
+    }
+    else
+    {
+        // object already created , so just update the associations
+        auto currentAssociations = getAssociations(path);
+        AssociationsObj newAssociations;
+
+        for (const auto& association : assoc)
+        {
+            if (std::find(currentAssociations.begin(),
+                          currentAssociations.end(),
+                          association) != currentAssociations.end())
+            {
+                // association is present in current associations
+                // do nothing
+            }
+            else
+            {
+                currentAssociations.push_back(association);
+            }
+        }
+
+        associations.at(path)->associations(currentAssociations);
+    }
+}
+
+const AssociationsObj CustomDBus::getAssociations(const std::string& path)
+{
+    if (associations.find(path) != associations.end())
+    {
+        return associations.at(path)->associations();
+    }
+    return {};
+}
+
+void CustomDBus::deleteObject(const std::string& path)
+{
+    if (location.contains(path))
+    {
+        location.erase(location.find(path));
+    }
+    if (pcieSlot.contains(path))
+    {
+        pcieSlot.erase(pcieSlot.find(path));
+    }
+    if (associations.contains(path))
+    {
+        associations.erase(associations.find(path));
+    }
+    if (cable.contains(path))
+    {
+        cable.erase(cable.find(path));
+    }
+    if (asset.contains(path))
+    {
+        asset.erase(asset.find(path));
+    }
+    if (pcieDevice.contains(path))
+    {
+        pcieDevice.erase(pcieDevice.find(path));
+    }
+}
+
 } // namespace dbus
 } // namespace pldm
--- host-bmc/dbus/custom_dbus.hpp
+++ host-bmc/dbus/custom_dbus.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include "asset.hpp"
+#include "associations.hpp"
+#include "cable.hpp"
 #include "common/utils.hpp"
+#include "pcie_device.hpp"
+#include "pcie_slot.hpp"
 
 #include <sdbusplus/server.hpp>
 #include <xyz/openbmc_project/Inventory/Decorator/LocationCode/server.hpp>
@@ -60,8 +65,57 @@ class CustomDBus
      */
     std::optional<std::string> getLocationCode(const std::string& path) const;
 
+    /** @brief Implement Slot Interface
+     *  @param[in] path - the object path
+     */
+    void implementPCIeSlotInterface(const std::string& path);
+
+    /** @brief Implement Device Interface
+     *  @param[in] path - the object path
+     */
+    void implementPCIeDeviceInterface(const std::string& path);
+
+    /** @brief Implement Cable Interface
+     *  @param[in] path - the object path
+     */
+    void implementCableInterface(const std::string& path);
+
+    /** @brief Implement Asset Interface
+     *  @param[in] path - the object path
+     */
+    void implementAssetInterface(const std::string& path);
+
+    /** @brief Set the Associations property
+     *
+     *  @param[in] path     - The object path
+     *
+     *  @param[in] value    - An array of forward, reverse, endpoint tuples
+     */
+    void setAssociations(const std::string& path, AssociationsObj assoc);
+
+    /** @brief Get the current Associations property
+     *
+     *  @param[in] path     - The object path
+     *
+     *  @return current Associations -  An array of forward, reverse, endpoint
+     * tuples
+     */
+    const std::vector<std::tuple<std::string, std::string, std::string>>
+        getAssociations(const std::string& path);
+
+    /** @brief Remove all DBus object paths from cache
+     *
+     *  @param[in] types  - entity type
+     */
+    void deleteObject(const std::string& path);
+
   private:
     std::unordered_map<ObjectPath, std::unique_ptr<LocationIntf>> location;
+    std::unordered_map<ObjectPath, std::unique_ptr<PCIeDevice>> pcieDevice;
+    std::unordered_map<ObjectPath, std::unique_ptr<PCIeSlot>> pcieSlot;
+    std::unordered_map<ObjectPath, std::unique_ptr<Cable>> cable;
+    std::unordered_map<ObjectPath, std::unique_ptr<Asset>> asset;
+    std::unordered_map<ObjectPath, std::unique_ptr<Associations>> associations;
 };
 
 } // namespace dbus
