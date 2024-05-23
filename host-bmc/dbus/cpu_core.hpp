aa72a2709495c9d8672243e19fddaf3ff13a2274
Kamalkumar Patel <kamalkumar.patel@ibm.com>
2024-05-23 04:53:07 -0500

    Adding CPUCore interface support
    This commit introduces support for tracking the level of CPU microcode
    level during BMC power-on and populates this information to a DBus
    interface named microcode. Upon BMC power-on, the system populates the
    type of microcode and set the microcode property accordingly. The
    basic idea behind microcode is to replace the custom logic implementing
    the instruction sequencing with a series of simple instructions run in
    a "microcode engine" in the processor. Whereas a custom logic system
    might have a series of diodes and gates that output a series of
    voltages on various control lines, the microcode engine is connected to
    these lines instead, and these are turned on and off as the engine
    reads the microcode instructions in sequence.
    
    Testing:
    Unit tests passed
    
    Change-Id: I728522b34e96ee7d6609efb5746b40cf923812e8
    Signed-off-by: Kamalkumar Patel <kamalkumar.patel@ibm.com>

---
+++ host-bmc/dbus/cpu_core.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <xyz/openbmc_project/Inventory/Item/CpuCore/server.hpp>
+
+#include <string>
+
+namespace pldm
+{
+namespace dbus
+{
+using CoreIntf = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Inventory::Item::server::CpuCore>;
+
+class CPUCore : public CoreIntf
+{
+  public:
+    CPUCore() = delete;
+    ~CPUCore() = default;
+    CPUCore(const CPUCore&) = delete;
+    CPUCore& operator=(const CPUCore&) = delete;
+    CPUCore(CPUCore&&) = default;
+    CPUCore& operator=(CPUCore&&) = default;
+
+    CPUCore(sdbusplus::bus::bus& bus, const std::string& objPath) :
+        CoreIntf(bus, objPath.c_str()), path(objPath)
+    {}
+
+    /** Get value of Microcode */
+    uint32_t microcode() const override;
+
+    /** Set value of Microcode */
+    uint32_t microcode(uint32_t value) override;
+
+  private:
+    std::string path;
+};
+
+} // namespace dbus
+} // namespace pldm
