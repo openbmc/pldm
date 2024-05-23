965b76b1983b4792dea7606c7d88ee353506fb01
Kamalkumar Patel <kamalkumar.patel@ibm.com>
2024-05-23 04:53:07 -0500

    Adding CPUCore interface support
    This commit introduces support for tracking the level of CPU microcode
    level during BMC power-on and populates this information to a DBus
    interface named microcode. Upon BMC power-on, the system populates the
    type of microcode and set the microcode property accordingly. The
    basic idea behind microcode is to replace the custom logic implementing
    the instruction sequencing with a series of simple instructions run in
    a "microcode engine" in the processor. Whereas a custom logic system
    might have a series of diodes and gates that output a series of
    voltages on various control lines, the microcode engine is connected to
    these lines instead, and these are turned on and off as the engine
    reads the microcode instructions in sequence.
    
    Change-Id: I728522b34e96ee7d6609efb5746b40cf923812e8
    Signed-off-by: Kamalkumar Patel <kamalkumar.patel@ibm.com>

---
+++ host-bmc/dbus/custom_dbus.cpp
@@ -0,0 +1,59 @@
+#include "custom_dbus.hpp"
+
+namespace pldm
+{
+namespace dbus
+{
+void CustomDBus::setLocationCode(const std::string& path, std::string value)
+{
+    if (!location.contains(path))
+    {
+        location.emplace(path,
+                         std::make_unique<LocationIntf>(
+                             pldm::utils::DBusHandler::getBus(), path.c_str()));
+    }
+
+    location.at(path)->locationCode(value);
+}
+
+std::optional<std::string>
+    CustomDBus::getLocationCode(const std::string& path) const
+{
+    if (location.contains(path))
+    {
+        return location.at(path)->locationCode();
+    }
+
+    return std::nullopt;
+}
+
+void CustomDBus::implementCpuCoreInterface(const std::string& path)
+{
+    if (!cpuCore.contains(path))
+    {
+        cpuCore.emplace(path, std::make_unique<CPUCore>(
+                                  pldm::utils::DBusHandler::getBus(), path));
+    }
+}
+
+void CustomDBus::setMicroCode(const std::string& path, uint32_t value)
+{
+    if (!cpuCore.contains(path))
+    {
+        cpuCore.emplace(path, std::make_unique<CPUCore>(
+                                  pldm::utils::DBusHandler::getBus(), path));
+    }
+    cpuCore.at(path)->microcode(value);
+}
+
+std::optional<uint32_t> CustomDBus::getMicroCode(const std::string& path) const
+{
+    if (cpuCore.contains(path))
+    {
+        return cpuCore.at(path)->microcode();
+    }
+
+    return std::nullopt;
+}
+} // namespace dbus
+} // namespace pldm
