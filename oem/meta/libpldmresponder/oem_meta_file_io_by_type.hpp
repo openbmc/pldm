aa32214d7dfe47b2a0b1d95a9ca2dd6d342997c4
Jeff Shih <Jeff_Shih@wiwynn.com>
2024-06-11 13:21:04 +0800

    Support OEM-META command for getting Http boot certification
    Support parsing read file IO for Http Boot command (type: 0x03) and returning the Http boot certification stored in BMC by reading from settingsd
    
    Change-Id: Iaa0da923a8703703744eccccbbc8d95146444c14
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>

--- oem/meta/libpldmresponder/oem_meta_file_io_by_type.hpp
+++ oem/meta/libpldmresponder/oem_meta_file_io_by_type.hpp
@@ -19,13 +19,20 @@ enum pldm_oem_meta_file_io_type : uint8_t
     POST_CODE = 0x00,
     BIOS_VERSION = 0x01,
     POWER_CONTROL = 0x02,
+    HTTP_BOOT = 0x03,
+};
+
+enum pldm_oem_meta_decode_file_io_minisize : size_t
+{
+    WRITE_FILE_IO_MINISIZE = 0x06,
+    READ_FILE_IO_MINISIZE = 0x01,
 };
 
 class FileHandler
 {
   public:
     virtual int write(const message& data) = 0;
-    virtual int read(const message& data) = 0;
+    virtual int read(std::vector<uint8_t>& data) = 0;
     virtual ~FileHandler() {}
 };
 
--- oem/meta/libpldmresponder/oem_meta_file_io_type_http_boot.hpp
+++ oem/meta/libpldmresponder/oem_meta_file_io_type_http_boot.hpp
@@ -11,39 +11,30 @@ namespace pldm::responder::oem_meta
  *  @brief Inherits and implements FileHandler. This class is used
  *  to store incoming postcode
  */
-class PostCodeHandler : public FileHandler
+class HttpBootHandler : public FileHandler
 {
   public:
-    PostCodeHandler(pldm_tid_t tid,
+    HttpBootHandler(pldm_tid_t tid,
                     const std::map<std::string, MctpEndpoint>& configurations) :
         tid(tid),
         configurations(configurations)
     {}
 
-    ~PostCodeHandler() = default;
-
-    /** @brief Method to store postcode list
-     *  @param[in] data - post code
-     *  @return  PLDM status code
-     */
-    int write(const message& data);
+    ~HttpBootHandler() = default;
 
     /** @brief Method to read postcode list
      *  @param[in] data - post code
      *  @return  PLDM status code
      */
-    int read(std::vector<uint8_t>& data)
+    int read(std::vector<uint8_t>& data);
+
+    int write(const message& data)
     {
         (void)data; // Unused
         return PLDM_ERROR_UNSUPPORTED_PLDM_CMD;
     }
 
   private:
-    /** @brief Parse object path to get correspond slot number
-     *  @param[in] slot - slot number
-     */
-    void parseObjectPathToGetSlot(uint64_t& slot);
-
     /** @brief The terminus ID of the message source*/
     pldm_tid_t tid = 0;
 
