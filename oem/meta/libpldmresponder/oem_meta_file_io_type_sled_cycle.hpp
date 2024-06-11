a1e8410da507b0b0db0dcc810337384a39b4df3b
Jeff Shih <Jeff_Shih@wiwynn.com>
2024-06-11 13:21:04 +0800

    Support OEM-META command for getting Http boot certification
    Support parsing read file IO for Http Boot command (type: 0x03) and returning the Http boot certification stored in BMC by reading from settingsd
    
    Change-Id: Iaa0da923a8703703744eccccbbc8d95146444c14
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>

---
+++ oem/meta/libpldmresponder/oem_meta_file_io_type_sled_cycle.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "common/utils.hpp"
+#include "oem_meta_file_io_by_type.hpp"
+
+namespace pldm::responder::oem_meta
+{
+/** @class SledCycleHandler
+ *
+ *  @brief Inherits and implements FileHandler. This class is used
+ *  to handle incoming sled cycle request from Hosts
+ */
+class SledCycleHandler : public FileHandler
+{
+  public:
+    SledCycleHandler() = delete;
+    SledCycleHandler(const SledCycleHandler&) = delete;
+    SledCycleHandler(SledCycleHandler&&) = delete;
+    SledCycleHandler& operator=(const SledCycleHandler&) = delete;
+    SledCycleHandler& operator=(SledCycleHandler&&) = delete;
+    ~SledCycleHandler() = default;
+
+    explicit SledCycleHandler(uint8_t tid,
+                              const pldm::utils::DBusHandler* dBusIntf) :
+        tid(tid),
+        dBusIntf(dBusIntf)
+    {}
+
+    int write(const message& data) override;
+    int read(std::vector<uint8_t>& data) override
+    {
+        (void)data; // Unused
+        return PLDM_ERROR_UNSUPPORTED_PLDM_CMD;
+    }
+
+  private:
+    /** @brief The requester's TID */
+    uint8_t tid = 0;
+
+    /** @brief D-Bus Interface object*/
+    const pldm::utils::DBusHandler* dBusIntf;
+};
+
+} // namespace pldm::responder::oem_meta
