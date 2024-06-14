64a6956cc0b1c1065d1081dc1cdb46b37d54a3e5
Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
2024-06-14 16:24:31 +0800

    Support OEM-META command for power control
    Summary:
      - Add handler for write-file command "SLED_CYCLE, 12V-CYCLE
    and DC-cycle" to let Host can trigger power control to the system.
        - Option:
          - 0x00: Sled-cycle
          - 0x01: Slot 12V-cycle
          - 0x02: Slot DC-cycle
    
    Change-Id: Ie6264217359f722be8181919f15bc08df510750a
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
    Signed-off-by: MandyMCHung <mandy.hung.wiwynn@gmail.com>

--- oem/meta/libpldmresponder/oem_meta_file_io.cpp
+++ oem/meta/libpldmresponder/oem_meta_file_io.cpp
@@ -2,6 +2,7 @@
 
 #include "oem_meta_file_io_type_bios_version.hpp"
 #include "oem_meta_file_io_type_post_code.hpp"
+#include "oem_meta_file_io_type_power_control.hpp"
 #include "xyz/openbmc_project/Common/error.hpp"
 
 #include <libpldm/oem/meta/file_io.h>
@@ -25,6 +26,10 @@ std::unique_ptr<FileHandler>
                 messageTid, configurationDiscovery->getConfigurations());
         case BIOS_VERSION:
             return std::make_unique<BIOSVersionHandler>(
+                messageTid, configurationDiscovery->getConfigurations(),
+                dBusIntf);
+        case POWER_CONTROL:
+            return std::make_unique<PowerControlHandler>(
                 messageTid, configurationDiscovery->getConfigurations(),
                 dBusIntf);
         default:
@@ -51,6 +56,7 @@ Response FileIOHandler::writeFileIO(pldm_tid_t tid, const pldm_msg* request,
 
     if (rc != 0)
     {
+        error("Failed to decode OEM Meta file IO request, rc={RC}", "RC", rc);
         return ccOnlyResponse(request, rc);
     }
 
--- oem/meta/libpldmresponder/oem_meta_file_io_by_type.hpp
+++ oem/meta/libpldmresponder/oem_meta_file_io_by_type.hpp
@@ -20,6 +20,7 @@ enum pldm_oem_meta_file_io_type : uint8_t
 {
     POST_CODE = 0x00,
     BIOS_VERSION = 0x01,
+    POWER_CONTROL = 0x02,
 };
 
 class FileHandler
