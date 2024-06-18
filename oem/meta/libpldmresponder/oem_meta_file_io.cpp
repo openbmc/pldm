e9029f645a98051bfac721001bf70be65ccbb044
Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
2024-06-18 13:28:10 +0800

    Support OEM-META command for getting Http boot certification
    Summary:
    - Support parsing read file IO for Http Boot command (type: 0x03) and
    returning the Http boot certification stored in BMC by reading from
    settingsd.
    
    Change-Id: Iaa0da923a8703703744eccccbbc8d95146444c14
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>

--- oem/meta/libpldmresponder/oem_meta_file_io.cpp
+++ oem/meta/libpldmresponder/oem_meta_file_io.cpp
@@ -1,6 +1,7 @@
 #include "oem_meta_file_io.hpp"
 
 #include "oem_meta_file_io_type_bios_version.hpp"
+#include "oem_meta_file_io_type_http_boot.hpp"
 #include "oem_meta_file_io_type_post_code.hpp"
 #include "oem_meta_file_io_type_power_control.hpp"
 #include "xyz/openbmc_project/Common/error.hpp"
@@ -32,6 +33,9 @@ std::unique_ptr<FileHandler> FileIOHandler::getHandlerByType(uint8_t messageTid,
             return std::make_unique<PowerControlHandler>(
                 messageTid, configurationDiscovery->getConfigurations(),
                 dBusIntf);
+        case HTTP_BOOT:
+            return std::make_unique<HttpBootHandler>(
+                messageTid, configurationDiscovery->getConfigurations());
         default:
             error("Get invalid file io type, FILEIOTYPE={FILEIOTYPE}",
                   "FILEIOTYPE", fileIOType);
@@ -48,8 +52,8 @@ Response FileIOHandler::writeFileIO(pldm_tid_t tid, const pldm_msg* request,
 
     std::array<uint8_t, decodeDataMaxLength> retDataField{};
 
-    auto rc = decode_oem_meta_file_io_req(request, payloadLength, &fileIOType,
-                                          &length, retDataField.data());
+    auto rc = decode_oem_meta_write_file_io_req(
+        request, payloadLength, &fileIOType, &length, retDataField.data());
 
     if (rc != PLDM_SUCCESS)
     {
@@ -69,6 +73,47 @@ Response FileIOHandler::writeFileIO(pldm_tid_t tid, const pldm_msg* request,
     rc = handler->write(data);
 
     return ccOnlyResponse(request, rc);
+}
+
+Response FileIOHandler::readFileIO(pldm_tid_t tid, const pldm_msg* request,
+                                   size_t payloadLength)
+{
+    uint8_t completionCode = PLDM_SUCCESS;
+    struct pldm_oem_meta_read_file_req req = {0};
+
+    auto rc = decode_oem_meta_read_file_io_req(
+        request, payloadLength, &req.file_handle, &req.length,
+        &req.transferFlag, &req.highOffset, &req.lowOffset);
+
+    if (rc != PLDM_SUCCESS)
+    {
+        return ccOnlyResponse(request, rc);
+    }
+
+    auto handler = getHandlerByType(tid, req.file_handle);
+
+    if (!handler)
+    {
+        return ccOnlyResponse(request, PLDM_ERROR_UNSUPPORTED_PLDM_CMD);
+    }
+
+    std::vector<uint8_t> data = {req.length, req.transferFlag, req.highOffset,
+                                 req.lowOffset};
+
+    rc = handler->read(data);
+
+    Response response(sizeof(pldm_msg_hdr) + sizeof(completionCode), 0);
+
+    auto responseMsg = reinterpret_cast<pldm_msg*>(response.data());
+
+    rc = encode_http_boot_header_resp(request->hdr.instance_id, completionCode,
+                                      responseMsg);
+
+    response.insert(response.begin() + sizeof(pldm_msg_hdr) +
+                        sizeof(completionCode),
+                    data.begin(), data.end());
+
+    return response;
 }
 
 } // namespace pldm::responder::oem_meta
--- oem/meta/libpldmresponder/oem_meta_file_io_type_bios_version.hpp
+++ oem/meta/libpldmresponder/oem_meta_file_io_type_bios_version.hpp
@@ -35,8 +35,9 @@ class BIOSVersionHandler : public FileHandler
      */
     int write(const message& data) override;
 
-    int read(const message&) override
+    int read(std::vector<uint8_t>& data) override
     {
+        (void)data; // Unused
         return PLDM_ERROR_UNSUPPORTED_PLDM_CMD;
     }
 
