582835131f24f29ff02e36238ef96e3c5d68e802
Bright Cheng <bright.cheng.wiwymm@gmail.com>
2024-06-26 17:27:57 +0800

    Support OEM-META command for Event Logs from BIC
    Summary:
      - Add handler for write-file command "EVENT_LOG", which is sent
    when host events are triggered, parse event type from a list and
    add SEL in BMC.
    
    Change-Id: Id8957ca9585f81902fe184851d0df237cf3937ab
    Signed-off-by: Bright Cheng <bright.cheng.wiwynn@gmail.com>

--- oem/meta/libpldmresponder/oem_meta_file_io.cpp
+++ oem/meta/libpldmresponder/oem_meta_file_io.cpp
@@ -4,6 +4,7 @@
 #include "oem_meta_file_io_type_http_boot.hpp"
 #include "oem_meta_file_io_type_post_code.hpp"
 #include "oem_meta_file_io_type_power_control.hpp"
+#include "oem_meta_file_io_type_event_log.hpp"
 #include "xyz/openbmc_project/Common/error.hpp"
 
 #include <libpldm/oem/meta/file_io.h>
@@ -35,6 +36,8 @@ std::unique_ptr<FileHandler> FileIOHandler::getHandlerByType(uint8_t messageTid,
                 dBusIntf);
         case HTTP_BOOT:
             return std::make_unique<HttpBootHandler>(
+        case EVENT_LOG:
+            return std::make_unique<EventLogHandler>(
                 messageTid, configurationDiscovery->getConfigurations());
         default:
             error("Get invalid file io type, FILEIOTYPE={FILEIOTYPE}",
