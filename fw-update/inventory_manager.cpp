1382da99188ffed9063d27d2d2d979b4b9ce44de
Chris Wang <chris.wang.wiwynn@gmail.com>
2024-08-08 15:59:06 +0800

    fw_update: Support getting downstream device fw parameters
    After discover a endpoint support Downstream Devices, get their
    firmware parameters such as component version string.
    
    The code is developed based on DSP0267_1.1.0 Section 10.5 -
    GetDownstreamFirmwareParameters command format.
    
    Tested: Add unit tests for the new command handler.
    
    Change-Id: I6da33c00ead2d920bf931c7f7165b24aae228782
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>

--- fw-update/inventory_manager.cpp
+++ fw-update/inventory_manager.cpp
@@ -432,7 +432,148 @@ void InventoryManager::queryDownstreamIdentifiers(mctp_eid_t eid,
             break;
         case PLDM_START_AND_END:
             downstreamDescriptorMap.emplace(eid, std::move(downstreamDevices));
+            /** DataTransferHandle will be skipped when TransferOperationFlag is
+             *  `GetFirstPart`. Use 0x0 as default by following example in
+             *  Figure 9 in DSP0267 1.1.0
+             */
+            sendGetDownstreamFirmwareParametersRequest(eid, 0x0,
+                                                       PLDM_GET_FIRSTPART);
             break;
+        case PLDM_END:
+            sendGetDownstreamFirmwareParametersRequest(eid, 0x0,
+                                                       PLDM_GET_FIRSTPART);
+            break;
+    }
+}
+
+void InventoryManager::sendGetDownstreamFirmwareParametersRequest(
+    mctp_eid_t eid, uint32_t dataTransferHandle,
+    const enum transfer_op_flag transferOperationFlag)
+{
+    Request requestMsg(sizeof(pldm_msg_hdr) +
+                       PLDM_GET_DOWNSTREAM_FIRMWARE_PARAMS_REQ_BYTES);
+    auto instanceId = instanceIdDb.next(eid);
+    auto request = reinterpret_cast<pldm_msg*>(requestMsg.data());
+    auto rc = encode_get_downstream_firmware_params_req(
+        instanceId, dataTransferHandle, transferOperationFlag, request,
+        PLDM_GET_DOWNSTREAM_FIRMWARE_PARAMS_REQ_BYTES);
+    if (rc)
+    {
+        instanceIdDb.free(eid, instanceId);
+        error(
+            "encode_query_downstream_firmware_param_req failed, EID={EID}, RC = {RC}",
+            "EID", unsigned(eid), "RC", rc);
+        throw std::runtime_error(
+            "encode_query_device_firmware_param_req failed");
+    }
+
+    rc = handler.registerRequest(
+        eid, instanceId, PLDM_FWUP, PLDM_QUERY_DOWNSTREAM_FIRMWARE_PARAMETERS,
+        std::move(requestMsg),
+        std::move(std::bind_front(
+            &InventoryManager::getDownstreamFirmwareParameters, this)));
+    if (rc)
+    {
+        error(
+            "Failed to send QueryDownstreamFirmwareParameters request, EID={EID}, RC = {RC}",
+            "EID", unsigned(eid), "RC", rc);
+    }
+}
+
+void InventoryManager::getDownstreamFirmwareParameters(mctp_eid_t eid,
+                                                       const pldm_msg* response,
+                                                       size_t respMsgLen)
+{
+    if (response == nullptr || !respMsgLen)
+    {
+        error(
+            "No response received for QueryDownstreamFirmwareParameters, EID={EID}",
+            "EID", unsigned(eid));
+        descriptorMap.erase(eid);
+        return;
+    }
+
+    pldm_get_downstream_firmware_params_resp downstreamFirmwareParams{};
+    variable_field downstreamDeviceParamTable{};
+
+    auto rc = decode_get_downstream_firmware_params_resp(
+        response, respMsgLen, &downstreamFirmwareParams,
+        &downstreamDeviceParamTable);
+
+    if (rc)
+    {
+        error(
+            "Decoding QueryDownstreamFirmwareParameters response failed, EID={EID}, RC = {RC}",
+            "EID", unsigned(eid), "RC", rc);
+        return;
+    }
+
+    if (downstreamFirmwareParams.completion_code)
+    {
+        error(
+            "QueryDownstreamFirmwareParameters response failed with error completion code, EID={EID}, CC = {CC}",
+            "EID", unsigned(eid), "CC",
+            unsigned(downstreamFirmwareParams.completion_code));
+        return;
+    }
+
+    struct pldm_downstream_device_parameter_entry_versions entry_version = {};
+    struct variable_field versions = {};
+    ComponentInfo componentInfo{};
+
+    while (downstreamFirmwareParams.downstream_device_count-- &&
+           (downstreamDeviceParamTable.length > 0))
+    {
+        auto rc = decode_downstream_device_parameter_table_entry(
+            &downstreamDeviceParamTable, &entry_version.entry, &versions);
+        if (rc)
+        {
+            error(
+                "Decoding downstream device parameter table entry failed, EID={EID}, RC = {RC}",
+                "EID", unsigned(eid), "RC", rc);
+            return;
+        }
+
+        rc = decode_downstream_device_parameter_table_entry_versions(
+            &versions, &entry_version.entry, entry_version.active_comp_ver_str,
+            entry_version.pending_comp_ver_str);
+        if (rc)
+        {
+            error(
+                "Decoding downstream device parameter table entry versions failed, EID={EID}, RC = {RC}",
+                "EID", unsigned(eid), "RC", rc);
+            return;
+        }
+
+        // Classification for downstream devices defined in DSP0267_1.1.0 Table
+        // 27 – ComponentClassification values
+        auto compClassification = 0xFFFF;
+        auto deviceIndex = entry_version.entry.downstream_device_index;
+        // Update only the downsltream device which match the target
+        // deviceIndex, 0x0 is defined in DSP0267_1.1.0 Table 25 –
+        // PassComponentTable command format
+        auto componentClassificationIndex = 0x0;
+        componentInfo.emplace(std::make_pair(compClassification, deviceIndex),
+                              componentClassificationIndex);
+    }
+
+    switch (downstreamFirmwareParams.transfer_flag)
+    {
+        case PLDM_START:
+        case PLDM_MIDDLE:
+            sendGetDownstreamFirmwareParametersRequest(
+                eid, downstreamFirmwareParams.next_data_transfer_handle,
+                PLDM_GET_NEXTPART);
+            break;
+    }
+
+    if (componentInfoMap.contains(eid))
+    {
+        componentInfoMap.at(eid).merge(componentInfo);
+    }
+    else
+    {
+        componentInfoMap.emplace(eid, std::move(componentInfo));
     }
 }
 
