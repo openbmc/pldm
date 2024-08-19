e8a2c2dc826dded4585606f830665054b1c368db
Unive Tien <unive.tien.wiwynn@gmail.com>
2024-08-19 10:52:14 +0800

    fw_update: Add AggregateUpdateManager
    Change-Id: Icfdb8d238121f9f44a624396e00b378e491ce652
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

---
+++ fw-update/aggregate_update_manager.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "update_manager.hpp"
+
+namespace pldm::fw_update
+{
+
+class AggregateUpdateManager final :
+    public std::vector<std::shared_ptr<UpdateManager>>,
+    public UpdateManager
+{
+  public:
+    using UpdateManager::UpdateManager;
+
+    Response handleRequest(mctp_eid_t eid, uint8_t command,
+                           const pldm_msg* request, size_t reqMsgLen) override
+    {
+        auto response =
+            UpdateManager::handleRequest(eid, command, request, reqMsgLen);
+        auto responseMsg = new (response.data()) pldm_msg;
+
+        if (responseMsg->payload[0] != PLDM_FWUP_COMMAND_NOT_EXPECTED)
+        {
+            return response;
+        }
+        else
+        {
+            for (auto& manager : *this)
+            {
+                response =
+                    manager->handleRequest(eid, command, request, reqMsgLen);
+                responseMsg = new (response.data()) pldm_msg;
+                if (responseMsg->payload[0] != PLDM_FWUP_COMMAND_NOT_EXPECTED)
+                {
+                    return response;
+                }
+            }
+
+            return response;
+        }
+    }
+};
+
+} // namespace pldm::fw_update
