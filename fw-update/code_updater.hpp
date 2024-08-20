90664faa761053e785418066da077f9247e583c9
Unive Tien <unive.tien.wiwynn@gmail.com>
2024-08-20 15:26:49 +0800

    impliment codeupdater
    Change-Id: I724f0987eb882a1b4e6bce8c209b319c927689e9
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

---
+++ fw-update/code_updater.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "update_manager.hpp"
+
+#include <xyz/openbmc_project/Software/ApplyTime/server.hpp>
+#include <xyz/openbmc_project/Software/Update/server.hpp>
+
+#include <filesystem>
+#include <memory>
+
+namespace pldm::fw_update
+{
+
+using UpdateIntf = sdbusplus::xyz::openbmc_project::Software::server::Update;
+using ApplyTimeIntf =
+    sdbusplus::xyz::openbmc_project::Software::server::ApplyTime;
+
+class CodeUpdater : public UpdateIntf
+{
+  public:
+    CodeUpdater() = delete;
+    CodeUpdater(const CodeUpdater&) = delete;
+    CodeUpdater(CodeUpdater&&) = delete;
+    CodeUpdater& operator=(const CodeUpdater&) = delete;
+    CodeUpdater& operator=(CodeUpdater&&) = delete;
+    ~CodeUpdater() = default;
+
+    CodeUpdater(sdbusplus::bus::bus& bus, const std::string& path,
+                const std::shared_ptr<UpdateManager>& updateManager) :
+        UpdateIntf(bus, path.c_str()),
+        updateManager(updateManager)
+    {}
+
+  private:
+    virtual sdbusplus::message::object_path
+        startUpdate(sdbusplus::message::unix_fd image,
+                    ApplyTimeIntf::RequestedApplyTimes applyTime) override;
+
+    bool writeToFile(int imageFd, const std::string& path);
+
+    std::shared_ptr<UpdateManager> updateManager;
+};
+
+} // namespace pldm::fw_update
+                              
