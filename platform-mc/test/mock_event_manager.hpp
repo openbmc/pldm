248d0c6492fd1aa2b408f1c4d391821588fc814f
Thu Nguyen <thu@os.amperecomputing.com>
2024-09-10 10:15:01 +0000

    platform-mc: Support CPER Event handler
    Handle CPER event(0x07) which is defined in `Table 11 - PLDM Event
    Type` and section `16.17 eventData format for CPEREvent` in DSP0248
    v1.3.0.
    
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>
    Change-Id: I85c53933183178c6b5acdfc12c805e8a4cf1ca2a

--- platform-mc/test/mock_event_manager.hpp
+++ platform-mc/test/mock_event_manager.hpp
@@ -21,6 +21,10 @@ class MockEventManager : public EventManager
                  const double reading, const double threshold),
                 (override));
 
+    MOCK_METHOD(int, processCperEvent,
+                (uint16_t eventId, const uint8_t* eventData,
+                 size_t eventDataSize),
+                (override));
 };
 
 } // namespace platform_mc
