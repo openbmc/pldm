2554041bf3799ce6b2e57b72cd4451ac98569931
Unive Tien <unive.tien.wiwynn@gmail.com>
2024-12-06 15:58:21 +0800

    requester: support multi-host MCTP devices hot plug
    Currently, pldmd listens for new MCTP endpoint exposed by mctpd, but
    they only shows their EID, Network Id, and SupportedMessageTypes, which
    cannot fulfill some requirements, e.g., get the device's name or check
    the host slot number which contains the MCTP endpoint in multi-host
    system.
    
    In openbmc, the additional information are exposed by Entity Manager,
    so that we add ConfigurationDiscoveryHandler, search for Entity
    Manager's configuration when a new MCTP endpoint has been register by
    mctpd.
    
    Objects who want to obtain the configuration can include the
    ConfigurationDiscoveryHandler as their attribute, get the configuration
    when needed.
    
    Change-Id: I59b8bf434576cbdea651848a8da11e5b870e2dfa
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

---
+++ requester/configuration_discovery_handler.cpp
@@ -0,0 +1,128 @@
+#include "configuration_discovery_handler.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+
+#include <algorithm>
+
+PHOSPHOR_LOG2_USING;
+
+namespace pldm
+{
+
+void ConfigurationDiscoveryHandler::handleMctpEndpoints(
+    const MctpInfos& newMctpInfos)
+{
+    for (const auto& newMctpInfo : newMctpInfos)
+    {
+        searchConfigurationFor(newMctpInfo);
+    }
+}
+
+void ConfigurationDiscoveryHandler::handleRemovedMctpEndpoints(
+    const MctpInfos& removedMctpInfos)
+{
+    for (const auto& removedMctpInfo : removedMctpInfos)
+    {
+        removeConfigByEid(std::get<0>(removedMctpInfo));
+    }
+}
+
+void ConfigurationDiscoveryHandler::updateMctpEndpointAvailability(
+    [[maybe_unused]] const MctpInfo& mctpinfo,
+    [[maybe_unused]] Availability availability)
+{
+    return;
+}
+
+const std::map<std::string, MCTPEndpoint>&
+    ConfigurationDiscoveryHandler::getConfigurations() const
+{
+    return configurations;
+}
+
+void ConfigurationDiscoveryHandler::searchConfigurationFor(MctpInfo mctpInfo)
+{
+    const auto eidValue = std::get<eid>(mctpInfo);
+    const auto mctpReactorObjectPath =
+        std::string{"/au/com/codeconstruct/mctp1/networks/1/endpoints/"} +
+        std::to_string(eidValue) + "/configured_by";
+    std::string associatedObjPath;
+    std::string associatedService;
+    std::string associatedInterface;
+    std::vector<std::string> interfaceFilter = {
+        "xyz.openbmc_project.Configuration.MCTPI2CTarget",
+        "xyz.openbmc_project.Configuration.MCTPI3CTarget"};
+    std::string subtreePath = "/xyz/openbmc_project/inventory/system";
+    try
+    {
+        auto response = dBusIntf->getAssociatedSubTree(
+            mctpReactorObjectPath, subtreePath, 3, interfaceFilter);
+        if (response.empty())
+        {
+            error("No associated subtree found for path {PATH}", "PATH",
+                  mctpReactorObjectPath);
+            return;
+        }
+        associatedObjPath = response.begin()->first;
+        associatedService = response.begin()->second.begin()->first;
+        auto dBusIntfList = response.begin()->second.begin()->second;
+        for (const auto& intf : dBusIntfList)
+        {
+            debug("Associated interface for path {PATH} is {INTERFACE}", "PATH",
+                  mctpReactorObjectPath, "INTERFACE", intf);
+        }
+        auto associatedInterfaceItr = std::find_if(
+            dBusIntfList.begin(), dBusIntfList.end(),
+            [&interfaceFilter](const auto& intf) {
+                return std::find(interfaceFilter.begin(), interfaceFilter.end(),
+                                 intf) != interfaceFilter.end();
+            });
+        if (associatedInterfaceItr == dBusIntfList.end())
+        {
+            error("No associated interface found for path {PATH}", "PATH",
+                  mctpReactorObjectPath);
+            return;
+        }
+        associatedInterface = *associatedInterfaceItr;
+    }
+    catch (const std::exception& e)
+    {
+        error("Failed to get associated subtree for path {PATH}, {ERROR}",
+              "PATH", mctpReactorObjectPath, "ERROR", e);
+    }
+    try
+    {
+        auto mctpTargetProperties = dBusIntf->getDbusPropertiesVariant(
+            associatedService.c_str(), associatedObjPath.c_str(),
+            associatedInterface.c_str());
+        if (!mctpTargetProperties.contains("Address") ||
+            !mctpTargetProperties.contains("Bus"))
+        {
+            error("No Address or Bus property found for path {PATH}", "PATH",
+                  associatedObjPath);
+            return;
+        }
+        auto address = std::visit(utils::AddressVisitor<uint64_t, uint8_t>(),
+                                  mctpTargetProperties.at("Address"));
+        auto bus = std::visit(utils::VariantToIntegralVisitor<uint8_t>(),
+                              mctpTargetProperties.at("Bus"));
+        configurations.insert_or_assign(associatedObjPath,
+                                        MCTPEndpoint{eidValue, address, bus});
+        debug("Added MCTP endpoint {EID} with address {ADDRESS} and bus {BUS}",
+              "EID", eidValue, "ADDRESS", address, "BUS", bus);
+    }
+    catch (const std::exception& e)
+    {
+        error("Failed to get PLDM device properties at path {PATH}, {ERROR}",
+              "PATH", associatedObjPath, "ERROR", e);
+    }
+}
+
+void ConfigurationDiscoveryHandler::removeConfigByEid(uint8_t eid)
+{
+    std::erase_if(configurations, [eid](const auto& config) {
+        return config.second.endpointId == eid;
+    });
+}
+
+} // namespace pldm
