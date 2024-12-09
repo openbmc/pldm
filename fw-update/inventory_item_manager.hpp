0265baade97e5605fae69146abc7812e4400c698
Unive Tien <unive.tien.wiwynn@gmail.com>
2024-12-09 10:21:39 +0800

    fw_update: Create inventory item for downstream devices
    Inventory items are used to handle each downstream devices process and
    create necessary D-Bus interfaces.
    
    Change-Id: Idebd7d013c82c60f08309a1860d5de1deeb3829a
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

---
+++ fw-update/inventory_item_manager.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "common/types.hpp"
+#include "version.hpp"
+
+#include <xyz/openbmc_project/Association/Definitions/server.hpp>
+#include <xyz/openbmc_project/Inventory/Item/Board/server.hpp>
+#include <xyz/openbmc_project/Software/Version/server.hpp>
+
+namespace pldm::fw_update
+{
+
+using InventoryItemBoardIntf = sdbusplus::server::object_t<
+    sdbusplus::xyz::openbmc_project::Inventory::Item::server::Board>;
+using AssociationDefinitionsIntf = sdbusplus::server::object_t<
+    sdbusplus::xyz::openbmc_project::Association::server::Definitions>;
+using VersionPurpose =
+    sdbusplus::server::xyz::openbmc_project::software::Version::VersionPurpose;
+
+using ObjectPath = pldm::dbus::ObjectPath;
+
+class InventoryItemManager
+{
+  public:
+    InventoryItemManager() = default;
+    InventoryItemManager(const InventoryItemManager&) = delete;
+    InventoryItemManager(InventoryItemManager&&) = delete;
+    InventoryItemManager& operator=(const InventoryItemManager&) = delete;
+    InventoryItemManager& operator=(InventoryItemManager&&) = delete;
+    ~InventoryItemManager() = default;
+
+    void createInventoryItem(const eid& eid,
+                             const FirmwareDeviceName& deviceName,
+                             const std::string& activeVersion);
+
+    void refreshInventoryPath(const eid& eid, const InventoryPath& path);
+
+  private:
+    void createVersion(const std::string& path, std::string version,
+                       VersionPurpose purpose);
+
+    void createAssociation(const std::string& path, const std::string& foward,
+                           const std::string& reverse,
+                           const std::string& assocEndPoint);
+
+    const ObjectPath getBoardPath(const InventoryPath& path);
+
+    std::map<eid, InventoryPath> inventoryPathMap;
+
+    std::vector<std::unique_ptr<InventoryItemBoardIntf>> inventoryItems;
+
+    std::vector<std::unique_ptr<Version>> softwareVersions;
+
+    std::vector<std::unique_ptr<AssociationDefinitionsIntf>> associations;
+};
+
+} // namespace pldm::fw_update
