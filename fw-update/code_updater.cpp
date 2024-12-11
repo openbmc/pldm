cdd35bcb4948a77a5bce536c6df02779c012542d
Unive Tien <unive.tien.wiwynn@gmail.com>
2024-12-11 11:24:51 +0800

    fw_update: Add CodeUpdater
    Change-Id: I724f0987eb882a1b4e6bce8c209b319c927689e9
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

---
+++ fw-update/code_updater.cpp
@@ -0,0 +1,35 @@
+#include "code_updater.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+
+#include <sstream>
+
+PHOSPHOR_LOG2_USING;
+
+namespace pldm::fw_update
+{
+
+sdbusplus::message::object_path
+    CodeUpdater::startUpdate(sdbusplus::message::unix_fd image,
+                             ApplyTimeIntf::RequestedApplyTimes /*applyTime*/)
+{
+    info("Starting update for image {FD}", "FD", static_cast<int>(image));
+    char buffer[4096];
+    ssize_t bytesRead;
+
+    while ((bytesRead = read(image, buffer, sizeof(buffer))) > 0)
+    {
+        imageStream.write(buffer, bytesRead);
+    }
+
+    if (bytesRead < 0)
+    {
+        throw std::runtime_error("Failed to read image file descriptor");
+    }
+
+    updateManager->processStream(imageStream, imageStream.str().size());
+
+    return sdbusplus::message::object_path(objPath);
+}
+
+} // namespace pldm::fw_update
