344b01ff522a199d0ef7cf09fce34154c4ca4044
Eric Yang <eric.yang.wiwynn@gmail.com>
2025-01-20 17:13:33 +0800

    pldatform-mc: startSensorPolling post PDR init
    Move startSensorPolling after confirming PDR initialization of pldm
    device to avoid missing the start sensor polling opportunity if GetPDR
    fails initially and succeeds later.
    
    Change-Id: I4e090a0f3c5b0a7ce85b56f9f3f2c585e42b638f
    Signed-off-by: Eric Yang <eric.yang.wiwynn@gmail.com>

--- platform-mc/platform_manager.cpp
+++ platform-mc/platform_manager.cpp
@@ -1,5 +1,7 @@
 #include "platform_manager.hpp"
 
+#include "manager.hpp"
+
 #include "terminus_manager.hpp"
 
 #include <phosphor-logging/lg2.hpp>
@@ -109,6 +111,10 @@ exec::task<int> PlatformManager::initTerminus()
                 "TID", tid, "ERROR", rc);
         }
         terminus->initialized = true;
+        if (auto manager = terminusManager.getManager())
+        {
+            manager->startSensorPolling(tid);
+        }
     }
 
     co_return PLDM_SUCCESS;
