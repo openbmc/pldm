9859e30438ef9f394f3320895a93225b808a49db
Archana Kakani <archana.kakani@ibm.com>
2025-02-04 03:13:24 -0600

    host-bmc: Implement Availability interface
    Adding support to host Availability dbus interface. PLDM hosts this dbus
    interface to provide the availability of the FRUs hosted by PLDM. The
    Availability interface is defined at [1].
    
    Tested:
       Functional test passed
    
    [1]: https://github.com/openbmc/phosphor-dbus-interfaces/blob/master/yaml/xyz/openbmc_project/State/Decorator/Availability.interface.yaml
    
    Change-Id: Ie5912b3683ce102a249c8b503ea9f455f0fbcabf
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

---
+++ host-bmc/dbus/availability.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <xyz/openbmc_project/State/Decorator/Availability/server.hpp>
+
+#include <string>
+
+namespace pldm
+{
+namespace dbus
+{
+using AvailabilityIntf = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::State::Decorator::server::Availability>;
+
+class Availability : public AvailabilityIntf
+{
+  public:
+    Availability() = delete;
+    ~Availability() = default;
+    Availability(const Availability&) = delete;
+    Availability& operator=(const Availability&) = delete;
+    Availability(Availability&&) = delete;
+    Availability& operator=(Availability&&) = delete;
+
+    Availability(sdbusplus::bus::bus& bus, const std::string& objPath) :
+        AvailabilityIntf(bus, objPath.c_str()), path(objPath)
+    {}
+
+    /** Get value of Available */
+    bool available() const override;
+
+    /** Set value of Available */
+    bool available(bool value) override;
+
+  private:
+    std::string path;
+};
+
+} // namespace dbus
+} // namespace pldm
