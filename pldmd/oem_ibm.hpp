c97557d43efffc072b4e1e813110cbf4f627527d
Archana Kakani <archana.kakani@ibm.com>
2025-03-10 05:39:38 -0500

    oem-ibm: Sync boot side file with bios attributes
    This change adds support to update the boot side file to keep it
    in sync with boot side bios attributes
    
    Change-Id: I2bea293500c69a883f400ddaadc436705bbe5ab4
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

--- pldmd/oem_ibm.hpp
+++ pldmd/oem_ibm.hpp
@@ -52,6 +52,7 @@ class OemIBM
      * @param[in] platformHandler - platformHandler handler
      * @param[in] fruHandler - fruHandler handler
      * @param[in] baseHandler - baseHandler handler
+     * @param[in] biosHandler - biosHandler handler
      * @param[in] reqHandler - reqHandler handler
      */
     explicit OemIBM(
@@ -62,6 +63,7 @@ class OemIBM
         responder::platform::Handler* platformHandler,
         responder::fru::Handler* fruHandler,
         responder::base::Handler* baseHandler,
+        responder::bios::Handler* biosHandler,
         pldm::requester::Handler<pldm::requester::Request>* reqHandler) :
         dBusIntf(dBusIntf), mctp_fd(mctp_fd), mctp_eid(mctp_eid), repo(repo),
         instanceIdDb(instanceIdDb), event(event), invoker(invoker),
@@ -72,6 +74,10 @@ class OemIBM
 
         createOemIbmFruHandler();
         oemIbmFruHandler->setIBMFruHandler(fruHandler);
+
+        createOemBiosHandler();
+        createOemIbmBiosHandler();
+        biosHandler->setOemBiosHandler(oemBiosHandler.get());
 
         createCodeUpdate();
         createSlotHandler();
@@ -117,6 +123,23 @@ class OemIBM
         oemPlatformHandler = std::make_unique<oem_ibm_platform::Handler>(
             dBusIntf, codeUpdate.get(), slotHandler.get(), mctp_fd, mctp_eid,
             instanceIdDb, event, reqHandler);
+    }
+
+    /** @brief Method for creating oemBiosHandler */
+    void createOemBiosHandler()
+    {
+        oemBiosHandler = std::make_unique<responder::oem_ibm_bios::Handler>();
+    }
+
+    /** @brief Method for creating oemIbmBiosHandler
+     *
+     *  different handlers.
+     */
+    void createOemIbmBiosHandler()
+    {
+        oemIbmBiosHandler =
+            dynamic_cast<pldm::responder::oem_ibm_bios::Handler*>(
+                oemBiosHandler.get());
     }
 
     /** @brief Method for creating oemIbmPlatformHandler */
@@ -199,6 +222,9 @@ class OemIBM
     /** @brief pointer to the oem_ibm_fru class*/
     std::unique_ptr<responder::oem_fru::Handler> oemFruHandler{};
 
+    /** @brief pointer to the oem_ibm_bios class*/
+    std::unique_ptr<responder::oem_bios::Handler> oemBiosHandler{};
+
     /** @brief pointer to the CodeUpdate class*/
     std::unique_ptr<pldm::responder::CodeUpdate> codeUpdate{};
 
@@ -210,6 +236,9 @@ class OemIBM
 
     /** @brief oem IBM Fru handler*/
     pldm::responder::oem_ibm_fru::Handler* oemIbmFruHandler = nullptr;
+
+    /** @brief oem IBM BIOS handler*/
+    pldm::responder::oem_ibm_bios::Handler* oemIbmBiosHandler = nullptr;
 
     std::unique_ptr<pldm::led::HostLampTest> hostLampTest;
 
