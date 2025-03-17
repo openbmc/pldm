99fa3916bcf2a594281d81dec8e57d8bc957f00c
Archana Kakani <archana.kakani@ibm.com>
2025-03-17 01:37:29 -0500

    oem-ibm: Support bios attributes for boot types
    This commits adds supports to two bios attributes one as boot initiator
    and other as boot type, which lets the host know who initiated the boot
    through boot initiator, and what type of boot(boot or reboot) through the
    boot type attribute.
    
    Change-Id: Iec4bc4ceec3a48ffd3f59b4d31f419c8806187ac
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

--- oem/ibm/libpldmresponder/oem_ibm_handler.hpp
+++ oem/ibm/libpldmresponder/oem_ibm_handler.hpp
@@ -363,6 +363,12 @@ class Handler : public oem_platform::Handler
      */
     void setSurvTimer(uint8_t tid, bool value);
 
+     /** @brief To handle the boot types bios attributes at power on*/
+    void handleBootTypesAtPowerOn();
+
+    /** @brief To handle the boot types bios attributes at shutdown*/
+    void handleBootTypesAtChassisOff();
+
     ~Handler() = default;
 
     pldm::responder::CodeUpdate* codeUpdate; //!< pointer to CodeUpdate object
