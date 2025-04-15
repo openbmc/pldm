de28224f7f3b4cee6b3ae93c8514f5653ce6540b
P Arun Kumar Reddy <parunkumarr@nvidia.com>
2025-04-15 16:06:06 +0530

    fw-update: Add support for canceling component updates
    This commit implements the CancelUpdateComponent command functionality
    to handle component update failures during firmware updates.
    This change improves firmware update reliability by gracefully handling
    component update failures and allowing partial updates to proceed when
    possible.
    
    Tests:
    - Triggered a verification failure with a corrupted package. The
      relevant logs are seen in the journal
    
    ```
    pldmd[24962]: Failed to verify component endpoint ID '16' and version
                  'cec1736Ecfw-01.04.0022.0000' with transfer result - '149'
    pldmd[24962]: Sending cancel update component request for endpoint ID 16
    pldmd[24962]: Received cancel update component response from endpoint ID
                  16
    ```
    
    Change-Id: I7618045f3d3df3fc4f64c493fc39f2a0f7898b95
    Signed-off-by: P Arun Kumar Reddy <parunkumarr@nvidia.com>

--- fw-update/device_updater.hpp
+++ fw-update/device_updater.hpp
@@ -152,6 +152,20 @@ class DeviceUpdater
      */
     void activateFirmware(mctp_eid_t eid, const pldm_msg* response,
                           size_t respMsgLen);
+    /**
+     * @brief send cancel update component request
+     *
+     */
+    void sendCancelUpdateComponentRequest();
+    /**
+     * @brief Handler for CancelUpdateComponent command response
+     *
+     * @param[in] eid - Remote MCTP endpoint
+     * @param[in] response - PLDM Response message
+     * @param[in] respMsgLen - Response message length
+     */
+    void cancelUpdateComponent(mctp_eid_t eid, const pldm_msg* response,
+                               size_t respMsgLen);
 
   private:
     /** @brief Send PassComponentTable command request
@@ -251,6 +265,12 @@ class DeviceUpdater
 
     /** @brief To send a PLDM request after the current command handling */
     std::unique_ptr<sdeventplus::source::Defer> pldmRequest;
+
+    /**
+     * @brief Map to hold component update status. True - success, False -
+     *        cancelled
+     */
+    std::map<size_t, bool> componentUpdateStatus;
 };
 
 } // namespace fw_update
