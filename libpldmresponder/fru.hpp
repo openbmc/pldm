36d0a0feb96376d33b1cf8ee4b61785235c06084
Pavithra Barithaya <pavithrabarithaya07@gmail.com>
2025-04-24 16:30:55 +0530

    fru: Function to send the PDR Repo Change event to Host
    The PDR Repository Change Event is sent to host informing that
    the PDR repository has changed with either some new records added
    or some records could have been deleted or some records could have
    been modified.
    
    This commit adds a new function to send the repository change event
    to host with the changed record handles.
    
    Change-Id: I95d922b81c79442e026a376697c9a044ebc3b30b
    Signed-off-by: Pavithra Barithaya <pavithrabarithaya07@gmail.com>

--- libpldmresponder/fru.hpp
+++ libpldmresponder/fru.hpp
@@ -3,7 +3,9 @@
 #include "fru_parser.hpp"
 #include "libpldmresponder/pdr_utils.hpp"
 #include "oem_handler.hpp"
+#include "pldmd/dbus_impl_requester.hpp"
 #include "pldmd/handler.hpp"
+#include "requester/handler.hpp"
 
 #include <libpldm/fru.h>
 #include <libpldm/pdr.h>
@@ -14,6 +16,9 @@
 #include <string>
 #include <variant>
 #include <vector>
+
+using namespace pldm::dbus_api;
+using namespace pldm::utils;
 
 namespace pldm
 {
@@ -34,6 +39,8 @@ using ObjectPath = std::string;
 using AssociatedEntityMap = std::map<ObjectPath, pldm_entity>;
 
 } // namespace dbus
+
+using ChangeEntry = uint32_t;
 
 /** @class FruImpl
  *
@@ -66,9 +73,12 @@ class FruImpl
     FruImpl(const std::string& configPath,
             const std::filesystem::path& fruMasterJsonPath, pldm_pdr* pdrRepo,
             pldm_entity_association_tree* entityTree,
-            pldm_entity_association_tree* bmcEntityTree) :
+            pldm_entity_association_tree* bmcEntityTree, Requester& requester,
+            pldm::requester::Handler<pldm::requester::Request>* handler,
+            uint8_t mctp_eid) :
         parser(configPath, fruMasterJsonPath), pdrRepo(pdrRepo),
-        entityTree(entityTree), bmcEntityTree(bmcEntityTree)
+        entityTree(entityTree), bmcEntityTree(bmcEntityTree),
+        requester(requester), handler(handler), mctp_eid(mctp_eid)
     {}
 
     /** @brief Total length of the FRU table in bytes, this includes the pad
@@ -233,6 +243,9 @@ class FruImpl
     pldm_entity_association_tree* bmcEntityTree;
     pldm::responder::oem_fru::Handler* oemFruHandler = nullptr;
     dbus::ObjectValueTree objects;
+    Requester& requester;
+    pldm::requester::Handler<pldm::requester::Request>* handler;
+    uint8_t mctp_eid;
 
     std::map<dbus::ObjectPath, pldm_entity_node*> objToEntityNode{};
 
@@ -254,6 +267,17 @@ class FruImpl
      */
     void deleteFRURecord(uint16_t rsi);
 
+    /* @brief Send a PLDM event to host firmware containing a list of record
+     *        handles of PDRs that the host firmware has to fetch.
+     *
+     * @param[in] pdrRecordHandles - list of PDR record handles
+     * @param[in] eventDataOps - event data operation for PDRRepositoryChgEvent
+     *                           as in DSP0248 SPEC
+     */
+    void sendPDRRepositoryChgEventbyPDRHandles(
+        std::vector<uint32_t>&& pdrRecordHandles,
+        std::vector<uint8_t>&& eventDataOps);
+
     /** @brief Associate sensor/effecter to FRU entity
      */
     dbus::AssociatedEntityMap associatedEntityMap;
@@ -268,8 +292,11 @@ class Handler : public CmdHandler
     Handler(const std::string& configPath,
             const std::filesystem::path& fruMasterJsonPath, pldm_pdr* pdrRepo,
             pldm_entity_association_tree* entityTree,
-            pldm_entity_association_tree* bmcEntityTree) :
-        impl(configPath, fruMasterJsonPath, pdrRepo, entityTree, bmcEntityTree)
+            pldm_entity_association_tree* bmcEntityTree, Requester& requester,
+            pldm::requester::Handler<pldm::requester::Request>* handler,
+            uint8_t mctp_eid) :
+        impl(configPath, fruMasterJsonPath, pdrRepo, entityTree, bmcEntityTree,
+             equester, handler, mctp_eid)
     {
         handlers.emplace(
             PLDM_GET_FRU_RECORD_TABLE_METADATA,
