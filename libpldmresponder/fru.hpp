8b44ababf4aec8a2526031300c2dba00db68544a
Pavithra Barithaya <pavithrabarithaya07@gmail.com>
2025-04-24 16:36:18 +0530

    fru: Function to remove the individual FRU Record
    When there is a concurrent maintenance operation a FRU can be
    removed or added. When the FRU is removed we must make sure that
    the associated FRU Record set PDRs, State Effecter PDRs, State
    Sensor PDRs, EntityAssociation PDRs need to be removed from the
    repository. This commit adds the function that does the required
    functionality.
    
    Tested: Removal of a FRU say a Fan by making the present property
    of the FRU as false. This action removes the corresponding
    FRURecordSet PDRs, State effecter PDRs, State sensor PDRs and
    also the Entity association PDRs from the repo. This also removes
    the node corresponding to that FRU from the entity association
    tree.
    This behavior is observed for other FRUs as well.
    
    Change-Id: I6c6e17e21807940ce6d4a230dbceafd9f7c5c958
    Signed-off-by: Pavithra Barithaya <pavithrabarithaya07@gmail.com>

--- libpldmresponder/fru.hpp
+++ libpldmresponder/fru.hpp
@@ -20,6 +20,10 @@ namespace pldm
 
 namespace responder
 {
+namespace platform
+{
+class Handler; // forward declaration
+}
 
 namespace dbus
 {
@@ -33,6 +37,7 @@ using InterfaceMap = std::map<Interface, PropertyMap>;
 using ObjectValueTree = std::map<sdbusplus::message::object_path, InterfaceMap>;
 using ObjectPath = std::string;
 using AssociatedEntityMap = std::map<ObjectPath, pldm_entity>;
+using ObjectPathToRSIMap = std::map<ObjectPath, uint16_t>;
 
 } // namespace dbus
 
@@ -219,6 +224,11 @@ class FruImpl
         oemFruHandler = handler;
     }
 
+    inline void setPlatformHandler(pldm::responder::platform::Handler* handler)
+    {
+        platformHandler = handler;
+    }
+
   private:
     uint16_t nextRSI()
     {
@@ -244,11 +254,17 @@ class FruImpl
     pldm_entity_association_tree* bmcEntityTree;
     pldm::responder::oem_fru::Handler* oemFruHandler = nullptr;
     dbus::ObjectValueTree objects;
+    pldm::responder::platform::Handler* platformHandler = nullptr;
 
     /** @OEM platform handler */
     pldm::responder::oem_platform::Handler* oemPlatformHandler;
 
     std::map<dbus::ObjectPath, pldm_entity_node*> objToEntityNode{};
+
+    dbus::ObjectPathToRSIMap objectPathToRSIMap{};
+
+    pdr_utils::DbusObjMaps effecterDbusObjMaps{};
+    pdr_utils::DbusObjMaps sensorDbusObjMaps{};
 
     /** @brief populateRecord builds the FRU records for an instance of FRU and
      *         updates the FRU table with the FRU records.
@@ -278,6 +294,13 @@ class FruImpl
      *  @return
      */
     void deleteFRURecord(uint16_t rsi);
+
+    /** @brief Deletes a FRU record set PDR and it's associated PDRs after
+     *         a concurrent remove operation.
+     *  @param[in] fruObjectPath - the FRU object path
+     *  @return
+     */
+    void removeIndividualFRU(const std::string& fruObjPath);
 
     /** @brief Associate sensor/effecter to FRU entity
      */
@@ -392,6 +415,11 @@ class Handler : public CmdHandler
     void setOemFruHandler(pldm::responder::oem_fru::Handler* handler)
     {
         impl.setOemFruHandler(handler);
+    }
+
+    void setPlatformHandler(pldm::responder::platform::Handler* handler)
+    {
+        impl.setPlatformHandler(handler);
     }
 
     using Table = std::vector<uint8_t>;
