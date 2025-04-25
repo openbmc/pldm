4c3fb8ee0b0b20522623b1949d2eb39beb7195e3
P Arun Kumar Reddy <arunpapannagari23@gmail.com>
2025-04-25 11:24:06 +0530

    fwupdate: Implement RequestFirmwareData timeout (UA_T2)
    This commit implements the UA_T2 timeout mechanism for the
    RequestFirmwareData command during firmware updates. If no command is
    received from the FD within 60 seconds, the UA will:
    1. Cancel the current component update
    2. Mark the component update as failed
    3. Notify the UpdateManager of the failure
    4. Allow the update process to continue with remaining components
    
    The timeout value of 60 seconds is set according to the PLDM
    specification's minimum requirement for UA_T2.
    
    Tests:
    - Simulating a timeout by turning off the components during a FW update
    ```
    Decoded fw request data at offset '589824' and length '4096' for endpoint ID 26 and Component index 0
    Decoded fw request data at offset '663552' and length '4096' for endpoint ID 18 and Component index 0
    Request firmware data timed out. No command received from FD within the expected time of 60s from endpoint ID 26, component index 0
    Sending cancel update component request for endpoint ID 26
    8c 05 1c
    Request firmware data timed out. No command received from FD within the expected time of 60s from endpoint ID 18, component index 0
    Sending cancel update component request for endpoint ID 18
    8c 05 1c
    ```
    
    Change-Id: I9d34bffa0a4be81d3e8564994a2ea0ef6f2aecd6
    Signed-off-by: P Arun Kumar Reddy <arunpapannagari23@gmail.com>

--- fw-update/device_updater.hpp
+++ fw-update/device_updater.hpp
@@ -4,6 +4,7 @@
 #include "requester/handler.hpp"
 #include "requester/request.hpp"
 
+#include <sdbusplus/timer.hpp>
 #include <sdeventplus/event.hpp>
 #include <sdeventplus/source/event.hpp>
 
@@ -227,6 +228,11 @@ class DeviceUpdater
         }
     }
 
+    /**
+     * @brief Create a timer to handle RequestFirmwareData timeout (UA_T2)
+     */
+    void createRequestFwDataTimer();
+
     /** @brief Endpoint ID of the firmware device */
     mctp_eid_t eid;
 
@@ -272,8 +278,21 @@ class DeviceUpdater
      *           - false: Component update was cancelled
      */
     ComponentUpdateStatusMap componentUpdateStatus;
+
+    /**
+     * @brief Timeout in seconds for the UA to cancel the component update if no
+     * command is received from the FD during component image transfer stage
+     *
+     */
+    auto static constexpr updateTimeoutSeconds = 60;
+
+    /**
+     * @brief Timer to handle RequestFirmwareData timeout(UA_T2)
+     *
+     */
+    std::unique_ptr<sdbusplus::Timer> reqFwDataTimer;
 };
 
 } // namespace fw_update
-
 } // namespace pldm
+
