a891191ef3a37d34c01607b4cf39744b72e7bf45
Unive Tien <unive.tien.wiwynn@gmail.com>
2025-05-14 13:24:02 +0800

    fw_update: Add SoftwareManager and DeviceDedicatedUpdater
    DeviceDedicatedUpdater is a class that manages all of the D-Bus
    utilities for the firmware update functionality for each software entry,
    SoftwareManager is used to manage multiple DeviceDedicatedUpdater
    instances.
    
    The update feature for these is not implemented yet in this commit.
    
    Change-Id: Idebd7d013c82c60f08309a1860d5de1deeb3829a
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

---
+++ fw-update/software_update.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <sdbusplus/async/context.hpp>
+#include <xyz/openbmc_project/Software/Update/aserver.hpp>
+
+namespace pldm::fw_update
+{
+
+class DeviceDedicatedUpdater;
+
+using RequestedApplyTimes = sdbusplus::common::xyz::openbmc_project::software::
+    ApplyTime::RequestedApplyTimes;
+
+class SoftwareUpdate :
+    public sdbusplus::aserver::xyz::openbmc_project::software::Update<
+        SoftwareUpdate>
+{
+  public:
+    SoftwareUpdate(sdbusplus::async::context& ctx, const char* path,
+                   DeviceDedicatedUpdater& updater,
+                   const std::set<RequestedApplyTimes>& allowedApplyTimes = {
+                       RequestedApplyTimes::Immediate});
+
+    auto method_call(start_update_t su, auto image, auto applyTime)
+        -> sdbusplus::async::task<start_update_t::return_type>;
+
+    auto get_property(
+        sdbusplus::aserver::xyz::openbmc_project::software::details::Update<
+            pldm::fw_update::SoftwareUpdate,
+            sdbusplus::async::server::server<
+                pldm::fw_update::SoftwareUpdate,
+                sdbusplus::aserver::xyz::openbmc_project::software::details::
+                    Update>>::allowed_apply_times_t aat) const
+        -> sdbusplus::message::variant<std::string, std::vector<std::string>>;
+    ;
+
+  private:
+    DeviceDedicatedUpdater& updater;
+
+    const std::set<RequestedApplyTimes> allowedApplyTimes;
+};
+
+} // namespace pldm::fw_update
