2df09e07390e7399da179632d6e9d030e0c61fc1
Unive Tien <unive.tien.wiwynn@gmail.com>
2025-05-14 13:24:02 +0800

    fw_update: Add SoftwareManager and DeviceDedicatedUpdater
    DeviceDedicatedUpdater is a class that manages all of the D-Bus
    utilities for the firmware update functionality for each software entry,
    SoftwareManager is used to manage multiple DeviceDedicatedUpdater
    instances.
    
    The update feature for these is not implemented yet in this commit.
    
    Change-Id: Idebd7d013c82c60f08309a1860d5de1deeb3829a
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

---
+++ fw-update/device_dedicated_updater.cpp
@@ -0,0 +1,45 @@
+#include "device_dedicated_updater.hpp"
+
+#include "common/utils.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+
+#include <string>
+
+using InternalFailure =
+    sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+
+PHOSPHOR_LOG2_USING;
+
+namespace pldm
+{
+
+namespace fw_update
+{
+
+DeviceDedicatedUpdater::DeviceDedicatedUpdater(
+    Context& ctx, pldm::eid eid, const std::string& softwarePath,
+    const std::string& softwareVersion, const std::string& associatedEndpoint,
+    const Descriptors& descriptors, const ComponentInfo& componentInfo,
+    SoftwareVersionPurpose purpose) :
+    ctx(ctx),
+    eid(eid), softwarePath(softwarePath),
+    activation(std::make_unique<SoftwareActivation>(ctx, softwarePath.c_str())),
+    association(std::make_unique<SoftwareAssociationDefinitions>(
+        ctx, softwarePath.c_str())),
+    version(std::make_unique<SoftwareVersion>(ctx, softwarePath.c_str())),
+    updateInterface(
+        std::make_unique<SoftwareUpdate>(ctx, softwarePath.c_str(), *this)),
+    descriptors(descriptors), componentInfo(componentInfo)
+{
+    this->association->associations(
+        {{"running", "ran_on", associatedEndpoint.c_str()}});
+    this->activation->activation(SoftwareActivation::Activations::Active);
+    this->activation->emit_added();
+    this->version->version(softwareVersion.c_str());
+    this->version->purpose(purpose);
+    this->version->emit_added();
+}
+
+} // namespace fw_update
+} // namespace pldm
