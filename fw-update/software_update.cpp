ca084f9b1ce15137d3641692e6fb92d9e0a0f1a1
Unive Tien <unive.tien.wiwynn@gmail.com>
2025-05-14 13:24:02 +0800

    fw_update: Add SoftwareManager and DeviceDedicatedUpdater
    DeviceDedicatedUpdater is a class that manages all of the D-Bus
    utilities for the firmware update functionality of each software entry,
    SoftwareManager is used to manage multiple DeviceDedicatedUpdater
    instances.
    
    Change-Id: Idebd7d013c82c60f08309a1860d5de1deeb3829a
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

---
+++ fw-update/software_update.cpp
@@ -0,0 +1,73 @@
+#include "software_update.hpp"
+
+#include "device_dedicated_updater.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async/context.hpp>
+#include <xyz/openbmc_project/Software/Update/aserver.hpp>
+
+PHOSPHOR_LOG2_USING;
+namespace pldm::fw_update
+{
+
+using Unavailable = sdbusplus::xyz::openbmc_project::Common::Error::Unavailable;
+
+using namespace phosphor::logging;
+
+SoftwareUpdate::SoftwareUpdate(
+    sdbusplus::async::context& ctx, const std::string& path,
+    DeviceDedicatedUpdater& updater,
+    const std::set<RequestedApplyTimes>& allowedApplyTimes) :
+    sdbusplus::aserver::xyz::openbmc_project::software::Update<SoftwareUpdate>(
+        ctx, path.c_str()),
+    updater(updater), allowedApplyTimes(allowedApplyTimes), softwarePath(path)
+{}
+
+auto SoftwareUpdate::method_call(start_update_t /*unused*/, auto image,
+                                 auto applyTime)
+    -> sdbusplus::async::task<start_update_t::return_type>
+{
+    debug("Requesting Image update with {FD}", "FD", image.fd);
+
+    if (updater.isUpdateInProgress)
+    {
+        error("An update is already in progress for the device");
+        report<Unavailable>();
+        co_return softwarePath;
+    }
+
+    updater.isUpdateInProgress = true;
+
+    // check if the apply time is allowed by our device
+    if (!allowedApplyTimes.contains(applyTime))
+    {
+        error(
+            "the selected apply time {APPLYTIME} is not allowed by the device",
+            "APPLYTIME", applyTime);
+        updater.isUpdateInProgress = false;
+        report<Unavailable>();
+        co_return softwarePath;
+    }
+
+    updater.activation->activation(SoftwareActivation::Activations::NotReady);
+
+    ctx.spawn([](sdbusplus::message::unix_fd& image,
+                 DeviceDedicatedUpdater& updater,
+                 RequestedApplyTimes applyTime) -> sdbusplus::async::task<> {
+        co_await updater.initUpdate(image, applyTime);
+        co_return;
+    }(image, updater, applyTime));
+
+    co_return softwarePath;
+}
+
+auto SoftwareUpdate::get_property(allowed_apply_times_t /*unused*/) const
+    -> allowed_apply_times_t::value_type
+{
+    // Return the allowed apply times for this software update.
+    return allowedApplyTimes;
+}
+
+} // namespace pldm::fw_update
