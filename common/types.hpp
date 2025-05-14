eda09d35528c61a4a3fe548e9de3d077d3707a78
Unive Tien <unive.tien.wiwynn@gmail.com>
2025-05-14 09:12:02 +0800

    fw_update: Reimplement package parser to use new libpldm API
    libpldm has revised the APIs for firmware package, the old APIs are
    now deprecated. Switch to the new APIs and use iterators to ensure
    compatibility with the latest libpldm version.
    
    Change-Id: I56106c66b61167bae8398c8616c376941097c627
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

--- common/types.hpp
+++ common/types.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <libpldm/firmware_update.h>
+
 #include <sdbusplus/message/types.hpp>
 
 #include <bitset>
@@ -108,14 +110,16 @@ using VendorDefinedDescriptorTitle = std::string;
 using VendorDefinedDescriptorData = std::vector<uint8_t>;
 using VendorDefinedDescriptorInfo =
     std::tuple<VendorDefinedDescriptorTitle, VendorDefinedDescriptorData>;
-using Descriptors =
-    std::multimap<DescriptorType,
-                  std::variant<DescriptorData, VendorDefinedDescriptorInfo>>;
+using DescriptorValue =
+    std::variant<DescriptorData, VendorDefinedDescriptorInfo>;
+using Descriptor = std::pair<DescriptorType, DescriptorValue>;
+using Descriptors = std::multimap<DescriptorType, DescriptorValue>;
 using DownstreamDeviceIndex = uint16_t;
 using DownstreamDeviceInfo =
     std::unordered_map<DownstreamDeviceIndex, Descriptors>;
 
 using DescriptorMap = std::unordered_map<eid, Descriptors>;
+using DownstreamDescriptorIndex = std::pair<eid, DownstreamDeviceIndex>;
 using DownstreamDescriptorMap = std::unordered_map<eid, DownstreamDeviceInfo>;
 
 // Component information
@@ -137,38 +141,34 @@ using DeviceIDRecordCount = uint8_t;
 using DeviceUpdateOptionFlags = std::bitset<32>;
 using ApplicableComponents = std::vector<size_t>;
 using ComponentImageSetVersion = std::string;
+using SelfContainedActivationMinVersionComparisonStamp =
+    std::optional<uint32_t>;
+using SelfContainedActivationMinVersion = std::optional<std::string>;
 using FirmwareDevicePackageData = std::vector<uint8_t>;
 using FirmwareDeviceIDRecord =
     std::tuple<DeviceUpdateOptionFlags, ApplicableComponents,
                ComponentImageSetVersion, Descriptors,
                FirmwareDevicePackageData>;
 using FirmwareDeviceIDRecords = std::vector<FirmwareDeviceIDRecord>;
+using DownstreamDeviceIDRecord =
+    std::tuple<DeviceUpdateOptionFlags, ApplicableComponents,
+               SelfContainedActivationMinVersion,
+               SelfContainedActivationMinVersionComparisonStamp, Descriptors,
+               FirmwareDevicePackageData>;
+using DownstreamDeviceIDRecords = std::vector<DownstreamDeviceIDRecord>;
+using DeviceIDRecord =
+    std::variant<FirmwareDeviceIDRecord, DownstreamDeviceIDRecord>;
+using DeviceIDRecords = std::vector<DeviceIDRecord>;
 
 // ComponentImageInformation
 using ComponentImageCount = uint16_t;
 using CompComparisonStamp = uint32_t;
 using CompOptions = std::bitset<16>;
 using ReqCompActivationMethod = std::bitset<16>;
-using CompLocationOffset = uint32_t;
-using CompSize = uint32_t;
 using CompVersion = std::string;
-using ComponentImageInfo =
-    std::tuple<CompClassification, CompIdentifier, CompComparisonStamp,
-               CompOptions, ReqCompActivationMethod, CompLocationOffset,
-               CompSize, CompVersion>;
-using ComponentImageInfos = std::vector<ComponentImageInfo>;
-
-enum class ComponentImageInfoPos : size_t
-{
-    CompClassificationPos = 0,
-    CompIdentifierPos = 1,
-    CompComparisonStampPos = 2,
-    CompOptionsPos = 3,
-    ReqCompActivationMethodPos = 4,
-    CompLocationOffsetPos = 5,
-    CompSizePos = 6,
-    CompVersionPos = 7,
-};
+using CompImage = std::vector<uint8_t>;
+using ComponentImageInfos =
+    std::vector<pldm_package_component_image_information>;
 
 } // namespace fw_update
 
