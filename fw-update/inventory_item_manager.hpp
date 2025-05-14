5976bce5b75f9312e9bbc97464c3bee2e43a7fc9
Unive Tien <unive.tien.wiwynn@gmail.com>
2025-05-14 13:24:02 +0800

    fw_update: Add InventoryItemManager for firmware update
    InventoryItemManager are used to manage each of the software entries,
    including version, association, and the update utilities. so that it can
    be used to update the device firmware.
    
    Change-Id: Idebd7d013c82c60f08309a1860d5de1deeb3829a
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

---
+++ fw-update/inventory_item_manager.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "common/types.hpp"
+
+#include <xyz/openbmc_project/Association/Definitions/aserver.hpp>
+#include <xyz/openbmc_project/Software/Activation/aserver.hpp>
+#include <xyz/openbmc_project/Software/ActivationBlocksTransition/aserver.hpp>
+#include <xyz/openbmc_project/Software/Version/aserver.hpp>
+
+namespace pldm::fw_update
+{
+
+class Software;
+using ActivationBlocksTransitionIntf = sdbusplus::aserver::xyz::
+    openbmc_project::software::ActivationBlocksTransition<Software>;
+using SoftwareActivationIntf =
+    sdbusplus::aserver::xyz::openbmc_project::software::Activation<Software>;
+using SoftwareVersionIntf =
+    sdbusplus::aserver::xyz::openbmc_project::software::Version<Software>;
+using AssociationDefinitionsIntf =
+    sdbusplus::aserver::xyz::openbmc_project::association::Definitions<
+        Software>;
+using VersionPurpose = SoftwareVersionIntf::VersionPurpose;
+
+using ObjectPath = pldm::dbus::ObjectPath;
+using Context = sdbusplus::async::context;
+
+class Software
+{
+  public:
+    Software() = delete;
+    Software(const Software&) = delete;
+    Software(Software&&) = delete;
+    Software& operator=(const Software&) = delete;
+    Software& operator=(Software&&) = delete;
+    ~Software() = default;
+
+    explicit Software(Context& ctx, const std::string& path,
+                      const std::string& assocEndpoint,
+                      const std::string& version,
+                      VersionPurpose purpose = VersionPurpose::Unknown);
+
+  private:
+    std::unique_ptr<AssociationDefinitionsIntf> association;
+    std::unique_ptr<ActivationBlocksTransitionIntf> blocksTransition;
+    std::unique_ptr<SoftwareActivationIntf> activation;
+    std::unique_ptr<SoftwareVersionIntf> version;
+};
+
+using SoftwarePtr = std::unique_ptr<Software>;
+
+class InventoryItemManager
+{
+  public:
+    InventoryItemManager() = delete;
+    InventoryItemManager(Context& ctx) : ctx(ctx) {}
+    InventoryItemManager(const InventoryItemManager&) = delete;
+    InventoryItemManager(InventoryItemManager&&) = delete;
+    InventoryItemManager& operator=(const InventoryItemManager&) = delete;
+    InventoryItemManager& operator=(InventoryItemManager&&) = delete;
+    ~InventoryItemManager() = default;
+
+    void createSoftwareEntry(const SoftwareIdentifier& softwareIdentifier,
+                             const SoftwareName& softwareName,
+                             const std::string& activeVersion);
+
+    void refreshInventoryPath(const eid& eid, const InventoryPath& path);
+
+  private:
+    std::string getVersionId(const std::string& version);
+
+    Context& ctx;
+
+    const ObjectPath getBoardPath(const InventoryPath& path);
+
+    std::map<eid, InventoryPath> inventoryPathMap;
+
+    std::map<SoftwareIdentifier, SoftwarePtr> softwareMap;
+};
+
+} // namespace pldm::fw_update
