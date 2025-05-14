914c6f17fa0a2dfead1c58d2b6c573a1449343e8
Unive Tien <unive.tien.wiwynn@gmail.com>
2025-05-14 13:24:02 +0800

    fw_update: Add SoftwareManager and DeviceDedicatedUpdater
    DeviceDedicatedUpdater is a class that manages all of the D-Bus
    utilities for the firmware update functionality for each software entry,
    SoftwareManager is used to manage multiple DeviceDedicatedUpdater
    instances.
    
    The update feature for these is not implemented yet in this commit.
    
    Change-Id: Idebd7d013c82c60f08309a1860d5de1deeb3829a
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

---
+++ fw-update/software_update.cpp
@@ -0,0 +1,45 @@
+#include "software_update.hpp"
+
+#include "device_dedicated_updater.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async/context.hpp>
+#include <xyz/openbmc_project/Software/Update/aserver.hpp>
+
+PHOSPHOR_LOG2_USING;
+namespace pldm
+{
+
+namespace fw_update
+{
+
+using Unavailable = sdbusplus::xyz::openbmc_project::Common::Error::Unavailable;
+
+using namespace phosphor::logging;
+
+SoftwareUpdate::SoftwareUpdate(
+    sdbusplus::async::context& ctx, const char* path,
+    DeviceDedicatedUpdater& updater,
+    const std::set<RequestedApplyTimes>& allowedApplyTimes) :
+    sdbusplus::aserver::xyz::openbmc_project::software::Update<SoftwareUpdate>(
+        ctx, path),
+    updater(updater), allowedApplyTimes(allowedApplyTimes)
+{}
+
+auto SoftwareUpdate::method_call(start_update_t /*unused*/, auto /*image*/,
+                                 auto /*applyTime*/)
+    -> sdbusplus::async::task<start_update_t::return_type>
+{
+    co_return std::string{};
+}
+
+auto SoftwareUpdate::get_property(allowed_apply_times_t /*unused*/) const
+{
+    return allowedApplyTimes;
+}
+
+} // namespace fw_update
+
+} // namespace pldm
