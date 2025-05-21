bf7806316a664173f13f88f3f923aecd52b59f0a
Amithash Prasad <amithash@meta.com>
2025-05-21 16:23:33 -0700

    common: Add blocking instance ID allocation
    Adds an optional parameter for users to provide a timeout
    in seconds so we could optionally wait for the instance ID
    to be allocated. This might be useful for critical paths
    like firmware upgrade where a failure at one point can
    be catastrophic
    
    Change-Id: I0cef570a1610b930accd2e7d122d5377e63cfc9f
    Signed-off-by: Amithash Prasad <amithash@meta.com>

---
+++ test/instance_id_test.cpp
@@ -0,0 +1,58 @@
+#include "test/test_instance_id.hpp"
+
+#include <chrono>
+
+#include <gtest/gtest.h>
+
+TEST(TestInstanceIds, NoTimeout)
+{
+    TestInstanceIdDb db;
+    for (int i = 0; i < 32; i++)
+    {
+        auto iid = db.next(12);
+        ASSERT_TRUE(iid.has_value());
+        EXPECT_LT(iid.value(), 32);
+    }
+    {
+        auto iid = db.next(12);
+        EXPECT_FALSE(iid.has_value());
+        EXPECT_EQ(iid.error().rc(), -EAGAIN);
+        EXPECT_EQ(
+            iid.error().msg(),
+            "Failed to allocate instance ID for EID 12: No free instance ids");
+    }
+    db.free(12, 0);
+    {
+        auto iid = db.next(12);
+        ASSERT_TRUE(iid.has_value());
+        EXPECT_EQ(iid.value(), 0);
+    }
+    EXPECT_NO_THROW(db.free(12, 24));
+    EXPECT_THROW(db.free(12, 24), std::runtime_error);
+}
+
+TEST(TestInstanceIds, Timeout)
+{
+    using namespace std::literals;
+    TestInstanceIdDb db;
+    for (int i = 0; i < 32; i++)
+    {
+        auto iid = db.next(12);
+        ASSERT_TRUE(iid.has_value());
+        EXPECT_LT(iid.value(), 32);
+    }
+    {
+        auto iid = db.next(12);
+        EXPECT_FALSE(iid.has_value());
+    }
+    auto tid = std::thread([&]() {
+        std::this_thread::sleep_for(100ms);
+        db.free(12, 0);
+    });
+    {
+        auto iid = db.next(12, 1000);
+        ASSERT_TRUE(iid.has_value());
+        EXPECT_EQ(iid.value(), 0);
+    }
+    tid.join();
+}
