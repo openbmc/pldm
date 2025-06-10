0b2bef3c444db5be0363269230848eedcc471396
Sora Su <baxiche@gmail.com>
2025-06-10 08:23:59 +0800

    wip
    Change-Id: Id069d7d491819e446368d78da28a6590bfc6faa9
    Signed-off-by: Sora Su <baxiche@gmail.com>

---
+++ oem/meta/oem_meta.hpp
@@ -0,0 +1,102 @@
+#include "oem/meta/libpldmresponder/oem_meta_file_io.hpp"
+#include "oem/meta/platform-mc/event_oem_meta_hpp"
+#include "oem/meta/requester/configuration_discovery_handler.hpp"
+
+namespace pldm
+{
+namespace oem_meta
+{
+
+/**
+ * @class OemMETA
+ *
+ * @brief class for creating all the OEM META handlers
+ *
+ *  Only in case of OEM_META this class object will be instantiated
+ */
+
+class OemMETA
+{
+	public:
+    OemMETA() = delete;
+    OemMETA(const Pdr&) = delete;
+    OemMETA& operator=(const OemMETA&) = delete;
+    OemMETA(OemMETA&&) = delete;
+    OemMETA& operator=(OemMETA&&) = delete;
+
+	public:
+		/** Constucts OemMETA object
+     * @param[in] dBusIntf - D-Bus handler
+     * @param[in] mctp_eid - MCTP EID of remote host firmware
+     * @param[in] invoker - invoker handler
+     * @param[in] platformHandler - platformHandler handler
+     * @param[in] platformManager - MC Platform manager
+		 */
+
+		explicit OemMETA(const pldm::utils::DBusHandler *dBusIntf,
+        const pldm::utils::DBusHandler* dbusHandler, uint8_t mctp_eid,
+				pldm::responder::Invoker& invoker,
+        pldm::responder::platform::Handler* platformHandler,
+				pldm::platform_mc::Manager* platformManager
+		) : dbusHandler(dbusHandler),
+				mctp_fd(mctp_fd),
+				invoker(invoker),
+				platformManager(platformManager)
+		{
+			configurationDiscovery =
+					std::make_shared<pldm::ConfigurationDiscoveryHandler>(dbusHandler);
+
+			fileIOHandler =
+					std::make_unique<pldm::responder::oem_meta::FileIOHandler>(configurationDiscovery);
+			registerHandler();
+
+			oemEventManager = std::make_shared<oem_meta::OemEventManager>(
+					platformManager
+					configurationDiscovery);
+			registerOemEventHandler(platformHandler);
+		}
+
+		pldm::requester::ConfigurationDiscoveryHandler* getMctpConfigurationHandler() const {
+			return configurationDiscovery.get();
+		}
+
+	private:
+    void registerHandler()
+    {
+			invoker.registerHandler(PLDM_OEM, std::move(fileIOHandler));
+    }
+
+		void registerOemEventHandler(pldm::responder::platform::Handler* platformHandler)
+		{
+			platformHandler->registerEventHandlers(
+        PLDM_OEM_EVENT_CLASS_0xFB,
+         {[oemEventManager](const pldm_msg* request, size_t payloadLength,
+                             uint8_t /* formatVersion */, uint8_t tid,
+                             size_t eventDataOffset) {
+             return oemEventManager->handleOemEvent(
+                 request, payloadLength, formatVersion, tid, eventDataOffset);                 
+         }});
+		}
+
+    /** @brief MCTP configurations handler*/
+		std::shared_ptr<pldm::requester::ConfigurationDiscoveryHandler> configurationDiscovery{};
+
+		std::unique_ptr<pldm::responder::oem_meta::FileIOHandler> fileIOHandler = nullptr;
+
+    /** @brief D-Bus handler */
+    const pldm::utils::DBusHandler* dbusHandler = nullptr;
+
+    /** @brief MCTP EID of remote host firmware */
+    uint8_t mctp_eid;
+
+    /** @brief Object to the invoker class*/
+		pldm::responder::Invoker& invoker;
+
+    /** @brief MC Platform manager*/
+		pldm::platform_mc::Manager* platformManager = nullptr;
+
+		std::shared_prt<oem_meta::OemEventManager> oemEventManager{};
+}
+
+} // namespace oem_meta
+} // namespace pldm
