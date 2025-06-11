4308681e60688d2660618448b367a84b1e910260
Sora Su <baxiche@gmail.com>
2025-06-11 09:41:35 +0800

    oem-meta: Add OEM Meta event handler
    Add OEM Meta handler with event type 0xFB. It will print the logs
    with prefix "BIOS_IPMI_SEL", so the user can utilize it to filter
    the journal logs. It will also send logs on D-Bus.
    
    Change-Id: Id069d7d491819e446368d78da28a6590bfc6faa9
    Signed-off-by: Sora Su <baxiche@gmail.com>

---
+++ oem/meta/event/oem_event_manager.hpp
@@ -0,0 +1,95 @@
+#pragma once
+
+#define PLDM_OEM_EVENT_CLASS_0xFB 0xFB
+
+#include "common/types.hpp"
+#include "oem/meta/requester/configuration_discovery_handler.hpp"
+#include "platform-mc/manager.hpp"
+
+namespace pldm
+{
+namespace oem_meta
+{
+
+struct dimm_info
+{
+    uint8_t sled;
+    uint8_t socket;
+    uint8_t channel;
+    uint8_t slot;
+};
+
+constexpr auto MetaIANA = "0015A000";
+
+class OemEventManager
+{
+  public:
+    OemEventManager() = delete;
+    OemEventManager(const OemEventManager&) = delete;
+    OemEventManager(OemEventManager&&) = delete;
+    OemEventManager& operator=(const OemEventManager&) = delete;
+    OemEventManager& operator=(OemEventManager&&) = delete;
+    virtual ~OemEventManager() = default;
+
+    explicit OemEventManager(
+        std::shared_ptr<pldm::requester::ConfigurationDiscoveryHandler>
+            configurationDiscovery,
+        pldm::platform_mc::Manager* platformManager) :
+        configurationDiscovery(configurationDiscovery),
+        platformManager(platformManager)
+    {}
+
+    int handleOemEvent(const pldm_msg* request, size_t payloadLength,
+                       uint8_t /* formatVersion */, uint8_t tid,
+                       size_t eventDataOffset);
+
+  private:
+    void covertToDimmString(uint8_t cpu, uint8_t channel, uint8_t slot,
+                            std::string& str) const;
+
+    void getCommonDimmLocation(const dimm_info& dimmInfo,
+                               std::string& dimmLocation,
+                               std::string& dimm) const;
+
+    inline auto to_hex_string(uint8_t value, size_t len = 2) const
+    {
+        return std::format("{:02x}", value, len);
+    }
+
+    inline bool checkMetaIana(
+        pldm_tid_t tid,
+        const std::map<std::string, pldm::requester::MctpEndpoint>&
+            configurations)
+    {
+        for (const auto& [configDbusPath, mctpEndpoint] : configurations)
+        {
+            if (mctpEndpoint.EndpointId == tid)
+            {
+                if (mctpEndpoint.iana.has_value() &&
+                    mctpEndpoint.iana.value() == MetaIANA)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    int processOemMetaEvent(
+        pldm_tid_t tid, const uint8_t* eventData, size_t eventDataSize,
+        const std::map<std::string, pldm::requester::MctpEndpoint>&
+            configurations) const;
+
+    std::string getSlotNumberString(
+        pldm_tid_t tid,
+        const std::map<std::string, pldm::requester::MctpEndpoint>&
+            configurations) const;
+
+    std::shared_ptr<pldm::requester::ConfigurationDiscoveryHandler>
+        configurationDiscovery;
+
+    pldm::platform_mc::Manager* platformManager;
+};
+
+} // namespace oem_meta
+} // namespace pldm
