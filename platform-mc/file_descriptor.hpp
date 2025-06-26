aee233f432c4b327a01cd644c571f1e17af61891
Chau Ly <chaul@amperecomputing.com>
2025-06-26 04:03:02 +0000

    platform-mc: Create File Descriptor class
    This commit adds File Descriptor class to represent File Descriptor PDR
    objects to D-Bus. This initially parses PDR fields to member variables,
    supports properties and methods specified in the `PLDM.File` D-Bus
    interface patch under-review [1], except for `Size` property. The result
    from a `Open` method call will be:
    
    1. A socketpair is setup and has the client side immediately returned to
       callers.
    2. An asynchronous task is triggered to interact with the actual file
       and write file data to the server side of the socketpair
    3. The socketpair will be closed when the asynchronous task is done or
       when error happens.
    
    Detail information about the design of this implementation is in the
    patch under review `pldm-stack: PLDM File Transfer` [2].
    
    This currently support one session opened to a file via Open() at a
    time. New new session can only be opened after the previous session is
    closed, which means the desired file data has been read from the actual
    file and  written to the socket pair.
    
    [1]: https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/81694
    [2]: https://gerrit.openbmc.org/c/openbmc/docs/+/76960
    
    Change-Id: If9e58bf25ce58a9fe1bc151f823940e1e8e40e38
    Signed-off-by: Chau Ly <chaul@amperecomputing.com>

---
+++ platform-mc/file_descriptor.hpp
@@ -0,0 +1,132 @@
+#pragma once
+
+#include "common/types.hpp"
+#include "common/utils.hpp"
+#include "requester/handler.hpp"
+
+#include <libpldm/platform.h>
+#include <libpldm/pldm.h>
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <xyz/openbmc_project/PLDM/File/server.hpp>
+
+#include <string>
+
+PHOSPHOR_LOG2_USING;
+
+namespace pldm
+{
+namespace platform_mc
+{
+
+class TerminusManager;
+
+using FileInterface = sdbusplus::server::object_t<
+    sdbusplus::xyz::openbmc_project::PLDM::server::File>;
+
+using namespace pldm::pdr;
+using namespace pldm::file_transfer;
+
+/**
+ * @brief FileDescriptor
+ *
+ * This class represents a File Descriptor object introduced by File Descriptor
+ * PDR to D-Bus.
+ */
+class FileDescriptor : virtual public FileInterface
+{
+  public:
+    FileDescriptor(sdbusplus::bus_t& bus, const std::string& path,
+                   const pldm_tid_t tid, const bool fileDisabled,
+                   std::shared_ptr<pldm_platform_file_descriptor_pdr> pdr,
+                   TerminusManager& terminusManager);
+
+    ~FileDescriptor() = default;
+
+  private:
+    bool isDirectory = false;
+    bool isRegular = false;
+    bool exReadPermitted = false;
+    pldm_tid_t tid;
+    FileID identifier;
+    FileID supDirIdentifier;
+    FileSize maxSize;
+    FDCount maxFdCount;
+    std::string oemClassName;
+    TerminusManager& terminusManager;
+
+    // TODO: support multiple socket pairs based on
+    // FileMaximumFileDescriptorCount
+    int sockets[2];
+
+    /** @brief Set value to D-Bus Purpose property from FileClassification
+     * field specified in the PDR
+     *
+     *  @param[in] classification - FileClassification field value
+     */
+    void setPurpose(const FileClassification& classification);
+
+    /** @brief Write data of certain length to a unix socket
+     *
+     *  @param[in] dataSize - Data length to be written
+     *  @param[in] data - Pointer to the array of data
+     *  @param[in] socket - The socket fd
+     *  @param[in] maxfd - Highest-num fd of all the fd set. maxfd + 1 will be
+     *                     passed as nfds argument to select()
+     *  @return coroutine task returning int
+     */
+    exec::task<int> writeDataToSocket(const size_t& dataSize,
+                                      const uint8_t* const data,
+                                      const int& socket, const int& maxFd);
+
+    /** @brief Call DfRead(s) to read a section of certain length to a buffer.
+     *  With reponse data from each part, write it to the socket pair
+     *
+     *  @param[in] fd - fd to a session of a PLDM file open with DfOpen
+     *  @param[in] offset - The desired section offset
+     *  @param[in] length - The desired section length
+     *  @param[in,out] readBuffer - The output buffer
+     *  @return coroutine task returning int
+     */
+    exec::task<int> readFileBySection(const FD& fd, const SectionOffset& offset,
+                                      const SectionOffset& length,
+                                      std::vector<uint8_t>& readBuffer);
+
+    /** @brief Read a certain length of data based on file type
+     *
+     *  @param[in] fd - fd to a session of a PLDM file open with DfOpen
+     *  @param[in] offset - The offset to start reading at
+     *  @param[in] readLength - The desired length to read from the file
+     *  @return coroutine task returning int
+     */
+    exec::task<int> readFileByLength(const FD& fd, const size_t& offset,
+                                     const size_t& readLength);
+
+    /** @brief Open, read and close the actual file and write to the socket pair
+     * as a request from the Open() method to the D-Bus file object
+     *
+     *  @param[in] fd - fd to a session of a PLDM file open with DfOpen
+     *  @param[in] offset - The offset to start reading at
+     *  @param[in] readSize - The desired length to read from the file
+     *  @return coroutine task returning int
+     */
+    exec::task<int> readTask(const size_t& offset, const size_t& length,
+                             bool exclusivity);
+
+    /** @brief Override the handler of D-Bus Open() method that returns a
+     * socket, and before that, spawn a task to interact with the actual file
+     * and write data to the socket
+     *
+     *  @param[in] offset - The offset to start reading at
+     *  @param[in] readSize - The desired length to read from the file
+     *  @param[in] exclusivity - Whether to call DfOpen for an exclusive session
+     *  @return coroutine task returning int
+     */
+    sdbusplus::message::unix_fd open(size_t offset, size_t length,
+                                     bool exclusivity) override;
+
+    std::optional<std::pair<exec::async_scope, std::optional<int>>> taskHandle;
+};
+} // namespace platform_mc
+} // namespace pldm
