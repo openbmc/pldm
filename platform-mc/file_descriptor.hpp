a93416ec55800afe15cc7dd506583831f0952044
Chau Ly <chaul@amperecomputing.com>
2025-06-26 04:03:02 +0000

    platform-mc: Create File Descriptor class
    This commit adds File Descriptor class to represent File Descriptor PDR
    objects to D-Bus. This initially parses PDR fields to member variables,
    supports properties and methods specified in the `Common.File` D-Bus
    interface patch under-review [1], except for `Size` property. The result
    from a `Read` method call will be:
    
    1. A socketpair is setup and has the client side immediately returned to
       callers.
    2. An asynchronous task is triggered to interact with the actual file
       and write file data to the server side of the socketpair
    3. The socketpair will be closed when the asynchronous task is done or
       when error happens.
    
    Detail information about the design of this implementation is in the
    patch under review `pldm-stack: PLDM File Transfer` [2].
    
    [1]: https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/81694
    [2]: https://gerrit.openbmc.org/c/openbmc/docs/+/76960
    
    Change-Id: If9e58bf25ce58a9fe1bc151f823940e1e8e40e38
    Signed-off-by: Chau Ly <chaul@amperecomputing.com>

---
+++ platform-mc/file_descriptor.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "common/types.hpp"
+#include "common/utils.hpp"
+#include "requester/handler.hpp"
+
+#include <libpldm/platform.h>
+#include <libpldm/pldm.h>
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <xyz/openbmc_project/Common/File/server.hpp>
+
+#include <string>
+
+PHOSPHOR_LOG2_USING;
+
+namespace pldm
+{
+namespace platform_mc
+{
+
+class TerminusManager;
+
+using FileInterface = sdbusplus::server::object_t<
+    sdbusplus::xyz::openbmc_project::Common::server::File>;
+
+using namespace pldm::pdr;
+using namespace pldm::file_transfer;
+
+/**
+ * @brief FileDescriptor
+ *
+ * This class represents a File Descriptor object introduced by File Descriptor
+ * PDR to D-Bus.
+ */
+class FileDescriptor : virtual public FileInterface
+{
+  public:
+    FileDescriptor(sdbusplus::bus_t& bus, const std::string& path,
+                   const pldm_tid_t tid, const bool fileDisabled,
+                   std::shared_ptr<pldm_platform_file_descriptor_pdr> pdr,
+                   TerminusManager& terminusManager);
+
+    ~FileDescriptor() = default;
+
+  private:
+    bool isDirectory = false;
+    bool isRegular = false;
+    bool exReadPermitted = false;
+    pldm_tid_t tid;
+    FileID identifier;
+    FileID supDirIdentifier;
+    FileSize maxSize;
+    FDCount maxFdCount;
+    std::string oemClassName;
+    TerminusManager& terminusManager;
+
+    // TODO: support multiple socket pairs based on
+    // FileMaximumFileDescriptorCount
+    int sockets[2];
+
+    void setPurpose(const FileClassification& classification);
+    exec::task<int> writeDataToSocket(const size_t& dataSize,
+                                      const uint8_t* const data,
+                                      const int& socket, const int& maxFd);
+    exec::task<int> readFileToSocket(const FD& fd, const SectionOffset& offset,
+                                     const SectionOffset& length,
+                                     std::vector<uint8_t>& readBuffer);
+    exec::task<int> readFile(const FD& fd, const size_t& offset,
+                             const size_t& readSize);
+    exec::task<int> readTask(const size_t& offset, const size_t& length,
+                             bool exclusivity);
+    sdbusplus::message::unix_fd open(size_t offset, size_t length,
+                                     bool exclusivity) override;
+    std::optional<std::pair<exec::async_scope, std::optional<int>>> taskHandle;
+};
+} // namespace platform_mc
+} // namespace pldm
