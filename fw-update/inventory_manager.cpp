36896e81a66ef319565a4d80980a57ae256497f5
rajeeranjan <ranjan.rajeev1609@gmail.com>
2025-06-30 12:02:09 +0530

    fw_update: Fix multiple component software object creation
    Software entries were being created inside the component parsing loop
    with an incomplete ComponentInfo map. This caused only the first
    component to be created successfully.
    
    Fix by collecting all components first, then creating software entries
    after the ComponentInfo map is complete.
    
    Tested: Verified all components now create software objects in D-Bus.
    
    Change-Id: I5c91de3a886a4335e7357f4498db787624f330d1
    Signed-off-by: Rajeev Ranjan <ranjan.rajeev1609@gmail.com>

--- fw-update/inventory_manager.cpp
+++ fw-update/inventory_manager.cpp
@@ -784,6 +784,7 @@ void InventoryManager::getFirmwareParameters(
     variable_field pendingCompVerStr{};
 
     ComponentInfo componentInfo{};
+    std::vector<std::tuple<uint16_t, std::string>> componentVersions;
     while (fwParams.comp_count-- && (compParamTableLen > 0))
     {
         auto rc = decode_get_firmware_parameters_resp_comp_entry(
@@ -802,6 +803,15 @@ void InventoryManager::getFirmwareParameters(
         componentInfo.emplace(
             std::make_pair(compClassification, compIdentifier),
             compEntry.comp_classification_index);
+
+        std::string versionStr = utils::toString(activeCompVerStr);
+        componentVersions.push_back(
+            std::make_tuple(compIdentifier, versionStr));
+
+        info("Component found: ID={ID}, Classification={CLASS}, Version={VER}",
+             "ID", compIdentifier, "CLASS", compClassification, "VER",
+             versionStr);
+
         compParamPtr += sizeof(pldm_component_parameter_entry) +
                         activeCompVerStr.length + pendingCompVerStr.length;
         compParamTableLen -= sizeof(pldm_component_parameter_entry) +
@@ -810,10 +820,29 @@ void InventoryManager::getFirmwareParameters(
 
     if (firmwareDeviceNameMap.contains(eid))
     {
-        softwareManager.createSoftwareEntry(
-            SoftwareIdentifier(eid, 0), firmwareDeviceNameMap.at(eid),
-            utils::toString(activeCompImageSetVerStr), descriptorMap[eid],
-            componentInfo);
+        if (!descriptorMap.contains(eid))
+        {
+            error(
+                "Cannot create software entries for EID {EID}: descriptorMap not populated",
+                "EID", eid);
+            return;
+        }
+
+        for (const auto& [compIdentifier, componentVersion] : componentVersions)
+        {
+            std::string componentName =
+                firmwareDeviceNameMap.at(eid) + "_Component_" +
+                std::to_string(compIdentifier);
+
+            info(
+                "Creating software entry: EID={EID}, CompID={COMP_ID}, Name={NAME}, Version={VER}",
+                "EID", eid, "COMP_ID", compIdentifier, "NAME", componentName,
+                "VER", componentVersion);
+
+            softwareManager.createSoftwareEntry(
+                SoftwareIdentifier(eid, compIdentifier), componentName,
+                componentVersion, descriptorMap[eid], componentInfo);
+        }
     }
     else
     {
