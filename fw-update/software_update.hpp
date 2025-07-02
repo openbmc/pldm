25b7fb9e6f30bff6613516b9c2297fb7fb37b408
Unive Tien <unive.tien.wiwynn@gmail.com>
2025-07-02 19:23:42 +0800

    fw_update: Implement software update interface
    This change adds the `SoftwareUpdate` class to implement the software
    update interface. When the method is called, it uses a 1-second
    interval to wait for the Redfish task to create, so that it won't miss
    the property change event.
    
    Change-Id: I724f0987eb882a1b4e6bce8c209b319c927689e9
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

---
+++ fw-update/software_update.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <xyz/openbmc_project/Software/Update/server.hpp>
+
+namespace pldm::fw_update
+{
+
+class DeviceDedicatedUpdater;
+
+using RequestedApplyTimes = sdbusplus::common::xyz::openbmc_project::software::
+    ApplyTime::RequestedApplyTimes;
+
+using UpdateIntf = sdbusplus::xyz::openbmc_project::Software::server::Update;
+
+class SoftwareUpdate : public UpdateIntf
+{
+  public:
+    SoftwareUpdate(sdbusplus::bus_t& bus,
+                   const std::string& path,
+                   DeviceDedicatedUpdater& updater,
+                   const std::set<RequestedApplyTimes>& allowedApplyTimes = {
+                       RequestedApplyTimes::Immediate});
+
+    virtual sdbusplus::message::object_path
+        startUpdate(sdbusplus::message::unix_fd image,
+                    RequestedApplyTimes applyTime) override;
+
+  private:
+
+    DeviceDedicatedUpdater& updater;
+
+    const std::set<RequestedApplyTimes> allowedApplyTimes;
+
+    const std::string& softwarePath;
+};
+
+} // namespace pldm::fw_update
