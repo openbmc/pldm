79973181a38d9793808c6896e009c4a3a2aa6e8b
Sora Su <baxiche@gmail.com>
2025-07-11 16:32:51 +0800

    oem-meta: Add event manager
    Add OEM Meta event manager for handling meta oem 0xFB event class.
    
    Change-Id: Idcf4be755124c4a361d4eeb93cc494c36aff6d10
    Signed-off-by: Lora Lin <lora.lin.wiwynn@gmail.com>
    Signed-off-by: Sora Su <baxiche@gmail.com>

--- oem/meta/oem_meta.cpp
+++ oem/meta/oem_meta.cpp
@@ -6,16 +6,33 @@ namespace pldm
 namespace oem_meta
 {
 
-OemMETA::OemMETA(const pldm::utils::DBusHandler* dbusHandler)
+OemMETA::OemMETA(const pldm::utils::DBusHandler* dbusHandler,
+                 pldm::responder::platform::Handler* platformHandler)
 {
     configurationDiscovery = std::make_shared<
         pldm::requester::oem_meta::ConfigurationDiscoveryHandler>(dbusHandler);
+
+    oemEventManager =
+        std::make_unique<oem_meta::OemEventManager>(configurationDiscovery);
+    registerOemEventHandler(platformHandler);
 }
 
 pldm::requester::oem_meta::ConfigurationDiscoveryHandler*
     OemMETA::getMctpConfigurationHandler() const
 {
     return configurationDiscovery.get();
+}
+
+void OemMETA::registerOemEventHandler(
+    pldm::responder::platform::Handler* platformHandler)
+{
+    platformHandler->registerEventHandlers(
+        PLDM_OEM_EVENT_CLASS_0xFB,
+        {[this](const pldm_msg* request, size_t payloadLength,
+                uint8_t formatVersion, uint8_t tid, size_t eventDataOffset) {
+            return this->oemEventManager->handleOemEvent(
+                request, payloadLength, formatVersion, tid, eventDataOffset);
+        }});
 }
 
 } // namespace oem_meta
