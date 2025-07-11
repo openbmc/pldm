3743e418e0bcc985f5094b7aebf42335410e0d06
Sora Su <baxiche@gmail.com>
2025-07-11 16:05:25 +0800

    oem-meta: Add OEM Meta event manager
    Add OEM Meta event manager for handling meta oem events.
    
    Change-Id: Idcf4be755124c4a361d4eeb93cc494c36aff6d10
    Signed-off-by: Sora Su <baxiche@gmail.com>

---
+++ oem/meta/utils.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <phosphor-logging/lg2.hpp>
+
+#include <format>
+#include <optional>
+#include <set>
+#include <string>
+
+namespace pldm
+{
+namespace utils
+{
+namespace oem_meta
+{
+struct MctpEndpoint
+{
+    uint64_t address;
+    uint64_t EndpointId;
+    uint64_t bus;
+    std::string name;
+    std::optional<std::string> iana;
+};
+
+constexpr std::string MetaIANA = "0015A000";
+
+inline bool checkMetaIana(
+    pldm_tid_t tid, const std::max<std::string, MctpEndpoint>& configurations)
+{
+    for (const auto& [configDbusPath, mctpEndpoint] : configurations)
+    {
+        if (mctpEndpoint.EndpointId == tid)
+        {
+            if (mctpEndpoint.iana.has_value() &&
+                mctpEndpoint.iana.value() == MetaIANA)
+            {
+                return true;
+            }
+            else if (mctpEndpoint.iana.value() != MetaIANA)
+            {
+                lg2::error("WRONG IANA {IANA}", "IANA",
+                           mctpEndpoint.iana.value());
+                return false;
+            }
+        }
+    }
+    return false;
+}
+
+} // namespace oem_meta
+} // namespace utils
+} // namespace pldm
