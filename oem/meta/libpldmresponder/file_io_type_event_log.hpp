e6b979ff56ce254216a86ce6ea888e4b3b39f442
Sora Su <baxiche@gmail.com>
2025-07-11 15:51:20 +0800

    oem-meta: Add OEM Meta file IO responder
    Support write file req when multiple hosts send postcode to BMC
    and store its post code history.
    
    Change-Id: I4a630bffc5ab3fddea0618130d970b4c7ecf59f0
    Signed-off-by: Sora Su <baxiche@gmail.com>

---
+++ oem/meta/libpldmresponder/file_io_type_event_log.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "common/utils.hpp"
+#include "file_io_by_type.hpp"
+#include "oem/meta/requester/configuration_discovery_handler.hpp"
+
+namespace pldm::responder::oem_meta
+{
+enum eventAssert : uint8_t
+{
+    EVENT_DEASSERTED = 0x00,
+    EVENT_ASSERTED = 0x01,
+};
+
+struct VRSource
+{
+    std::string netName;
+    bool isPMBus;
+};
+
+/** @class EventLogHandler
+ *
+ *  @brief Inherits and implements FileHandler. This class is used
+ *  to parse Event logs from BIC and add SEL
+ */
+class EventLogHandler : public FileHandler
+{
+  public:
+    EventLogHandler(pldm_tid_t tid,
+                    const std::map<std::string, pldm::oem_meta::MctpEndpoint>&
+                        configurations) :
+        tid(tid), configurations(configurations)
+    {}
+
+    ~EventLogHandler() = default;
+
+    /** @brief Method to parse event log from eventList
+     *  @param[in] data - eventData
+     *  @return  PLDM status code
+     */
+    int write(const message& data);
+
+    /** @brief Method to read data from BIC
+     *  @param[in] data - eventData
+     *  @return  PLDM status code
+     */
+    int read(struct pldm_oem_meta_file_io_read_resp* data)
+    {
+        (void)data; // Unused
+        return PLDM_ERROR_UNSUPPORTED_PLDM_CMD;
+    }
+
+  private:
+    /** @brief Parse object path to get correspond slot number
+     *  @param[in] slot - slot number
+     */
+    void parseObjectPathToGetSlot(uint64_t& slot);
+
+    /** @brief The terminus ID of the message source*/
+    pldm_tid_t tid = 0;
+
+    /** @brief Get existing configurations with MctpEndpoint*/
+    const std::map<configDbusPath, pldm::oem_meta::MctpEndpoint>&
+        configurations;
+};
+
+} // namespace pldm::responder::oem_meta
