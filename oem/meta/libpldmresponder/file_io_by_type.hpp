266310a7c4d16d35cb4a475e12debf8dfc19aee3
Sora Su <baxiche@gmail.com>
2025-07-18 18:11:26 +0800

    oem-meta: Add file IO responder
    Support handling file I/O request from BIC.
    
    Change-Id: I4a630bffc5ab3fddea0618130d970b4c7ecf59f0
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
    Signed-off-by: Sora Su <baxiche@gmail.com>

---
+++ oem/meta/libpldmresponder/file_io_by_type.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <libpldm/oem/meta/file_io.h>
+
+#include <cstdint>
+#include <span>
+
+namespace pldm::responder::oem_meta
+{
+using message = std::span<uint8_t>;
+
+enum class FileIOType : uint8_t
+{
+};
+
+/**
+ *  @class FileHandler
+ *
+ *  Base class to handle read/write of all OEM file types
+ */
+class FileHandler
+{
+	protected:
+		FileHandler() = default;
+
+  public:
+		FileHandler(const FileHandler&) = delete;
+		FileHandler(FileHandler&&) = delete;
+		FileHandler& operator=(const FileHandler&) = delete;
+		FileHandler& operator=(FileHandler&&) = delete;
+    virtual ~FileHandler() = default;
+		
+    /** @brief Method to write data to BMC
+     *  @param[in] data - eventData
+     *  @return  PLDM status code
+     */
+    virtual int write([[maybe_unused]] const message& data)
+    {
+        return PLDM_ERROR_UNSUPPORTED_PLDM_CMD;
+    }
+
+    /** @brief Method to read data from BIC
+     *  @param[in] data - eventData
+     *  @return  PLDM status code
+     */
+    virtual int read(
+        [[maybe_unused]] struct pldm_oem_meta_file_io_read_resp* data)
+    {
+        return PLDM_ERROR_UNSUPPORTED_PLDM_CMD;
+    }
+};
+
+} // namespace pldm::responder::oem_meta
