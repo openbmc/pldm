51d12b70a1d58487d095aa294bd3dbac1a1bed87
Sora Su <baxiche@gmail.com>
2025-07-18 18:11:26 +0800

    oem-meta: Add file IO responder
    Support handling file I/O request from BIC.
    
    Change-Id: I4a630bffc5ab3fddea0618130d970b4c7ecf59f0
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
    Signed-off-by: Sora Su <baxiche@gmail.com>

---
+++ oem/meta/libpldmresponder/file_io.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "common/utils.hpp"
+#include "file_io_by_type.hpp"
+#include "oem/meta/requester/configuration_discovery_handler.hpp"
+#include "pldmd/handler.hpp"
+
+#include <libpldm/oem/meta/file_io.h>
+
+#include <phosphor-logging/lg2.hpp>
+
+PHOSPHOR_LOG2_USING;
+
+namespace pldm::responder::oem_meta
+{
+
+class FileIOHandler : public CmdHandler
+{
+  public:
+    FileIOHandler(std::shared_ptr<
+                      pldm::requester::oem_meta::ConfigurationDiscoveryHandler>
+                      configurationDiscovery,
+                  const pldm::utils::DBusHandler* dBusHandler) :
+        configurationDiscovery(configurationDiscovery), dBusHandler(dBusHandler)
+    {
+        handlers.emplace(
+            PLDM_OEM_META_FILE_IO_CMD_WRITE_FILE,
+            [this](pldm_tid_t tid, const pldm_msg* request,
+                   size_t payloadLength) {
+                return this->writeFileIO(tid, request, payloadLength);
+            });
+        handlers.emplace(
+            PLDM_OEM_META_FILE_IO_CMD_READ_FILE,
+            [this](pldm_tid_t tid, const pldm_msg* request,
+                   size_t payloadLength) {
+                return this->readFileIO(tid, request, payloadLength);
+            });
+    }
+
+  private:
+    /** @brief Handler for writeFileIO command
+     *
+     *  @param[in] tid - the device tid
+     *  @param[in] request - pointer to PLDM request payload
+     *  @param[in] payloadLength - length of the message
+     *
+     *  @return PLDM response message
+     */
+    Response writeFileIO(pldm_tid_t tid, const pldm_msg* request,
+                         size_t payloadLength);
+
+    /** @brief Handler for readFileIO command
+     *
+     *  @param[in] tid - the device tid
+     *  @param[in] request - pointer to PLDM request payload
+     *  @param[in] payloadLength - length of the message
+     *
+     *  @return PLDM response message
+     */
+    Response readFileIO(pldm_tid_t tid, const pldm_msg* request,
+                        size_t payloadLength);
+
+    std::unique_ptr<FileHandler> getHandlerByType(pldm_tid_t tid,
+                                                  uint8_t fileIOType);
+
+    std::shared_ptr<pldm::requester::oem_meta::ConfigurationDiscoveryHandler>
+        configurationDiscovery;
+
+    [[maybe_unused]] const pldm::utils::DBusHandler* dBusHandler;
+};
+
+} // namespace pldm::responder::oem_meta
---
+++ oem/meta/libpldmresponder/file_io_by_type.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <libpldm/base.h>
+#include <libpldm/oem/meta/file_io.h>
+
+#include <cstdint>
+#include <span>
+#include <string>
+#include <vector>
+
+namespace pldm::responder::oem_meta
+{
+/**
+ *  @class FileHandler
+ *
+ *  Base class to handle read/write of all OEM file types
+ */
+
+using message = std::span<uint8_t>;
+using configDbusPath = std::string;
+constexpr auto decodeDataMaxLength = 240;
+
+enum class FileIOType : uint8_t
+{
+};
+
+class FileHandler
+{
+  public:
+    /** @brief Method to write data to BMC
+     *  @param[in] data - eventData
+     *  @return  PLDM status code
+     */
+    virtual int write([[maybe_unused]] const message& data)
+    {
+        return PLDM_ERROR_UNSUPPORTED_PLDM_CMD;
+    }
+
+    /** @brief Method to read data from BIC
+     *  @param[in] data - eventData
+     *  @return  PLDM status code
+     */
+    virtual int read(
+        [[maybe_unused]] struct pldm_oem_meta_file_io_read_resp* data)
+    {
+        return PLDM_ERROR_UNSUPPORTED_PLDM_CMD;
+    }
+    virtual ~FileHandler() {}
+};
+
+} // namespace pldm::responder::oem_meta
