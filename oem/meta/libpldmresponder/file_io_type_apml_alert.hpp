06880d1057c0f98530dd5b05476009c0ac7b1993
Sora Su <baxiche@gmail.com>
2025-07-21 14:04:39 +0800

    oem-meta: Add file IO responder for APML alert
    Add APML alert handler.
    
    Change-Id: I66c40bdd0c1ce506711996268f15c23217afbd3c
    Signed-off-by: Peter Ho <Peter_MH_Ho@wiwynn.com>
    Signed-off-by: Sora Su <baxiche@gmail.com>

---
+++ oem/meta/libpldmresponder/file_io_type_apml_alert.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "file_io_by_type.hpp"
+
+namespace pldm::responder::oem_meta
+{
+/** @class APMLAlertHandler
+ *
+ *  @brief Inherits and implements FileHandler. This class is used
+ *  to handle incoming sled cycle request from Hosts
+ */
+class APMLAlertHandler : public FileHandler
+{
+  public:
+    APMLAlertHandler() = delete;
+    APMLAlertHandler(const APMLAlertHandler&) = delete;
+    APMLAlertHandler(APMLAlertHandler&&) = delete;
+    APMLAlertHandler& operator=(const APMLAlertHandler&) = delete;
+    APMLAlertHandler& operator=(APMLAlertHandler&&) = delete;
+
+    explicit APMLAlertHandler(pldm_tid_t tid) : tid(tid) {}
+
+    ~APMLAlertHandler() = default;
+
+  private:
+    /** @brief The requester's TID */
+    [[maybe_unused]] pldm_tid_t tid = 0;
+};
+
+} // namespace pldm::responder::oem_meta
