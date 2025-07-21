22349c2b366aab84e01a1e0498d1491a65030101
Sora Su <baxiche@gmail.com>
2025-07-21 13:31:01 +0800

    oem-meta: Add file IO responder for post code
    Support write file request when multiple hosts send postcode to BMC
    and store its post code history.
    
    Change-Id: Ic4c7223b9e80f04f9c737f50b02af0d1cc119a52
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
    Signed-off-by: Sora Su <baxiche@gmail.com>

--- oem/meta/libpldmresponder/file_io.cpp
+++ oem/meta/libpldmresponder/file_io.cpp
@@ -1,5 +1,7 @@
 #include "file_io.hpp"
 
+#include "file_io_type_post_code.hpp"
+
 #include <libpldm/oem/meta/file_io.h>
 
 #include <filesystem>
@@ -11,6 +13,8 @@ std::unique_ptr<FileHandler> FileIOHandler::getHandlerByType(
 {
     switch (fileIOType)
     {
+        case FileIOType::POST_CODE:
+            return std::make_unique<PostCodeHandler>(messageTid);
         default:
             error("Get invalid file io type {FILEIOTYPE}", "FILEIOTYPE",
                   fileIOType);
