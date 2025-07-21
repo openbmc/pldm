bca95a5aa082281ffac81bdbaf6a22a0cff60405
Sora Su <baxiche@gmail.com>
2025-07-21 13:37:18 +0800

    oem-meta: Add file IO responder for host bios version
    Add handler for write-file command "BIOS_VERSION", which is sent
    when host's postcode complete triggered, and update the bios version
    to settingsd.
    
    Tested: Verified in yosemite4 platform. The BIOS version was recorded
    correctly.
    
    Change-Id: I99580e3593cb417f6e350f5f1e7735fccf6e010b
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
    Signed-off-by: Sora Su <baxiche@gmail.com>

---
+++ oem/meta/libpldmresponder/file_io_type_bios_version.cpp
@@ -0,0 +1,69 @@
+#include "file_io_type_bios_version.hpp"
+
+#include "oem/meta/utils.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+
+#include <sstream>
+#include <string>
+
+PHOSPHOR_LOG2_USING;
+namespace pldm::responder::oem_meta
+{
+
+int BIOSVersionHandler::write(const message& data)
+{
+    int completionCode = checkDataIntegrity(data);
+    if (completionCode != PLDM_SUCCESS)
+    {
+        error(
+            "Invalid incoming data for setting BIOS version with completion code {CC}",
+            "CC", completionCode);
+        return completionCode;
+    }
+
+    std::string slotNum = pldm::oem_meta::getSlotNumberStringByTID(tid);
+    pldm::utils::DBusMapping dbusMapping{
+        std::string("/xyz/openbmc_project/software/host") + slotNum +
+            "/Sentinel_Dome_bios",
+        "xyz.openbmc_project.Software.Version", "Version", "string"};
+
+    try
+    {
+        dBusIntf->setDbusProperty(dbusMapping, convertToBIOSVersionStr(data));
+    }
+    catch (const sdbusplus::exception_t& e)
+    {
+        error("Failed to execute Dbus call with error code {ERROR}", "ERROR",
+              e);
+    }
+    catch (const std::exception& e)
+    {
+        error("Failed to set BIOS version with error code {ERROR}", "ERROR", e);
+        return PLDM_ERROR;
+    }
+
+    return PLDM_SUCCESS;
+}
+
+int BIOSVersionHandler::checkDataIntegrity(const message& data)
+{
+    if (data.empty())
+    {
+        return PLDM_ERROR_INVALID_DATA;
+    }
+    else
+    {
+        return PLDM_SUCCESS;
+    }
+}
+
+pldm::utils::PropertyValue BIOSVersionHandler::convertToBIOSVersionStr(
+    const message& data)
+{
+    std::string biosVersion(data.begin(), data.end());
+
+    return pldm::utils::PropertyValue{biosVersion};
+}
+
+} // namespace pldm::responder::oem_meta
