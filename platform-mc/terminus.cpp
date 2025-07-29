59e39e74a112d13dc58cbc5432e606a0b7ca14e1
Chau Ly <chaul@amperecomputing.com>
2025-07-29 07:11:32 +0000

    platform-mc: Enlist common types for Terminus
    platform_mc::Terminus class is using its own defined types for generic
    types that are already defined in common/types.hpp. This commit removes
    those redundantly defined types and enlists common types.
    
    Change-Id: I2f3fb4adc99235aa530eb338787f66eee4bc3aa2
    Signed-off-by: Chau Ly <chaul@amperecomputing.com>

--- platform-mc/terminus.cpp
+++ platform-mc/terminus.cpp
@@ -288,7 +288,7 @@ void Terminus::addNextSensorFromPDRs()
 }
 
 std::shared_ptr<SensorAuxiliaryNames> Terminus::getSensorAuxiliaryNames(
-    SensorId id)
+    SensorID id)
 {
     auto it = std::find_if(
         sensorAuxiliaryNamesTbl.begin(), sensorAuxiliaryNamesTbl.end(),
@@ -589,7 +589,7 @@ void Terminus::addCompactNumericSensor(
     addNextSensorFromPDRs();
 }
 
-std::shared_ptr<NumericSensor> Terminus::getSensorObject(SensorId id)
+std::shared_ptr<NumericSensor> Terminus::getSensorObject(SensorID id)
 {
     if (terminusName.empty())
     {
@@ -749,7 +749,7 @@ void Terminus::updateInventoryWithFru(const uint8_t* fruData,
     }
 }
 
-std::vector<std::string> Terminus::getSensorNames(const SensorId& sensorId)
+std::vector<std::string> Terminus::getSensorNames(const SensorID& sensorId)
 {
     std::vector<std::string> sensorNames;
     std::string defaultName =
