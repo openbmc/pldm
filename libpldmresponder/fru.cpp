a8caf276a0c225db367956009ef2999c848fc614
ManojKiran Eda <manojkiran.eda@gmail.com>
2025-10-15 14:41:50 +0000

    Revert "fru: Add support to insert hot-plug PDRs into repository"
    This reverts commit cee5eccaef9f0f100845748dfce8c0dce87ee1c5.
    
    Reason for revert: OEM constants are checked in the common code , which broke the non-oem code base.
    
    Change-Id: Ib57312335a93c41e556a024ddb998b7bce842111
    Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>

--- libpldmresponder/fru.cpp
+++ libpldmresponder/fru.cpp
@@ -2,10 +2,6 @@
 
 #include "common/utils.hpp"
 
-#ifdef OEM_IBM
-#include "oem/ibm/libpldmresponder/utils.hpp"
-#endif
-
 #include <libpldm/entity.h>
 #include <libpldm/utils.h>
 #include <systemd/sd-journal.h>
@@ -506,24 +502,6 @@ int FruImpl::setFRUTable(const std::vector<uint8_t>& fruData)
         }
     }
     return PLDM_ERROR_UNSUPPORTED_PLDM_CMD;
-}
-
-uint32_t FruImpl::addHotPlugRecord(
-    pldm::responder::pdr_utils::PdrEntry pdrEntry)
-{
-    uint32_t lastHandle = 0;
-    uint32_t record_handle = 0;
-    if (oemUtilsHandler)
-    {
-        auto lastLocalRecord = pldm_pdr_find_last_in_range(
-            pdrRepo, pldm::responder::utils::BMC_PDR_START_RANGE,
-            pldm::responder::utils::BMC_PDR_END_RANGE);
-        lastHandle = pldm_pdr_get_record_handle(pdrRepo, lastLocalRecord);
-    }
-    pdrEntry.handle.recordHandle = lastHandle + 1;
-    pldm_pdr_add(pdrRepo, pdrEntry.data, pdrEntry.size, false,
-                 pdrEntry.handle.recordHandle, &record_handle);
-    return record_handle;
 }
 
 namespace fru
