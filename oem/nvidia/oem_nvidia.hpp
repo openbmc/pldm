6722669b5f6f258dfe33ec07655366d9f6d0f0b3
Aditya Kurdunkar <adityakurdunkar2@gmail.com>
2025-11-27 10:18:01 +0530

    oem-nvidia: Add support for legacy CPER event handling
    Add NVIDIA OEM support to handle legacy CPER (Common Platform Error
    Record) events using event class 0xFA for backwards compatibility.
    
    Prior to DSP0248 V1.3.0, NVIDIA used OEM event class 0xFA to report
    CPER events. The spec later standardized CPER events as class 0x07
    (PLDM_CPER_EVENT). This change maintains backwards compatibility by
    registering handlers for both the legacy 0xFA and the standardized
    0x07 event classes.
    
    Changes include:
    - Add OEM_NVIDIA build option in meson configuration (enabled by default)
    - Create OemNVIDIA class to register event handlers for both legacy
      (0xFA) and standard (0x07) CPER event classes
    - Register handlers for both regular and polled CPER events with the
      legacy event class
    - Integrate OemNVIDIA into pldmd main initialization flow
    
    This ensures continued support for NVIDIA platforms using older
    firmware while being compatible with the current PLDM specification.
    
    Change-Id: Ia0bb6c78b6abdc2c9ac2ea9249f180177d4aabcf
    Signed-off-by: Aditya Kurdunkar <adityakurdunkar2@gmail.com>

---
+++ oem/nvidia/oem_nvidia.hpp
@@ -0,0 +1,108 @@
+#pragma once
+
+#include "../../common/utils.hpp"
+#include "../../libpldmresponder/base.hpp"
+#include "../../libpldmresponder/bios.hpp"
+#include "../../libpldmresponder/fru.hpp"
+#include "../../libpldmresponder/platform.hpp"
+#include "../../platform-mc/manager.hpp"
+#include "../../pldmd/invoker.hpp"
+#include "../../requester/request.hpp"
+
+namespace pldm
+{
+namespace oem_nvidia
+{
+
+/**
+ * @class OemNVIDIA
+ *
+ * @brief class for creating all the OEM NVIDIA handlers
+ *
+ *  Only in case of OEM_NVIDIA this class object will be instantiated
+ */
+class OemNVIDIA
+{
+  public:
+    OemNVIDIA() = delete;
+    OemNVIDIA& operator=(const OemNVIDIA&) = delete;
+    OemNVIDIA(OemNVIDIA&&) = delete;
+    OemNVIDIA& operator=(OemNVIDIA&&) = delete;
+
+  public:
+    /** Constructs OemNVIDIA object
+     *
+     * @param[in] dBusIntf - D-Bus handler
+     * @param[in] mctp_fd - fd of MCTP communications socket
+     * @param[in] mctp_eid - MCTP EID of remote host firmware
+     * @param[in] repo - pointer to BMC's primary PDR repo
+     * @param[in] instanceIdDb - pointer to an InstanceIdDb object
+     * @param[in] event - sd_event handler
+     * @param[in] invoker - invoker handler
+     * @param[in] hostPDRHandler - hostPDRHandler handler
+     * @param[in] platformHandler - platformHandler handler
+     * @param[in] fruHandler - fruHandler handler
+     * @param[in] baseHandler - baseHandler handler
+     * @param[in] biosHandler - biosHandler handler
+     * @param[in] reqHandler - reqHandler handler
+     */
+    explicit OemNVIDIA(
+        const pldm::utils::DBusHandler* /* dBusIntf */, int /* mctp_fd */,
+        pldm_pdr* /* repo */, pldm::InstanceIdDb& /* instanceIdDb */,
+        sdeventplus::Event& /* event */, responder::Invoker& /* invoker */,
+        HostPDRHandler* /* hostPDRHandler */,
+        responder::platform::Handler* platformHandler,
+        responder::fru::Handler* /* fruHandler */,
+        responder::base::Handler* /* baseHandler */,
+        responder::bios::Handler* /* biosHandler */,
+        platform_mc::Manager* platformManager,
+        pldm::requester::Handler<pldm::requester::Request>* /* reqHandler */) :
+        platformHandler(platformHandler), platformManager(platformManager)
+    {
+        createOemEventHandler(this->platformHandler, this->platformManager);
+    }
+
+  private:
+    /** @brief Method for creating OEM event handlers
+     *
+     *  This method registers handlers for backwards compatibility with
+     *  event class 0xFA (legacy CPER event class before DSP0248 V1.3.0)
+     */
+    void createOemEventHandler(responder::platform::Handler* platformHandler,
+                               platform_mc::Manager* platformManager)
+    {
+        /** CPEREvent class (0x07) is only available in DSP0248 V1.3.0.
+         *  Before DSP0248 V1.3.0 spec, NVIDIA used OEM event class 0xFA to
+         *  report CPER events. For backwards compatibility, handle both
+         *  0xFA and standard PLDM_CPER_EVENT (0x07).
+         */
+        platformHandler->registerEventHandlers(
+            0xFA,
+            {[platformManager](const pldm_msg* request, size_t payloadLength,
+                               uint8_t formatVersion, uint8_t tid,
+                               size_t eventDataOffset) {
+                return platformManager->handleCperEvent(
+                    request, payloadLength, formatVersion, tid,
+                    eventDataOffset);
+            }});
+
+        /* Support handling polled events with NVIDIA OEM CPER event class 0xFA */
+        platformManager->registerPolledEventHandler(
+            0xFA,
+            {[platformManager](pldm_tid_t tid, uint16_t eventId,
+                               const uint8_t* eventData, size_t eventDataSize) {
+                return platformManager->handlePolledCperEvent(
+                    tid, eventId, eventData, eventDataSize);
+            }});
+    }
+
+  private:
+    /** @brief Platform handler*/
+    responder::platform::Handler* platformHandler = nullptr;
+
+    /** @brief MC Platform manager*/
+    platform_mc::Manager* platformManager = nullptr;
+};
+
+} // namespace oem_nvidia
+} // namespace pldm
