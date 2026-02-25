a62eaf3bfbfdc1cdd6ea96ecaffe12abd9d2abd3
Chau Ly <chaul@amperecomputing.com>
2026-02-25 10:23:30 +0000

    platform-mc: NumericSensor: Introduce non-DBus implementation
    NumericSensor has been imposed an assumption that each sensor object
    must have a representation on D-Bus. However, not all sensor base units
    listed in `Table 75 – sensorUnits enumeration of DSP0248 v1.3.0` can be
    subject to valid D-Bus interfaces or Redfish representations. Some
    units are used for sensor types that are currently consumed only within
    `pldmd` and do not need a D-Bus interface (e.g Byte unit used by File
    Size Monitoring Sensor introduced in `Section 8.8.3 File Size Monitoring
    Sensor of DSP0242 v1.0.0` - implementation proposed in [1]).
    
    This commit removes this assumption and let NumericSensor class deal
    with sensor PDRs that can not be presented by any D-Bus interface but
    are still consumed by `pldm` code and need to be in the sensor polling
    queue of SensorManager. Those sensors need to pass the check in
    `validateSupportedSensorType` function to be successfully constructed as
    non-DBus sensors and this check has valid condition yet. After the
    sensor object is constructed, a sensor is considered as a non-DBus
    sensor if non of `valueIntf` or `metricIntf` has a valid pointer.
    
    Those sensors are still periodically polled by SensorManager and theirs
    information is kept as NumericSensor class's member variables instead of
    exposing to D-Bus as they are not consumed outside of `pldmd`.
    
    Change-Id: I8fb8b4691f7107bdce2da2bff0b352765a2568f5
    Signed-off-by: Chau Ly <chaul@amperecomputing.com>

--- platform-mc/numeric_sensor.hpp
+++ platform-mc/numeric_sensor.hpp
@@ -370,6 +370,24 @@ class NumericSensor
                               pldm::utils::Direction direction, double rawValue,
                               bool newAlarm, bool assert);
 
+    /** @brief Get the current value of the sensor
+     *
+     *  @return The current sensor's value
+     */
+
+    double getSensorValue(void)
+    {
+        if (valueIntf)
+        {
+            return valueIntf->value();
+        }
+        if (metricIntf)
+        {
+            return metricIntf->value();
+        }
+        return sensorValue;
+    }
+
     /** @brief Terminus ID which the sensor belongs to */
     pldm_tid_t tid;
 
@@ -425,8 +443,10 @@ class NumericSensor
 
     /**
      * @brief Update the object units based on the PDR baseUnit
+     *
+     * @return True when success, otherwise False
      */
-    void setSensorUnit(uint8_t baseUnit);
+    bool setSensorUnit(uint8_t baseUnit);
 
     /** @brief Create the sensor inventory path.
      *
@@ -467,6 +487,19 @@ class NumericSensor
 
     /** @brief A power-of-10 multiplier for baseUnit */
     int8_t baseUnitModifier;
+
+    /****** Non-D-Bus sensor internal variables *************/
+    /** @brief Current sensor reading */
+    double sensorValue;
+
+    /** @brief Sensor's entity type */
+    uint16_t entityType;
+
+    /** @brief Sensor's entity instance number */
+    uint16_t entityInstanceNum;
+
+    /** @brief Sensor's container ID */
+    uint16_t containerId;
 
     /** @brief An internal mapping of thresholds and its associated log
      * entry. */
