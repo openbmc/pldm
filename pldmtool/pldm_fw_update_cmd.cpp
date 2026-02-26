345f8dc1c1eb445dd195290630d35178b0c0eca1
Konstantin Aladyshev <aladyshev22@gmail.com>
2026-02-26 17:54:56 +0300

    pldmtool: Correct GetFirmwareParameters command name
    According to the PLDM for Firmware Update Specification DSP0267 v1.3.0
    the correct name for the command to acquire the component details such
    as classification types and corresponding versions of the FD is
    'GetFirmwareParameters'.
    Currently pldmtool uses 'GetFwParams' name for such functionality.
    Correct name to 'GetFirmwareParameters' to fix the issue.
    Leave the old 'GetFwParams' command for now for client compatibility
    but mark it as deprecated.
    
    Tested:
    
    ~# pldmtool fw_update --help
    <...>
    Subcommands:
    <...>
      GetFirmwareParameters       To get the component details of the FD
      GetFwParams                 [Deprecated] To get the component details
                                  of the FD
    <...>
    
    Change-Id: I7cbb4c90bacdab28085fd6a4e18023e06f0923e4
    Signed-off-by: Konstantin Aladyshev <aladyshev22@gmail.com>

--- pldmtool/pldm_fw_update_cmd.cpp
+++ pldmtool/pldm_fw_update_cmd.cpp
@@ -206,15 +206,15 @@ const std::map<uint16_t, std::string> componentClassification{
     {PLDM_COMP_SOFTWARE_BUNDLE, "Software Bundle"},
     {PLDM_COMP_DOWNSTREAM_DEVICE, "Downstream Device"}};
 
-class GetFwParams : public CommandInterface
+class GetFirmwareParameters : public CommandInterface
 {
   public:
-    ~GetFwParams() = default;
-    GetFwParams() = delete;
-    GetFwParams(const GetFwParams&) = delete;
-    GetFwParams(GetFwParams&&) = default;
-    GetFwParams& operator=(const GetFwParams&) = delete;
-    GetFwParams& operator=(GetFwParams&&) = delete;
+    ~GetFirmwareParameters() = default;
+    GetFirmwareParameters() = delete;
+    GetFirmwareParameters(const GetFirmwareParameters&) = delete;
+    GetFirmwareParameters(GetFirmwareParameters&&) = default;
+    GetFirmwareParameters& operator=(const GetFirmwareParameters&) = delete;
+    GetFirmwareParameters& operator=(GetFirmwareParameters&&) = delete;
 
     using CommandInterface::CommandInterface;
 
@@ -1323,10 +1323,14 @@ void registerCommand(CLI::App& app)
     commands.push_back(
         std::make_unique<GetStatus>("fw_update", "GetStatus", getStatus));
 
+    auto getFirmwareParameters = fwUpdate->add_subcommand(
+        "GetFirmwareParameters", "To get the component details of the FD");
+    commands.push_back(std::make_unique<GetFirmwareParameters>(
+        "fw_update", "GetFirmwareParameters", getFirmwareParameters));
     auto getFwParams = fwUpdate->add_subcommand(
-        "GetFwParams", "To get the component details of the FD");
-    commands.push_back(
-        std::make_unique<GetFwParams>("fw_update", "GetFwParams", getFwParams));
+        "GetFwParams", "[Deprecated] To get the component details of the FD");
+    commands.push_back(std::make_unique<GetFirmwareParameters>(
+        "fw_update", "GetFwParams", getFwParams));
 
     auto queryDeviceIdentifiers = fwUpdate->add_subcommand(
         "QueryDeviceIdentifiers", "To query device identifiers of the FD");
