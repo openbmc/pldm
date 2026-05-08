openbmc/pldm

commit 4b8c540ef7d8be448bf7ece8e75595045abbe0fd

fix readability-make-member-function-const
A number of getter methods can be made const.  This is a clang-tidy
check, but readability-make-member-function-const is not added to the
configuration as it does not yet exist in the reference tidy
configuration.

Tested: Ran unit tests
Change-Id: Ib2a094b72611c7adce74042ada3796ab4c0c369e
Signed-off-by: Brad Bishop <bradbish@qti.qualcomm.com>

oem/ibm/libpldmresponder/file_io_type_dump.cpp
@@ -39,7 +39,7 @@ static constexpr auto resDumpDirPath = "/var/lib/pldm/resourcedump/1";
 int DumpHandler::fd = -1;
 namespace fs = std::filesystem;
 
-std::string DumpHandler::findDumpObjPath(uint32_t fileHandle)
+std::string DumpHandler::findDumpObjPath(uint32_t fileHandle) const
 {
     static constexpr auto DUMP_MANAGER_BUSNAME =
         "xyz.openbmc_project.Dump.Manager";

host-bmc/host_pdr_handler.cpp
@@ -788,7 +788,7 @@ void HostPDRHandler::setHostFirmwareCondition()
     }
 }
 
-bool HostPDRHandler::isHostUp()
+bool HostPDRHandler::isHostUp() const
 {
     return responseReceived;
 }

host-bmc/host_pdr_handler.hpp
@@ -172,7 +172,7 @@ class HostPDRHandler
 
     /** @brief check whether Host is running when pldmd starts
      */
-    bool isHostUp();
+    bool isHostUp() const;
 
     /* @brief Method to set the oem platform handler in host pdr handler class
      *

oem/ibm/libpldmresponder/file_io_type_dump.hpp
@@ -52,7 +52,7 @@ class DumpHandler : public FileHandler
         uint64_t length, uint32_t metaDataValue1, uint32_t /*metaDataValue2*/,
         uint32_t /*metaDataValue3*/, uint32_t /*metaDataValue4*/);
 
-    std::string findDumpObjPath(uint32_t fileHandle);
+    std::string findDumpObjPath(uint32_t fileHandle) const;
     std::string getOffloadUri(uint32_t fileHandle);
 
     /** @brief DumpHandler destructor

oem/ibm/libpldmresponder/inband_code_update.hpp
@@ -99,7 +99,7 @@ class CodeUpdate
      *        going on
      *  @return - bool
      */
-    bool isCodeUpdateInProgress()
+    bool isCodeUpdateInProgress() const
     {
         return codeUpdateInProgress;
     }
@@ -138,7 +138,7 @@ class CodeUpdate
      * validation PDR
      * @return - sensor id
      */
-    uint16_t getMarkerLidSensor()
+    uint16_t getMarkerLidSensor() const
     {
         return markerLidSensorId;
     }
@@ -163,7 +163,7 @@ class CodeUpdate
     /* @brief Method to fetch the sensor id for firmware update state
      * @return - sensor id
      */
-    uint16_t getFirmwareUpdateSensor()
+    uint16_t getFirmwareUpdateSensor() const
     {
         return firmwareUpdateSensorId;
     }
@@ -180,7 +180,7 @@ class CodeUpdate
     /* @brief Method to fetch the sensor id for boot side rename state
      * @return - sensor id
      */
-    uint16_t getBootSideRenameStateSensor()
+    uint16_t getBootSideRenameStateSensor() const
     {
         return bootSideRenameStateSensorId;
     }

platform-mc/numeric_sensor.cpp
@@ -682,7 +682,7 @@ NumericSensor::NumericSensor(
     }
 }
 
-double NumericSensor::conversionFormula(double value)
+double NumericSensor::conversionFormula(double value) const
 {
     double convertedValue = value;
     if (std::isfinite(resolution))
@@ -696,7 +696,7 @@ double NumericSensor::conversionFormula(double value)
     return convertedValue;
 }
 
-double NumericSensor::unitModifier(double value)
+double NumericSensor::unitModifier(double value) const
 {
     if (!std::isfinite(value))
     {
@@ -1052,7 +1052,7 @@ void NumericSensor::createThresholdLog(
     }
 }
 
-void NumericSensor::createNormalRangeLog(double value)
+void NumericSensor::createNormalRangeLog(double value) const
 {
     namespace Events =
         sdbusplus::event::xyz::openbmc_project::sensor::Threshold;

platform-mc/numeric_sensor.hpp
@@ -79,14 +79,14 @@ class NumericSensor
      *  @param[in] value - raw value
      *  @return double - converted value
      */
-    double conversionFormula(double value);
+    double conversionFormula(double value) const;
 
     /** @brief UnitModifier is used to apply the unit modifier specified in PDR
      *
      *  @param[in] value - raw value
      *  @return double - converted value
      */
-    double unitModifier(double value);
+    double unitModifier(double value) const;
 
     /** @brief Check if value is over threshold.
      *
@@ -403,7 +403,7 @@ class NumericSensor
      *
      *  @param[in] value - The current sensor value in normal range.
      */
-    void createNormalRangeLog(double value);
+    void createNormalRangeLog(double value) const;
 
     /**
      *  @brief Create a threshold log for the given level/direction tuple.

platform-mc/terminus.hpp
@@ -98,7 +98,7 @@ class Terminus
     void parseTerminusPDRs();
 
     /** @brief The getter to return terminus's TID */
-    pldm_tid_t getTid()
+    pldm_tid_t getTid() const
     {
         return tid;
     }

commit 12d64d4b5ac54cc011d19072e1db5d2e75b073d6

fix another readability-make-member-function-const
Missed one.

Fixes: 4b8c540ef7d8be448bf7ece8e75595045abbe0fd
Tested: Ran unit tests
Change-Id: I82f1b025a79b9516ba306537038d9f11846a144c
Signed-off-by: Brad Bishop <bradbish@qti.qualcomm.com>

oem/ibm/libpldmresponder/file_io_type_dump.cpp
@@ -143,7 +143,7 @@ int DumpHandler::newFileAvailable(uint64_t length)
     return PLDM_SUCCESS;
 }
 
-std::string DumpHandler::getOffloadUri(uint32_t fileHandle)
+std::string DumpHandler::getOffloadUri(uint32_t fileHandle) const
 {
     auto path = findDumpObjPath(fileHandle);
     if (path.empty())