b06fa8ce95f806009d9a0dd281da0c0736c57989
Unive Tien <unive.tien.wiwynn@gmail.com>
2026-05-12 03:24:03 +0000

    fw-update: enable both inotify and non-inotify path
    Description:
        This commit enables both inotify and non-inotify path for firmware
        update package monitoring. The inotify-based monitoring is enabled
        by default, but can be disabled by setting the
        'fw-update-pkg-inotify' option to 'disabled' in the configuration.
    
        When 'fw-update-pkg-inotify' is set to 'enabled', the D-Bus path
        won't be disabled, user can choose whether to use inotify-based
        monitoring or D-Bus method to trigger firmware update.
        When 'fw-update-pkg-inotify' is set to 'disabled', the ininotify-
        based monitoring will be disabled, and the D-Bus method will be
        used to trigger firmware update.
    
    Motivation:
        Previously, the inotify-based and dbus-based firmware update
        package monitoring were mutually exclusive, which restricted the
        compatibility for some platforms and causing CI failure.
        By enabling both paths, we can provide more flexibility and
        compatibility for different platforms and use cases.
    
    Change-Id: I97813a516437be26adf87c1ace82bf6857f32e69
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

--- fw-update/update_manager.hpp
+++ fw-update/update_manager.hpp
@@ -60,11 +60,10 @@ class UpdateManager
                   return this->processPackage(
                       std::filesystem::path(packageFilePath));
               }),
-#else
+#endif
         updater(std::make_unique<Update>(pldm::utils::DBusHandler::getBus(),
                                          "/xyz/openbmc_project/software/pldm",
                                          this)),
-#endif
         totalNumComponentUpdates(0)
     {}
 
@@ -136,6 +135,13 @@ class UpdateManager
 
     std::unique_ptr<Activation> activation;
 
+#ifdef FW_UPDATE_INOTIFY_ENABLED
+    bool updateInProgress() const
+    {
+        return updateInProgressFlag;
+    }
+#endif
+
   private:
     /** @brief Device identifiers of the managed FDs */
     const DescriptorMap& descriptorMap;
@@ -143,9 +149,9 @@ class UpdateManager
     const ComponentInfoMap& componentInfoMap;
 #ifdef FW_UPDATE_INOTIFY_ENABLED
     Watch watch;
-#else
-    std::unique_ptr<Update> updater;
+    bool updateInProgressFlag{false};
 #endif
+    std::unique_ptr<Update> updater;
 
     std::unique_ptr<ActivationProgress> activationProgress;
     std::string objPath;
