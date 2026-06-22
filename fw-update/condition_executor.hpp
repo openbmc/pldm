e649784247b3ced069cca3190ccd1f78969a279c
Unive Tien <unive.tien.wiwynn@gmail.com>
2026-06-22 07:54:15 +0000

    fw_update: add pre-post update conditions
    Description:
    This commit introduces the concept of pre and post update conditions in
    the firmware update process to meet the code-update design[1]. Allow
    routines to be executed before and after the firmware update process,
    which can be used to perform necessary checks, preparations, or cleanup
    tasks.
    
    [1]: https://github.com/openbmc/docs/blob/master/designs/code-update.md#pre-and-post-update-conditions
    
    - Added a new meson option `update-platform` to specify the platform for
      which the firmware update conditions are defined.
    - Added a new JSON schema for defining pre and post update conditions
    
    Motivation:
    The firmware update process may require certain conditions to be met
    before and after the update. This feature allows for greater flexibility
    and control over the update process, ensuring that the system is in a
    valid state before and after the update.
    
    Change-Id: Ie862c8ec6aa41db3239a6e7438953326e99a7bc3
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

---
+++ fw-update/condition_executor.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "common/types.hpp"
+#include "common/utils.hpp"
+
+#include <libpldm/base.h>
+
+#include <sdbusplus/async.hpp>
+#include <sdbusplus/server/object.hpp>
+
+#include <future>
+
+namespace pldm::fw_update
+{
+
+using namespace pldm;
+using Context = sdbusplus::async::context;
+
+namespace sdbusRule = sdbusplus::bus::match::rules;
+
+constexpr auto SYSTEMD_SERVICE = "org.freedesktop.systemd1";
+constexpr auto SYSTEMD_ROOT = "/org/freedesktop/systemd1";
+constexpr auto SYSTEMD_INTERFACE = "org.freedesktop.systemd1.Manager";
+const auto SYSTEMD_JOB_REMOVED_EVENT =
+    sdbusRule::type::signal() + sdbusRule::path(SYSTEMD_ROOT) +
+    sdbusRule::interface(SYSTEMD_INTERFACE) + sdbusRule::member("JobRemoved");
+
+class SystemdInterface
+{
+  public:
+    SystemdInterface() = delete;
+    SystemdInterface(const SystemdInterface&) = delete;
+    SystemdInterface(SystemdInterface&&) = delete;
+    SystemdInterface& operator=(const SystemdInterface&) = delete;
+    SystemdInterface& operator=(SystemdInterface&&) = delete;
+    ~SystemdInterface() = default;
+
+    explicit SystemdInterface(sdbusplus::bus_t& bus,
+                              const ConditionPath& conditionPath,
+                              const std::string& args);
+
+    void executeAndWait();
+
+    void execute();
+
+    void handleSystemdJobRemoved(sdbusplus::message_t& msg);
+
+  private:
+    std::string serviceName();
+
+    sdbusplus::bus_t& bus;
+
+    const std::string& conditionPath;
+
+    const std::string& args;
+
+    sdbusplus::object_path jobPath;
+
+    std::promise<void> promise;
+
+    sdbusplus::bus::match_t systemdSignals;
+};
+
+} // namespace pldm::fw_update
