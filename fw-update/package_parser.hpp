063da5d29a47187e1e5c9b5e2d1399ac99cb6dfe
Alexander Hansen <alexander.hansen@9elements.com>
2026-07-14 16:56:31 +0200

    migrate to libpldm++ PackageParser
    migrate pldm repo to use libpldm++ PackageParser instead of iterator
    macros. The existing data structures used throughout the repo have been
    kept intact, libpldm++ data structures are translated to those in order
    to limit the scope of this patch.
    
    What happened to 'validatePkgTotalSize' ?
    
    libpldm already validates the offset and size of component images when
    using the iterator macros.
    
    Tested: technically should be covered by existing PackageParser tests in
    this repo. Practically some manual testing would be good as an
    additional check but i don't have any PLDM devices to test with.
    
    Change-Id: Ieeb07678cd30828c47429c7c005b854b80d6933a
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- fw-update/package_parser.hpp
+++ fw-update/package_parser.hpp
@@ -4,6 +4,8 @@
 
 #include <libpldm/firmware_update.h>
 
+#include <libpldm++/firmware_update.hpp>
+
 #include <cstdint>
 #include <memory>
 #include <vector>
@@ -21,28 +23,12 @@ namespace fw_update
 class WrapPackageParser
 {
   public:
-    WrapPackageParser() = delete;
+    explicit WrapPackageParser() {}
     WrapPackageParser(const WrapPackageParser&) = delete;
     WrapPackageParser(WrapPackageParser&&) = default;
     WrapPackageParser& operator=(const WrapPackageParser&) = delete;
     WrapPackageParser& operator=(WrapPackageParser&&) = delete;
     ~WrapPackageParser() = default;
-
-    /** @brief Constructor
-     *
-     *  @param[in] pkgHeaderSize - Size of package header section
-     *  @param[in] pkgVersion - Package version
-     *  @param[in] componentBitmapBitLength - The number of bits used to
-     *                                        represent the bitmap in the
-     *                                        ApplicableComponents field for a
-     *                                        matching device.
-     */
-    explicit WrapPackageParser(
-        PackageHeaderSize pkgHeaderSize, const PackageVersion& pkgVersion,
-        ComponentBitmapBitLength componentBitmapBitLength) :
-        pkgHeaderSize(pkgHeaderSize), pkgVersion(pkgVersion),
-        componentBitmapBitLength(componentBitmapBitLength)
-    {}
 
     /** @brief Parse the firmware update package
      *
@@ -72,64 +58,15 @@ class WrapPackageParser
         return componentImageInfos;
     }
 
-    /** @brief Device identifiers of the managed FDs */
-    const PackageHeaderSize pkgHeaderSize;
-
-    /** @brief Package version string */
-    const PackageVersion pkgVersion;
-
-  protected:
-    /** @brief Parse the firmware device identification area
-     *
-     *  @param[in] deviceIdRecCount - count of firmware device ID records
-     *  @param[in] pkgHdr - firmware package header
-     *  @param[in] offset - offset in package header which is the start of the
-     *                      firmware device identification area
-     *
-     *  @return On success return the offset which is the end of the firmware
-     *          device identification area, on error throw exception.
-     */
-    size_t parseFDIdentificationArea(DeviceIDRecordCount deviceIdRecCount,
-                                     const std::vector<uint8_t>& pkgHdr,
-                                     size_t offset);
-
-    /** @brief Parse the component image information area
-     *
-     *  @param[in] compImageCount - component image count
-     *  @param[in] pkgHdr - firmware package header
-     *  @param[in] offset - offset in package header which is the start of the
-     *                      component image information area
-     *
-     *  @return On success return the offset which is the end of the component
-     *          image information area, on error throw exception.
-     */
-    size_t parseCompImageInfoArea(ComponentImageCount compImageCount,
-                                  const std::vector<uint8_t>& pkgHdr,
-                                  size_t offset);
-
-    /** @brief Validate the total size of the package
-     *
-     *  Verify the total size of the package is the sum of package header and
-     *  the size of each component.
-     *
-     *  @param[in] pkgSize - firmware update package size
-     *
-     *  @note Throws exception if validation fails
-     */
-    void validatePkgTotalSize(uintmax_t pkgSize);
-
+  private:
     /** @brief Firmware Device ID Records in the package */
     FirmwareDeviceIDRecords fwDeviceIDRecords;
 
     /** @brief Component Image Information in the package */
     ComponentImageInfos componentImageInfos;
 
-    /** @brief The number of bits that will be used to represent the bitmap in
-     *         the ApplicableComponents field for matching device. The value
-     *         shall be a multiple of 8 and be large enough to contain a bit
-     *         for each component in the package.
-     */
-    const ComponentBitmapBitLength componentBitmapBitLength;
+    // populated by parse(...)
+    std::unique_ptr<Package> package;
 };
 
 /** @brief Parse the package header information
