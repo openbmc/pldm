e864f349791ffdcedd81e26045db73519218e474
JY Voon <jvoon@nvidia.com>
2026-07-26 18:46:45 +0900

    platform-mc: add health state set support
    Publish the Health state set of a component state sensor through
    xyz.openbmc_project.State.Decorator.OperationalStatus, whose Functional
    property carries the health of the entity the state sensor monitors.
    
    Design: https://gerrit.openbmc.org/c/openbmc/docs/+/92069
    The monitoring association is defined by:
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/92656
    
    Tested: Build an image for the vr-nvl-obmc machine with the following
    patches cherry-picked:
    
    ```
    1. Kernel
       https://lore.kernel.org/all/20260702165524.2168091-1-jackyhuang@nvidia.com/
    2. u-boot
       https://lore.kernel.org/openbmc/20260629105202.1417679-1-jackyhuang@nvidia.com/
    3. meta-nvidia
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/90959
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/92346
    4. platform-init
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92189
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92190
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92191
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92192
    5. mctp-over-usb-config
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/80452
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/86236
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/86791
    6. libpldm
       https://gerrit.openbmc.org/c/openbmc/libpldm/+/90952
       https://gerrit.openbmc.org/c/openbmc/libpldm/+/91583
       https://gerrit.openbmc.org/c/openbmc/libpldm/+/92309
    ```
    
    The terminus of TID 18 reports three composite state sensors, each
    carrying a single Health component sensor beside component sensors of
    state sets which are not mapped.
    
    ```
    $ busctl tree xyz.openbmc_project.PLDM | grep state/health/
            |- /xyz/openbmc_project/state/health/Terminus_18_Sensor_5
            |- /xyz/openbmc_project/state/health/Terminus_18_Sensor_60
            |- /xyz/openbmc_project/state/health/Terminus_18_Sensor_700
    
    $ busctl introspect xyz.openbmc_project.PLDM \
      /xyz/openbmc_project/state/health/Terminus_18_Sensor_60
    NAME                                                  TYPE
    ...
    xyz.openbmc_project.Association.Definitions           interface
    .Associations                                         property
    xyz.openbmc_project.Object.Enable                     interface
    .Enabled                                              property
    xyz.openbmc_project.State.Decorator.OperationalStatus interface
    .Functional                                           property
    
    $ busctl get-property xyz.openbmc_project.PLDM \
      /xyz/openbmc_project/state/health/Terminus_18_Sensor_60 \
      xyz.openbmc_project.Association.Definitions Associations
    a(sss) 1 "monitoring" "monitored_by" "/xyz/openbmc_project/inventory/system/board/Terminus_18"
    
    $ busctl get-property xyz.openbmc_project.PLDM \
      /xyz/openbmc_project/state/health/Terminus_18_Sensor_60 \
      xyz.openbmc_project.State.Decorator.OperationalStatus Functional
    b true
    
    $ pldmtool platform GetStateSensorReadings -m 23 -i 60 -r 0
    {
        "compositeSensorCount": 5,
        "sensorOpState[0]": "Sensor Enabled",
        "presentState[0]": "1",
        "previousState[0]": "0",
        "eventState[0]": "1",
        ...
    }
    ```
    
    Change-Id: I1fab2a3bfcf30fca3ace8496e2eab5266e10f2d2
    Signed-off-by: JY Voon <jvoon@nvidia.com>

--- platform-mc/test/state_sensor_test.cpp
+++ platform-mc/test/state_sensor_test.cpp
@@ -2,6 +2,7 @@
 #include "platform-mc/state_sensor.hpp"
 
 #include <libpldm/entity.h>
+#include <libpldm/state_set.h>
 
 #include <gtest/gtest.h>
 
@@ -19,13 +20,46 @@ static std::shared_ptr<pldm::platform_mc::StateSensorInfo> makeInfo(
     return info;
 }
 
-TEST(StateSensor, unmappedStateSetHasNoName)
+TEST(StateSensor, healthIsTheMappedStateSet)
 {
-    // No state set is mapped to a name yet, so no state set - including the
-    // OEM range - resolves to one.
-    EXPECT_FALSE(pldm::platform_mc::getStateSetName(1).has_value());
-    EXPECT_FALSE(pldm::platform_mc::getStateSetName(3).has_value());
+    // The Health state set is published, so it names the state namespace of
+    // the component sensor object path.
+    EXPECT_EQ("health",
+              pldm::platform_mc::getStateSetName(PLDM_STATE_SET_HEALTH_STATE));
+
+    // No other state set is mapped yet - including the OEM range - so their
+    // component sensors get no object.
+    EXPECT_FALSE(
+        pldm::platform_mc::getStateSetName(PLDM_STATE_SET_CONFIGURATION_STATE)
+            .has_value());
     EXPECT_FALSE(pldm::platform_mc::getStateSetName(0x8000).has_value());
+}
+
+TEST(StateSensor, healthStateCollapsesOntoFunctional)
+{
+    // Normal is the only healthy state of the state set.
+    EXPECT_TRUE(pldm::platform_mc::healthStateToFunctional(
+        PLDM_STATE_SET_HEALTH_STATE_NORMAL));
+
+    // Functional is a boolean, so the nine unhealthy values of the state set
+    // all collapse onto false, the non-critical ones included.
+    for (const auto state :
+         {PLDM_STATE_SET_HEALTH_STATE_NON_CRITICAL,
+          PLDM_STATE_SET_HEALTH_STATE_CRITICAL,
+          PLDM_STATE_SET_HEALTH_STATE_FATAL,
+          PLDM_STATE_SET_HEALTH_STATE_UPPER_NON_CRITICAL,
+          PLDM_STATE_SET_HEALTH_STATE_LOWER_NON_CRITICAL,
+          PLDM_STATE_SET_HEALTH_STATE_UPPER_CRITICAL,
+          PLDM_STATE_SET_HEALTH_STATE_LOWER_CRITICAL,
+          PLDM_STATE_SET_HEALTH_STATE_UPPER_FATAL,
+          PLDM_STATE_SET_HEALTH_STATE_LOWER_FATAL})
+    {
+        EXPECT_FALSE(pldm::platform_mc::healthStateToFunctional(state));
+    }
+
+    // A component sensor without a state is not functional either: every PLDM
+    // state set reserves state value 0.
+    EXPECT_FALSE(pldm::platform_mc::healthStateToFunctional(0));
 }
 
 TEST(StateSensor, createdObjectIsEnableable)
@@ -34,21 +68,21 @@ TEST(StateSensor, createdObjectIsEnableable)
     // xyz.openbmc_project.Object.Enable, and starts disabled.
     auto& bus = pldm::utils::DBusHandler::getBus();
     auto intf = pldm::platform_mc::createStateSensorObject(
-        bus, "/xyz/openbmc_project/state/test_state_set/S0_Sensor_1");
+        bus, "/xyz/openbmc_project/state/health/S0_Sensor_1");
     ASSERT_NE(nullptr, intf);
     EXPECT_FALSE(intf->enabled());
 }
 
 TEST(StateSensor, objectPathInStateSetNamespace)
 {
-    auto info = makeInfo(1, {{1, {1, 2}}, {3, {1, 2, 3, 4}}});
-    pldm::platform_mc::StateSensor sensor(0x01, info, 1, "test_state_set",
-                                          "S0_Sensor_1_1");
+    auto info = makeInfo(1, {{PLDM_STATE_SET_CONFIGURATION_STATE, {1, 2}},
+                             {PLDM_STATE_SET_HEALTH_STATE, {1, 2, 3, 4}}});
+    pldm::platform_mc::StateSensor sensor(0x01, info, 1, "health",
+                                          "S0_Sensor_1_1", "");
 
     // The component sensor's object path is the state set namespace plus the
     // component name, and it takes the composite sensor's sensor ID.
-    EXPECT_EQ("/xyz/openbmc_project/state/test_state_set/S0_Sensor_1_1",
-              sensor.path);
+    EXPECT_EQ("/xyz/openbmc_project/state/health/S0_Sensor_1_1", sensor.path);
     EXPECT_EQ(1, sensor.sensorId);
     EXPECT_EQ(1, sensor.offset);
 
@@ -58,39 +92,66 @@ TEST(StateSensor, objectPathInStateSetNamespace)
     EXPECT_TRUE(sensor.enabled());
 }
 
+TEST(StateSensor, associatedWithTheMonitoredInventoryItem)
+{
+    // A terminus with an inventory object gets the component sensor linked to
+    // it, and reads its state as any other one.
+    auto info = makeInfo(2, {{PLDM_STATE_SET_HEALTH_STATE, {1, 2}}});
+    pldm::platform_mc::StateSensor sensor(
+        0x01, info, 0, "health", "S0_Sensor_2",
+        "/xyz/openbmc_project/inventory/system/board/S0");
+
+    sensor.updateReading(PLDM_SENSOR_ENABLED,
+                         PLDM_STATE_SET_HEALTH_STATE_NORMAL);
+    EXPECT_TRUE(sensor.enabled());
+}
+
 TEST(StateSensor, offsetOutOfRangeThrows)
 {
-    auto info = makeInfo(4, {{1, {1, 2}}});
-    EXPECT_THROW(pldm::platform_mc::StateSensor(0x01, info, 1, "test_state_set",
-                                                "S0_Sensor_4_1"),
+    auto info = makeInfo(4, {{PLDM_STATE_SET_HEALTH_STATE, {1, 2}}});
+    EXPECT_THROW(pldm::platform_mc::StateSensor(0x01, info, 1, "health",
+                                                "S0_Sensor_4_1", ""),
+                 sdbusplus::exception_t);
+}
+
+TEST(StateSensor, unmappedStateSetThrows)
+{
+    // A component sensor of an unmapped state set has no interface to publish
+    // its state through, so no object is created for it.
+    auto info = makeInfo(5, {{PLDM_STATE_SET_CONFIGURATION_STATE, {1, 2}}});
+    EXPECT_THROW(pldm::platform_mc::StateSensor(0x01, info, 0, "health",
+                                                "S0_Sensor_5", ""),
                  sdbusplus::exception_t);
 }
 
 TEST(StateSensor, readingDrivesEnabled)
 {
-    auto info = makeInfo(1, {{1, {1, 2}}});
-    pldm::platform_mc::StateSensor sensor(0x01, info, 0, "test_state_set",
-                                          "S0_Sensor_1");
+    auto info = makeInfo(1, {{PLDM_STATE_SET_HEALTH_STATE, {1, 2}}});
+    pldm::platform_mc::StateSensor sensor(0x01, info, 0, "health",
+                                          "S0_Sensor_1", "");
 
     // A component sensor only has a state while its own operational state is
     // enabled.
-    sensor.updateReading(PLDM_SENSOR_ENABLED, 2);
+    sensor.updateReading(PLDM_SENSOR_ENABLED,
+                         PLDM_STATE_SET_HEALTH_STATE_CRITICAL);
     EXPECT_EQ(PLDM_SENSOR_ENABLED, sensor.sensorOpState);
-    EXPECT_EQ(2, sensor.presentState);
+    EXPECT_EQ(PLDM_STATE_SET_HEALTH_STATE_CRITICAL, sensor.presentState);
     EXPECT_TRUE(sensor.enabled());
 
-    sensor.updateReading(PLDM_SENSOR_FAILED, 1);
-    EXPECT_EQ(1, sensor.presentState);
+    sensor.updateReading(PLDM_SENSOR_FAILED,
+                         PLDM_STATE_SET_HEALTH_STATE_NORMAL);
+    EXPECT_EQ(PLDM_STATE_SET_HEALTH_STATE_NORMAL, sensor.presentState);
     EXPECT_FALSE(sensor.enabled());
 }
 
 TEST(StateSensor, readErrorClearsState)
 {
-    auto info = makeInfo(1, {{1, {1, 2}}});
-    pldm::platform_mc::StateSensor sensor(0x01, info, 0, "test_state_set",
-                                          "S0_Sensor_1_err");
+    auto info = makeInfo(1, {{PLDM_STATE_SET_HEALTH_STATE, {1, 2}}});
+    pldm::platform_mc::StateSensor sensor(0x01, info, 0, "health",
+                                          "S0_Sensor_1_err", "");
 
-    sensor.updateReading(PLDM_SENSOR_ENABLED, 2);
+    sensor.updateReading(PLDM_SENSOR_ENABLED,
+                         PLDM_STATE_SET_HEALTH_STATE_NORMAL);
     ASSERT_TRUE(sensor.enabled());
 
     // A failed read leaves the state unknown rather than stale.
