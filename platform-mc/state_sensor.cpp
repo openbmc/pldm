09187908d77f1a5246e2027ba0047980b7315093
JY Voon <jvoon@nvidia.com>
2026-07-26 18:46:45 +0900

    platform-mc: add health state set support
    Publish the Health state set of a component state sensor through
    xyz.openbmc_project.State.Decorator.OperationalStatus, whose Functional
    property carries the health of the entity the state sensor monitors.
    
    Only Normal is functional: Functional is a boolean while the state set
    holds ten values. The component sensor object is associated with the
    monitored entity through possessed_by.
    
    Design: https://gerrit.openbmc.org/c/openbmc/docs/+/92069
    The possessed_by association is defined by:
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/92656
    
    Change-Id: I8d3ae3b4d86180c14038de3c1aba960f848911dc
    Signed-off-by: JY Voon <jvoon@nvidia.com>

--- platform-mc/state_sensor.cpp
+++ platform-mc/state_sensor.cpp
@@ -2,6 +2,8 @@
 
 #include "common/utils.hpp"
 
+#include <libpldm/state_set.h>
+
 #include <phosphor-logging/lg2.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
 
@@ -16,8 +18,80 @@ std::optional<std::string> getStateSetName(StateSetId stateSetId)
 {
     switch (stateSetId)
     {
+        case PLDM_STATE_SET_HEALTH_STATE:
+            return "health";
         default:
             return std::nullopt;
+    }
+}
+
+bool healthStateToFunctional(uint8_t presentState)
+{
+    return presentState == PLDM_STATE_SET_HEALTH_STATE_NORMAL;
+}
+
+HealthStateSetIntf::HealthStateSetIntf(sdbusplus::bus_t& bus,
+                                       const std::string& path,
+                                       const std::string& inventoryPath)
+{
+    try
+    {
+        operationalStatusIntf =
+            std::make_unique<OperationalStatusIntf>(bus, path.c_str());
+    }
+    catch (const sdbusplus::exception_t& e)
+    {
+        lg2::error(
+            "Failed to create OperationalStatus interface for state sensor {PATH} error - {ERROR}",
+            "PATH", path, "ERROR", e);
+        throw sdbusplus::xyz::openbmc_project::Common::Error::InvalidArgument();
+    }
+
+    if (inventoryPath.empty())
+    {
+        return;
+    }
+
+    try
+    {
+        associationDefinitionsIntf =
+            std::make_unique<AssociationDefinitionsIntf>(bus, path.c_str());
+    }
+    catch (const sdbusplus::exception_t& e)
+    {
+        lg2::error(
+            "Failed to create association interface for state sensor {PATH} error - {ERROR}",
+            "PATH", path, "ERROR", e);
+        throw sdbusplus::xyz::openbmc_project::Common::Error::InvalidArgument();
+    }
+
+    /* The health reported here is the health of the monitored inventory item,
+     * not of this object, so the object is possessed by that item. */
+    associationDefinitionsIntf->associations(
+        {{"possessed_by", "possessing", inventoryPath}});
+}
+
+void HealthStateSetIntf::updateState(uint8_t presentState)
+{
+    operationalStatusIntf->functional(healthStateToFunctional(presentState));
+}
+
+void HealthStateSetIntf::clearState()
+{
+    operationalStatusIntf->functional(false);
+}
+
+std::unique_ptr<StateSetIntf> createStateSetIntf(
+    StateSetId stateSetId, sdbusplus::bus_t& bus, const std::string& path,
+    const std::string& inventoryPath)
+{
+    switch (stateSetId)
+    {
+        case PLDM_STATE_SET_HEALTH_STATE:
+            return std::make_unique<HealthStateSetIntf>(bus, path,
+                                                        inventoryPath);
+        default:
+            return nullptr;
     }
 }
 
@@ -41,10 +115,10 @@ std::unique_ptr<ObjectEnableIntf> createStateSensorObject(
     return objectEnableIntf;
 }
 
-StateSensor::StateSensor(const pldm_tid_t tid,
-                         std::shared_ptr<StateSensorInfo> info, uint8_t offset,
-                         const std::string& stateSetName,
-                         const std::string& name) :
+StateSensor::StateSensor(
+    const pldm_tid_t tid, std::shared_ptr<StateSensorInfo> info, uint8_t offset,
+    const std::string& stateSetName, const std::string& name,
+    const std::string& inventoryPath) :
     tid(tid), offset(offset), info(info), stateSetName(stateSetName), name(name)
 {
     if (!info || offset >= info->compositeInfo.size())
@@ -57,6 +131,13 @@ StateSensor::StateSensor(const pldm_tid_t tid,
 
     auto& bus = pldm::utils::DBusHandler::getBus();
     objectEnableIntf = createStateSensorObject(bus, path);
+
+    stateSetIntf = createStateSetIntf(info->compositeInfo[offset].first, bus,
+                                      path, inventoryPath);
+    if (!stateSetIntf)
+    {
+        throw sdbusplus::xyz::openbmc_project::Common::Error::InvalidArgument();
+    }
 }
 
 void StateSensor::updateReading(uint8_t sensorOpState, uint8_t presentState)
@@ -64,6 +145,15 @@ void StateSensor::updateReading(uint8_t sensorOpState, uint8_t presentState)
     this->sensorOpState = sensorOpState;
     this->presentState = presentState;
     enabled(sensorOpState == PLDM_SENSOR_ENABLED);
+
+    if (sensorOpState == PLDM_SENSOR_ENABLED)
+    {
+        stateSetIntf->updateState(presentState);
+    }
+    else
+    {
+        stateSetIntf->clearState();
+    }
 }
 
 void StateSensor::handleErrGetStateSensorReading()
@@ -71,6 +161,7 @@ void StateSensor::handleErrGetStateSensorReading()
     sensorOpState = PLDM_SENSOR_UNAVAILABLE;
     presentState = 0;
     enabled(false);
+    stateSetIntf->clearState();
 }
 
 } // namespace platform_mc
