d7925332b39a94277b80bb0a70b9eef8e72ebd33
JY Voon <jvoon@nvidia.com>
2026-08-08 01:35:35 +0900

    platform-mc: add the health state set interface
    Expose the health which a component sensor of the health state set
    reports on the State.Decorator.OperationalStatus interface of the entity
    D-Bus object. A state which reports a condition short of critical leaves
    the Functional property true.
    
    This follows the PLDM stack design document:
    https://gerrit.openbmc.org/c/openbmc/docs/+/92069
    
    Tested: Build an image for the vr-nvl-obmc machine with the following
    patches cherry-picked:
    ```
    1. Kernel
       https://lore.kernel.org/all/20260702165524.2168091-1-jackyhuang@nvidia.com/
    2. u-boot
       https://lore.kernel.org/openbmc/20260629105202.1417679-1-jackyhuang@nvidia.com/
    3. meta-nvidia
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/90959
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/92346
    4. platform-init
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92189
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92190
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92191
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92192
    5. mctp-over-usb-config
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/80452
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/86236
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/86791
    6. libpldm
       https://gerrit.openbmc.org/c/openbmc/libpldm/+/90952
       https://gerrit.openbmc.org/c/openbmc/libpldm/+/91583
       https://gerrit.openbmc.org/c/openbmc/libpldm/+/92309
       https://gerrit.openbmc.org/c/openbmc/libpldm/+/92814
       https://gerrit.openbmc.org/c/openbmc/libpldm/+/92815
       https://gerrit.openbmc.org/c/openbmc/libpldm/+/92816
       https://gerrit.openbmc.org/c/openbmc/libpldm/+/92926
       https://gerrit.openbmc.org/c/openbmc/libpldm/+/92817
    ```
    
    ```
    $ P=/xyz/openbmc_project/inventory/system/Terminus_15_PCIeSwitch_1_100
    $ I=xyz.openbmc_project.State.Decorator.OperationalStatus
    $ busctl get-property xyz.openbmc_project.PLDM $P $I Functional
    b true
    
    $ busctl introspect xyz.openbmc_project.PLDM $P | grep State.Decorator
    xyz.openbmc_project.State.Decorator.OperationalStatus interface -
    
    $ journalctl -u pldmd | grep -c "Skip state set 1 of"
    0
    ```
    
    Change-Id: I1fab2a3bfcf30fca3ace8496e2eab5266e10f2d2
    Signed-off-by: JY Voon <jvoon@nvidia.com>

--- platform-mc/state_set.cpp
+++ platform-mc/state_set.cpp
@@ -3,12 +3,52 @@
 #include <common/utils.hpp>
 #include <phosphor-logging/lg2.hpp>
 
+#include <utility>
+
 PHOSPHOR_LOG2_USING;
 
 namespace pldm
 {
 namespace platform_mc
 {
+
+void StateSetHealthState::setPresentState(uint8_t presentState)
+{
+    /* Functional is a boolean and a false is reported as a Redfish Health of
+     * `Critical`, so a state which reports a condition short of critical
+     * leaves the entity functional.
+     */
+    bool functional = false;
+    switch (presentState)
+    {
+        case PLDM_STATE_SET_HEALTH_STATE_NORMAL:
+        case PLDM_STATE_SET_HEALTH_STATE_NON_CRITICAL:
+        case PLDM_STATE_SET_HEALTH_STATE_UPPER_NON_CRITICAL:
+        case PLDM_STATE_SET_HEALTH_STATE_LOWER_NON_CRITICAL:
+            functional = true;
+            break;
+        case PLDM_STATE_SET_HEALTH_STATE_CRITICAL:
+        case PLDM_STATE_SET_HEALTH_STATE_UPPER_CRITICAL:
+        case PLDM_STATE_SET_HEALTH_STATE_LOWER_CRITICAL:
+        case PLDM_STATE_SET_HEALTH_STATE_FATAL:
+        case PLDM_STATE_SET_HEALTH_STATE_UPPER_FATAL:
+        case PLDM_STATE_SET_HEALTH_STATE_LOWER_FATAL:
+            break;
+        default:
+            /* A terminus which reports a state the state set does not define
+             * keeps reporting it, so the entity is logged once.
+             */
+            if (!std::exchange(unknownStateLogged, true))
+            {
+                lg2::error(
+                    "The health state set has no state value {STATE}, so the entity on {PATH} is not functional.",
+                    "STATE", presentState, "PATH", path);
+            }
+            break;
+    }
+
+    interface.functional(functional);
+}
 
 StateSetBase* StateSets::getStateSet(StateSetId stateSetId)
 {
