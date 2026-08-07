9c2c4e1f9f5c01146dee00713001a3b795110760
JY Voon <jvoon@nvidia.com>
2026-08-07 19:28:25 +0900

    platform-mc: enable state sensors on entities
    Parse the State Sensor PDRs of a terminus, resolve the entity of each
    state sensor to its D-Bus object, and bind each component sensor to the
    D-Bus interface of its state set on that object.
    
    This follows the PLDM stack design document:
    https://gerrit.openbmc.org/c/openbmc/docs/+/92069
    
    Tested: Build an image for the vr-nvl-obmc machine with the following
    patches cherry-picked:
    ```
    1. Kernel
       https://lore.kernel.org/all/20260702165524.2168091-1-jackyhuang@nvidia.com/
    2. u-boot
       https://lore.kernel.org/openbmc/20260629105202.1417679-1-jackyhuang@nvidia.com/
    3. meta-nvidia
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/90959
       https://gerrit.openbmc.org/c/openbmc/openbmc/+/92346
    4. platform-init
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92189
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92190
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92191
       https://gerrit.openbmc.org/c/openbmc/platform-init/+/92192
    5. mctp-over-usb-config
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/80452
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/86236
       https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/86791
    6. libpldm
       https://gerrit.openbmc.org/c/openbmc/libpldm/+/90952
       https://gerrit.openbmc.org/c/openbmc/libpldm/+/91583
    ```
    
    ```
    $ pldmtool platform GetPDR -m 48 -t stateSensor -n 5 | \
      grep -E "sensorID|entityType\"|compositeSensorCount|stateSetID"
        "sensorID": 512,
        "entityType": "[Physical] System Bus",
        "compositeSensorCount": 1,
        "stateSetID[0]": "32768",
        "sensorID": 528,
        "entityType": "[Physical] Processor",
        "compositeSensorCount": 1,
        "stateSetID[0]": "Performance(14)",
        "sensorID": 560,
        "entityType": "[Physical] Processor",
        "compositeSensorCount": 1,
        "stateSetID[0]": "Power Supply State(256)",
        "sensorID": 576,
        "entityType": "[Physical] Processor",
        "compositeSensorCount": 1,
        "stateSetID[0]": "Performance(14)",
        "sensorID": 592,
        "entityType": "[Physical] Memory Controller",
        "compositeSensorCount": 1,
        "stateSetID[0]": "Presence(13)",
        "sensorID": 624,
        "entityType": "[Physical] PCI Express Bus including CXL",
        "compositeSensorCount": 1,
        "stateSetID[0]": "Link State(33)",
    
    $ journalctl -u pldmd | grep "Terminus ID 14"
    Terminus ID 14: Created Inventory path /xyz/openbmc_project/inventory/system/Terminus_14.
    Terminus ID 14: Created Entity path /xyz/openbmc_project/inventory/system/Terminus_14_Cpu_0_3.
    Terminus ID 14: Skip state set 14 of state sensor 528 - no D-Bus interface.
    Terminus ID 14: Skip state set 256 of state sensor 560 - no D-Bus interface.
    Terminus ID 14: Skip state set 14 of state sensor 576 - no D-Bus interface.
    Terminus ID 14: Skip state sensor 512 - no D-Bus object for Entity type 161 instance 0 container 3.
    Terminus ID 14: Skip state sensor 592 - no D-Bus object for Entity type 143 instance 0 container 3.
    Terminus ID 14: Skip state sensor 624 - no D-Bus object for Entity type 166 instance 0 container 3.
    
    $ busctl tree xyz.openbmc_project.PLDM | grep -c "/xyz/openbmc_project/state/"
    0
    ```
    
    Change-Id: I098dc81284ecff918bf82eedd0b25474b76d85ab
    Signed-off-by: JY Voon <jvoon@nvidia.com>

---
+++ platform-mc/state_sensor.cpp
@@ -0,0 +1,50 @@
+#include "state_sensor.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+
+PHOSPHOR_LOG2_USING;
+
+namespace pldm
+{
+namespace platform_mc
+{
+
+StateSensor::StateSensor(pldm_tid_t tid, std::shared_ptr<StateSensorInfo> info,
+                         StateSets& stateSets) : tid(tid), info(std::move(info))
+{
+    for (const auto& componentInfo : this->info->componentInfo)
+    {
+        const auto stateSetId = componentInfo.first;
+        auto stateSet = stateSets.getStateSet(stateSetId);
+        if (!stateSet)
+        {
+            lg2::info(
+                "Terminus ID {TID}: Skip state set {STATESETID} of state sensor {SENSORID} - no D-Bus interface.",
+                "TID", tid, "STATESETID", stateSetId, "SENSORID",
+                this->info->pdr.sensor_id);
+        }
+        componentStateSets.emplace_back(stateSet);
+    }
+}
+
+void StateSensor::updatePresentState(uint8_t offset, uint8_t presentState)
+{
+    if (offset >= componentStateSets.size())
+    {
+        lg2::error(
+            "Terminus ID {TID}: State sensor {SENSORID} has no composite sensor offset {OFFSET}.",
+            "TID", tid, "SENSORID", info->pdr.sensor_id, "OFFSET", offset);
+        return;
+    }
+
+    auto stateSet = componentStateSets[offset];
+    if (!stateSet)
+    {
+        return;
+    }
+
+    stateSet->setPresentState(presentState);
+}
+
+} // namespace platform_mc
+} // namespace pldm
